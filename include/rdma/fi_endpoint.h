0b366d3271d4e2d00db42e15bbb4dedb60c2ec3c
Sean Hefty <sean.hefty@intel.com>
2014-03-04 13:43:20 -0800

    fabric: Apply FI_CANCEL flag to an endpoint, not per op
    Only define FI_CANCEL as a configuration of an endpoint
    and not allowed per operation.  If an endpoint has been
    configured to support cancelable operations, any operation
    may be canceled.
    
    This avoids the need of the provider to check on a per
    call basis to see if the call may be canceled.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- include/rdma/fi_endpoint.h
+++ include/rdma/fi_endpoint.h
@@ -101,7 +101,7 @@ enum {
 struct fi_ops_ep {
 	size_t	size;
 	int	(*enable)(fid_t fid);
-	ssize_t	(*cancel)(fid_t fid, struct fi_context *context);
+	ssize_t	(*cancel)(fid_t fid, void *context);
 	int	(*getopt)(fid_t fid, int level, int optname,
 			  void *optval, size_t *optlen);
 	int	(*setopt)(fid_t fid, int level, int optname,
@@ -192,7 +192,7 @@ static inline ssize_t fi_enable(fid_t fid)
 	return ep->ops->enable(fid);
 }
 
-static inline ssize_t fi_cancel(fid_t fid, struct fi_context *context)
+static inline ssize_t fi_cancel(fid_t fid, void *context)
 {
 	struct fid_ep *ep = container_of(fid, struct fid_ep, fid);
 	FI_ASSERT_CLASS(fid, FID_CLASS_EP);
