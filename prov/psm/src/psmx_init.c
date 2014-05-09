7ed2fba31b95c78f107f73ccc4273429f605c709
Jianxin Xiong <jianxin.xiong@intel.com>
2014-05-08 21:49:07 -0700

    prov/psm: set the progress type in fi_info.
    Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>

--- prov/psm/src/psmx_init.c
+++ prov/psm/src/psmx_init.c
@@ -137,6 +137,8 @@ static int psmx_getinfo(const char *node, const char *service,
 	psmx_info->dest_addr = dest_addr;
 	psmx_info->auth_keylen = sizeof(psm_uuid_t);
 	psmx_info->auth_key = uuid;
+	psmx_info->control_progress = FI_PROGRESS_IMPLICIT;
+	psmx_info->data_progress = FI_PROGRESS_IMPLICIT;
 	psmx_info->fabric_name = strdup("psm");
 	psmx_info->domain_name = strdup("psm");
 	psmx_info->datalen = 0;
