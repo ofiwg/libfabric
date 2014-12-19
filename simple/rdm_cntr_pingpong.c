401c0635404d3525654cead90cc1660099448fd3
shantonu <shantonu.hossain@intel.com>
2014-12-19 14:35:37 -0800

    Added default port number and set default transfer size to 1024. Signed-off-by: Shantonu Hossain shantonu.hossain@intel.com

--- simple/rdm_cntr_pingpong.c
+++ simple/rdm_cntr_pingpong.c
@@ -46,7 +46,7 @@
 static int custom;
 static int size_option;
 static int iterations = 1000;
-static int transfer_size = 1000;
+static int transfer_size = 1024;
 static int max_credits = 128;
 static int credits = 128;
 static int send_count = 0;
@@ -61,7 +61,7 @@ static struct fi_info hints;
 static struct fi_domain_attr domain_hints;
 static struct fi_ep_attr ep_hints;
 static char *dst_addr, *src_addr;
-static char *port = NULL;
+static char *port = "9228";
 
 static struct fid_fabric *fab;
 static struct fid_domain *dom;
