2b3008440c17cbe17e9fa3d3614f6b31229bdb74
Sayantan Sur <sayantan.sur@intel.com>
2015-01-23 13:33:11 -0800

    build fixes
    Remove AC_CONFIG_MACRO_DIR(config), since we didn't have any
    additional macros in directory 'config'.
    
    Check for an existing file simple/info.c instead of src/fabric.c,
    which seems to be a result of copy/paste for libfabric.
    
    Fixes issue #125.
    
    Signed-off-by: Sayantan Sur <sayantan.sur@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if test ! -d .git && test ! -f src/fabric.c; then
+if test ! -d .git && test ! -f simple/info.c; then
     echo You really need to run this script in the top-level fabtests directory
     exit 1
 fi
