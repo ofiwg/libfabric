d12717618ee8414b5f4e7199342a4c526680b8bc
Jeff Squyres <jsquyres@cisco.com>
2015-02-13 08:52:11 -0800

    usnic: jump to "done" on failure to read()
    This was Open MPI Coverity ID (CID) 1270194.
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

--- prov/usnic/src/usdf_eq.c
+++ prov/usnic/src/usdf_eq.c
@@ -298,6 +298,7 @@ usdf_eq_read_fd(struct fid_eq *feq, uint32_t *event, void *buf, size_t len,
 		ret = read(eq->eq_fd, &val, sizeof(val));
 		if (ret != sizeof(val)) {
 			ret = -FI_EIO;
+			goto done;
 		}
 	}
 
