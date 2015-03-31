ofiwg/libfabric

commit a819b7ec08c3c589cecb4849f3883dd0f4b4f062

prov/verbs: Accept FI_INJECT in fi_tx_attr::op_flags
The verbs provider now sets the FI_INJECT bit in all applicable transfer
operations if the FI_INJECT flag was set in hints->tx_attr->op_flags
when creating the endpoint, unless the operation itself takes a flags
parameter.

This commit preserves the previous behavior where the provider automatically
sends small messages inline, since this theoretically will provide
better out-of-the-box performance.

Signed-off-by: Patrick MacArthur <pmacarth@iol.unh.edu>

prov/verbs/src/fi_verbs.c
@@ -78,6 +78,7 @@
 		FI_SEND | FI_RECV | FI_REMOTE_READ | FI_REMOTE_WRITE | \
 		FI_REMOTE_SIGNAL)
 #define VERBS_MODE (FI_LOCAL_MR | FI_PROV_MR_ATTR)
+#define VERBS_TX_OP_FLAGS FI_INJECT
 #define VERBS_MSG_ORDER (FI_ORDER_RAR | FI_ORDER_RAW | FI_ORDER_RAS | \
 		FI_ORDER_WAW | FI_ORDER_WAS | FI_ORDER_SAW | FI_ORDER_SAS )
 
@@ -129,6 +130,7 @@ struct fi_ibv_msg_ep {
 	struct fi_ibv_cq	*rcq;
 	struct fi_ibv_cq	*scq;
 	uint32_t		inline_size;
+	uint32_t		tx_op_flags;
 };
 
 static const char *local_node = "localhost";
@@ -179,6 +181,7 @@ const struct fi_rx_attr verbs_rx_attr = {
 const struct fi_tx_attr verbs_tx_attr = {
 	.caps			= VERBS_CAPS,
 	.mode			= VERBS_MODE,
+	.op_flags		= VERBS_TX_OP_FLAGS,
 	.msg_order		= VERBS_MSG_ORDER,
 	.inject_size		= 0,
 	.size			= 256,
@@ -367,6 +370,11 @@ static int fi_ibv_check_tx_attr(struct fi_tx_attr *attr, struct fi_info *info)
 	if (((attr->mode ? attr->mode : info->mode) & 
 				verbs_tx_attr.mode) != verbs_tx_attr.mode) {
 		VERBS_INFO("Given tx_attr->mode not supported\n");
+		return -FI_ENODATA;
+	}
+
+	if (attr->op_flags & ~verbs_tx_attr.op_flags) {
+		VERBS_INFO("Given tx_attr->op_flags not supported\n");
 		return -FI_ENODATA;
 	}
 
@@ -594,6 +602,12 @@ static int fi_ibv_fill_info_attr(struct ibv_context *ctx, struct ibv_qp *qp,
 
 	fi->ep_attr->protocol_version = 1;
 	fi->ep_attr->max_msg_size = port_attr.max_msg_sz;
+
+	if (hints && hints->tx_attr) {
+		fi->tx_attr->op_flags = hints->tx_attr->op_flags;
+	} else {
+		fi->tx_attr->op_flags = 0;
+	}
 
 	return 0;
 }
@@ -854,6 +868,7 @@ fi_ibv_msg_ep_sendmsg(struct fid_ep *ep, const struct fi_msg *msg, uint64_t flag
 
 	_ep = container_of(ep, struct fi_ibv_msg_ep, ep_fid);
 	wr.num_sge = msg->iov_count;
+	wr.send_flags = 0;
 	if (msg->iov_count) {
 		sge = alloca(sizeof(*sge) * msg->iov_count);
 		for (len = 0, i = 0; i < msg->iov_count; i++) {
@@ -861,16 +876,15 @@ fi_ibv_msg_ep_sendmsg(struct fid_ep *ep, const struct fi_msg *msg, uint64_t flag
 			sge[i].length = (uint32_t) msg->msg_iov[i].iov_len;
 			len += sge[i].length;
 		}
-		if (!(flags & FI_INJECT)) {
+		if ((flags & FI_INJECT) || (len <= _ep->inline_size)) {
+			wr.send_flags |= IBV_SEND_INLINE;
+		} else {
 			for (i = 0; i < msg->iov_count; i++) {
 				sge[i].lkey = (uint32_t)(uintptr_t)(msg->desc[i]);
 			}
 		}
 
 		wr.sg_list = sge;
-		wr.send_flags = (flags & FI_INJECT) ? IBV_SEND_INLINE : 0;
-	} else {
-		wr.send_flags = 0;
 	}
 
 	wr.wr_id = (uintptr_t) msg->context;
@@ -889,6 +903,7 @@ static ssize_t
 fi_ibv_msg_ep_send(struct fid_ep *ep, const void *buf, size_t len,
 		void *desc, fi_addr_t dest_addr, void *context)
 {
+	struct fi_ibv_msg_ep *_ep;
 	struct iovec iov;
 	struct fi_msg msg;
 
@@ -901,13 +916,15 @@ fi_ibv_msg_ep_send(struct fid_ep *ep, const void *buf, size_t len,
 	msg.addr = dest_addr;
 	msg.context = context;
 
-	return fi_ibv_msg_ep_sendmsg(ep, &msg, 0);
+	_ep = container_of(ep, struct fi_ibv_msg_ep, ep_fid);
+	return fi_ibv_msg_ep_sendmsg(ep, &msg, _ep->tx_op_flags);
 }
 
 static ssize_t
 fi_ibv_msg_ep_senddata(struct fid_ep *ep, const void *buf, size_t len,
 		    void *desc, uint64_t data, fi_addr_t dest_addr, void *context)
 {
+	struct fi_ibv_msg_ep *_ep;
 	struct iovec iov;
 	struct fi_msg msg;
 
@@ -921,13 +938,16 @@ fi_ibv_msg_ep_senddata(struct fid_ep *ep, const void *buf, size_t len,
 	msg.context = context;
 	msg.data = data;
 
-	return fi_ibv_msg_ep_sendmsg(ep, &msg, FI_REMOTE_CQ_DATA);
+	_ep = container_of(ep, struct fi_ibv_msg_ep, ep_fid);
+	return fi_ibv_msg_ep_sendmsg(ep, &msg,
+					FI_REMOTE_CQ_DATA | _ep->tx_op_flags);
 }
 
 static ssize_t
 fi_ibv_msg_ep_sendv(struct fid_ep *ep, const struct iovec *iov, void **desc,
                  size_t count, fi_addr_t dest_addr, void *context)
 {
+	struct fi_ibv_msg_ep *_ep;
 	struct fi_msg msg;
 
 	msg.msg_iov = iov;
@@ -936,7 +956,8 @@ fi_ibv_msg_ep_sendv(struct fid_ep *ep, const struct iovec *iov, void **desc,
 	msg.addr = dest_addr;
 	msg.context = context;
 
-	return fi_ibv_msg_ep_sendmsg(ep, &msg, 0);
+	_ep = container_of(ep, struct fi_ibv_msg_ep, ep_fid);
+	return fi_ibv_msg_ep_sendmsg(ep, &msg, _ep->tx_op_flags);
 }
 
 static struct fi_ops_msg fi_ibv_msg_ep_msg_ops = {
@@ -971,7 +992,9 @@ fi_ibv_msg_ep_rma_write(struct fid_ep *ep, const void *buf, size_t len,
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
 	wr.opcode = IBV_WR_RDMA_WRITE;
-	wr.send_flags = (len <= _ep->inline_size) ? IBV_SEND_INLINE : 0;
+	wr.send_flags = 0;
+	if ((_ep->tx_op_flags & FI_INJECT) || (len <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 	wr.wr.rdma.remote_addr = addr;
 	wr.wr.rdma.rkey = (uint32_t) key;
 
@@ -1003,9 +1026,13 @@ fi_ibv_msg_ep_rma_writev(struct fid_ep *ep, const struct iovec *iov, void **desc
 		sge[i].addr = (uintptr_t) iov[i].iov_base;
 		sge[i].length = (uint32_t) iov[i].iov_len;
 		len += iov[i].iov_len;
-		sge[i].lkey = (uint32_t) (uintptr_t) desc[i];
+		if (!(_ep->tx_op_flags & FI_INJECT))
+			sge[i].lkey = (uint32_t) (uintptr_t) desc[i];
 	}
-	wr.send_flags = (len <= _ep->inline_size) ? IBV_SEND_INLINE : 0;
+
+	wr.send_flags = 0;
+	if ((_ep->tx_op_flags & FI_INJECT) || (len <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 
 	return -ibv_post_send(_ep->id->qp, &wr, &bad);
 }
@@ -1031,13 +1058,13 @@ fi_ibv_msg_ep_rma_writemsg(struct fid_ep *ep, const struct fi_msg_rma *msg,
 			sge[i].length = (uint32_t) msg->msg_iov[i].iov_len;
 			len += sge[i].length;
 		}
-		if (!(flags & FI_INJECT)) {
+		if ((flags & FI_INJECT) || (len <= _ep->inline_size)) {
+			wr.send_flags |= IBV_SEND_INLINE;
+		} else {
 			for (i = 0; i < msg->iov_count; i++) {
 				sge[i].lkey = (uint32_t)(uintptr_t)(msg->desc[i]);
 			}
 		}
-
-		wr.send_flags = (flags & FI_INJECT) ? IBV_SEND_INLINE : 0;
 	}
 	wr.sg_list = sge;
 
@@ -1160,7 +1187,9 @@ fi_ibv_msg_ep_rma_writedata(struct fid_ep *ep, const void *buf, size_t len,
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
 	wr.opcode = IBV_WR_RDMA_WRITE_WITH_IMM;
-	wr.send_flags = (len <= _ep->inline_size) ? IBV_SEND_INLINE : 0;
+	wr.send_flags = 0;
+	if ((_ep->tx_op_flags & FI_INJECT) || (len <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 	wr.imm_data = (uint32_t) data;
 
 	wr.wr.rdma.remote_addr = addr;
@@ -1225,8 +1254,10 @@ fi_ibv_msg_ep_atomic_write(struct fid_ep *ep, const void *buf, size_t count,
 	wr.next = NULL;
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
-	wr.send_flags = (sge.length <= _ep->inline_size) ? IBV_SEND_INLINE : 0;
-	wr.send_flags |= IBV_SEND_FENCE; 
+	wr.send_flags = IBV_SEND_FENCE;
+	if ((_ep->tx_op_flags & FI_INJECT)
+				|| (sizeof(uint64_t) <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 
 	return -ibv_post_send(_ep->id->qp, &wr, &bad);
 }
@@ -1293,8 +1324,10 @@ fi_ibv_msg_ep_atomic_writemsg(struct fid_ep *ep,
 	wr.next = NULL;
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
-	wr.send_flags = (flags & FI_INJECT) ? IBV_SEND_INLINE : 0;
-	wr.send_flags |= IBV_SEND_FENCE; 
+	wr.send_flags = IBV_SEND_FENCE;
+	if ((_ep->tx_op_flags & FI_INJECT)
+				|| (sizeof(uint64_t) <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 
 	return -ibv_post_send(_ep->id->qp, &wr, &bad);
 }
@@ -1353,6 +1386,9 @@ fi_ibv_msg_ep_atomic_readwrite(struct fid_ep *ep, const void *buf, size_t count,
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
 	wr.send_flags = IBV_SEND_FENCE; 
+	if ((_ep->tx_op_flags & FI_INJECT)
+				|| (sizeof(uint64_t) <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 
 	return -ibv_post_send(_ep->id->qp, &wr, &bad);
 }
@@ -1428,6 +1464,9 @@ fi_ibv_msg_ep_atomic_readwritemsg(struct fid_ep *ep,
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
 	wr.send_flags = IBV_SEND_FENCE; 
+	if ((_ep->tx_op_flags & FI_INJECT)
+				|| (sizeof(uint64_t) <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 	if (flags & FI_REMOTE_CQ_DATA)
 		wr.imm_data = (uint32_t) msg->data;
 
@@ -1482,6 +1521,9 @@ fi_ibv_msg_ep_atomic_compwrite(struct fid_ep *ep, const void *buf, size_t count,
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
 	wr.send_flags = IBV_SEND_FENCE; 
+	if ((_ep->tx_op_flags & FI_INJECT)
+				|| (sizeof(uint64_t) <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 
 	return -ibv_post_send(_ep->id->qp, &wr, &bad);
 }
@@ -1556,6 +1598,9 @@ fi_ibv_msg_ep_atomic_compwritemsg(struct fid_ep *ep,
 	wr.sg_list = &sge;
 	wr.num_sge = 1;
 	wr.send_flags = IBV_SEND_FENCE; 
+	if ((_ep->tx_op_flags & FI_INJECT)
+				|| (sizeof(uint64_t) <= _ep->inline_size))
+		wr.send_flags |= IBV_SEND_INLINE;
 	if (flags & FI_REMOTE_CQ_DATA)
 		wr.imm_data = (uint32_t) msg->data;
 
@@ -1913,10 +1958,12 @@ fi_ibv_open_ep(struct fid_domain *domain, struct fi_info *info,
 
 	if (info->tx_attr) {
 		_ep->inline_size = info->tx_attr->inject_size;
+		_ep->tx_op_flags = info->tx_attr->op_flags;
 	} else {
 		fi_read_file(FI_CONF_DIR, "def_inline_data",
 			def_inline_data, sizeof def_inline_data);
 		_ep->inline_size = atoi(def_inline_data);
+		_ep->tx_op_flags = 0;
 	}
 
 	*ep = &_ep->ep_fid;

commit eaf2d5e6001a3b8cbbdaccf933837772a4b5d610

prov/verbs: Fix bug where op_flags incorrectly set
Commit a819b7ec08c3c589cecb4849f3883dd0f4b4f062 introduced the
tx_op_flags variable to support FI_INJECT and set the
verbs_tx_attr.op_flags field to contain all supported tx_op_flags.
This was only to check whether or not the set of operation flags
requested in hints was supported; the flags would get overwritten with
the subset requested in hints.

However, when creating the info structure for a passive endpoint (with
src_addr but not dest_addr specified in hints), we exit out of
fi_ibv_fill_info_attr early, and never overwrite the op_flags value, so
it contains all supported attributes whether the user requested them or
not!

This commit fixes this issue by setting the verbs_tx_attr.op_flags to 0
and using the VERBS_TX_OP_FLAGS macro directly when checking the value
in hints, which has the added benefit of being more intuitive.

Signed-off-by: Patrick MacArthur <pmacarth@iol.unh.edu>

prov/verbs/src/fi_verbs.c
@@ -194,7 +194,7 @@ const struct fi_rx_attr verbs_rx_attr = {
 const struct fi_tx_attr verbs_tx_attr = {
 	.caps			= VERBS_CAPS,
 	.mode			= VERBS_TX_MODE,
-	.op_flags		= VERBS_TX_OP_FLAGS,
+	.op_flags		= 0,
 	.msg_order		= VERBS_MSG_ORDER,
 	.inject_size		= 0,
 	.size			= 256,
@@ -412,7 +412,7 @@ static int fi_ibv_check_tx_attr(const struct fi_tx_attr *attr, const struct fi_i
 		return -FI_ENODATA;
 	}
 
-	if (attr->op_flags & ~verbs_tx_attr.op_flags) {
+	if (attr->op_flags & ~(VERBS_TX_OP_FLAGS)) {
 		FI_INFO(&fi_ibv_prov, FI_LOG_CORE,
 			"Given tx_attr->op_flags not supported\n");
 		return -FI_ENODATA;