e07c81c4c16436f4057d0fd476c40a6079c4ed52
pmmccorm <patrick.m.mccormick@intel.com>
2015-04-01 16:28:25 -0700

    Update fabtests.sh script and add features.
    Make output yaml-parsable.
    Add -v switch to see output when test fails.
    Check if test is not supported (return FI-ENODATA) and skip (vs fail).
    Clean up more correct and quiet.
    
    Signed-off-by: pmmccorm <patrick.m.mccormick@intel.com>

--- scripts/runFabtests.sh
+++ scripts/runFabtests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ $# != 5 ]; then
-	echo "Usage: $0 <test_bin_path> <provider_name> <all|quick> <server_addr> <client_addr>"
+# TODO: parse all this stuff in standard fashion
+if [[ $# < 5 ]]; then
+	echo "Usage: $0 [-v] <test_bin_path> <provider_name> <all|quick> <server_addr> <client_addr>"
 	exit 1
+fi
+
+if [[ "$1" == "-v" ]]; then
+	VERBOSE=1
+	shift;
+else
+	VERBOSE=0
 fi
 
 BIN_PATH=$1
@@ -20,70 +28,79 @@ fi
 declare -r ssh="ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 -o BatchMode=yes"
 declare -r tssh="timeout ${TO} ${ssh}"
 
-output=""
-pass_count=0
-fail_count=0
+declare -r c_outp=$(mktemp)
+declare -r s_outp=$(mktemp)
+
+declare -i skip_count=0
+declare -i pass_count=0
+declare -i fail_count=0
 
 simple_tests=(
-    "cq_data"
-    "dgram"
-    "dgram_waitset"
-    "msg"
-    "msg_epoll"
-    "poll"
-    "rdm"
-    "rdm_rma_simple"
-    "rdm_shared_ctx"
-    "rdm_tagged_search"
-    "scalable_ep"
-    "cmatose"
+	"cq_data"
+	"dgram"
+	"dgram_waitset"
+	"msg"
+	"msg_epoll"
+	"poll"
+	"rdm"
+	"rdm_rma_simple"
+	"rdm_shared_ctx"
+	"rdm_tagged_search"
+	"scalable_ep"
+	"cmatose"
 )
 
 quick_tests=(
-    "msg_pingpong -I 100 -S 1024"
-    "msg_rma -o write -I 100 -S 1024"
-    "msg_rma -o read -I 100 -S 1024"
-    "msg_rma -o writedata -I 100 -S 1024"
-    "rdm_atomic -I 100 -S 1024 -o all"
-    "rdm_cntr_pingpong -I 100 -S 1024"
-    "rdm_inject_pingpong -I 100 -S 128"
-    "rdm_multi_recv -I 100 -S 1024"
-    "rdm_pingpong -I 100 -S 1024"
-    "rdm_rma -o write -I 100 -S 1024"
-    "rdm_rma -o read -I 100 -S 1024"
-    "rdm_rma -o writedata -I 100 -S 1024"
-    "rdm_tagged_pingpong -I 100 -S 1024"
-    "ud_pingpong -I 100 -S 1024"
-    "rc_pingpong -n 100 -S 1024"
+	"msg_pingpong -I 100 -S 1024"
+	"msg_rma -o write -I 100 -S 1024"
+	"msg_rma -o read -I 100 -S 1024"
+	"msg_rma -o writedata -I 100 -S 1024"
+	"rdm_atomic -I 100 -S 1024 -o all"
+	"rdm_cntr_pingpong -I 100 -S 1024"
+	"rdm_inject_pingpong -I 100 -S 128"
+	"rdm_multi_recv -I 100 -S 1024"
+	"rdm_pingpong -I 100 -S 1024"
+	"rdm_rma -o write -I 100 -S 1024"
+	"rdm_rma -o read -I 100 -S 1024"
+	"rdm_rma -o writedata -I 100 -S 1024"
+	"rdm_tagged_pingpong -I 100 -S 1024"
+	"ud_pingpong -I 100 -S 1024"
+	"rc_pingpong -n 100 -S 1024"
 )
 
 all_tests=(
-    "msg_pingpong"
-    "msg_rma -o write"
-    "msg_rma -o read"
-    "msg_rma -o writedata"
-    "rdm_atomic -o all"
-    "rdm_cntr_pingpong"
-    "rdm_inject_pingpong"
-    "rdm_multi_recv"
-    "rdm_pingpong"
-    "rdm_rma -o write"
-    "rdm_rma -o read"
-    "rdm_rma -o writedata"
-    "rdm_tagged_pingpong"
-    "ud_pingpong"
-    "rc_pingpong"
+	"msg_pingpong"
+	"msg_rma -o write"
+	"msg_rma -o read"
+	"msg_rma -o writedata"
+	"rdm_atomic -o all"
+	"rdm_cntr_pingpong"
+	"rdm_inject_pingpong"
+	"rdm_multi_recv"
+	"rdm_pingpong"
+	"rdm_rma -o write"
+	"rdm_rma -o read"
+	"rdm_rma -o writedata"
+	"rdm_tagged_pingpong"
+	"ud_pingpong"
+	"rc_pingpong"
 )
 
 unit_tests=(
-    "av_test -d 192.168.10.1 -n 1"
-    "dom_test -n 2"
-    "eq_test"	
-    "size_left_test")
+	"av_test -d 192.168.10.1 -n 1"
+	"dom_test -n 2"
+	"eq_test"	
+	"size_left_test"
+)
 
-function print_border 
-{
-	echo "--------------------------------------------------------------"
+function print_border {
+	echo "# --------------------------------------------------------------"
+}
+
+# TODO: prefix with something, check for "" input
+function print_results {
+	#echo "$1" | sed ':a;N;$!ba;s/\n/\n  # /g'
+	echo "$1" | sed 's/^/  # /g'
 }
 
 function cleanup_and_exit {
@@ -92,8 +109,9 @@ function cleanup_and_exit {
 }
 
 function cleanup {
-        $ssh ${CLIENT} "ps aux | grep fi_ | grep -v grep | awk '{print \$2}' | xargs -r kill -9" > /dev/null
-        $ssh ${SERVER} "ps aux | grep fi_ | grep -v grep | awk '{print \$2}' | xargs -r kill -9" > /dev/null
+	$ssh ${CLIENT} "ps -eo comm,pid | grep ^fi_ | awk '{print \$2}' | xargs -r kill -9" > /dev/null
+	$ssh ${SERVER} "ps -eo comm,pid | grep ^fi_ | awk '{print \$2}' | xargs -r kill -9" > /dev/null
+	rm -f $c_outp $s_outp
 }
 
 function run_test {
@@ -102,22 +120,25 @@ function run_test {
 	local ret1=0
 	local ret2=0
 	local test_exe="fi_${test} -f $PROV"
+	local SO=""
+	local CO=""
 
 	if [ "${type}" = 'unit' ]; then
-		echo "Running test $test_exe"
-		(set -x; $tssh $SERVER "${BIN_PATH}/fi_${test} -f $PROV") &
-		
+		$tssh $SERVER "${BIN_PATH}/fi_${test} -f $PROV" &> $s_outp &
 		p1=$!
 
 		wait $p1
 		ret1=$?
 
+		SO=$(cat $s_outp)
+
 	elif [ "${type}" = 'client-server' ]; then
-		echo "Running test $test_exe"
-		(set -x; $tssh $SERVER "${BIN_PATH}/fi_${test} -f $PROV -s $SERVER") &
+
+		$tssh $SERVER "${BIN_PATH}/fi_${test} -f $PROV -s $SERVER" &> $s_outp &
 		p1=$!
 		sleep 1s
-		(set -x; $tssh $CLIENT "${BIN_PATH}/fi_${test} $SERVER -f $PROV") &
+
+		$tssh $CLIENT "${BIN_PATH}/fi_${test} $SERVER -f $PROV" &> $c_outp &
 		p2=$!
 
 		wait $p1
@@ -125,35 +146,40 @@ function run_test {
 
 		wait $p2
 		ret2=$?
+
+		SO=$(cat $s_outp)
+		CO=$(cat $c_outp)
 	fi
 
-	if [ $ret1 != 0 -o $ret1 != 0 ]; then
+	if [ "$ret1" == "61" -o "$ret1" == "61" ]; then
+		printf "%-50s%10s\n" "$test_exe:" "Notrun"
+		skip_count+=1
+	
+	elif [ "$ret1" != "0" -o "$ret1" != "0" ]; then
 		if [ $ret1 == 124 -o $ret2 == 124 ]; then
-			echo "Test timed out."
 			cleanup
 		fi
-		printf -v output "%s%-50s%10s\n" "$output" "$test_exe:" "Fail"
-		fail_count=$((fail_count + 1))
+		printf "%-50s%10s\n" "$test_exe:" "Fail"
+		fail_count+=1
+		[ "$VERBOSE" == "1" ] && print_results "$SO" && print_results "$CO"
 	else
-		printf -v output "%s%-50s%10s\n" "$output" "$test_exe:" "Pass"
-		pass_count=$((pass_count + 1))
+		printf "%-50s%10s\n" "$test_exe:" "Pass"
+		pass_count+=1
 	fi
-	echo ""
 }
 
 trap cleanup_and_exit SIGINT
 
-#unit tests
-testset=("${unit_tests[@]}")  
+printf "# %-50s%10s\n" "Test" "Result"
+print_border
 
-for test in "${testset[@]}"; do
+#unit tests
+for test in "${unit_tests[@]}"; do
 	run_test "unit" "$test"
 done
 
 #simple tests
-testset=("${simple_tests[@]}")  
-
-for test in "${testset[@]}"; do
+for test in "${simple_tests[@]}"; do
 	run_test "client-server" "$test"
 done
 
@@ -171,17 +197,12 @@ done
 
 total=$(( $pass_count + $fail_count ))
 
-printf "\n%-50s%10s\n" "Test" "Result"
 print_border
 
-printf "$output"
+printf "# %-50s%10d\n" "Total Pass" $pass_count
+printf "# %-50s%10d\n" "Total Fail" $fail_count
+printf "# %-50s%10.2f\n" "Percentage of Pass" `echo "scale=2; $pass_count * 100 / $total" | bc`
 print_border
 
-printf "%-50s%10d\n" "Total Pass" $pass_count
-printf "%-50s%10d\n" "Total Fail" $fail_count
-printf "%-50s%10.2f\n" "Percentage of Pass" `echo "scale=2; $pass_count * 100 / $total" | bc`
-print_border
-
-if (( $fail_count )); then
-	exit 1
-fi
+cleanup
+exit $fail_count
