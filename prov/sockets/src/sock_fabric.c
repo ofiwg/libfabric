ofiwg/libfabric

commit cad9e622b3b67d05031e8004e673161685449db6

- Set src_addr in fi_getinfo based on getaddrinfo(hostname) if src_addr    is not provided.  - Set dst_addr to NULL, if dst_addr is not requested
Signed-off-by: Jithin Jose <jithin.jose@intel.com>

prov/sockets/src/sock_fabric.c
@@ -208,6 +208,7 @@ static int sock_get_src_addr(struct sockaddr_in *dest_addr,
 	}
 
 	ret = getsockname(sock, (struct sockaddr *) src_addr, &len);
+	src_addr->sin_port = 0;
 	if (ret) {
 		SOCK_LOG_ERROR("getsockname failed\n");
 		ret = -errno;
@@ -239,6 +240,9 @@ static int sock_ep_getinfo(const char *node, const char *service, uint64_t flags
 			return -FI_ENODATA;
 		}
 		src_addr = (struct sockaddr_in *) rai->ai_addr;
+		if (service == NULL)
+			src_addr->sin_port = 0;
+
 		if (hints && hints->dest_addr)
 			dest_addr = hints->dest_addr;
 	} else {
@@ -314,6 +318,11 @@ static int sock_getinfo(uint32_t version, const char *node, const char *service,
 
 	if (!node && !service && !hints) {
 		flags |= FI_SOURCE;
+		gethostname(hostname, sizeof hostname);
+		node = hostname;
+	}
+
+	if (!node && !service && !(flags & FI_SOURCE)) {
 		gethostname(hostname, sizeof hostname);
 		node = hostname;
 	}

prov/sockets/src/sock_ep.c
@@ -1131,18 +1131,18 @@ struct fi_info *sock_fi_info(enum fi_ep_type ep_type, struct fi_info *hints,
 	if (!info)
 		return NULL;
 	
-	info->src_addr = calloc(1, sizeof(struct sockaddr_in));
-	info->dest_addr = calloc(1, sizeof(struct sockaddr_in));
-	
+	info->src_addr = calloc(1, sizeof(struct sockaddr_in));	
 	info->mode = SOCK_MODE;
 	info->addr_format = FI_SOCKADDR_IN;
-	info->dest_addrlen = info->src_addrlen = sizeof(struct sockaddr_in);
 
 	if (src_addr)
 		memcpy(info->src_addr, src_addr, sizeof(struct sockaddr_in));
 	
-	if (dest_addr)
+	if (dest_addr) {
+		info->dest_addr = calloc(1, sizeof(struct sockaddr_in));
+		info->dest_addrlen = info->src_addrlen = sizeof(struct sockaddr_in);
 		memcpy(info->dest_addr, dest_addr, sizeof(struct sockaddr_in));
+	}
 
 	if (hints) {
 		if (hints->caps)

commit b6ed699832b2b1922ec030979aeb40810ca1392c

prov/sockets: Do not ignore dest_addr in hints
Commit cad9e622b3b67d05031e8004e673161685449db6 introduced a regression
when the user ran the following sequence of code:

> hints->dest_addr = address;
> hints->dest_addrlen = address_len;
> ret = fi_getinfo(EXS_FI_VERSION, NULL, NULL, 0, &hints, all_info);

The dest_addr supplied by the user is ignored and replaced with
the address corresponding to the hostname for both source and
destination address.  In my cluster setup, the RDMA interfaces use a
different naming scheme than the Ethernet management interfaces, so this
causes the sockets provider to use the wrong interface for establishing
connections.

This commit fixes the issue by not implicitly using the hostname if the
user in fact specified a destination address in hints.

Signed-off-by: Patrick MacArthur <pmacarth@iol.unh.edu>

prov/sockets/src/sock_fabric.c
@@ -512,7 +512,7 @@ static int sock_getinfo(uint32_t version, const char *node, const char *service,
 		node = hostname;
 	}
 
-	if (!node && !service && !(flags & FI_SOURCE)) {
+	if (!node && !service && !(flags & FI_SOURCE) && !hints->dest_addr) {
 		gethostname(hostname, sizeof hostname);
 		node = hostname;
 	}