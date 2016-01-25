ofiwg/libfabric

commit 656f12e74c2519cae985407cdeaa2bdc36484374

libnl: lots of things can fail temporarily
Similar to d4260d5, the receive side of libnl calls can fail
temporarily (e.g., because the kernel is currently out of resources).
This commit adds two cases on the receive side where libnl can fail
temporarily:

1. nl_recvmsgs_default() itself returns "try again later"
2. The message returned from the kernel contains -ECONNREFUSED

In both cases, delay and try again.

Many thanks to Ioannis Tsatsaronakis and Landon Noll for their
patience and the use of their cluster in tracking down this issue.

Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

prov/usnic/src/usnic_direct/libnl_utils_common.c
@@ -80,14 +80,23 @@ static int usnic_is_nlreply_expected(struct usnic_nl_sk *unlsk,
 	return 1;
 }
 
-static int usnic_is_nlreply_err(struct nlmsghdr *nlm_hdr)
+static int usnic_is_nlreply_err(struct nlmsghdr *nlm_hdr,
+                                struct usnic_rt_cb_arg *arg)
 {
 	if (nlm_hdr->nlmsg_type == NLMSG_ERROR) {
 		struct nlmsgerr *e = (struct nlmsgerr *)nlmsg_data(nlm_hdr);
-		if (nlm_hdr->nlmsg_len >= (__u32)NLMSG_SIZE(sizeof(*e)))
+		if (nlm_hdr->nlmsg_len >= (__u32)NLMSG_SIZE(sizeof(*e))) {
 			usnic_strerror(e->error,
 					"Received a netlink error message");
-		else
+			/* Sometimes nl_send() succeeds, but the
+			 * request fails because the kernel is
+			 * temporarily out of resources.  In these
+			 * cases, we should tell the caller that they
+			 * should try again. */
+			if (e->error == -ECONNREFUSED) {
+				arg->retry = 1;
+			}
+		} else
 			usnic_err(
 				"Received a truncated netlink error message\n");
 		return 1;
@@ -213,7 +222,7 @@ static int usnic_rt_raw_parse_cb(struct nl_msg *msg, void *arg)
 		return NL_SKIP;
 	}
 
-	if (usnic_is_nlreply_err(nlm_hdr)) {
+	if (usnic_is_nlreply_err(nlm_hdr, lookup_arg)) {
 		usnic_nlmsg_dump(msg);
 		return NL_SKIP;
 	}
@@ -268,6 +277,7 @@ int usnic_nl_rt_lookup(uint32_t src_addr, uint32_t dst_addr, int oif,
 	struct usnic_rt_cb_arg	arg;
 	int			err;
 
+retry:
 	unlsk = NULL;
 	err = usnic_nl_sk_alloc(&unlsk, NETLINK_ROUTE);
 	if (err)
@@ -310,7 +320,24 @@ int usnic_nl_rt_lookup(uint32_t src_addr, uint32_t dst_addr, int oif,
 		goto out;
 	}
 
-	NL_RECVMSGS(unlsk->nlh, arg, EHOSTUNREACH, err, out);
+	/* Sometimes the recvmsg can fail because something is
+	 * temporarily out of resources.  In this case, delay a little
+	 * and try again. */
+	do {
+		err = 0;
+		NL_RECVMSGS(unlsk->nlh, arg, EAGAIN, err, out);
+		if (err == EAGAIN) {
+			usleep(5);
+		}
+	} while (err != EAGAIN);
+
+	/* If we got a reply back that indicated that the kernel was
+	 * too busy to handle this request, delay a little and try
+	 * again. */
+        if (arg.retry) {
+            usleep(5);
+            goto retry;
+        }
 
 	if (arg.found) {
 		*nh_addr = arg.nh_addr;

prov/usnic/src/usnic_direct/libnl1_utils.h
@@ -92,6 +92,7 @@ struct usnic_rt_cb_arg {
 	int			oif;
 	int			found;
 	int			msg_cnt;
+	int			retry;
 	struct usnic_nl_sk	*unlsk;
 };
 

prov/usnic/src/usnic_direct/libnl3_utils.h
@@ -77,6 +77,7 @@ struct usnic_rt_cb_arg {
 	int			oif;
 	int			found;
 	int			replied;
+	int			retry;
 	struct usnic_nl_sk	*unlsk;
 };
 

commit 1b04446791231b617b715a30df042f327ed25d56

usnic: fix typo introduced in 656f12e
Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

prov/usnic/src/usnic_direct/libnl_utils_common.c
@@ -329,7 +329,7 @@ retry:
 		if (err == EAGAIN) {
 			usleep(5);
 		}
-	} while (err != EAGAIN);
+	} while (err == EAGAIN);
 
 	/* If we got a reply back that indicated that the kernel was
 	 * too busy to handle this request, delay a little and try