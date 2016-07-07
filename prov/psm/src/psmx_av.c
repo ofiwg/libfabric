ofiwg/libfabric

commit 74e9037df1c209539373bc7822710d0df42440dd

prov/psm,psm2: prevent segfault in fi_av_insert due to invalid input
It is possible that applications call fi_av_insert with NULL as
the address array(s). One example is to use "fi->dest_addr" as the
input without verifying it's value. This would lead to a segfault.
Add parameter checking to return error in such case.

Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>

prov/psm/src/psmx_av.c
@@ -149,6 +149,12 @@ static int psmx_av_insert(struct fid_av *av, const void *addr, size_t count,
 	fi_addr_t *result = NULL;
 	struct psmx_epaddr_context *epaddr_context;
 
+	if (count && (!addr || !fi_addr)) {
+		FI_INFO(&psmx_prov, FI_LOG_AV,
+			"NULL address array: addr=%p fi_addr=%p.\n", addr, fi_addr);
+		return -FI_EINVAL;
+	}
+
 	av_priv = container_of(av, struct psmx_fid_av, av);
 
 	if ((av_priv->flags & FI_EVENT) && !av_priv->eq)

prov/psm2/src/psmx2_av.c
@@ -241,6 +241,12 @@ static int psmx2_av_insert(struct fid_av *av, const void *addr,
 	int error_count;
 	int i;
 
+	if (count && (!addr || !fi_addr)) {
+		FI_INFO(&psmx2_prov, FI_LOG_AV,
+			"NULL address array: addr=%p fi_addr=%p.\n", addr, fi_addr);
+		return -FI_EINVAL;
+	}
+
 	av_priv = container_of(av, struct psmx2_fid_av, av);
 
 	if ((av_priv->flags & FI_EVENT) && !av_priv->eq)

commit a2fb66fb463be6b77e260963e05e309cab349dff

prov/psm,psm2: Fix a bug introduced by commit 74e9037
The commit "prov/psm,psm2: prevent segfault in fi_av_insert due to
invalid input" incorrectly returns error when the output address
array is NULL, which should be allowed.

Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>

prov/psm/src/psmx_av.c
@@ -149,9 +149,9 @@ static int psmx_av_insert(struct fid_av *av, const void *addr, size_t count,
 	fi_addr_t *result = NULL;
 	struct psmx_epaddr_context *epaddr_context;
 
-	if (count && (!addr || !fi_addr)) {
+	if (count && !addr) {
 		FI_INFO(&psmx_prov, FI_LOG_AV,
-			"NULL address array: addr=%p fi_addr=%p.\n", addr, fi_addr);
+			"the input address array is NULL.\n");
 		return -FI_EINVAL;
 	}
 

prov/psm2/src/psmx2_av.c
@@ -241,9 +241,9 @@ static int psmx2_av_insert(struct fid_av *av, const void *addr,
 	int error_count;
 	int i;
 
-	if (count && (!addr || !fi_addr)) {
+	if (count && !addr) {
 		FI_INFO(&psmx2_prov, FI_LOG_AV,
-			"NULL address array: addr=%p fi_addr=%p.\n", addr, fi_addr);
+			"the input address array is NULL.\n");
 		return -FI_EINVAL;
 	}
 