d34217f85c6379aa02dc9a0bd51da061bedf1b55
soblomov <sergey.oblomov@intel.com>
2016-08-12 17:04:09 +0300

    fixed windows build
    fixed build on windows platform
    - disabled processing of object in sock_wait_get_obj for
      windows platform
    - updated Linux specific header replacer to provide compilation
    
    Signed-off-by: Oblomov, Sergery <sergey.oblomov@intel.com>

--- include/windows/netinet/in.h
+++ include/windows/netinet/in.h
@@ -1,4 +1,9 @@
 
-#pragma once
+#ifndef _FI_WIN_NETINET_IN_H_
+#define _FI_WIN_NETINET_IN_H_
 
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
 
+#endif /* _FI_WIN_NETINET_IN_H_ */
