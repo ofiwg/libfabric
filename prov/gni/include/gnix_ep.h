ztiffany/libfabric-cray

prov/gni: Adds four byte alignment for sendv/recvv
	  - Each send vector is checked for alignment on the
	  client. If unaligned buffers are found, the
	  unaligned head and tail sections are sent to the
	  remote peer in the control message.

	  - When the iov txd's are being built, the recv iov
	  is checked for alignment. If unaligned recv addrs
	  are found, a chained transaction is created to
	  GET the head and tail sections.
	  	  - A head tail data buffer pool was added
		  to each endpoint for GETting the head and
		  tail sections on the remote peer.

	  - The heads and tails are finally copied to the
	  posted recv buffer just before sending the rndzv
	  fin message back to the sender.

Signed-off-by: Evan Harvey <eharvey@cray.com>

prov/gni/include/gnix_ep.h
@@ -142,6 +142,36 @@ typedef ssize_t (*trecvmsg_func_t)(struct fid_ep *ep,
  * inline functions
  */
 
+static inline struct slist_entry *_gnix_ep_get_htd_buf(struct gnix_fid_ep *ep)
+{
+	struct slist_entry *e;
+
+	fastlock_acquire(&ep->htd_pool.lock);
+
+	e = slist_remove_head(&ep->htd_pool.sl);
+
+	fastlock_release(&ep->htd_pool.lock);
+
+	return e;
+}
+
+static inline gni_mem_handle_t _gnix_ep_get_htd_mdh(struct gnix_fid_ep *ep)
+{
+	return ep->htd_pool.md->mem_hndl;
+}
+
+static inline void _gnix_ep_release_htd_buf(struct gnix_fid_ep *ep, struct slist_entry *e)
+{
+	fastlock_acquire(&ep->htd_pool.lock);
+
+	GNIX_DEBUG(FI_LOG_EP_DATA, "sl.head = %p, sl.tail = %p\n", ep->htd_pool.sl.head,
+		   ep->htd_pool.sl.tail);
+
+	slist_insert_tail(e, &ep->htd_pool.sl);
+
+	fastlock_release(&ep->htd_pool.lock);
+}
+
 static inline struct gnix_fab_req *
 _gnix_fr_alloc(struct gnix_fid_ep *ep)
 {
@@ -170,6 +200,13 @@ static inline void
 _gnix_fr_free(struct gnix_fid_ep *ep, struct gnix_fab_req *fr)
 {
 	assert(fr->gnix_ep == ep);
+
+	if (fr->msg.htd_buf_e != NULL) {
+		_gnix_ep_release_htd_buf(ep, fr->msg.htd_buf_e);
+		fr->msg.htd_buf_e = NULL;
+		fr->msg.htd_buf = NULL;
+	}
+
 	_gnix_fl_free(&fr->dlist, &ep->fr_freelist);
 	_gnix_ref_put(ep);
 }

ofiwg/libfabric

prov/gni: Ensure FI_PEEK CQE is correct.
	  - Updated send/recv code to use the minimum
	  of the send length/recv length for generating
	  CQEs.
	  - Updated the peek_event_present_small_buff_provid
	  gnitest to use the recv buffer length for
	  validating the CQE contents.
	  - Updated __gnix_peek_request to support iovectors.
	  - Enabled and tested xpmem with sendv/recvv.
upstream merge of ofi-cray/libfabric-cray#922
(cherry picked from commit ofi-cray/libfabric-cray@8a9234cc5cc4dfa2b71e5907dc627adc28bcc4a2)
(cherry picked from commit ofi-cray/libfabric-cray@42bd4f183709d84fb177dc24bf8fc9d5657311e4)
(cherry picked from commit ofi-cray/libfabric-cray@59e05e18b2597ea8ff67ec9b25d900a764d2b804)

prov/gni/include/gnix_ep.h
@@ -142,6 +142,36 @@ typedef ssize_t (*trecvmsg_func_t)(struct fid_ep *ep,
  * inline functions
  */
 
+static inline struct slist_entry *_gnix_ep_get_htd_buf(struct gnix_fid_ep *ep)
+{
+	struct slist_entry *e;
+
+	fastlock_acquire(&ep->htd_pool.lock);
+
+	e = slist_remove_head(&ep->htd_pool.sl);
+
+	fastlock_release(&ep->htd_pool.lock);
+
+	return e;
+}
+
+static inline gni_mem_handle_t _gnix_ep_get_htd_mdh(struct gnix_fid_ep *ep)
+{
+	return ep->htd_pool.md->mem_hndl;
+}
+
+static inline void _gnix_ep_release_htd_buf(struct gnix_fid_ep *ep, struct slist_entry *e)
+{
+	fastlock_acquire(&ep->htd_pool.lock);
+
+	GNIX_DEBUG(FI_LOG_EP_DATA, "sl.head = %p, sl.tail = %p\n", ep->htd_pool.sl.head,
+		   ep->htd_pool.sl.tail);
+
+	slist_insert_head(e, &ep->htd_pool.sl);
+
+	fastlock_release(&ep->htd_pool.lock);
+}
+
 static inline struct gnix_fab_req *
 _gnix_fr_alloc(struct gnix_fid_ep *ep)
 {
@@ -170,6 +200,13 @@ static inline void
 _gnix_fr_free(struct gnix_fid_ep *ep, struct gnix_fab_req *fr)
 {
 	assert(fr->gnix_ep == ep);
+
+	if (fr->msg.htd_buf_e != NULL) {
+		_gnix_ep_release_htd_buf(ep, fr->msg.htd_buf_e);
+		fr->msg.htd_buf_e = NULL;
+		fr->msg.htd_buf = NULL;
+	}
+
 	_gnix_fl_free(&fr->dlist, &ep->fr_freelist);
 	_gnix_ref_put(ep);
 }