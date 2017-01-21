ab767b5c7da25c8cd2610ee8ad655490af01b2ba
Sean Hefty <sean.hefty@intel.com>
2017-01-20 16:46:11 -0800

    prov/rxd: Cleanup of provider code
    The progress thread spins, resulting in failures of nearly all
    fabtests when run on virtual box.  To fix this, switch from auto
    to manual progress.  The rxd cq and ep code is moved to make
    better use of the util cq and ep code.  This allows the util
    progress code to be used in conjunction with the rxd code.
    
    Apply several code cleanups as part of restructuring the
    provider code
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- prov/util/src/util_ep.c
+++ prov/util/src/util_ep.c
@@ -72,7 +72,9 @@ int ofi_endpoint_init(struct fid_domain *domain, const struct util_prov *util_pr
 
 	ep->ep_fid.fid.fclass = FI_CLASS_EP;
 	ep->ep_fid.fid.context = context;
+
 	ep->domain = util_domain;
+	ep->caps = info->caps;
 	ep->progress = progress;
 	ofi_atomic_inc32(&util_domain->ref);
 	return 0;
