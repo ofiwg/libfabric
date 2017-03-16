5c95bc8bf2b37d43e12342ebd89c615b046fcc5f
Ben Turrubiates <bturrubi@cisco.com>
2017-03-16 13:49:56 -0700

    fabric: Free hints struct used for compatibility.
    Signed-off-by: Ben Turrubiates <bturrubi@cisco.com>

--- src/abi_1_0.c
+++ src/abi_1_0.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 Intel Corporation. All rights reserved.
+ * Copyright (c) 2017, Cisco Systems, Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -207,16 +208,20 @@ int fi_getinfo_1_0(uint32_t version, const char *node, const char *service,
 		    struct fi_info_1_0 **info)
 {
 	struct fi_info *hints;
+	int ret;
 
 	if (hints_1_0) {
-		hints = (struct fi_info *) fi_dupinfo_1_0(hints_1_0);
+		hints = (struct fi_info *)fi_dupinfo_1_0(hints_1_0);
 		if (!hints)
 			return -FI_ENOMEM;
 	} else {
 		hints = NULL;
 	}
-	return fi_getinfo(version, node, service, flags, hints,
-			  (struct fi_info **) info);
+	ret = fi_getinfo(version, node, service, flags, hints,
+			 (struct fi_info **)info);
+	fi_freeinfo(hints);
+
+	return ret;
 }
 COMPAT_SYMVER(fi_getinfo_1_0, fi_getinfo, FABRIC_1.0);
 
