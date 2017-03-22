7a10d67d48b75becf0637fb1582d64bd04a53909
Jianxin Xiong <jianxin.xiong@intel.com>
2017-03-22 07:31:22 -0700

    prov/psm2: Handle CQs that is not bound to any EP
    There is Tx/Rx context associated to CQs that are not bound to EP. They
    need to be handled specially when being polled.
    
    Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>

--- prov/psm2/src/psmx2_cq.c
+++ prov/psm2/src/psmx2_cq.c
@@ -651,13 +651,17 @@ static ssize_t psmx2_cq_readfrom(struct fid_cq *cq, void *buf, size_t count,
 	cq_priv = container_of(cq, struct psmx2_fid_cq, cq);
 
 	if (slist_empty(&cq_priv->event_queue) || !buf) {
-		ret = psmx2_cq_poll_mq(cq_priv, cq_priv->trx_ctxt,
-				       (struct psmx2_cq_event *)buf, count, src_addr);
-		if (ret > 0)
-			return ret;
+		if (cq_priv->trx_ctxt) {
+			ret = psmx2_cq_poll_mq(cq_priv, cq_priv->trx_ctxt,
+					       (struct psmx2_cq_event *)buf, count, src_addr);
+			if (ret > 0)
+				return ret;
 
-		if (cq_priv->trx_ctxt->am_initialized)
-			psmx2_am_progress(cq_priv->trx_ctxt);
+			if (cq_priv->trx_ctxt->am_initialized)
+				psmx2_am_progress(cq_priv->trx_ctxt);
+		} else {
+			psmx2_progress_all(cq_priv->domain);
+		}
 	}
 
 	if (cq_priv->pending_error)
@@ -762,8 +766,12 @@ static ssize_t psmx2_cq_sreadfrom(struct fid_cq *cq, void *buf, size_t count,
 		} else {
 			clock_gettime(CLOCK_REALTIME, &ts0);
 			while (1) {
-				if (psmx2_cq_poll_mq(cq_priv, cq_priv->trx_ctxt, NULL, 0, NULL))
-					break;
+				if (cq_priv->trx_ctxt) {
+					if (psmx2_cq_poll_mq(cq_priv, cq_priv->trx_ctxt, NULL, 0, NULL))
+						break;
+				} else {
+					psmx2_progress_all(cq_priv->domain);
+				}
 
 				/* CQ may be updated asynchronously by the AM handlers */
 				if (cq_priv->event_count > event_count)
