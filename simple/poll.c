703881e861679ccf360584281aab2e5fee98d872
Dmitry Gladkov <dmitry.gladkov@intel.com>
2017-04-11 13:23:07 +0300

    simple/poll: Optimize getopt arguments - remove excessive options
    Change-Id: I2add904e7c900d4318d52d7223a4586e4d024ea3
    Signed-off-by: Dmitry Gladkov <dmitry.gladkov@intel.com>

--- simple/poll.c
+++ simple/poll.c
@@ -216,7 +216,7 @@ int main(int argc, char **argv)
 	if (!hints)
 		return EXIT_FAILURE;
 
-	while ((op = getopt(argc, argv, "h" ADDR_OPTS CS_OPTS INFO_OPTS)) != -1) {
+	while ((op = getopt(argc, argv, "h" CS_OPTS INFO_OPTS)) != -1) {
 		switch (op) {
 		default:
 			ft_parse_addr_opts(op, optarg, &opts);
