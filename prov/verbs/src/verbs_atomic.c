ofiwg/libfabric

commit 6ae1b565a349caddf2e5081e9e189ad7f445bea6

prov/verbs: Support fi_query_atomic
Signed-off-by: Arun C Ilango <arun.ilango@intel.com>

prov/verbs/src/verbs_atomic.c
@@ -35,54 +35,75 @@
 #include "fi_verbs.h"
 
 
-static int
-fi_ibv_msg_ep_atomic_writevalid(struct fid_ep *ep, enum fi_datatype datatype,
-				enum fi_op op, size_t *count)
+#define fi_ibv_atomicvalid(name, flags)					\
+static int fi_ibv_msg_ep_atomic_ ## name(struct fid_ep *ep_fid,		\
+			      enum fi_datatype datatype,  		\
+			      enum fi_op op, size_t *count)             \
+{                                                                       \
+	struct fi_ibv_msg_ep *ep = container_of(ep_fid,			\
+						struct fi_ibv_msg_ep,   \
+						ep_fid);                \
+	struct fi_atomic_attr attr;                                     \
+	int ret;                                                        \
+                                                                        \
+	ret = fi_ibv_query_atomic(&ep->domain->domain_fid, datatype,	\
+				  op, &attr, flags);                    \
+	if (!ret)                                                       \
+		*count = attr.count;                                    \
+	return ret;                                                     \
+}                                                                       \
+
+fi_ibv_atomicvalid(writevalid, 0);
+fi_ibv_atomicvalid(readwritevalid, FI_FETCH_ATOMIC);
+fi_ibv_atomicvalid(compwritevalid, FI_COMPARE_ATOMIC);
+
+int fi_ibv_query_atomic(struct fid_domain *domain_fid, enum fi_datatype datatype,
+			enum fi_op op, struct fi_atomic_attr *attr,
+			uint64_t flags)
 {
-	switch (op) {
-	case FI_ATOMIC_WRITE:
-		break;
-	default:
+	struct fi_ibv_domain *domain = container_of(domain_fid,
+						    struct fi_ibv_domain,
+						    domain_fid);
+	char *log_str_fetch = "fi_fetch_atomic with FI_SUM op";
+	char *log_str_comp = "fi_compare_atomic";
+	char *log_str;
+
+	if (flags & FI_TAGGED)
 		return -FI_ENOSYS;
-	}
 
-	switch (datatype) {
-	case FI_INT64:
-	case FI_UINT64:
-#if __BITS_PER_LONG == 64
-	case FI_DOUBLE:
-	case FI_FLOAT:
-#endif
-		break;
-	default:
-		return -FI_EINVAL;
-	}
+	if ((flags & FI_FETCH_ATOMIC) && (flags & FI_COMPARE_ATOMIC))
+		return -FI_EBADFLAGS;
 
-	if (count)
-		*count = 1;
-	return 0;
-}
-
-static int
-fi_ibv_msg_ep_atomic_readwritevalid(struct fid_ep *ep, enum fi_datatype datatype,
-				enum fi_op op, size_t *count)
-{
-	struct fi_ibv_msg_ep *_ep = container_of(ep, struct fi_ibv_msg_ep, ep_fid);
-
-	switch (op) {
-	case FI_ATOMIC_READ:
-		break;
-	case FI_SUM:
-		if (_ep->info->tx_attr->op_flags & FI_INJECT) {
-			VERBS_INFO(FI_LOG_EP_DATA,"FI_INJECT not "
-				"supported for fi_fetch_atomic with FI_SUM op\n");
+	if (!flags) {
+		switch (op) {
+		case FI_ATOMIC_WRITE:
+			break;
+		default:
+			return -FI_ENOSYS;
+		}
+	} else {
+		if (flags & FI_FETCH_ATOMIC) {
+			switch (op) {
+			case FI_ATOMIC_READ:
+				goto check_datatype;
+			case FI_SUM:
+				log_str = log_str_fetch;
+				break;
+			default:
+				return -FI_ENOSYS;
+			}
+		} else if (flags & FI_COMPARE_ATOMIC) {
+			if (op != FI_CSWAP)
+				return -FI_ENOSYS;
+			log_str = log_str_comp;
+		}
+		if (domain->info->tx_attr->op_flags & FI_INJECT) {
+			VERBS_INFO(FI_LOG_EP_DATA,
+				   "FI_INJECT not supported for %s\n", log_str);
 			return -FI_EINVAL;
 		}
-		break;
-	default:
-		return -FI_ENOSYS;
 	}
-
+check_datatype:
 	switch (datatype) {
 	case FI_INT64:
 	case FI_UINT64:
@@ -95,40 +116,11 @@ fi_ibv_msg_ep_atomic_readwritevalid(struct fid_ep *ep, enum fi_datatype datatype
 		return -FI_EINVAL;
 	}
 
-	if (count)
-		*count = 1;
-	return 0;
-}
-
-static int
-fi_ibv_msg_ep_atomic_compwritevalid(struct fid_ep *ep, enum fi_datatype datatype,
-				enum fi_op op, size_t *count)
-{
-	struct fi_ibv_msg_ep *_ep = container_of(ep, struct fi_ibv_msg_ep, ep_fid);
-
-	if (op != FI_CSWAP)
-		return -FI_ENOSYS;
-
-	if (_ep->info->tx_attr->op_flags & FI_INJECT) {
-		VERBS_INFO(FI_LOG_EP_DATA, "FI_INJECT not supported "
-			   "for fi_compare_atomic\n");
+	attr->size = fi_datatype_size(datatype);
+	if (attr->size == 0)
 		return -FI_EINVAL;
-	}
 
-	switch (datatype) {
-	case FI_INT64:
-	case FI_UINT64:
-#if __BITS_PER_LONG == 64
-	case FI_DOUBLE:
-	case FI_FLOAT:
-#endif
-		break;
-	default:
-		return -FI_EINVAL;
-	}
-
-	if (count)
-		*count = 1;
+	attr->count = 1;
 	return 0;
 }
 

prov/verbs/src/fi_verbs.h
@@ -289,6 +289,7 @@ struct fi_ibv_msg_ep {
 	ofi_atomic32_t		unsignaled_send_cnt;
 	ofi_atomic32_t		comp_pending;
 	uint64_t		ep_id;
+	struct fi_ibv_domain	*domain;
 };
 
 struct fi_ibv_msg_epe {
@@ -369,6 +370,10 @@ int fi_ibv_cq_signal(struct fid_cq *cq);
 
 ssize_t fi_ibv_eq_write_event(struct fi_ibv_eq *eq, uint32_t event,
 		const void *buf, size_t len);
+
+int fi_ibv_query_atomic(struct fid_domain *domain_fid, enum fi_datatype datatype,
+			enum fi_op op, struct fi_atomic_attr *attr,
+			uint64_t flags);
 
 #define fi_ibv_set_sge(sge, buf, len, desc)				\
 	do {								\

prov/verbs/src/verbs_domain.c
@@ -307,6 +307,7 @@ static struct fi_ops_domain fi_ibv_rdm_domain_ops = {
 	.poll_open = fi_no_poll_open,
 	.stx_ctx = fi_no_stx_context,
 	.srx_ctx = fi_no_srx_context,
+	.query_atomic = fi_ibv_query_atomic,
 };
 
 static int

prov/verbs/src/verbs_msg_ep.c
@@ -361,6 +361,7 @@ int fi_ibv_open_ep(struct fid_domain *domain, struct fi_info *info,
 	ofi_atomic_initialize32(&_ep->unsignaled_send_cnt, 0);
 	ofi_atomic_initialize32(&_ep->comp_pending, 0);
 
+	_ep->domain = dom;
 	*ep = &_ep->ep_fid;
 
 	return 0;

commit c07b4c310896973e88f1c75f43ec4f6b2ad04a74

Make Travis CI treat warnings as errors and fix compiler warnings
- travis CI: Treat warnings as errors. Also break long lines.

- Fix following gcc warnings:

  - ‘for’ loop initial declarations are only allowed in C99 mode
  src/var.c:131:2: note: use option -std=c99 or -std=gnu99 to compile your code

  - prov/verbs: Fix uninitialized variable issue introduced by commit 6ae1b56

Signed-off-by: Arun C Ilango <arun.ilango@intel.com>

.travis.yml
@@ -25,25 +25,36 @@ env:
     global:
         - PREFIX=$HOME/install
         - PATH=$PREFIX/bin:$PATH
-        - CPPFLAGS=-I$PREFIX/include
+        - CPPFLAGS="-Werror -I$PREFIX/include"
         - LDFLAGS=-L$PREFIX/lib
         - LD_LIBRARY_PATH=$PREFIX/lib
         - LIBFABRIC_CONFIGURE_ARGS="--prefix=$PREFIX --enable-sockets"
 
 # Brew update GNU Autotools so that autogen can succeed
 before_install:
-    - if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; brew upgrade automake || true; brew upgrade libtool || true; fi
+    - if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update;
+        brew upgrade automake || true; brew upgrade libtool || true;
+      fi
 
 install:
     - ./autogen.sh
     # Build verbs only in linux as OS X doesn't have verbs support
-    - if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then LIBRARY_CONFIGURE_ARGS="$LIBFABRIC_CONFIGURE_ARGS --enable-usnic --enable-verbs"; fi
-    - if [[ "$TRAVIS_OS_NAME" == "linux" && "`basename $CC`" == "clang" ]]; then ./configure CFLAGS="-Werror $CFLAGS" $LIBFABRIC_CONFIGURE_ARGS --enable-debug && make -j2; fi
+    - if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+        LIBRARY_CONFIGURE_ARGS="$LIBFABRIC_CONFIGURE_ARGS --enable-usnic
+        --enable-verbs";
+      fi
+    - if [[ "$TRAVIS_OS_NAME" == "linux" && "`basename $CC`" == "clang" ]]; then
+        ./configure CFLAGS="-Werror $CFLAGS" $LIBFABRIC_CONFIGURE_ARGS
+        --enable-debug && make -j2;
+      fi
     # Test fabric direct
-    - ./configure --prefix=$PREFIX --enable-direct=sockets --enable-udp=no --enable-psm=no --enable-gni=no --enable-psm2=no --enable-verbs=no --enable-usnic=no --enable-rxm=no --enable-rxd=no
+    - ./configure --prefix=$PREFIX --enable-direct=sockets --enable-udp=no
+      --enable-psm=no --enable-gni=no --enable-psm2=no --enable-verbs=no
+      --enable-usnic=no --enable-rxm=no --enable-rxd=no
     - make -j2
     # Test loadable library option
-    - ./configure --enable-sockets=dl --disable-udp --disable-rxm --disable-rxd --disable-verbs --disable-usnic --prefix=$PREFIX
+    - ./configure --enable-sockets=dl --disable-udp --disable-rxm --disable-rxd
+      --disable-verbs --disable-usnic --prefix=$PREFIX
     - make -j2
     - make install
     - make test

prov/verbs/src/verbs_atomic.c
@@ -96,6 +96,8 @@ int fi_ibv_query_atomic(struct fid_domain *domain_fid, enum fi_datatype datatype
 			if (op != FI_CSWAP)
 				return -FI_ENOSYS;
 			log_str = log_str_comp;
+		} else {
+			return  -FI_EBADFLAGS;
 		}
 		if (domain->info->tx_attr->op_flags & FI_INJECT) {
 			VERBS_INFO(FI_LOG_EP_DATA,

src/var.c
@@ -128,7 +128,8 @@ DEFAULT_SYMVER(fi_getparams_, fi_getparams, FABRIC_1.0);
 __attribute__((visibility ("default")))
 void DEFAULT_SYMVER_PRE(fi_freeparams)(struct fi_param *params)
 {
-	for (int i = 0; params[i].name; ++i) {
+	int i;
+	for (i = 0; params[i].name; ++i) {
 		free((void*) params[i].name);
 		free((void*) params[i].help_string);
 		free((void*) params[i].value);

util/info.c
@@ -221,7 +221,7 @@ static const char *param_type(enum fi_param_type type)
 
 static int print_vars(void)
 {
-	int ret, count;
+	int ret, count, i;
 	struct fi_param *params;
 	char delim;
 
@@ -229,7 +229,7 @@ static int print_vars(void)
 	if (ret)
 		return ret;
 
-	for (int i = 0; i < count; ++i) {
+	for (i = 0; i < count; ++i) {
 		printf("# %s: %s\n", params[i].name, param_type(params[i].type));
 		printf("# %s\n", params[i].help_string);
 
@@ -248,7 +248,8 @@ static int print_vars(void)
 
 static int print_providers(struct fi_info *info)
 {
-	for (struct fi_info *cur = info; cur; cur = cur->next) {
+	struct fi_info *cur;
+	for (cur = info; cur; cur = cur->next) {
 		printf("%s:\n", cur->fabric_attr->prov_name);
 		printf("    version: %d.%d\n",
 			FI_MAJOR(cur->fabric_attr->prov_version),
@@ -259,7 +260,8 @@ static int print_providers(struct fi_info *info)
 
 static int print_short_info(struct fi_info *info)
 {
-	for (struct fi_info *cur = info; cur; cur = cur->next) {
+	struct fi_info *cur;
+	for (cur = info; cur; cur = cur->next) {
 		printf("provider: %s\n", cur->fabric_attr->prov_name);
 		printf("    fabric: %s\n", cur->fabric_attr->name),
 		printf("    domain: %s\n", cur->domain_attr->name),
@@ -275,7 +277,8 @@ static int print_short_info(struct fi_info *info)
 
 static int print_long_info(struct fi_info *info)
 {
-	for (struct fi_info *cur = info; cur; cur = cur->next) {
+	struct fi_info *cur;
+	for (cur = info; cur; cur = cur->next) {
 		printf("---\n");
 		printf("%s", fi_tostr(cur, FI_TYPE_INFO));
 	}