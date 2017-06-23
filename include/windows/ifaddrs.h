0439cd176bbf4dea4db9adbd9c42d8b92dcf475b
Oblomov, Sergey <sergey.oblomov@intel.com>
2017-06-23 16:02:50 +0300

    win/utils: added getifaddr implementation
    - added implementation of Linux call 'getifaddr' to
      Windows part, also added "struct ifaddrs" datatype
      and implementation of 'freeifaddrs'
    - added header file where interfaces are declared
    - all changes are windows specific
    
    Signed-off-by: Oblomov, Sergey <sergey.oblomov@intel.com>

--- include/windows/ifaddrs.h
+++ include/windows/ifaddrs.h
@@ -1,4 +1,34 @@
+/*
+* Copyright (c) 2016 Intel Corporation.  All rights reserved.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 
 #pragma once
 
+#include <Winsock2.h>
+
+/* here is minimal subset of ifaddr API required for sockets & UDP
+   providers */
+struct ifaddrs {
+	struct ifaddrs  *ifa_next;    /* Next item in list */
+	char            *ifa_name;    /* Name of interface */
+	unsigned int     ifa_flags;   /* Flags from SIOCGIFFLAGS */
+	struct sockaddr *ifa_addr;    /* Address of interface */
+	struct sockaddr *ifa_netmask; /* Netmask of interface */
+
+	struct sockaddr_in in_addr;
+	struct sockaddr_in in_netmask;
+	char		   ad_name[16];
+};
+
+int getifaddrs(struct ifaddrs **ifap);
+void freeifaddrs(struct ifaddrs *ifa);
 
