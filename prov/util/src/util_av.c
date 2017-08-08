776095385cc57e2dc7cee4b8497f7855e424ce79
Arun C Ilango <arun.ilango@intel.com>
2017-08-08 12:16:57 -0700

    prov/rxm: Fix an issue where fi_addr was not stored with connection info
    Also remove an unnecessary branch
    
    Signed-off-by: Arun C Ilango <arun.ilango@intel.com>
    (cherry picked from commit 1fa299524c58c4f4b98ec33aae74dff315f751bb)

--- prov/util/src/util_av.c
+++ prov/util/src/util_av.c
@@ -1284,9 +1284,14 @@ int ofi_cmap_process_connreq(struct util_cmap *cmap, void *addr,
 		handle = util_cmap_get_handle(cmap, (fi_addr_t)index, addr);
 
 	if (!handle) {
-		FI_DBG(cmap->av->prov, FI_LOG_EP_CTRL,
-		       "No handle found for given addr\n");
-		ret = util_cmap_alloc_handle_peer(cmap, addr, CMAP_CONNREQ_RECV, &handle);
+		if (index < 0)
+			ret = util_cmap_alloc_handle_peer(cmap, addr,
+							  CMAP_CONNREQ_RECV,
+							  &handle);
+		else
+			ret = util_cmap_alloc_handle(cmap, (fi_addr_t)index,
+						     CMAP_CONNREQ_RECV,
+						     &handle);
 		if (ret)
 			goto unlock;
 	}
