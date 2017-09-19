9b1fdef0002e08d0bfdc5e6824af0bb65003700f
Arun C Ilango <arun.ilango@intel.com>
2017-09-19 12:28:26 -0700

    prov/util, rxm: Allow connection to self
    Issue:
    When two endpoints inititiate connection requests to each other at the same
    time we have to drop one of those connections to keep memory usage low. We
    compare the remote endpoint's listening address with that of local endpoint
    to decide which connection to keep. The listening address is also the one used
    to do a lookup in the AV table / cmap for fetching the corresponding connection
    object.
    
    Some middleware have communication patterns where an endpoint tries to talk to
    itself. This causes a collision in the AV table / cmap lookup.
    
    Fix:
    To avoid this issue just create the connection object in the peer list instead
    of doing a AV / cmap lookup.
    
    Signed-off-by: Arun C Ilango <arun.ilango@intel.com>

--- prov/util/src/util_av.c
+++ prov/util/src/util_av.c
@@ -1292,7 +1292,14 @@ int ofi_cmap_process_connreq(struct util_cmap *cmap, void *addr,
 
 	ofi_straddr_dbg(cmap->av->prov, FI_LOG_EP_CTRL,
 			"Processing connreq for addr", addr);
-	index = ip_av_get_index(cmap->av, addr);
+
+	if (!ofi_addr_cmp(cmap->av->prov, addr, cmap->attr.name)) {
+		FI_DBG(cmap->av->prov, FI_LOG_EP_CTRL,
+			"Endpoint connects to itself. Not checking AV table\n");
+		index = -1;
+	} else {
+		index = ip_av_get_index(cmap->av, addr);
+	}
 	fastlock_acquire(&cmap->lock);
 	if (index < 0)
 		handle = util_cmap_get_handle_peer(cmap, addr);
