a510dd3e095b466fb4d4e0a3426112d6fbf88b08
Jianxin Xiong <jianxin.xiong@intel.com>
2017-10-19 16:44:55 -0700

    prov/psm2: Add option to build with PSM2 source included
    Compiling the source code of PSM2 library together with the provider
    allows more aggressive compile-time/link-time optimization. In addition,
    the provider can be further optimized by having access to the internal
    definitions of the PSM2 library.
    
    This option is enabled with the configure option "--with-psm2-src=DIR".
    The resulting provider no longer uses or depends on the stand-alone PSM2
    library.
    
    Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>

---
+++ prov/psm2/src/psm2_revision.c
@@ -0,0 +1,6 @@
+char psmi_hfi_revision[] = "libfabric: psm-hfi-gxxxxxxx_open";
+char psmi_hfi_IFS_version[]="";
+char psmi_hfi_build_timestamp[] ="2017-09-07 17:29:26+00:00";
+char psmi_hfi_sources_checksum[] ="5b993e9931792d37d21be38e668f3968ae6e8819";
+char psmi_hfi_git_checksum[] ="d7e63ede75bc080c68117cfa3f4e9e8b2d85e2cd";
+
