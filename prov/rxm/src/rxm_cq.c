10ba368fee8b4f916f87572dde249581e0b237df
Arun C Ilango <arun.ilango@intel.com>
2017-11-21 16:47:13 -0800

    prov/rxm: Refactor rma iov match code to fix klockworks issue
    This fixes out of bounds access in recv_entry->(iov|desc)
    
    Signed-off-by: Arun C Ilango <arun.ilango@intel.com>

--- prov/rxm/src/rxm_cq.c
+++ prov/rxm/src/rxm_cq.c
@@ -161,13 +161,12 @@ static int rxm_match_iov(const struct iovec *iov, void **desc,
 
 	assert(count <= RXM_IOV_LIMIT);
 
-	if (match_len > ofi_total_iov_len(iov, count)) {
-		FI_WARN(&rxm_prov, FI_LOG_CQ, "match_len > iov size!\n");
-		return -FI_EINVAL;
-	}
-
 	for (i = 0; i < count; i++) {
-		assert(offset <= iov[i].iov_len);
+		if (offset >= iov[i].iov_len) {
+			offset -= iov[i].iov_len;
+			continue;
+		}
+
 		match_iov->iov[i].iov_base = (char *)iov[i].iov_base + offset;
 		match_iov->iov[i].iov_len = MIN(iov[i].iov_len - offset, match_len);
 		if (desc)
@@ -178,7 +177,12 @@ static int rxm_match_iov(const struct iovec *iov, void **desc,
 			break;
 		offset = 0;
 	}
-	assert(!match_len);
+
+	if (match_len) {
+		FI_WARN(&rxm_prov, FI_LOG_CQ, "Given iov size < match_len!\n");
+		return -FI_ETOOSMALL;
+	}
+
 	match_iov->count = i + 1;
 	return FI_SUCCESS;
 }
@@ -194,24 +198,29 @@ static int rxm_match_rma_iov(struct rxm_recv_entry *recv_entry,
 
 	assert(rma_iov->count <= RXM_IOV_LIMIT);
 
-	for (i = 0, j = 0; i < rma_iov->count; i++) {
+	for (i = 0, j = 0; i < rma_iov->count; ) {
 		ret = rxm_match_iov(&recv_entry->iov[j], &recv_entry->desc[j],
-				    recv_entry->count, offset,
+				    recv_entry->count - j, offset,
 				    rma_iov->iov[i].len, &match_iov[i]);
 		if (ret)
 			return ret;
+
 		count = match_iov[i].count;
 		offset = match_iov[i].iov[count - 1].iov_len;
 
-		assert((j + count - 1) < recv_entry->count);
-		if (offset == recv_entry->iov[j + count - 1].iov_len) {
-			/* This iov has been completely used */
-			j += count;
-			offset = 0;
-		} else {
-			j += count - 1;
-		}
+		i++;
+		j += count - 1;
+
+		if (j >= recv_entry->count)
+			break;
 	}
+
+	if (i < rma_iov->count) {
+		FI_WARN(&rxm_prov, FI_LOG_CQ, "posted recv_entry size < "
+			"rndv rma read size!\n");
+		return -FI_ETOOSMALL;
+	}
+
 	return FI_SUCCESS;
 }
 
