ofiwg/libfabric

commit d8ac992fd24744367f856766ed63ad5989cca87d

prov/sockets: make use of the common fi_epoll_* interface
This patch completely removes the file sock_epoll.c, which has been
replaced by fi_epoll_* interface.
It also removes the index map, which becomes useless after this patch.

Signed-off-by: Sylvain Didelot <sdidelot@ddn.com>

prov/sockets/include/sock.h
@@ -90,6 +90,7 @@
 #define SOCK_CQ_DEF_SZ (1<<8)
 #define SOCK_AV_DEF_SZ (1<<8)
 #define SOCK_CMAP_DEF_SZ (1<<10)
+#define SOCK_EPOLL_WAIT_EVENTS 32
 
 #define SOCK_CQ_DATA_SIZE (sizeof(uint64_t))
 #define SOCK_TAG_SIZE (sizeof(uint64_t))
@@ -153,7 +154,7 @@
 #define SOCK_PE_COMM_BUFF_SZ (1024)
 #define SOCK_PE_OVERFLOW_COMM_BUFF_SZ (128)
 
-/* it must be adjusted if error data size in CQ/EQ 
+/* it must be adjusted if error data size in CQ/EQ
  * will be larger than SOCK_EP_MAX_CM_DATA_SZ */
 #define SOCK_MAX_ERR_CQ_EQ_DATA_SZ SOCK_EP_MAX_CM_DATA_SZ
 
@@ -171,21 +172,6 @@ struct sock_service_entry {
 	int service;
 	struct dlist_entry entry;
 };
-
-#ifdef HAVE_EPOLL
-struct sock_epoll_set {
-	int fd;
-	int size;
-	int used;
-	struct epoll_event *events;
-};
-#else
-struct sock_epoll_set {
-	struct pollfd *pollfds;
-	int size;
-	int used;
-};
-#endif
 
 struct sock_fabric {
 	struct fid_fabric fab_fid;
@@ -212,7 +198,7 @@ struct sock_conn {
 
 struct sock_conn_map {
 	struct sock_conn *table;
-	struct sock_epoll_set epoll_set;
+	fi_epoll_t epoll_set;
 	int used;
 	int size;
 	fastlock_t lock;
@@ -564,7 +550,6 @@ struct sock_ep_attr {
 	struct sock_conn_listener listener;
 	fastlock_t lock;
 
-	struct index_map conn_idm;
 	struct index_map av_idm;
 	struct sock_conn_map cmap;
 };
@@ -855,7 +840,7 @@ struct sock_pe {
 	pthread_t progress_thread;
 	volatile int do_progress;
 	struct sock_pe_entry *pe_atomic;
-	struct sock_epoll_set epoll_set;
+	fi_epoll_t epoll_set;
 };
 
 typedef int (*sock_cq_report_fn) (struct sock_cq *cq, fi_addr_t addr,
@@ -1204,13 +1189,6 @@ ssize_t sock_queue_msg_op(struct fid_ep *ep, const struct fi_msg *msg,
 			  uint64_t flags, enum fi_op_type op_type);
 ssize_t sock_queue_cntr_op(struct fi_deferred_work *work, uint64_t flags);
 void sock_cntr_check_trigger_list(struct sock_cntr *cntr);
-
-int sock_epoll_create(struct sock_epoll_set *set, int size);
-int sock_epoll_add(struct sock_epoll_set *set, int fd);
-int sock_epoll_del(struct sock_epoll_set *set, int fd);
-int sock_epoll_wait(struct sock_epoll_set *set, int timeout);
-int sock_epoll_get_fd_at_index(struct sock_epoll_set *set, int index);
-void sock_epoll_close(struct sock_epoll_set *set);
 
 static inline size_t sock_rx_avail_len(struct sock_rx_entry *rx_entry)
 {

prov/sockets/src/sock_conn.c
@@ -96,11 +96,11 @@ int sock_conn_map_init(struct sock_ep *ep, int init_size)
 	if (!map->table)
 		return -FI_ENOMEM;
 
-	if (sock_epoll_create(&map->epoll_set, init_size) < 0) {
-                SOCK_LOG_ERROR("failed to create epoll set\n");
-                free(map->table);
-                return -FI_ENOMEM;
-        }
+	if (fi_epoll_create(&map->epoll_set) < 0) {
+		SOCK_LOG_ERROR("failed to create epoll set\n");
+		free(map->table);
+		return -FI_ENOMEM;
+	}
 
 	fastlock_init(&map->lock);
 	map->used = 0;
@@ -137,13 +137,13 @@ void sock_conn_map_destroy(struct sock_ep_attr *ep_attr)
 	free(cmap->table);
 	cmap->table = NULL;
 	cmap->used = cmap->size = 0;
-	sock_epoll_close(&cmap->epoll_set);
+	fi_epoll_close(cmap->epoll_set);
 	fastlock_destroy(&cmap->lock);
 }
 
 void sock_conn_release_entry(struct sock_conn_map *map, struct sock_conn *conn)
 {
-	sock_epoll_del(&map->epoll_set, conn->sock_fd);
+	fi_epoll_del(map->epoll_set, conn->sock_fd);
 	ofi_close_socket(conn->sock_fd);
 
 	conn->address_published = 0;
@@ -188,11 +188,7 @@ static struct sock_conn *sock_conn_map_insert(struct sock_ep_attr *ep_attr,
 	map->table[index].ep_attr = ep_attr;
 	sock_set_sockopts(conn_fd);
 
-
-	if (ofi_idm_set(&ep_attr->conn_idm, conn_fd, &map->table[index]) < 0)
-		SOCK_LOG_ERROR("ofi_idm_set failed\n");
-
-	if (sock_epoll_add(&map->epoll_set, conn_fd))
+	if (fi_epoll_add(map->epoll_set, conn_fd, &map->table[index]))
 		SOCK_LOG_ERROR("failed to add to epoll set: %d\n", conn_fd);
 
 	map->table[index].address_published = addr_published;

prov/sockets/src/sock_progress.c
@@ -2273,17 +2273,16 @@ void sock_pe_signal(struct sock_pe *pe)
 void sock_pe_poll_add(struct sock_pe *pe, int fd)
 {
         fastlock_acquire(&pe->signal_lock);
-        if (sock_epoll_add(&pe->epoll_set, fd))
-                SOCK_LOG_ERROR("failed to add to epoll set: %d, size: %d, used: %d\n", fd, pe->epoll_set.size, pe->epoll_set.used);
+        if (fi_epoll_add(pe->epoll_set, fd, NULL))
+			SOCK_LOG_ERROR("failed to add to epoll set: %d\n", fd);
         fastlock_release(&pe->signal_lock);
 }
 
 void sock_pe_poll_del(struct sock_pe *pe, int fd)
 {
         fastlock_acquire(&pe->signal_lock);
-        if (sock_epoll_del(&pe->epoll_set, fd))
-                SOCK_LOG_DBG("failed to del from epoll set: %d, size: %d, used: %d\n",
-			       fd, pe->epoll_set.size, pe->epoll_set.used);
+        if (fi_epoll_del(pe->epoll_set, fd))
+			SOCK_LOG_DBG("failed to del from epoll set: %d\n", fd);
         fastlock_release(&pe->signal_lock);
 }
 
@@ -2341,29 +2340,28 @@ void sock_pe_remove_rx_ctx(struct sock_rx_ctx *rx_ctx)
 static int sock_pe_progress_rx_ep(struct sock_pe *pe, struct sock_ep_attr *ep_attr,
 					struct sock_rx_ctx *rx_ctx)
 {
-	int ret = 0, i, fd, num_fds;
+	int ret = 0, i, num_fds;
 	struct sock_conn *conn;
 	struct sock_conn_map *map;
+	void *ep_contexts[SOCK_EPOLL_WAIT_EVENTS];
 
 	map = &ep_attr->cmap;
 
-        if (!map->used)
-                return 0;
+	if (!map->used)
+		return 0;
 
-        num_fds = sock_epoll_wait(&map->epoll_set, 0);
-        if (num_fds < 0 || num_fds == 0) {
-                if (num_fds < 0)
-                        SOCK_LOG_ERROR("poll failed: %s\n", strerror(errno));
-                return num_fds;
-        }
+epoll_wait_retry:
+	num_fds = fi_epoll_wait(map->epoll_set, ep_contexts,
+			SOCK_EPOLL_WAIT_EVENTS, 0);
+	if (num_fds < 0 || num_fds == 0) {
+		if (num_fds < 0)
+			SOCK_LOG_ERROR("poll failed: %s\n", strerror(errno));
+		return num_fds;
+	}
 
 	fastlock_acquire(&map->lock);
 	for (i = 0; i < num_fds; i++) {
-		fd = sock_epoll_get_fd_at_index(&map->epoll_set, i);
-		if (fd == -1) /* failed to lookup fd due to connection failures */
-			continue;
-
-		conn = ofi_idm_lookup(&ep_attr->conn_idm, fd);
+		conn = ep_contexts[i];
 		if (!conn)
 			SOCK_LOG_ERROR("ofi_idm_lookup failed\n");
 
@@ -2372,8 +2370,12 @@ static int sock_pe_progress_rx_ep(struct sock_pe *pe, struct sock_ep_attr *ep_at
 
 		sock_pe_new_rx_entry(pe, rx_ctx, ep_attr, conn);
 	}
-
 	fastlock_release(&map->lock);
+
+	/* There is possibly more fds to progress */
+	if (num_fds == SOCK_EPOLL_WAIT_EVENTS)
+		goto epoll_wait_retry;
+
 	return ret;
 }
 
@@ -2528,10 +2530,11 @@ static void sock_pe_wait(struct sock_pe *pe)
 {
 	char tmp;
 	int ret;
+	void *ep_contexts[1];
 
-	ret = sock_epoll_wait(&pe->epoll_set, -1);
-        if (ret < 0)
-                SOCK_LOG_ERROR("poll failed : %s\n", strerror(errno));
+	ret = fi_epoll_wait(pe->epoll_set, ep_contexts, 1, -1);
+	if (ret < 0)
+		SOCK_LOG_ERROR("poll failed : %s\n", strerror(errno));
 
 	fastlock_acquire(&pe->signal_lock);
 	if (pe->rcnt != pe->wcnt) {
@@ -2717,17 +2720,17 @@ struct sock_pe *sock_pe_init(struct sock_domain *domain)
 		goto err2;
 	}
 
-	if (sock_epoll_create(&pe->epoll_set, sock_cm_def_map_sz) < 0) {
+	if (fi_epoll_create(&pe->epoll_set) < 0) {
                 SOCK_LOG_ERROR("failed to create epoll set\n");
                 goto err3;
-        }
+	}
 
 	if (domain->progress_mode == FI_PROGRESS_AUTO) {
 		if (socketpair(AF_UNIX, SOCK_STREAM, 0, pe->signal_fds) < 0)
 			goto err4;
 
 		fd_set_nonblock(pe->signal_fds[SOCK_SIGNAL_RD_FD]);
-		sock_epoll_add(&pe->epoll_set, pe->signal_fds[SOCK_SIGNAL_RD_FD]);
+		fi_epoll_add(pe->epoll_set, pe->signal_fds[SOCK_SIGNAL_RD_FD], NULL);
 
 		pe->do_progress = 1;
 		if (pthread_create(&pe->progress_thread, NULL,
@@ -2743,7 +2746,7 @@ err5:
 	ofi_close_socket(pe->signal_fds[0]);
 	ofi_close_socket(pe->signal_fds[1]);
 err4:
-	sock_epoll_close(&pe->epoll_set);
+	fi_epoll_close(pe->epoll_set);
 err3:
 	util_buf_pool_destroy(pe->atomic_rx_pool);
 err2:
@@ -2790,7 +2793,7 @@ void sock_pe_finalize(struct sock_pe *pe)
 	fastlock_destroy(&pe->lock);
 	fastlock_destroy(&pe->signal_lock);
 	pthread_mutex_destroy(&pe->list_lock);
-	sock_epoll_close(&pe->epoll_set);
+	fi_epoll_close(pe->epoll_set);
 	free(pe);
 	SOCK_LOG_DBG("Progress engine finalize: OK\n");
 }

libfabric.vcxproj
@@ -292,12 +292,6 @@
       <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Release|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Release - ICC|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
     </ClCompile>
-    <ClCompile Include="prov\sockets\src\sock_epoll.c">
-      <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-      <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Debug - ICC|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-      <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Release|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-      <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Release - ICC|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
-    </ClCompile>
     <ClCompile Include="prov\sockets\src\sock_ep_dgram.c">
       <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <AdditionalIncludeDirectories Condition="'$(Configuration)|$(Platform)'=='Debug - ICC|x64'">$(ProjectDir)prov\sockets\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>

prov/sockets/Makefile.include
@@ -25,8 +25,7 @@ _sockets_files = \
 	prov/sockets/src/sock_msg.c \
 	prov/sockets/src/sock_rma.c \
 	prov/sockets/src/sock_atomic.c \
-	prov/sockets/src/sock_trigger.c \
-	prov/sockets/src/sock_epoll.c
+	prov/sockets/src/sock_trigger.c
 
 _sockets_headers = \
 	prov/sockets/include/sock.h \

prov/sockets/src/sock_ep.c
@@ -754,7 +754,6 @@ static int sock_ep_close(struct fid *fid)
 		free(sock_ep->attr->dest_addr);
 
 	fastlock_acquire(&sock_ep->attr->domain->pe->lock);
-	ofi_idm_reset(&sock_ep->attr->conn_idm);
 	ofi_idm_reset(&sock_ep->attr->av_idm);
 	sock_conn_map_destroy(sock_ep->attr);
 	fastlock_release(&sock_ep->attr->domain->pe->lock);
@@ -1813,7 +1812,6 @@ err1:
 void sock_ep_remove_conn(struct sock_ep_attr *attr, struct sock_conn *conn)
 {
 	sock_pe_poll_del(attr->domain->pe, conn->sock_fd);
-	ofi_idm_clear(&attr->conn_idm, conn->sock_fd);
 	sock_conn_release_entry(&attr->cmap, conn);
 }
 

prov/sockets/src/sock_epoll.c
@@ -1,199 +1,0 @@
-/*
- * Copyright (c) 2014 Intel Corporation, Inc.  All rights reserved.
- * Copyright (c) 2016 Cisco Systems, Inc.  All rights reserved.
- *
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
- * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the
- * BSD license below:
- *
- *     Redistribution and use in source and binary forms, with or
- *     without modification, are permitted provided that the following
- *     conditions are met:
- *
- *      - Redistributions of source code must retain the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer.
- *
- *      - Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials
- *        provided with the distribution.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-#include <sys/types.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#include <errno.h>
-#include <fcntl.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <net/if.h>
-#include <ifaddrs.h>
-#include <limits.h>
-#include "sock.h"
-#include "sock_util.h"
-
-#ifdef HAVE_EPOLL
-#include <sys/epoll.h>
-#else
-#include <poll.h>
-#endif
-
-#define SOCK_LOG_DBG(...) _SOCK_LOG_DBG(FI_LOG_EP_CTRL, __VA_ARGS__)
-#define SOCK_LOG_ERROR(...) _SOCK_LOG_ERROR(FI_LOG_EP_CTRL, __VA_ARGS__)
-
-#ifdef HAVE_EPOLL
-int sock_epoll_create(struct sock_epoll_set *set, int size)
-{
-	int ret;
-	set->size = size;
-	set->used = 0;
-	set->events = calloc(size, sizeof(struct epoll_event));
-	if (!set->events)
-		return -FI_ENOMEM;
-	ret = set->fd = epoll_create(size);
-	if (ret < 0)
-		free(set->events);
-	return ret;
-}
-
-int sock_epoll_add(struct sock_epoll_set *set, int fd)
-{
-	int ret;
-	struct epoll_event event;
-
-	memset(&event, 0, sizeof(event));
-	event.data.fd = fd;
-	event.events = EPOLLIN;
-
-	if (set->used == set->size)
-		return -1;
-
-	ret = epoll_ctl(set->fd, EPOLL_CTL_ADD, fd, &event);
-	if (!ret)
-		set->used++;
-
-	return ret;
-}
-
-int sock_epoll_del(struct sock_epoll_set *set, int fd)
-{
-	int ret;
-
-	if (!set->used)
-		return -1;
-
-	ret = epoll_ctl(set->fd, EPOLL_CTL_DEL, fd, NULL);
-	if (!ret)
-		set->used--;
-
-	return ret;
-}
-
-int sock_epoll_wait(struct sock_epoll_set *set, int timeout)
-{
-	if (!set->used)
-		return 0;
-	return epoll_wait(set->fd, set->events, set->used, timeout);
-}
-
-int sock_epoll_get_fd_at_index(struct sock_epoll_set *set, int index)
-{
-	return set->events[index].data.fd;
-}
-
-void sock_epoll_close(struct sock_epoll_set *set)
-{
-	free(set->events);
-	close(set->fd);
-	set->used = 0;
-}
-
-#else
-
-int sock_epoll_create(struct sock_epoll_set *set, int size)
-{
-	set->size = size;
-	set->used = 0;
-	set->pollfds = calloc(size, sizeof(struct pollfd));
-	return set->pollfds ? 0 : -1;
-}
-
-int sock_epoll_add(struct sock_epoll_set *set, int fd)
-{
-	if (set->used == set->size)
-		return -1;
-
-	set->pollfds[set->used].fd = fd;
-	set->pollfds[set->used].events = POLLIN;
-
-	set->used++;
-	return 0;
-}
-
-int sock_epoll_del(struct sock_epoll_set *set, int fd)
-{
-	int i;
-
-	for (i = 0; i < set->used; i++) {
-		if (set->pollfds[i].fd == fd)
-      			break;
-  	}
-
-	if (i == set->used)
-    		return -1;
-
-	set->pollfds[i].fd = set->pollfds[set->used - 1].fd;
-	set->used--;
-	return 0;
-}
-
-int sock_epoll_wait(struct sock_epoll_set *set, int timeout)
-{
-	if (!set->used)
-		return 0;
-
-	return poll(set->pollfds, set->used, timeout);
-}
-
-int sock_epoll_get_fd_at_index(struct sock_epoll_set *set, int index)
-{
-	int i;
-
-	for (i = 0; i < set->used; i++) {
-		if ((set->pollfds[i].revents & POLLIN)) {
-			if (index) {
-				index--;
-				continue;
-      			} else {
-				return set->pollfds[i].fd;
-      			}
-    		}
-  	}
-	return -1;
-}
-
-void sock_epoll_close(struct sock_epoll_set *set)
-{
-	free(set->pollfds);
-	set->used = 0;
-}
-
-#endif

commit 3284f82a7a3d907d37960438969e5f68e20038db

prov/sockets: do not retry after epoll_wait()
The epoll set 'map->epoll_set' is configured in level-triggered
mode, meaning that epoll_wait() will continue reporting events
for the monitored file descriptors until the file descriptors
that generated the events are effectively read.

We mustn't retry epoll_wait() if the returned value is equal to
the maximum number of events specified in arguments (32 by default),
otherwise the same events for the same file descriptors will be
indefinitely reported by epoll_wait(). This will cause libfabric
to allocate RX buffers in a loop, leading to the exhaustion of the
memory on the machine.

The patch dynamically resizes the array of epoll contexts to match
the number of connections ysed in the connection map.
Array resizing for the epoll contexts must be done in
sock_pe_progress_rx_ep() and not in sock_conn_map_increase() as we
need to make sure that the epoll contexts are not altered while
fi_epoll_wait() is using them.

This bug was introduced in d8ac992fd("prov/sockets: make use of the
common fi_epoll_* interface").

Fixes #3557

Signed-off-by: Sylvain Didelot <sdidelot@ddn.com>

prov/sockets/include/sock.h
@@ -205,6 +205,8 @@ struct sock_conn {
 struct sock_conn_map {
 	struct sock_conn *table;
 	fi_epoll_t epoll_set;
+	void **epoll_ctxs;
+	int epoll_ctxs_sz;
 	int used;
 	int size;
 	fastlock_t lock;

prov/sockets/src/sock_conn.c
@@ -98,12 +98,19 @@ int sock_conn_map_init(struct sock_ep *ep, int init_size)
 	if (!map->table)
 		return -FI_ENOMEM;
 
+	map->epoll_ctxs = calloc(init_size, sizeof(*map->epoll_ctxs));
+	if (!map->epoll_ctxs) {
+		free(map->table);
+		return -FI_ENOMEM;
+	}
+
 	ret = fi_epoll_create(&map->epoll_set);
 	if (ret < 0) {
 		SOCK_LOG_ERROR("failed to create epoll set, "
 			       "error - %d (%s)\n", ret,
 			       strerror(ret));
 		free(map->table);
+		free(map->epoll_ctxs);
 		return -FI_ENOMEM;
 	}
 
@@ -141,6 +148,9 @@ void sock_conn_map_destroy(struct sock_ep_attr *ep_attr)
 	}
 	free(cmap->table);
 	cmap->table = NULL;
+	free(cmap->epoll_ctxs);
+	cmap->epoll_ctxs = NULL;
+	cmap->epoll_ctxs_sz = 0;
 	cmap->used = cmap->size = 0;
 	fi_epoll_close(cmap->epoll_set);
 	fastlock_destroy(&cmap->lock);

prov/sockets/src/sock_progress.c
@@ -2344,19 +2344,30 @@ static int sock_pe_progress_rx_ep(struct sock_pe *pe,
 				  struct sock_ep_attr *ep_attr,
 				  struct sock_rx_ctx *rx_ctx)
 {
-	int ret = 0, i, num_fds;
+	int i, num_fds;
 	struct sock_conn *conn;
 	struct sock_conn_map *map;
-	void *ep_contexts[SOCK_EPOLL_WAIT_EVENTS];
 
 	map = &ep_attr->cmap;
 
 	if (!map->used)
 		return 0;
 
-epoll_wait_retry:
-	num_fds = fi_epoll_wait(map->epoll_set, ep_contexts,
-			SOCK_EPOLL_WAIT_EVENTS, 0);
+	/* Need to extend the number of contexts for epoll */
+	if (map->epoll_ctxs_sz < map->used) {
+		uint64_t new_size = map->used * 2;
+		map->epoll_ctxs = realloc(map->epoll_ctxs,
+		                          sizeof(*map->epoll_ctxs) * new_size);
+		if (!map->epoll_ctxs)
+		{
+			map->epoll_ctxs_sz = 0;
+			return -FI_ENOMEM;
+		}
+		map->epoll_ctxs_sz = new_size;
+	}
+
+	num_fds = fi_epoll_wait(map->epoll_set, map->epoll_ctxs,
+	                        map->used, 0);
 	if (num_fds < 0 || num_fds == 0) {
 		if (num_fds < 0)
 			SOCK_LOG_ERROR("poll failed: %d\n",
@@ -2366,7 +2377,7 @@ epoll_wait_retry:
 
 	fastlock_acquire(&map->lock);
 	for (i = 0; i < num_fds; i++) {
-		conn = ep_contexts[i];
+		conn = map->epoll_ctxs[i];
 		if (!conn)
 			SOCK_LOG_ERROR("ofi_idm_lookup failed\n");
 
@@ -2377,11 +2388,7 @@ epoll_wait_retry:
 	}
 	fastlock_release(&map->lock);
 
-	/* There is possibly more fds to progress */
-	if (num_fds == SOCK_EPOLL_WAIT_EVENTS)
-		goto epoll_wait_retry;
-
-	return ret;
+	return 0;
 }
 
 int sock_pe_progress_rx_ctx(struct sock_pe *pe, struct sock_rx_ctx *rx_ctx)