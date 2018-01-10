ofiwg/libfabric

commit 928e688373197e7cacc12af78e8b84ccc5b34e9c

prov/util: Return EAGAIN on CQ sread timeout
This change aligns the util code with the sockets provider.  The
fabtests check for and handle EAGAIN, but not ETIMEDOUT as a
return value.

This ensures that the return value is the same whether a timeout
value is specified (non-zero) or called with 0.

Signed-off-by: Sean Hefty <sean.hefty@intel.com>

prov/util/src/util_cq.c
@@ -283,11 +283,11 @@ ssize_t ofi_cq_sreadfrom(struct fid_cq *cq_fid, void *buf, size_t count,
 		if (ret != -FI_EAGAIN)
 			break;
 
-		if (timeout >= 0) {
+		if (timeout >= 0)
 			timeout -= (int) (fi_gettime_ms() - start);
-			if (timeout <= 0)
-				return -FI_ETIMEDOUT;
-		}
+
+		if (timeout <= 0)
+			return -FI_EAGAIN;
 
 		if (ofi_atomic_get32(&cq->signaled)) {
 			ofi_atomic_set32(&cq->signaled, 0);
@@ -297,7 +297,7 @@ ssize_t ofi_cq_sreadfrom(struct fid_cq *cq_fid, void *buf, size_t count,
 		ret = fi_wait(&cq->wait->wait_fid, timeout);
 	} while (!ret);
 
-	return ret;
+	return ret == -FI_ETIMEDOUT ? -FI_EAGAIN : ret;
 }
 
 ssize_t ofi_cq_sread(struct fid_cq *cq_fid, void *buf, size_t count,

commit fb9f706f98188f2473daf20305d93e5ffd401f19

prov/util: fix incorrect handling of infinite timeout in sread
This patch fixes a regression introduced by 928e688

Signed-off-by: Arun C Ilango <arun.ilango@intel.com>

prov/util/src/util_cq.c
@@ -283,11 +283,11 @@ ssize_t ofi_cq_sreadfrom(struct fid_cq *cq_fid, void *buf, size_t count,
 		if (ret != -FI_EAGAIN)
 			break;
 
-		if (timeout >= 0)
+		if (timeout >= 0) {
 			timeout -= (int) (fi_gettime_ms() - start);
-
-		if (timeout <= 0)
-			return -FI_EAGAIN;
+			if (timeout <= 0)
+				return -FI_EAGAIN;
+		}
 
 		if (ofi_atomic_get32(&cq->signaled)) {
 			ofi_atomic_set32(&cq->signaled, 0);