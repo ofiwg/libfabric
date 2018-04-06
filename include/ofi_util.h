f995cb26e6de762d4f542080b7c0352c86f2c3e0
Dmitry Gladkov <dmitry.gladkov@intel.com>
2018-04-06 16:45:09 +0300

    util/cq: Make ofi_cq_write static inline
    Since ofi_cq_write() is widely used by providers, this pathc
    makes ofi_cq_wrie() static inline.
    
    Signed-off-by: Dmitry Gladkov <dmitry.gladkov@intel.com>

--- include/ofi_util.h
+++ include/ofi_util.h
@@ -325,8 +325,32 @@ ssize_t ofi_cq_sread(struct fid_cq *cq_fid, void *buf, size_t count,
 ssize_t ofi_cq_sreadfrom(struct fid_cq *cq_fid, void *buf, size_t count,
 		fi_addr_t *src_addr, const void *cond, int timeout);
 int ofi_cq_signal(struct fid_cq *cq_fid);
-int ofi_cq_write(struct util_cq *cq, void *context, uint64_t flags, size_t len,
-		 void *buf, uint64_t data, uint64_t tag);
+static inline int
+ofi_cq_write(struct util_cq *cq, void *context, uint64_t flags, size_t len,
+	     void *buf, uint64_t data, uint64_t tag)
+{
+	struct fi_cq_tagged_entry *comp;
+	int ret = 0;
+
+	cq->cq_fastlock_acquire(&cq->cq_lock);
+	if (ofi_cirque_isfull(cq->cirq)) {
+		FI_DBG(cq->domain->prov, FI_LOG_CQ, "util_cq cirq is full!\n");
+		ret = -FI_EAGAIN;
+		goto out;
+	}
+
+	comp = ofi_cirque_tail(cq->cirq);
+	comp->op_context = context;
+	comp->flags = flags;
+	comp->len = len;
+	comp->buf = buf;
+	comp->data = data;
+	comp->tag = tag;
+	ofi_cirque_commit(cq->cirq);
+out:
+	cq->cq_fastlock_release(&cq->cq_lock);
+	return ret;
+}
 int ofi_cq_write_error(struct util_cq *cq,
 		       const struct fi_cq_err_entry *err_entry);
 int ofi_cq_write_error_peek(struct util_cq *cq, uint64_t tag, void *context);
