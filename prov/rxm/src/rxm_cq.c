c6c6ef79f250b1a88479c8d008b58d0e1e319cd3
Dmitry Gladkov <dmitry.gladkov@intel.com>
2018-07-19 09:06:37 +0300

    prov/rxm: repost directed recv when AV updated only FI_DIRECTED_RECV is used
    Signed-off-by: Dmitry Gladkov <dmitry.gladkov@intel.com>

--- prov/rxm/src/rxm_cq.c
+++ prov/rxm/src/rxm_cq.c
@@ -983,17 +983,19 @@ static int rxm_cq_reprocess_recv_queues(struct rxm_ep *rxm_ep)
 {
 	int count = 0;
 
-	fastlock_acquire(&rxm_ep->util_ep.cmap->lock);
+	if (rxm_ep->rxm_info->caps & FI_DIRECTED_RECV) {
+		fastlock_acquire(&rxm_ep->util_ep.cmap->lock);
 
-	if (!rxm_ep->util_ep.cmap->av_updated)
-		goto unlock;
+		if (!rxm_ep->util_ep.cmap->av_updated)
+			goto unlock;
 
-	rxm_ep->util_ep.cmap->av_updated = 0;
+		rxm_ep->util_ep.cmap->av_updated = 0;
 
-	count += rxm_cq_reprocess_directed_recvs(&rxm_ep->recv_queue);
-	count += rxm_cq_reprocess_directed_recvs(&rxm_ep->trecv_queue);
+		count += rxm_cq_reprocess_directed_recvs(&rxm_ep->recv_queue);
+		count += rxm_cq_reprocess_directed_recvs(&rxm_ep->trecv_queue);
 unlock:
-	fastlock_release(&rxm_ep->util_ep.cmap->lock);
+		fastlock_release(&rxm_ep->util_ep.cmap->lock);
+	}
 	return count;
 }
 
