ba904c912f56312cc681ba2f430846ddaf334c30
James Swaro <jswaro@cray.com>
2018-08-02 08:39:21 -0500

    prov/gni: Fix stale data issue with SMSG buffers
    SMSG buffers could contain stale data in the slab when first
    allocated on some systems or when reused. This commit fixes that by
    zero'ing the memory allocated upon first use, regardless of assumptions
    that may have been made about the underlying implementation.
    
    Signed-off-by: James Swaro <jswaro@cray.com>

--- prov/gni/src/gnix_nic.c
+++ prov/gni/src/gnix_nic.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2017 Cray Inc. All rights reserved.
+ * Copyright (c) 2015-2018 Cray Inc. All rights reserved.
  * Copyright (c) 2015-2018 Los Alamos National Security, LLC.
  *                         All rights reserved.
  *
@@ -219,6 +219,10 @@ static int __nic_setup_irq_cq(struct gnix_nic *nic)
 
 	nic->irq_mmap_addr = mmap_addr;
 	nic->irq_mmap_len = len;
+
+	/* On some systems, the page may not be zero'd from first use.
+		 Memset it here */
+	memset(mmap_addr, 0x0, len);
 
 	if (nic->using_vmdh) {
 		info = _gnix_auth_key_lookup(GNIX_PROV_DEFAULT_AUTH_KEY,
