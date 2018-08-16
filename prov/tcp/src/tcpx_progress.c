ofiwg/libfabric

commit 55192cde9a86f92b50cf6c427447ccfefe2ef5cd

prov/tcp: Modify which poll events the endpoint's sock can be blocked on.
When user app wants to block on the cq read, the try function checks for send queue and appropriately selects the poll events to block on. When send queue is not empty, it selects the EPOLL_OUT event so that cq read unblocks when socket is ready for sending. Thus, progressing the send queue.

However this patch would not take care of unblocking cq read, when the send comes after cq read is blocked. The following patch will address that issue.

Signed-off-by: Venkata Krishna Nimmagadda <venkata.krishna.nimmagadda@intel.com>

prov/tcp/src/tcpx_progress.c
@@ -434,9 +434,35 @@ void tcpx_progress(struct util_ep *util_ep)
 
 static int tcpx_try_func(void *util_ep)
 {
-	/* nothing to do here. When endpoints
-	   have incoming data, cq drives progress*/
+	uint32_t events;
+	struct util_wait_fd *wait_fd;
+	struct tcpx_ep *ep;
+	int ret;
+
+	ep = container_of(util_ep, struct tcpx_ep, util_ep);
+	wait_fd = container_of(((struct util_ep *)util_ep)->rx_cq->wait,
+			       struct util_wait_fd, util_wait);
+
+	fastlock_acquire(&ep->lock);
+	if (slist_empty(&ep->tx_queue) && !ep->send_ready_monitor) {
+		ep->send_ready_monitor = true;
+		events = FI_EPOLL_IN | FI_EPOLL_OUT;
+		goto epoll_mod;
+	} else if (!slist_empty(&ep->tx_queue) && ep->send_ready_monitor) {
+		ep->send_ready_monitor = false;
+		events = FI_EPOLL_IN;
+		goto epoll_mod;
+	}
+	fastlock_release(&ep->lock);
 	return FI_SUCCESS;
+
+epoll_mod:
+	ret = fi_epoll_mod(wait_fd->epoll_fd, ep->conn_fd, events, NULL);
+	if (ret)
+		FI_WARN(&tcpx_prov, FI_LOG_EP_DATA,
+			"invalid op type\n");
+	fastlock_release(&ep->lock);
+	return ret;
 }
 
 int tcpx_cq_wait_ep_add(struct tcpx_ep *ep)

prov/tcp/src/tcpx.h
@@ -154,6 +154,7 @@ struct tcpx_ep {
 	/* lock for protecting tx/rx queues,rma list,cm_state*/
 	fastlock_t		lock;
 	tcpx_ep_progress_func_t progress_func;
+	bool			send_ready_monitor;
 };
 
 struct tcpx_fabric {

commit c5f3ebc79ae4a8927676a369ab8528bebe39b9b4

prov/tcp: fix wrong conditions in tcpx_try_func()
This patch fixes a bug that has been introduced in commit
55192cde9a86f92b50cf6c427447ccfefe2ef5cd("prov/tcp: Modify which
poll events the endpoint's sock can be blocked on")

- FI_EPOLL_OUT must be monitored when ep->tx_queue is not empty
and send_ready_monitor is set to false.
- FI_EPOLL_OUT must be unmonitored when ep->tx_queue is empty and
send_ready_monitor is set to true.

Signed-off-by: Sylvain Didelot <sdidelot@ddn.com>

prov/tcp/src/tcpx_progress.c
@@ -444,11 +444,11 @@ static int tcpx_try_func(void *util_ep)
 			       struct util_wait_fd, util_wait);
 
 	fastlock_acquire(&ep->lock);
-	if (slist_empty(&ep->tx_queue) && !ep->send_ready_monitor) {
+	if (!slist_empty(&ep->tx_queue) && !ep->send_ready_monitor) {
 		ep->send_ready_monitor = true;
 		events = FI_EPOLL_IN | FI_EPOLL_OUT;
 		goto epoll_mod;
-	} else if (!slist_empty(&ep->tx_queue) && ep->send_ready_monitor) {
+	} else if (slist_empty(&ep->tx_queue) && ep->send_ready_monitor) {
 		ep->send_ready_monitor = false;
 		events = FI_EPOLL_IN;
 		goto epoll_mod;