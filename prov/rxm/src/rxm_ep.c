bc725baf91bdfe548cc8d03f9f55c2fe56d3a136
Arun C Ilango <arun.ilango@intel.com>
2018-08-20 09:40:10 -0700

    prov/rxm: make tx, rx queue sizes independent of MSG provider.
    RxM should advertise much higher tx and rx queue size than the
    FI_EP_MSG provider as it can have multiple FI_EP_MSG endpoints
    at a time. Use a smaller TX and RX size for the MSG provider
    by default. This can be adjusted through an env variable if needed.
    
    Signed-off-by: Arun C Ilango <arun.ilango@intel.com>

--- prov/rxm/src/rxm_ep.c
+++ prov/rxm/src/rxm_ep.c
@@ -1255,7 +1255,7 @@ rxm_ep_send_common(struct rxm_ep *rxm_ep, const struct iovec *iov, void **desc,
 		    /* Roughly estimate whether all SAR segments can be sent w/o
 		     * retransmission due to lack of space in the TX queue */
 		    (rxm_ep_sar_estimate_segments_num(rxm_ep, data_len) <=
-						rxm_ep->rxm_info->tx_attr->size)) {
+						rxm_ep->msg_info->tx_attr->size)) {
 			return rxm_ep_sar_tx_send(rxm_ep, rxm_conn, context, count, iov,
 						  data_len, data, flags, comp_flags, tag, op);
 		} else {
