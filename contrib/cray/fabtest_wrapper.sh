a030c25a33f03c6d93986480e2535e9e2b86059c
James Swaro <jswaro@cray.com>
2018-09-11 16:55:47 -0500

    contrib/cray: Wrap fabtests for client/server
    To ease the testing of fabtests on any system, this
    script will utilize known environment variables of the
    SLURM WLM to identify client and server. This should ease
    the transition of the tests to other development systems.
    
    Signed-off-by: James Swaro <jswaro@cray.com>

---
+++ contrib/cray/fabtest_wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export FABTESTS_PATH=/scratch/jenkins/builds/fabtests/stable
+export PATH=${FABTESTS_PATH}/bin:$PATH
+
+if [[ -z ${FABTEST_PROVIDER} ]] ; then
+    FABTEST_PROVIDER='verbs;ofi_rxm'
+fi
+
+if [[ -z ${SLURMD_NODENAME} ]] ; then
+    echo SLURM is the only supported WLM at this point for testing
+    exit 1
+fi
+
+SERVER=$(scontrol show hostname ${SLURM_NODELIST} | head -n1)
+CLIENT=$(scontrol show hostname ${SLURM_NODELIST} | tail -n1)
+
+
+
+SERVER_ADDR=$(getent hosts ${SERVER} | awk '{print $1}')
+CLIENT_ADDR=$(getent hosts ${CLIENT} | awk '{print $1}')
+
+if [[ "${SLURMD_NODENAME}" == "$SERVER" ]] ; then
+    runfabtests.sh -p ${FABTESTS_PATH}/bin -v -T 60 "${FABTEST_PROVIDER}" ${SERVER_ADDR} ${CLIENT_ADDR}
+fi
