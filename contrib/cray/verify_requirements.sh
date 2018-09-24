6deee2c3a74bf4f1a470af409d9a211a221916e0
James Swaro <jswaro@cray.com>
2018-09-24 10:19:23 -0500

    contrib/cray: Add script to verify test prereqs
    Added a script to test for software and environment prerequisites.
    
    Signed-off-by: James Swaro <jswaro@cray.com>

---
+++ contrib/cray/verify_requirements.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+
+function check_executable {
+    if [[ ! -x $1 ]] ; then
+        echo could not find executable: $1
+        exit 1
+    fi
+}
+
+function check_directory {
+    if [[ ! -d $1 ]] ; then
+        echo could not find directory: $1
+        exit 1
+    fi
+}
+
+function check_environment {
+    var=$1
+    val=$(eval "echo \$$var")
+    if [[ -z "$val" ]] ; then
+        echo could not find environment variable: $var
+        exit 1
+    fi
+}
+
+function find_executable {
+    if [[ -z "$(which $1)" ]] ; then
+        echo could not find executable: $1
+        exit 1
+    fi
+}
+
+# check prerequisite environment variables
+check_environment ROOT_BUILD_PATH
+check_environment FABTEST_PATH
+check_environment LIBFABRIC_BUILD_PATH
+check_environment OMB_BUILD_PATH
+check_environment MPICH_PATH
+
+# check directories
+check_directory $ROOT_BUILD_PATH
+check_directory $FABTEST_PATH
+check_directory $OMB_BUILD_PATH
+check_directory $MPICH_PATH
+check_directory $ROOT_BUILD_PATH/libfabric-sft/stable
+
+# check prerequisite installed software packages
+find_executable srun
+check_executable $FABTEST_PATH/bin/runfabtests.sh
+check_executable $OMB_BUILD_PATH/pt2pt/osu_bw
+check_executable $ROOT_BUILD_PATH/libfabric-sft/stable/bin/ci-all.sh
+
+exit 0
