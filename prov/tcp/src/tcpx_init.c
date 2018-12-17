d719911adb6bb037ab033597781938990e9c2df9
Dmitry Gladkov <dmitry.gladkov@intel.com>
2018-12-17 19:34:19 +0300

    prov/tcp: Add sorting of IP interfaces by their speed
    Signed-off-by: Dmitry Gladkov <dmitry.gladkov@intel.com>

--- prov/tcp/src/tcpx_init.c
+++ prov/tcp/src/tcpx_init.c
@@ -40,41 +40,35 @@
 #include <net/if.h>
 #include <ofi_util.h>
 
-/* TODO: merge with sock_get_list_of_addr() - sock_fabric.c */
 #if HAVE_GETIFADDRS
 static void tcpx_getinfo_ifs(struct fi_info **info)
 {
-	char *tcpx_interface_name = NULL;
-	struct ifaddrs *ifaddrs, *ifa;
-	struct fi_info *head, *tail, *cur, *loopback;
+	struct fi_info *head = NULL, *tail = NULL, *cur;
+	struct slist addr_list;
 	size_t addrlen;
 	uint32_t addr_format;
-	int ret;
+	struct slist_entry *entry, *prev;
+	struct ofi_addr_list_entry *addr_entry;
 
-	fi_param_get_str(&tcpx_prov, "iface", &tcpx_interface_name);
+	slist_init(&addr_list);
 
-	ret = ofi_getifaddrs(&ifaddrs);
-	if (ret)
-		return;
+	ofi_get_list_of_addr(&tcpx_prov, "iface", &addr_list);
 
-	head = tail = loopback = NULL;
-	for (ifa = ifaddrs; ifa != NULL; ifa = ifa->ifa_next) {
-		if (ifa->ifa_addr == NULL || !(ifa->ifa_flags & IFF_UP))
-			continue;
-		if (tcpx_interface_name) {
-			if (strncmp(tcpx_interface_name, ifa->ifa_name,
-				    strlen(tcpx_interface_name)) != 0) {
-				FI_DBG(&tcpx_prov, FI_LOG_CORE,
-				       "Skip (%s) interface\n", ifa->ifa_name);
-				continue;
-			} else {
-				FI_DBG(&tcpx_prov, FI_LOG_CORE,
-				       "Matching interface (%s) found\n",
-				       ifa->ifa_name);
-			}
-		}
+	(void) prev; /* Makes compiler happy */
+	slist_foreach(&addr_list, entry, prev) {
+		addr_entry = container_of(entry, struct ofi_addr_list_entry, entry);
 
-		switch (ifa->ifa_addr->sa_family) {
+		cur = fi_dupinfo(*info);
+		if (!cur)
+			break;
+
+		if (!head)
+			head = cur;
+		else
+			tail->next = cur;
+		tail = cur;
+
+		switch (addr_entry->ipaddr.sin.sin_family) {
 		case AF_INET:
 			addrlen = sizeof(struct sockaddr_in);
 			addr_format = FI_SOCKADDR_IN;
@@ -87,22 +81,7 @@ static void tcpx_getinfo_ifs(struct fi_info **info)
 			continue;
 		}
 
-		cur = fi_dupinfo(*info);
-		if (!cur)
-			break;
-
-		if (!ofi_is_loopback_addr(ifa->ifa_addr)) {
-			if (!head)
-				head = cur;
-			else
-				tail->next = cur;
-			tail = cur;
-		} else {
-			cur->next = loopback;
-			loopback = cur;
-		}
-
-		cur->src_addr = mem_dup(ifa->ifa_addr, addrlen);
+		cur->src_addr = mem_dup(&addr_entry->ipaddr.sa, addrlen);
 		if (cur->src_addr) {
 			cur->src_addrlen = addrlen;
 			cur->addr_format = addr_format;
@@ -111,18 +90,7 @@ static void tcpx_getinfo_ifs(struct fi_info **info)
 		util_set_fabric_domain(&tcpx_prov, cur);
 		*/
 	}
-	freeifaddrs(ifaddrs);
 
-	if (head || loopback) {
-		if (!head) { /* loopback interface only? */
-			head = loopback;
-		} else {
-			/* append loopback interfaces to tail */
-			assert(tail);
-			assert(!tail->next);
-			tail->next = loopback;
-		}
-	}
 	fi_freeinfo(*info);
 	*info = head;
 }
