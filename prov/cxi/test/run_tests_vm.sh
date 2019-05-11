9220fb4971452cec9d178c967ab604e3c292672c
Zach Tiffany <ztiffany@cray.com>
2019-05-11 15:54:26 -0500

    prov/cxi: automate SW issued RGet testing
    Signed-off-by: Zach Tiffany <ztiffany@cray.com>

--- prov/cxi/test/run_tests_vm.sh
+++ prov/cxi/test/run_tests_vm.sh
@@ -5,5 +5,19 @@ cd $(dirname $0)
 
 # Run unit tests.  $(CWD) should be writeable.
 ./cxitest --verbose --tap=cxitest.tap
+
 # Run tests again with RPut enabled
 RDZV_OFFLOAD=1 ./cxitest --verbose --tap=cxitest-rput.tap
+
+PYCXI="../../../../pycxi"
+CSRUTIL="$PYCXI/utils/csrutil"
+
+# Run tests with RPut and SW Gets
+if [ -e $CSRUTIL ]; then
+	. $PYCXI/.venv/bin/activate
+	$CSRUTIL store csr get_ctrl get_en=0
+	RDZV_OFFLOAD=1 ./cxitest --verbose --filter=tagged/* --tap=cxitest-rput-swget.tap
+	$CSRUTIL store csr get_ctrl get_en=1
+else
+	echo "No csrutil"
+fi
