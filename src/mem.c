ofiwg/libfabric

commit 8ce14923ba672cd914b900c33b1492f26de57eb8

core/mem: Obtain a list of available huge pages in system
It's possible for a system to support more than one huge page
size.  The current code reports the default huge page size, but
the memory registration cache needs to check against all sizes.

Create an array filled with all available page sizes.  This
will be filled on initialization and directly accessible
by providers and utility code.

Signed-off-by: Sean Hefty <sean.hefty@intel.com>

src/mem.c
@@ -36,6 +36,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <inttypes.h>
+#include <dirent.h>
 
 #include <ofi_osd.h>
 #include <ofi.h>
@@ -46,10 +47,66 @@
 
 static const int OFI_CACHE_SIZE = 64;
 
+size_t *page_sizes = NULL;
+size_t num_page_sizes = 0;
+
+
+void ofi_mem_init(void)
+{
+	struct dirent **pglist = NULL;
+	size_t max_cnt;
+	ssize_t hpsize;
+	long psize;
+	int n;
+
+	psize = ofi_get_page_size();
+	if (psize < 0)
+		return;
+
+	hpsize = ofi_get_hugepage_size();
+	if (hpsize > 0) {
+		n = scandir("/sys/kernel/mm/hugepages", &pglist, NULL, NULL);
+		max_cnt = (n < 0) ? 2 : n + 1;
+	} else {
+		max_cnt = 1;
+		n = 0;
+	}
+
+	page_sizes = calloc(max_cnt, sizeof(*page_sizes));
+	if (!page_sizes)
+		goto free_list;
+
+	page_sizes[OFI_PAGE_SIZE] = psize;
+	if (hpsize > 0) {
+		page_sizes[OFI_DEF_HUGEPAGE_SIZE] = hpsize;
+		num_page_sizes = 2;
+	} else {
+		num_page_sizes = 1;
+	}
+
+	while (n--) {
+		if (sscanf(pglist[n]->d_name, "hugepages-%lukB", &hpsize) == 1) {
+			hpsize *= 1024;
+			if (hpsize != page_sizes[OFI_DEF_HUGEPAGE_SIZE])
+				page_sizes[num_page_sizes++] = hpsize;
+		}
+		free(pglist[n]);
+	}
+
+free_list:
+	while (n-- > 0)
+		free(pglist[n]);
+	free(pglist);
+}
+
+void ofi_mem_fini(void)
+{
+	free(page_sizes);
+}
+
 
 uint64_t OFI_RMA_PMEM;
 void (*ofi_pmem_commit)(const void *addr, size_t len);
-
 
 static void pmem_commit_clwb(const void *addr, size_t len)
 {

include/ofi_mem.h
@@ -54,10 +54,24 @@
 #   define VALGRIND_MAKE_MEM_DEFINED(addr, len)
 #endif
 
+
+void ofi_mem_init(void);
+void ofi_mem_fini(void);
+
+enum {
+	OFI_PAGE_SIZE,
+	OFI_DEF_HUGEPAGE_SIZE,
+};
+
+extern size_t *page_sizes;
+extern size_t num_page_sizes;
+
 static inline long ofi_get_page_size()
 {
 	return ofi_sysconf(_SC_PAGESIZE);
 }
+ssize_t ofi_get_hugepage_size(void);
+
 
 /* We implement memdup to avoid external library dependency */
 static inline void *mem_dup(const void *src, size_t size)

include/windows/dirent.h
@@ -2,3 +2,15 @@
 #pragma once
 
 
+
+struct dirent {
+	char	d_name[256];
+};
+
+static inline int scandir(const char *dirp, struct dirent **namelist,
+	    int (*filter)(const struct dirent *),
+	    int (*compar)(const struct dirent **, const struct dirent **))
+{
+	errno = ENOSYS;
+	return -1;
+}

prov/verbs/src/fi_verbs.c
@@ -667,6 +667,7 @@ static void fi_ibv_fini(void)
 {
 #if HAVE_VERBS_DL
 	ofi_monitor_cleanup();
+	ofi_mem_fini();
 #endif
 	fi_freeinfo((void *)fi_ibv_util_prov.info);
 	fi_ibv_util_prov.info = NULL;
@@ -675,6 +676,7 @@ static void fi_ibv_fini(void)
 VERBS_INI
 {
 #if HAVE_VERBS_DL
+	ofi_mem_init();
 	ofi_monitor_init();
 #endif
 	if (fi_ibv_read_params()|| fi_ibv_init_info(&fi_ibv_util_prov.info))

src/fabric.c
@@ -551,6 +551,7 @@ void fi_ini(void)
 	fi_param_init();
 	fi_log_init();
 	ofi_osd_init();
+	ofi_mem_init();
 	ofi_pmem_init();
 	ofi_perf_init();
 	ofi_hook_init();
@@ -646,6 +647,7 @@ FI_DESTRUCTOR(fi_fini(void))
 
 	ofi_free_filter(&prov_filter);
 	ofi_monitor_cleanup();
+	ofi_mem_fini();
 	fi_log_fini();
 	fi_param_fini();
 	ofi_osd_fini();

commit b091a17b1ec7a5b546c2450bbd24bd26716c2f67

Fix segment fault issue for linux container
While run openmpi/mpirun with linux containers, the libfabric failed
with segment fault message.

 Signal: Segmentation fault (11)
 Signal code: Address not mapped (1)
 Failing at address: 0xfffffffffffffff0
 [ 0] /lib64/libpthread.so.0(+0x12d80)[0x14feb5d4dd80]
 [ 1] /lib64/libfabric.so.1(+0x23cd1)[0x14fea8105cd1]
 [ 2] /lib64/libfabric.so.1(+0x18240)[0x14fea80fa240]
 [ 3] /lib64/libfabric.so.1(fi_getinfo+0x695)[0x14fea80faea5]
 [ 4] /lib64/libfabric.so.1(fi_getinfo+0x4e)[0x14fea80ffe9e]
 [ 5] /usr/lib64/openmpi/lib/openmpi/mca_btl_usnic.so(+0xdf4e)[0x14fea8445f4e]
 [ 6] /usr/lib64/openmpi/lib/libopen-pal.so.40(mca_btl_base_select+0xed)[0x14feb547815d]
 [ 7] /usr/lib64/openmpi/lib/openmpi/mca_bml_r2.so(mca_bml_r2_component_init+0x16)[0x14fea9fab2f6]
 [ 8] /usr/lib64/openmpi/lib/libmpi.so.40(mca_bml_base_init+0xa4)[0x14feb5ffef94]
 [ 9] /usr/lib64/openmpi/lib/libmpi.so.40(ompi_mpi_init+0x654)[0x14feb5fac474]
 [10] /usr/lib64/openmpi/lib/libmpi.so.40(MPI_Init+0x72)[0x14feb5fdc6b2]
 [11] /home/mpi/ring[0x4009ad]
 [12] /lib64/libc.so.6(__libc_start_main+0xf3)[0x14feb599a813]
 [13] /home/mpi/ring[0x4008be]

The 'scandir' function called by 'ofi_mem_init' returned -1 with errno
set to ENOENT.

Fixes: 8ce14923ba67 (core/mem: Obtain a list of available huge pages in system)

Signed-off-by: Honggang Li <honli@redhat.com>

src/mem.c
@@ -84,7 +84,7 @@ void ofi_mem_init(void)
 		num_page_sizes = 1;
 	}
 
-	while (n--) {
+	while (n-- > 0) {
 		if (sscanf(pglist[n]->d_name, "hugepages-%zukB", &hpsize) == 1) {
 			hpsize *= 1024;
 			if (hpsize != page_sizes[OFI_DEF_HUGEPAGE_SIZE])