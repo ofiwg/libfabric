c44c66eecea21825938c715d6eb9e15aa5134737
Ian Ziemba <ian.ziemba@hpe.com>
2020-06-22 12:40:02 -0500

    prov/cxi: DWQ tests for FI_OP_RECV/TRECV
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>

--- prov/cxi/test/deferred_work.c
+++ prov/cxi/test/deferred_work.c
@@ -664,3 +664,224 @@ Test(deferred_work, cntr_set)
 {
 	deferred_cntr(false);
 }
+
+static void deferred_recv_op_test(bool comp_event, size_t xfer_size,
+				  uint64_t trig_thresh, bool is_tagged,
+				  uint64_t tag)
+{
+	int i;
+	int ret;
+	uint8_t *recv_buf;
+	uint8_t *send_buf;
+	struct fi_cq_tagged_entry tx_cqe;
+	struct fi_cq_tagged_entry rx_cqe;
+	int err = 0;
+	fi_addr_t from;
+	struct iovec iov = {};
+	struct fi_op_msg msg = {};
+	struct fi_op_tagged tagged = {};
+	struct fi_deferred_work work = {};
+	uint64_t expected_rx_flags =
+		is_tagged ? FI_TAGGED | FI_RECV : FI_MSG | FI_RECV;
+	uint64_t expected_rx_tag = is_tagged ? tag : 0;
+	uint64_t expected_tx_flags =
+		is_tagged ? FI_TAGGED | FI_SEND : FI_MSG | FI_SEND;
+	struct fi_cntr_attr attr = {};
+	struct fid_cntr *recv_cntr;
+
+	ret = fi_cntr_open(cxit_domain, &attr, &recv_cntr, NULL);
+	cr_assert_eq(ret, FI_SUCCESS, "fi_cntr_open failed %d", ret);
+
+	recv_buf = calloc(1, xfer_size);
+	cr_assert(recv_buf);
+
+	send_buf = calloc(1, xfer_size);
+	cr_assert(send_buf);
+
+	for (i = 0; i < xfer_size; i++)
+		send_buf[i] = i + 0xa0;
+
+	/* Recv deferred op */
+	iov.iov_base = recv_buf;
+	iov.iov_len = xfer_size;
+
+	work.threshold = trig_thresh;
+	work.triggering_cntr = recv_cntr;
+	work.completion_cntr = recv_cntr;
+
+	if (is_tagged) {
+		tagged.ep = cxit_ep;
+		tagged.msg.msg_iov = &iov;
+		tagged.msg.iov_count = 1;
+		tagged.msg.tag = tag;
+		tagged.msg.addr = cxit_ep_fi_addr;
+		tagged.flags = comp_event ? FI_COMPLETION : 0;
+
+		work.op_type = FI_OP_TRECV;
+		work.op.tagged = &tagged;
+	} else {
+		msg.ep = cxit_ep;
+		msg.msg.msg_iov = &iov;
+		msg.msg.iov_count = 1;
+		msg.msg.addr = cxit_ep_fi_addr;
+		msg.flags = comp_event ? FI_COMPLETION : 0;
+
+		work.op_type = FI_OP_RECV;
+		work.op.msg = &msg;
+	}
+
+	ret = fi_control(&cxit_domain->fid, FI_QUEUE_WORK, &work);
+	cr_assert_eq(ret, FI_SUCCESS, "FI_QUEUE_WORK failed %d", ret);
+
+	if (is_tagged)
+		ret = fi_tsend(cxit_ep, send_buf, xfer_size, NULL,
+			       cxit_ep_fi_addr, tag, NULL);
+	else
+		ret = fi_send(cxit_ep, send_buf, xfer_size, NULL,
+			      cxit_ep_fi_addr, NULL);
+	cr_assert_eq(ret, FI_SUCCESS, "fi_send failed %d", ret);
+
+	/* Wait for async send event. */
+	ret = cxit_await_completion(cxit_tx_cq, &tx_cqe);
+	cr_assert_eq(ret, 1, "fi_cq_read unexpected value %d", ret);
+
+	validate_tx_event(&tx_cqe, expected_tx_flags, NULL);
+
+	/* Verify optional receive event. */
+	if (comp_event) {
+		/* Wait for async event indicating data has been sent */
+		do {
+			ret = fi_cq_readfrom(cxit_rx_cq, &rx_cqe, 1, &from);
+		} while (ret == -FI_EAGAIN);
+		cr_assert_eq(ret, 1, "fi_cq_read unexpected value %d", ret);
+
+		validate_rx_event(&rx_cqe, NULL, xfer_size, expected_rx_flags,
+				  NULL, 0, expected_rx_tag);
+		cr_assert(from == cxit_ep_fi_addr, "Invalid source address");
+	} else {
+		ret = fi_cq_read(cxit_rx_cq, &rx_cqe, 1);
+		cr_assert_eq(ret, -FI_EAGAIN, "fi_cq_read unexpected value %d",
+			     ret);
+	}
+
+	/* Validate sent data */
+	for (i = 0; i < xfer_size; i++) {
+		cr_expect_eq(recv_buf[i], send_buf[i],
+			  "data mismatch, element[%d], exp=%d saw=%d, err=%d\n",
+			  i, send_buf[i], recv_buf[i], err++);
+	}
+	cr_assert_eq(err, 0, "Data errors seen\n");
+
+	/* Need to progress recv the transaction to increment the counter. */
+	ret = fi_cq_read(cxit_rx_cq, &rx_cqe, 1);
+	cr_assert_eq(ret, -FI_EAGAIN, "fi_cq_read unexpected value %d", ret);
+
+	poll_counter_assert(recv_cntr, 1, 5);
+
+	free(send_buf);
+	free(recv_buf);
+	fi_close(&recv_cntr->fid);
+}
+
+Test(deferred_work, recv_eager_message_comp_event)
+{
+	deferred_recv_op_test(true, 1024, 0, false, 0);
+}
+
+Test(deferred_work, recv_rendezvous_message_comp_event)
+{
+	deferred_recv_op_test(true, 1024 * 1024, 0, false, 0);
+}
+
+Test(deferred_work, recv_eager_message_no_comp_event)
+{
+	deferred_recv_op_test(false, 1024, 0, false, 0);
+}
+
+Test(deferred_work, recv_rendezvous_message_no_comp_event)
+{
+	deferred_recv_op_test(false, 1024 * 1024, 0, false, 0);
+}
+
+Test(deferred_work, recv_tagged_eager_message_comp_event)
+{
+	deferred_recv_op_test(true, 1024, 0, true, 987654321);
+}
+
+Test(deferred_work, recv_tagged_rendezvous_message_comp_event)
+{
+	deferred_recv_op_test(true, 1024 * 1024, 0, true, 987654321);
+}
+
+Test(deferred_work, recv_tagged_eager_message_no_comp_event)
+{
+	deferred_recv_op_test(false, 1024, 0, true, 987654321);
+}
+
+Test(deferred_work, recv_tagged_rendezvous_message_no_comp_event)
+{
+	deferred_recv_op_test(false, 1024 * 1024, 0, true, 987654321);
+}
+
+static void deferred_recv_non_zero_thresh(bool is_tagged)
+{
+	int ret;
+	uint8_t *recv_buf;
+	struct iovec iov = {};
+	struct fi_op_msg msg = {};
+	struct fi_op_tagged tagged = {};
+	struct fi_deferred_work work = {};
+	struct fi_cntr_attr attr = {};
+	struct fid_cntr *recv_cntr;
+
+	ret = fi_cntr_open(cxit_domain, &attr, &recv_cntr, NULL);
+	cr_assert_eq(ret, FI_SUCCESS, "fi_cntr_open failed %d", ret);
+
+	recv_buf = calloc(1, 5);
+	cr_assert(recv_buf);
+
+	/* Recv deferred op to self */
+	iov.iov_base = recv_buf;
+	iov.iov_len = 5;
+
+	work.threshold = 5;
+	work.triggering_cntr = recv_cntr;
+	work.completion_cntr = recv_cntr;
+
+	if (is_tagged) {
+		tagged.ep = cxit_ep;
+		tagged.msg.msg_iov = &iov;
+		tagged.msg.iov_count = 1;
+		tagged.msg.tag = 456;
+		tagged.msg.addr = cxit_ep_fi_addr;
+		tagged.flags = FI_COMPLETION;
+
+		work.op_type = FI_OP_TRECV;
+		work.op.tagged = &tagged;
+	} else {
+		msg.ep = cxit_ep;
+		msg.msg.msg_iov = &iov;
+		msg.msg.iov_count = 1;
+		msg.msg.addr = cxit_ep_fi_addr;
+		msg.flags = FI_COMPLETION;
+
+		work.op_type = FI_OP_RECV;
+		work.op.msg = &msg;
+	}
+
+	ret = fi_control(&cxit_domain->fid, FI_QUEUE_WORK, &work);
+	cr_assert_neq(ret, FI_SUCCESS, "FI_QUEUE_WORK failed %d", ret);
+
+	free(recv_buf);
+	fi_close(&recv_cntr->fid);
+}
+
+Test(deferred_work, recv_non_zero_thresh)
+{
+	deferred_recv_non_zero_thresh(false);
+}
+
+Test(deferred_work, recv_tagged_non_zero_thresh)
+{
+	deferred_recv_non_zero_thresh(true);
+}
