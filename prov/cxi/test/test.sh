b84af48c7df948e40be612649b5032ac936e31f5
Zach Tiffany <zachary.tiffany@hpe.com>
2020-06-24 11:37:53 -0500

    prov/cxi: trim test output
    Signed-off-by: Zach Tiffany <zachary.tiffany@hpe.com>

--- prov/cxi/test/test.sh
+++ prov/cxi/test/test.sh
@@ -4,6 +4,7 @@
 
 DIR=`dirname $0`
 cd $DIR
+TEST_OUTPUT=cxitest.out
 
 export DMA_FAULT_RATE=.1
 export MALLOC_FAULT_RATE=.1
@@ -15,18 +16,24 @@ if [[ $# -gt 0 ]]; then
 fi
 
 # Run unit tests.  $(CWD) should be writeable.
-./cxitest --verbose --tap=cxitest.tap
+echo "running: ./cxitest --verbose --tap=cxitest.tap > $TEST_OUTPUT 2>&1"
+./cxitest --verbose --tap=cxitest.tap > $TEST_OUTPUT 2>&1
 
 # Re-run messaging tests with RPut disabled
-FI_CXI_RDZV_OFFLOAD=0 ./cxitest --verbose --filter="@(tagged|msg)/*" --tap=cxitest-swrdzv.tap
+echo "running: FI_CXI_RDZV_OFFLOAD=0 ./cxitest --verbose --filter=\"@(tagged|msg)/*\" --tap=cxitest-swrdzv.tap >> $TEST_OUTPUT 2>&1"
+FI_CXI_RDZV_OFFLOAD=0 ./cxitest --verbose --filter="@(tagged|msg)/*" --tap=cxitest-swrdzv.tap >> $TEST_OUTPUT 2>&1
 
 # Run tests with RPut and SW Gets
-csrutil store csr get_ctrl get_en=0
-./cxitest --verbose --filter="@(tagged|msg)/*" --tap=cxitest-swget.tap
-csrutil store csr get_ctrl get_en=1
+csrutil store csr get_ctrl get_en=0 > /dev/null
+echo "running: ./cxitest --verbose --filter=\"@(tagged|msg)/*\" --tap=cxitest-swget.tap >> $TEST_OUTPUT 2>&1"
+./cxitest --verbose --filter="@(tagged|msg)/*" --tap=cxitest-swget.tap >> $TEST_OUTPUT 2>&1
+csrutil store csr get_ctrl get_en=1 > /dev/null
 
 # Run tests with constrained LE count
 MAX_ALLOC=`csrutil dump csr le_pools[63] |grep max_alloc |awk '{print $3}'`
-csrutil store csr le_pools[] max_alloc=10
-./cxitest --verbose --filter="tagged/fc*" --tap=cxitest-fc.tap
-csrutil store csr le_pools[] max_alloc=$MAX_ALLOC
+csrutil store csr le_pools[] max_alloc=10 > /dev/null
+echo "running; ./cxitest --verbose --filter=\"tagged/fc*\" --tap=cxitest-fc.tap >> $TEST_OUTPUT 2>&1"
+./cxitest --verbose --filter="tagged/fc*" --tap=cxitest-fc.tap >> $TEST_OUTPUT 2>&1
+csrutil store csr le_pools[] max_alloc=$MAX_ALLOC > /dev/null
+
+grep "Tested" $TEST_OUTPUT
