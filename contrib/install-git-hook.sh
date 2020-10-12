55217654cc20343f7dd1be63b6e13aed073b00db
djosifovich <dennis.c.josifovich@hpe.com>
2020-10-12 14:28:29 -0500

    changes to git pre-commit installer and hook
    Cleaned up both scripts.  Reworked both scripts to
    be  posix shell compliant scripts.  Added pre-commit
    sub-hook to prevent commit to the 'master branch'.
    Added environment variables to skip individual
    sub-hooks if needed.
    
    Signed-off-by: djosifovich <dennis.c.josifovich@hpe.com>

--- contrib/install-git-hook.sh
+++ contrib/install-git-hook.sh
@@ -1,17 +1,77 @@
-#!/bin/bash
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+#
 # Install git hook to run checkpatch on commits
+#
 
-BASE_REPO_DIR="$(git rev-parse --show-toplevel)"
+scriptname="${0##*/}"
 
-if [[ -e "${BASE_REPO_DIR}/.git/hooks/pre-commit" ]]; then
-	echo "Error! The pre-commit git hook is already present."
+set -e
+
+BASE_REPO_DIR="$(git rev-parse --show-toplevel 2>/dev/null)"
+if [ $? -ne 0 ]
+then
+	printf '%s: Error! not in a git clone or repo.\n'  "${scriptname}"
+	exit 1
+fi
+if [ ! -d "${BASE_REPO_DIR}/.git/hooks" ]
+then
+	printf '%s: Error! .git/hooks/ not found.\n'  "${scriptname}"
+	exit 1
+fi
+if [ -e "${BASE_REPO_DIR}/.git/hooks/pre-commit" ]
+then
+	printf '%s: Error! The pre-commit git hook is already present.\n' \
+		"${scriptname}"
 	exit 1
 fi
 
-cat > "${BASE_REPO_DIR}/.git/hooks/pre-commit" << EOL
-#!/usr/bin/env bash
-exec git diff --cached | ./contrib/checkpatch.pl --no-tree --no-signoff --ignore=AVOID_EXTERNS,FILE_PATH_CHANGES,SSCANF_TO_KSTRTO
-EOL
+cat >"${BASE_REPO_DIR}/.git/hooks/pre-commit" <<'EOI'
+#!/bin/sh
+
+scriptname="${0##*/}"
+
+#
+# overall error value (0 --> no-error) returned by this script.
+#
+# The intention is to allow each 'hook' run gather its own success/failure
+# setting errval to non-zero if this overall script should return error.
+#
+errval=0
+
+if [ ${GIT_HOOKS_PRE_COMMIT_HPE_CRAY_BRANCH_CHECK_SKIP:-0} -eq 0 ]
+then
+	branchname=$(git symbolic-ref --short --quiet HEAD \
+		|| ( git branch --list --format='%(refname:lstrip=2)' HEAD \
+			| sed -e 's/(HEAD detached at \(.*\))/detached@\1/g' ))
+	if [ "${branchname}" = master ]
+	then
+		errval=1
+		printf '\n%s: ERROR:  commits to master are disallowed\n\n'\
+			 "${scriptname}"
+	fi
+fi
+
+if [ ${GIT_HOOKS_PRE_COMMIT_HPE_CRAY_CHECKPATCH_SKIP:-0} -eq 0 ]
+then
+	git diff --no-ext-diff --cached \
+	| ./contrib/checkpatch.pl\
+		--no-tree\
+		--no-signoff\
+		--ignore=AVOID_EXTERNS,FILE_PATH_CHANGES,SSCANF_TO_KSTRTO
+	if [ $? -ne 0 ]
+	then
+		errval=1
+	fi
+fi
+
+#
+# indicate hook status
+#
+exit ${errval}
+EOI
 chmod +x "${BASE_REPO_DIR}/.git/hooks/pre-commit"
 
-echo "The pre-commit git hook was installed successfully."
+printf '%s: The pre-commit git hook was installed successfully.\n'\
+	"${scriptname}"
+exit 0
