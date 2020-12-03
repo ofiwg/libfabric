b411cd43a51df7271d35ad16f07e559ff2f6e3d2
Ken Raffenetti <raffenet@mcs.anl.gov>
2020-12-03 06:51:59 -0800

    osx/osd: Handle socket API size limitations
    While the macOS API uses `size_t count`, in practice it only supports
    up to INT_MAX size operations. Anything larger returns an error
    (Invalid argument). Borrow code from the windows/osd that limits
    transfer the size of each operation.
    
    Signed-off-by: Ken Raffenetti <raffenet@mcs.anl.gov>

--- include/osx/osd.h
+++ include/osx/osd.h
@@ -47,6 +47,8 @@
 
 #include <ifaddrs.h>
 
+#include <limits.h>
+
 #include "unix/osd.h"
 #include "rdma/fi_errno.h"
 #include "config.h"
@@ -115,61 +117,50 @@ static inline size_t ofi_process_vm_writev(pid_t pid,
 	return -FI_ENOSYS;
 }
 
+static inline ssize_t
+ofi_recv_socket(SOCKET fd, void *buf, size_t count, int flags)
+{
+	size_t len = count > INT_MAX ? INT_MAX : count;
+	return recv(fd, buf, len, flags);
+}
+
+static inline ssize_t
+ofi_send_socket(SOCKET fd, const void *buf, size_t count, int flags)
+{
+	size_t len = count > INT_MAX ? INT_MAX : count;
+	return send(fd, buf, len, flags);
+}
+
 static inline ssize_t ofi_read_socket(SOCKET fd, void *buf, size_t count)
 {
-	return read(fd, buf, count);
+	return ofi_recv_socket(fd, buf, count, 0);
 }
 
 static inline ssize_t ofi_write_socket(SOCKET fd, const void *buf, size_t count)
 {
-	return write(fd, buf, count);
-}
-
-static inline ssize_t ofi_recv_socket(SOCKET fd, void *buf, size_t count,
-				      int flags)
-{
-	return recv(fd, buf, count, flags);
-}
-
-static inline ssize_t ofi_recvfrom_socket(SOCKET fd, void *buf, size_t count, int flags,
-					  struct sockaddr *from, socklen_t *fromlen)
-{
-	return recvfrom(fd, buf, count, flags, from, fromlen);
-}
-
-static inline ssize_t ofi_send_socket(SOCKET fd, const void *buf, size_t count,
-				      int flags)
-{
-	return send(fd, buf, count, flags);
-}
-
-static inline ssize_t ofi_sendto_socket(SOCKET fd, const void *buf, size_t count, int flags,
-					const struct sockaddr *to, socklen_t tolen)
-{
-	return sendto(fd, buf, count, flags, to, tolen);
-}
-
-static inline ssize_t ofi_writev_socket(SOCKET fd, struct iovec *iov, size_t iov_cnt)
-{
-    return writev(fd, iov, iov_cnt);
-}
-
-static inline ssize_t ofi_readv_socket(SOCKET fd, struct iovec *iov, int iov_cnt)
-{
-    return readv(fd, iov, iov_cnt);
+	return ofi_send_socket(fd, buf, count, 0);
 }
 
 static inline ssize_t
-ofi_sendmsg_tcp(SOCKET fd, const struct msghdr *msg, int flags)
+ofi_recvfrom_socket(SOCKET fd, void *buf, size_t count, int flags,
+		    struct sockaddr *from, socklen_t *fromlen)
 {
-    return sendmsg(fd, msg, flags);
+	size_t len = count > INT_MAX ? INT_MAX : count;
+	return recvfrom(fd, buf, len, flags, from, fromlen);
 }
 
 static inline ssize_t
-ofi_recvmsg_tcp(SOCKET fd, struct msghdr *msg, int flags)
+ofi_sendto_socket(SOCKET fd, const void *buf, size_t count, int flags,
+		  const struct sockaddr *to, socklen_t tolen)
 {
-    return recvmsg(fd, msg, flags);
+	size_t len = count > INT_MAX ? INT_MAX : count;
+	return sendto(fd, buf, len, flags, to, tolen);
 }
+
+ssize_t ofi_writev_socket(SOCKET fd, const struct iovec *iovec, size_t iov_cnt);
+ssize_t ofi_readv_socket(SOCKET fd, const struct iovec *iovec, size_t iov_cnt);
+ssize_t ofi_sendmsg_tcp(SOCKET fd, const struct msghdr *msg, int flags);
+ssize_t ofi_recvmsg_tcp(SOCKET fd, struct msghdr *msg, int flags);
 
 #ifdef __cplusplus
 }
