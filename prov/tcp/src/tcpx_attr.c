8762b0afacbc4bf010d009e71f26d7cbbf08a1e2
Sean Hefty <sean.hefty@intel.com>
2021-03-31 21:03:57 -0700

    prov/tcp: Adjust max ep attributes
    The ep attributes are currently used to limit the size
    that a shared rx may be.  Increase the size to support
    larger rx sizes.  This has the effect of increasing the
    default tx/rx sizes, which will be addressed in follow
    on patches.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- prov/tcp/src/tcpx_attr.c
+++ prov/tcp/src/tcpx_attr.c
@@ -67,7 +67,7 @@ static struct fi_rx_attr tcpx_rx_attr = {
 	.comp_order = FI_ORDER_STRICT,
 	.msg_order = TCPX_MSG_ORDER,
 	.total_buffered_recv = 0,
-	.size = 1024,
+	.size = 8192,
 	.iov_limit = TCPX_IOV_LIMIT
 };
 
@@ -97,7 +97,7 @@ static struct fi_domain_attr tcpx_domain_attr = {
 	.ep_cnt = 8192,
 	.tx_ctx_cnt = 8192,
 	.rx_ctx_cnt = 8192,
-	.max_ep_srx_ctx = 128,
+	.max_ep_srx_ctx = 8192,
 	.max_ep_tx_ctx = 1,
 	.max_ep_rx_ctx = 1
 };
