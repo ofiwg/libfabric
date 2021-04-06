edeaed5e597b9f5bccab5e8cde574efd6d5a105f
Ian Ziemba <ian.ziemba@hpe.com>
2021-04-06 13:29:42 -0500

    prov/cxi: Check cxitest exit status
    For tests which encounter a error during teardown, the test will be
    marked as pass, but the cxitest exit status will be non-zero. Add in
    checks to verify cxitest exit status is zero.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>

--- prov/cxi/test/test.sh
+++ prov/cxi/test/test.sh
@@ -19,6 +19,10 @@ fi
 # Run unit tests.  $(CWD) should be writeable.
 echo "running: ./cxitest --verbose --tap=cxitest.tap -j2 > $TEST_OUTPUT 2>&1"
 ./cxitest --verbose --tap=cxitest.tap -j2 > $TEST_OUTPUT 2>&1
+if [[ $? -ne 0 ]]; then
+    echo "cxitest return non-zero exit code. Possible failures in test teardown"
+    exit 1
+fi
 
 # Re-run messaging tests with RPut disabled
 #echo "running: FI_CXI_RDZV_OFFLOAD=0 ./cxitest --verbose --filter=\"@(tagged|msg)/*\" --tap=cxitest-swrdzv.tap -j2 >> $TEST_OUTPUT 2>&1"
@@ -28,19 +32,33 @@ echo "running: ./cxitest --verbose --tap=cxitest.tap -j2 > $TEST_OUTPUT 2>&1"
 csrutil store csr get_ctrl get_en=0 > /dev/null
 echo "running: ./cxitest --verbose --filter=\"@(tagged|msg)/*\" --tap=cxitest-swget.tap -j2 >> $TEST_OUTPUT 2>&1"
 ./cxitest --verbose --filter="@(tagged|msg)/*" --tap=cxitest-swget.tap -j2 >> $TEST_OUTPUT 2>&1
+cxitest_exit_status=$?
 csrutil store csr get_ctrl get_en=1 > /dev/null
+if [[ $cxitest_exit_status -ne 0 ]]; then
+    echo "cxitest return non-zero exit code. Possible failures in test teardown"
+    exit 1
+fi
 
 # Run tests with constrained LE count
 MAX_ALLOC=`csrutil dump csr le_pools[63] |grep max_alloc |awk '{print $3}'`
 csrutil store csr le_pools[] max_alloc=10 > /dev/null
 echo "running; ./cxitest --verbose --filter=\"tagged/fc*\" --tap=cxitest-fc.tap -j2 >> $TEST_OUTPUT 2>&1"
 ./cxitest --verbose --filter="tagged/fc*" --tap=cxitest-fc.tap -j2 >> $TEST_OUTPUT 2>&1
+cxitest_exit_status=$?
 csrutil store csr le_pools[] max_alloc=$MAX_ALLOC > /dev/null
+if [[ $cxitest_exit_status -ne 0 ]]; then
+    echo "cxitest return non-zero exit code. Possible failures in test teardown"
+    exit 1
+fi
 
 # Verify tag matching with rendezvous
 test="FI_CXI_RDZV_GET_MIN=0 FI_CXI_RDZV_THRESHOLD=2048 ./cxitest --verbose -j2 --filter=\"tagged_directed/*\" --tap=cxitest-hw-rdzv-tag-matching.tap >> $TEST_OUTPUT 2>&1"
 echo "running: $test"
 eval $test
+if [[ $? -ne 0 ]]; then
+    echo "cxitest return non-zero exit code. Possible failures in test teardown"
+    exit 1
+fi
 
 #test="FI_CXI_RDZV_OFFLOAD=0 FI_CXI_RDZV_GET_MIN=0 FI_CXI_RDZV_THRESHOLD=2048 ./cxitest --verbose -j2 --filter=\"tagged_directed/*\" --tap=cxitest-sw-rdzv-tag-matching.tap >> $TEST_OUTPUT 2>&1"
 #echo "running: $test"
@@ -49,5 +67,9 @@ eval $test
 test="FI_CXI_MSG_OFFLOAD=0 FI_CXI_RDZV_GET_MIN=0 FI_CXI_RDZV_THRESHOLD=2048 ./cxitest --verbose -j 1 --tap=cxitest-sw-ep-mode.tap >> $TEST_OUTPUT 2>&1"
 echo "running: $test"
 eval $test
+if [[ $? -ne 0 ]]; then
+    echo "cxitest return non-zero exit code. Possible failures in test teardown"
+    exit 1
+fi
 
 grep "Tested" $TEST_OUTPUT
