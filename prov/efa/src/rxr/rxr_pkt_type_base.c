4c9e4446466955576863b4b31fb7be7d35470fa9
Wei Zhang <wzam@amazon.com>
2021-05-29 18:50:23 +0000

    prov/efa: add connid to EOR packet header
    This patch added an multi-purpose field "padding/connid"
    to the end of EOR packet.
    
    The field can be used as "connid" field when the RXR_PKT_CONNID_HDR
    flag is toggled in "flags".
    
    If the flag is not toggled, the field is used as a padding space.
    
    This patch uses it as "connid", (thus set the RXR_PKT_CONNID_HDF flag)
    
    It added a section to rxr_pkt_connid_ptr() to return connid
    pointer in EOR packet if RXR_PKT_CONNID_HDR is set.
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/efa/src/rxr/rxr_pkt_type_base.c
+++ prov/efa/src/rxr/rxr_pkt_type_base.c
@@ -74,6 +74,12 @@ uint32_t *rxr_pkt_connid_ptr(struct rxr_pkt_entry *pkt_entry)
 			: NULL;
 	}
 
+	if (base_hdr->type == RXR_EOR_PKT) {
+		return (base_hdr->flags & RXR_PKT_CONNID_HDR)
+			? &rxr_get_eor_hdr(pkt_entry->pkt)->connid
+			: NULL;
+	}
+
 	if (base_hdr->type == RXR_HANDSHAKE_PKT) {
 		return (base_hdr->flags & RXR_PKT_CONNID_HDR)
 			? &(rxr_get_handshake_opt_connid_hdr(pkt_entry->pkt)->connid)
