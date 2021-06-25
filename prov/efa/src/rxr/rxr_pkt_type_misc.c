dde4cedad6290fefc570cd3129075acf7a519b23
Jie Zhang <zhngaj@amazon.com>
2021-06-25 01:41:25 +0000

    prov/efa: Fix a bug in rxr_handle_rma_read_completion
    When handling rma read completion, we decrement tx_pending
    counter based on context_type. However, struct rxr_read_entry
    could already been released, thus we cannot get correct
    context_type from rxr_read_entry any more.
    
    Fix the issue by saving context_type before releasing
    rxr_read_entry.
    
    Signed-off-by: Jie Zhang <zhngaj@amazon.com>

--- prov/efa/src/rxr/rxr_pkt_type_misc.c
+++ prov/efa/src/rxr/rxr_pkt_type_misc.c
@@ -408,6 +408,7 @@ void rxr_pkt_handle_rma_read_completion(struct rxr_ep *ep,
 	struct rxr_pkt_entry *pkt_entry;
 	struct rxr_read_entry *read_entry;
 	struct rxr_rma_context_pkt *rma_context_pkt;
+	enum rxr_read_context_type read_context_type;
 	struct rdm_peer *peer;
 	int inject;
 	size_t data_size;
@@ -420,6 +421,7 @@ void rxr_pkt_handle_rma_read_completion(struct rxr_ep *ep,
 	read_entry = (struct rxr_read_entry *)context_pkt_entry->x_entry;
 	read_entry->bytes_finished += rma_context_pkt->seg_size;
 	assert(read_entry->bytes_finished <= read_entry->total_len);
+	read_context_type = read_entry->context_type;
 
 	if (read_entry->bytes_finished == read_entry->total_len) {
 		if (read_entry->context_type == RXR_READ_CONTEXT_TX_ENTRY) {
@@ -455,7 +457,7 @@ void rxr_pkt_handle_rma_read_completion(struct rxr_ep *ep,
 		rxr_read_release_entry(ep, read_entry);
 	}
 
-	if (read_entry->context_type == RXR_READ_CONTEXT_PKT_ENTRY) {
+	if (read_context_type == RXR_READ_CONTEXT_PKT_ENTRY) {
 		assert(context_pkt_entry->addr == FI_ADDR_NOTAVAIL);
 		ep->tx_pending--;
 	} else {
