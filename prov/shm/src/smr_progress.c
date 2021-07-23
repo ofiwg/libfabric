ae0b00f2499fe1040db7ab7a316fcc450ad1c7e3
Peter Gottesman <pgottes@amazon.com>
2021-07-23 20:43:01 +0000

    [v1.11.x] prov/shm: Correct return values in smr_progress.c
    In many functions the return value was -FI_EIO instead of -FI_ETRUNC,
    and in some there was a return value of 0 instead of FI_SUCCESS
    
    Signed-off-by: Peter Gottesman <pgottes@amazon.com>
    (cherry picked from commit 48454a811692a820bd674ff2e40213736055ab44)

--- prov/shm/src/smr_progress.c
+++ prov/shm/src/smr_progress.c
@@ -111,7 +111,7 @@ static int smr_progress_resp_entry(struct smr_ep *ep, struct smr_resp *resp,
 				if (pending->bytes_done != pending->cmd.msg.hdr.size) {
 					FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 						"Incomplete copy from mmapped file\n");
-					*err = -FI_EIO;
+					*err = -FI_ETRUNC;
 				}
 			}
 			munmap(pending->map_ptr, pending->cmd.msg.hdr.size);
@@ -136,7 +136,8 @@ static int smr_progress_resp_entry(struct smr_ep *ep, struct smr_resp *resp,
 		if (pending->bytes_done != pending->cmd.msg.hdr.size) {
 			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 				"Incomplete rma read/fetch buffer copied\n");
-			*err = FI_EIO;
+			*err = (pending->bytes_done < 0)
+				? ((int) pending->bytes_done) : -FI_ETRUNC;
 		}
 		break;
 	default:
@@ -163,7 +164,7 @@ static int smr_progress_resp_entry(struct smr_ep *ep, struct smr_resp *resp,
 	if (peer_smr != ep->region)
 		fastlock_release(&peer_smr->lock);
 
-	return 0;
+	return FI_SUCCESS;
 }
 
 static void smr_progress_resp(struct smr_ep *ep)
@@ -208,9 +209,9 @@ static int smr_progress_inline(struct smr_cmd *cmd, enum fi_hmem_iface iface,
 	if (*total_len != cmd->msg.hdr.size) {
 		FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 			"recv truncated");
-		return -FI_EIO;
+		return (*total_len < 0) ? ((int) *total_len) : -FI_ETRUNC;
 	}
-	return 0;
+	return FI_SUCCESS;
 }
 
 static int smr_progress_inject(struct smr_cmd *cmd, enum fi_hmem_iface iface,
@@ -241,7 +242,7 @@ static int smr_progress_inject(struct smr_cmd *cmd, enum fi_hmem_iface iface,
 	if (*total_len != cmd->msg.hdr.size) {
 		FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 			"recv truncated");
-		return -FI_EIO;
+		return (*total_len < 0) ? ((int) *total_len) : -FI_ETRUNC;
 	}
 
 	return FI_SUCCESS;
@@ -313,7 +314,7 @@ static int smr_mmap_peer_copy(struct smr_ep *ep, struct smr_cmd *cmd,
 		    != *total_len) {
 			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 				"mmap iov copy in error\n");
-			ret = -FI_EIO;
+			ret = -FI_ETRUNC;
 			goto munmap;
 		}
 	} else {
@@ -322,7 +323,7 @@ static int smr_mmap_peer_copy(struct smr_ep *ep, struct smr_cmd *cmd,
 		if (*total_len != cmd->msg.hdr.size) {
 			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 				"mmap iov copy out error\n");
-			ret = -FI_EIO;
+			ret = -FI_ETRUNC;
 			goto munmap;
 		}
 	}
@@ -473,9 +474,9 @@ static int smr_progress_inline_atomic(struct smr_cmd *cmd, struct fi_ioc *ioc,
 	if (*len != cmd->msg.hdr.size) {
 		FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 			"recv truncated");
-		return -FI_EIO;
+		return -FI_ETRUNC;
 	}
-	return 0;
+	return FI_SUCCESS;
 }
 
 static int smr_progress_inject_atomic(struct smr_cmd *cmd, struct fi_ioc *ioc,
@@ -513,7 +514,7 @@ static int smr_progress_inject_atomic(struct smr_cmd *cmd, struct fi_ioc *ioc,
 	if (*len != cmd->msg.hdr.size) {
 		FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 			"recv truncated");
-		err = -FI_EIO;
+		err = -FI_ETRUNC;
 	}
 
 out:
