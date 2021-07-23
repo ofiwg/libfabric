db45179a7cdb470b61bfeaed1b5685a1e5348805
Peter Gottesman <pgottes@amazon.com>
2021-07-23 20:19:36 +0000

    [v1.13.x] prov/shm: Fix smr_progress_ipc error handling
    The previous check would silently mask any copy errors, this resolves
    that and ensures that the correct error is returned if
    ofi_hmem_close_handle also fails.
    
    Fixes #6947
    
    Signed-off-by: Peter Gottesman <pgottes@amazon.com>
    (cherry picked from commit 37c4d3f6b57bbca79187e9c27e9f83db90d6a7cc)

--- prov/shm/src/smr_progress.c
+++ prov/shm/src/smr_progress.c
@@ -463,12 +463,16 @@ static int smr_progress_ipc(struct smr_cmd *cmd, enum fi_hmem_iface iface,
 						  device, iov, iov_count, 0, ptr,
 						  cmd->msg.hdr.size);
 	}
-	if (!ret)
-		*total_len = cmd->msg.hdr.size;
 
 	if (cmd->msg.data.ipc_info.iface == FI_HMEM_ZE)
 		close(ipc_fd);
+
+	/* Truncation error takes precedence over close_handle error */
 	ret = ofi_hmem_close_handle(cmd->msg.data.ipc_info.iface, base);
+
+	if (*total_len != cmd->msg.hdr.size)
+		ret = (*total_len < 0) ? ((int) *total_len) : -FI_ETRUNC;
+
 out:
 	//Status must be set last (signals peer: op done, valid resp entry)
 	resp->status = ret;
