0d9b632d3d47a2784c119ff59c453a152760df8c
Wei Zhang <wzam@amazon.com>
2021-09-19 21:09:55 +0000

    prov/efa: update TX counter for inject
    The function rxr_ep_record_tx_op_submitted() should be called for
    any TX operations that has successfully submitted to update TX
    related counters. However, it is not called for inject operations,
    which are also TX operations.
    
    This patch address the issue by calling rxr_ep_record_tx_op_submitted()
    in function rxr_pkt_entry_inject().
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/efa/src/rxr/rxr_pkt_entry.c
+++ prov/efa/src/rxr/rxr_pkt_entry.c
@@ -374,14 +374,21 @@ ssize_t rxr_pkt_entry_inject(struct rxr_ep *ep,
 			     fi_addr_t addr)
 {
 	struct rdm_peer *peer;
+	ssize_t ret;
 
 	/* currently only EOR packet is injected using shm ep */
 	peer = rxr_ep_get_peer(ep, addr);
 	assert(peer);
 
 	assert(ep->use_shm && peer->is_local);
-	return fi_inject(ep->shm_ep, rxr_pkt_start(pkt_entry), pkt_entry->pkt_size,
+	ret = fi_inject(ep->shm_ep, rxr_pkt_start(pkt_entry), pkt_entry->pkt_size,
 			 peer->shm_fiaddr);
+
+	if (OFI_UNLIKELY(ret))
+		return ret;
+
+	rxr_ep_record_tx_op_submitted(ep, pkt_entry);
+	return 0;
 }
 
 /*
