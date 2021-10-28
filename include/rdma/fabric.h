aee171ed4765632ddbd4b0f330eaf5cfc6a930a1
Sean Hefty <sean.hefty@intel.com>
2021-10-28 08:36:39 -0700

    core: Add new FI_PROTO_RXM_TCP
    When using passthrough transers, we remove the rxm protocol
    headers and use the core providers only.  However, we still
    need to setup the connections, which is managed by rxm.  To
    avoid mixing protocols, introduce a new protocol option for
    layering rxm over tcp with pass through transfers.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- include/rdma/fabric.h
+++ include/rdma/fabric.h
@@ -308,7 +308,7 @@ enum {
 	FI_PROTO_UDP,
 	FI_PROTO_SOCK_TCP,
 	/*  MXM provider is deprecated.
-	 *  We will keep  this value in order to save binary compatibility.
+	 *  We will keep this value in order to save binary compatibility.
 	 */
 	FI_PROTO_MXM,
 	FI_PROTO_IWARP_RDM,
@@ -324,7 +324,8 @@ enum {
 	FI_PROTO_RSTREAM,
 	FI_PROTO_RDMA_CM_IB_XRC,
 	FI_PROTO_EFA,
-	FI_PROTO_PSMX3
+	FI_PROTO_PSMX3,
+	FI_PROTO_RXM_TCP,
 };
 
 enum {
