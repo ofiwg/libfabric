0e2bea8e20f198df732eb2c35c1b29b27f576ed1
zhngaj <zhngaj@amazon.com>
2021-11-17 23:14:43 +0000

    fabtests: Update EFA specific RNR tests
    1. rdm_rnr_read_cq_error.c is used to test if an RNR error CQ entry
       can be read.
    2. rdm_rnr_queue_resend.c is used to check if queuing/re-sending logic
       works correctly for different packet types.
    3. efa_rnr_shared.c stores some routines shared by 1 and 2.
    
    Also update the Makefile accordingly.
    
    Signed-off-by: zhngaj <zhngaj@amazon.com>

--- fabtests/prov/efa/src/rdm_rnr_read_cq_error.c
+++ fabtests/prov/efa/src/rdm_rnr_read_cq_error.c
@@ -26,9 +26,9 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * This program tests the RNR retry counter reset via fi_setopt.
- * When running the test, use `-R` option to specify RNR retry counter.
- * The valid values are 0 - 7 (7 inidicates infinite retry on firmware).
+ * This test resets RNR retry counter to 0 via fi_setopt, and test if
+ * an RNR error CQ entry can be read.
+ *
  */
 
 #include <stdio.h>
@@ -36,18 +36,18 @@
 #include <getopt.h>
 
 #include <shared.h>
-#include <rdma/fi_cm.h>
-#include <rdma/fi_ext.h>
+#include "efa_rnr_shared.h"
 
-static int poll_rnr_cq_error(void)
+
+static int rnr_read_cq_error(void)
 {
 	struct fi_cq_data_entry comp;
 	struct fi_cq_err_entry comp_err;
 	int total_send, expected_rnr_error;
 	int ret, i, cnt, rnr_flag;
 
-	rnr_flag = 0;
 	expected_rnr_error = 1;
+	rnr_flag = 0;
 	/*
 	 * In order for the sender to get RNR error, we need to first consume
 	 * all pre-posted receive buffer (in efa provider, fi->rx_attr->size
@@ -97,68 +97,25 @@ static int poll_rnr_cq_error(void)
 	return (rnr_flag) ? 0 : -FI_EINVAL;
 }
 
-static int run(size_t rnr_retry)
+
+static int run()
 {
 	int ret;
 
-	ret = ft_init();
+	ret = ft_efa_rnr_init_fabric();
 	if (ret) {
-		FT_PRINTERR("ft_init", -ret);
+		FT_PRINTERR("ft_efa_rnr_init_fabric", -ret);
 		return ret;
 	}
 
-	ret = ft_init_oob();
-	if (ret) {
-		FT_PRINTERR("ft_init_oob", -ret);
-		return ret;
-	}
-
-	ret = ft_getinfo(hints, &fi);
-	if (ret) {
-		FT_PRINTERR("ft_getinfo", -ret);
-		return ret;
-	}
-
-	ret = ft_open_fabric_res();
-	if (ret) {
-		FT_PRINTERR("ft_open_fabric_res", -ret);
-		return ret;
-	}
-
-	ret = ft_alloc_active_res(fi);
-	if (ret) {
-		FT_PRINTERR("ft_alloc_active_res", -ret);
-		return ret;
-	}
-
-	fprintf(stdout, "Setting RNR retry count to %zu ...\n", rnr_retry);
-	ret = fi_setopt(&ep->fid, FI_OPT_ENDPOINT, FI_OPT_EFA_RNR_RETRY, &rnr_retry, sizeof(rnr_retry));
-	if (ret) {
-		FT_PRINTERR("fi_setopt", -ret);
-		return ret;
-	}
-	fprintf(stdout, "RNR retry count has been set to %zu.\n", rnr_retry);
-
-	ret = ft_enable_ep(ep);
-	if (ret) {
-		FT_PRINTERR("ft_enable_ep_recv", -ret);
-		return ret;
-	}
-
-	ret = ft_init_av();
-	if (ret) {
-		FT_PRINTERR("ft_init_av", -ret);
-		return ret;
-	}
 	/* client does fi_send and then poll CQ to get error (FI_ENORX) CQ entry */
 	if (opts.dst_addr) {
-		ret = poll_rnr_cq_error();
+		ret = rnr_read_cq_error();
 		if (ret) {
-			FT_PRINTERR("pingpong", -ret);
+			FT_PRINTERR("rnr_poll_cq_error", -ret);
 			return ret;
 		}
 	}
-
 	/*
 	 * To get RNR error on the client side, the server should not close its
 	 * endpoint while the client is still sending.
@@ -171,28 +128,22 @@ static int run(size_t rnr_retry)
 		FT_PRINTERR("ft_reset_oob", -ret);
 		return ret;
 	}
+
 	ret = ft_close_oob();
 	if (ret) {
 		FT_PRINTERR("ft_close_oob", -ret);
 		return ret;
 	}
+	ft_free_res();
 
 	return 0;
 }
 
-static void print_opts_usage(char *name, char *desc)
-{
-	ft_usage(name, desc);
-	/* rdm_ep_rnr_retry test usage */
-	FT_PRINT_OPTS_USAGE("-R <number>", "RNR retry count (valid values: 0-7, default: 1)");
-}
 
 int main(int argc, char **argv)
 {
 	int op, ret;
-	size_t rnr_retry;
 
-	rnr_retry = 1;
 	opts = INIT_OPTS;
 	opts.options |= FT_OPT_SIZE;
 
@@ -200,23 +151,16 @@ int main(int argc, char **argv)
 	if (!hints)
 		return EXIT_FAILURE;
 
-	while ((op = getopt(argc, argv, "R:h" ADDR_OPTS INFO_OPTS CS_OPTS)) != -1) {
+	while ((op = getopt(argc, argv, ADDR_OPTS INFO_OPTS CS_OPTS)) != -1) {
 		switch (op) {
 		default:
 			ft_parse_addr_opts(op, optarg, &opts);
 			ft_parseinfo(op, optarg, hints, &opts);
 			ft_parsecsopts(op, optarg, &opts);
 			break;
-		case 'R':
-			rnr_retry = atoi(optarg);
-			if (rnr_retry > 7) {
-				fprintf(stdout, "RNR retry count invalid, it must be 0-7.\n");
-				return EXIT_FAILURE;
-			}
-			break;
 		case '?':
 		case 'h':
-			print_opts_usage(argv[0], "RDM RNR retry counter reset test");
+			ft_usage(argv[0], "RDM RNR poll error CQ entry test");
 			return EXIT_FAILURE;
 		}
 	}
@@ -226,26 +170,21 @@ int main(int argc, char **argv)
 
 	hints->ep_attr->type = FI_EP_RDM;
 	hints->caps = FI_MSG;
+	hints->mode |= FI_CONTEXT;
+	hints->domain_attr->mr_mode = opts.mr_mode;
+
+	/* FI_RM_DISABLED is required to get RNR error CQ entry */
+	hints->domain_attr->resource_mgmt = FI_RM_DISABLED;
 	/*
 	 * RNR error is generated from EFA device, so disable shm transfer by
 	 * setting FI_REMOTE_COMM and unsetting FI_LOCAL_COMM in order to ensure
 	 * EFA device is being used when running this test on a single node.
 	 */
-	hints->caps |= FI_REMOTE_COMM;
-	hints->caps &= ~FI_LOCAL_COMM;
-	hints->mode |= FI_CONTEXT;
-	hints->domain_attr->mr_mode = opts.mr_mode;
-	/*
-	 * FI_RM_DISABLED is required to be set in order for the RNR error CQ entry
-	 * to be written to applications. Otherwise, the packet (failed with RNR error)
-	 * will be queued and resent.
-	 */
-	hints->domain_attr->resource_mgmt = FI_RM_DISABLED;
+	ft_efa_rnr_disable_hints_shm();
 
-	ret = run(rnr_retry);
+	ret = run();
 	if (ret)
 		FT_PRINTERR("run", -ret);
 
-	ft_free_res();
 	return ft_exit_code(ret);
 }
