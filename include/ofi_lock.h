ab1e48c00e7616baf251596c4e3daa9d5adb7a0f
Sean Hefty <sean.hefty@intel.com>
2021-11-29 14:56:55 -0800

    core: Define ofi_mutex
    Separate the spinlock from the mutex lock abstraction.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- include/ofi_lock.h
+++ include/ofi_lock.h
@@ -186,6 +186,130 @@ static inline void ofi_fastlock_release(fastlock_t *lock)
 	fastlock_release(lock);
 }
 
+
+#define ofi_mutex_t_ pthread_mutex_t
+#define ofi_mutex_init_(lock) pthread_mutex_init(lock, NULL)
+#define ofi_mutex_destroy_(lock) pthread_mutex_destroy(lock)
+#define ofi_mutex_lock_(lock) pthread_mutex_lock(lock)
+#define ofi_mutex_trylock_(lock) pthread_mutex_trylock(lock)
+#define ofi_mutex_unlock_(lock) pthread_mutex_unlock(lock)
+
+#if ENABLE_DEBUG
+
+typedef struct {
+	ofi_mutex_t_ impl;
+	int is_initialized;
+	int in_use;
+} ofi_mutex_t;
+
+static inline int ofi_mutex_init(ofi_mutex_t *lock)
+{
+	int ret;
+
+	ret = ofi_mutex_init_(&lock->impl);
+	lock->is_initialized = !ret;
+	lock->in_use = 0;
+
+	return ret;
+}
+
+static inline void ofi_mutex_destroy(ofi_mutex_t *lock)
+{
+	int ret;
+
+	assert(lock->is_initialized);
+	lock->is_initialized = 0;
+	ret = ofi_mutex_destroy_(&lock->impl);
+	assert(!ret);
+}
+
+static inline void ofi_mutex_lock(ofi_mutex_t *lock)
+{
+	int ret;
+
+	assert(lock->is_initialized);
+	ret = ofi_mutex_lock_(&lock->impl);
+	assert(!ret);
+	lock->in_use++;
+}
+
+static inline int ofi_mutex_trylock(ofi_mutex_t *lock)
+{
+	int ret;
+
+	assert(lock->is_initialized);
+	ret = ofi_mutex_trylock_(&lock->impl);
+	if (!ret)
+		lock->in_use++;
+
+	return ret;
+}
+
+static inline void ofi_mutex_unlock(ofi_mutex_t *lock)
+{
+	int ret;
+
+	assert(lock->in_use);
+	assert(lock->is_initialized);
+	lock->in_use--;
+	ret = ofi_mutex_unlock_(&lock->impl);
+	assert(!ret);
+}
+
+static inline int ofi_mutex_held(ofi_mutex_t *lock)
+{
+	return lock->in_use;
+}
+
+static inline void ofi_mutex_lock_noop(ofi_mutex_t *lock)
+{
+	/* These non-op routines must be used only by single-threaded code*/
+	assert(!lock->in_use);
+	lock->in_use = 1;
+}
+
+static inline void ofi_mutex_unlock_noop(ofi_mutex_t *lock)
+{
+	assert(lock->in_use);
+	lock->in_use = 0;
+}
+
+#else /* !ENABLE_DEBUG */
+
+#  define ofi_mutex_t ofi_mutex_t_
+#  define ofi_mutex_init(lock) ofi_mutex_init_(lock)
+#  define ofi_mutex_destroy(lock) ofi_mutex_destroy_(lock)
+#  define ofi_mutex_lock(lock) ofi_mutex_lock_(lock)
+#  define ofi_mutex_trylock(lock) ofi_mutex_trylock_(lock)
+#  define ofi_mutex_unlock(lock) ofi_mutex_unlock_(lock)
+#  define ofi_mutex_held(lock) true
+
+static inline void ofi_mutex_lock_noop(ofi_mutex_t *lock)
+{
+	(void) lock;
+}
+
+static inline void ofi_mutex_unlock_noop(ofi_mutex_t *lock)
+{
+	(void) lock;
+}
+
+#endif
+
+typedef void(*ofi_mutex_lock_t)(ofi_mutex_t *lock);
+typedef void(*ofi_mutex_unlock_t)(ofi_mutex_t *lock);
+
+static inline void ofi_mutex_lock_op(ofi_mutex_t *lock)
+{
+	ofi_mutex_lock(lock);
+}
+
+static inline void ofi_mutex_unlock_op(ofi_mutex_t *lock)
+{
+	ofi_mutex_unlock(lock);
+}
+
+
 #ifdef __cplusplus
 }
 #endif
