b09935c7b2208231b57eb415a489a7d51d19b3c6
Wei Zhang <wzam@amazon.com>
2022-01-01 07:26:22 +0000

    fabtests/runfabtests.py: add option --html
    this patch added the option --html to runfabtests.py, which
    can be used to generate a html report
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- fabtests/scripts/runfabtests.py
+++ fabtests/scripts/runfabtests.py
@@ -198,6 +198,11 @@ def fabtests_args_to_pytest_args(fabtests_args):
         pytest_args.append("-k")
         pytest_args.append(fabtests_args.expression)
 
+    if fabtests_args.html:
+        pytest_args.append("--html")
+        pytest_args.append(os.path.abspath(fabtests_args.html))
+        pytest_args.append("--self-contained-html")
+
     return pytest_args
 
 def get_pytest_root_dir():
@@ -267,6 +272,8 @@ def main():
                         help="out-of-band address exchange over the default port")
     parser.add_argument("--expression", dest="expression", type=str,
                         help="only run tests which match the given substring expression.")
+    parser.add_argument("--html", dest="html", type=str,
+                        help="path to generated html report")
 
     fabtests_args = parser.parse_args()
     pytest_args = fabtests_args_to_pytest_args(fabtests_args)
