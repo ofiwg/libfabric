4205bc38985cef1ef81c8849b54e545d384a0193
Sean Hefty <sean.hefty@intel.com>
2022-05-03 14:32:00 -0700

    prov/hook_hmem: Update provider version to align with other providers
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- prov/hook/hook_hmem/src/hook_hmem.c
+++ prov/hook/hook_hmem/src/hook_hmem.c
@@ -1734,7 +1734,7 @@ static int hook_hmem_fabric(struct fi_fabric_attr *attr,
 
 struct hook_prov_ctx hook_hmem_prov_ctx = {
 	.prov = {
-		.version = FI_VERSION(1,0),
+		.version = OFI_VERSION_DEF_PROV,
 		/* We're a pass-through provider
 		   so the fi_version is always the latest */
 		.fi_version = FI_VERSION(FI_MAJOR_VERSION, FI_MINOR_VERSION),
