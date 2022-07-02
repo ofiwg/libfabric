2fbc67f6ab3f71bdea6b3a8ef73bce8cc79b3208
Wei Zhang <wzam@amazon.com>
2022-07-02 03:11:12 +0000

    fabtests/efa: add more message range test
    This patch added two message size ranges :
    
    	begin=1k, interval=1k, end=1m
            begin=17000, intervel=4, end=18000
    
    to the list of message size ranges for test.
    
    The first range covers typical message size of application.
    The second range corresponds to 2 MTU size.
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- fabtests/pytest/efa/conftest.py
+++ fabtests/pytest/efa/conftest.py
@@ -5,7 +5,10 @@ import pytest
 def memory_type(request):
     return request.param
 
-@pytest.fixture(scope="module", params=["r:8000,4,9000", 
-                                         "r:0,4,64", "r:4048,4,4148"])
+@pytest.fixture(scope="module", params=["r:0,4,64",
+                                        "r:4048,4,4148",
+                                        "r:8000,4,9000",
+                                        "r:17000,4,18000",
+                                        "r:0,1024,1048576"])
 def message_size(request):
     return request.param
