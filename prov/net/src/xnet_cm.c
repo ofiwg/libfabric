d75bf3d788b266618edf92c12788416bb7f6f0f1
Sean Hefty <sean.hefty@intel.com>
2022-07-02 18:39:01 -0700

    prov/net: Add cntr to track when progress is made on an ep
    An ep is active if it successfully sends or receives data on the
    underlying socket.  Add counter to track the number of times
    that progress is made on a socket.
    
    This will be used to determine which ep's are truly active to
    optimize progress.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- prov/net/src/xnet_cm.c
+++ prov/net/src/xnet_cm.c
@@ -128,6 +128,7 @@ int xnet_send_cm_msg(struct xnet_ep *ep)
 	if ((size_t) ret != len)
 		return ofi_sockerr() ? -ofi_sockerr() : -FI_EIO;
 
+	ep->progress_cnt++;
 	return FI_SUCCESS;
 }
 
@@ -153,6 +154,7 @@ void xnet_req_done(struct xnet_ep *ep)
 		goto disable;
 	}
 
+	ep->progress_cnt++;
 	ep->hdr_bswap = (ep->cm_msg->hdr.conn_data == 1) ?
 			xnet_hdr_none : xnet_hdr_bswap;
 
