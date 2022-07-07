27140ad7fb66718f494a8c63e613a3d6d422ca81
Michael Margolin <mrgolin@amazon.com>
2022-07-07 08:32:19 +0000

    fabtests: Add cuda_memory marker in pytest
    Add pytest marker to allow tests filtering by cuda memory usage.
    
    Signed-off-by: Michael Margolin <mrgolin@amazon.com>

--- fabtests/pytest/efa/conftest.py
+++ fabtests/pytest/efa/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
-@pytest.fixture(scope="module", params=["host_to_host", "host_to_cuda",
-                                        "cuda_to_host", "cuda_to_cuda"])
+@pytest.fixture(scope="module", params=["host_to_host",
+                                        pytest.param("host_to_cuda", marks=pytest.mark.cuda_memory),
+                                        pytest.param("cuda_to_host", marks=pytest.mark.cuda_memory),
+                                        pytest.param("cuda_to_cuda", marks=pytest.mark.cuda_memory)])
 def memory_type(request):
     return request.param
 
