458ad9bed9d0e481e9130248c63520fa54cc95b4
Sean Hefty <sean.hefty@intel.com>
2022-09-01 16:10:51 -0700

    v1.16.0rc1
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- include/windows/config.h
+++ include/windows/config.h
@@ -174,7 +174,7 @@
 #define PACKAGE_TARNAME PACKAGE
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.16.0a1"
+#define PACKAGE_VERSION "1.16.0rc1"
 
 /* Define to the full name and version of this package. */
 #define PACKAGE_STRING PACKAGE_NAME " " PACKAGE_VERSION
