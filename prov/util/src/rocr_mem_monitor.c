ofiwg/libfabric

commit 32ead6680c8feb6ee933cf28482a6f3b6bc0a6b8

core and util/prov: introduce cuda ipc monitor
Change ofi_mem_monitor->valid API to

valid(struct ofi_mem_monitor *notifier,
const struct ofi_mr_info *info,
struct ofi_mr_entry *entry),
so it can compare the ipc_handle between info->ipc_handle
and entry->info.ipc_handle

Introduce cuda_ipc_monitor with the updated valid API.

Signed-off-by: Shi Jin <sjina@amazon.com>

prov/util/src/rocr_mem_monitor.c
@@ -57,8 +57,9 @@ static int rocr_mm_subscribe(struct ofi_mem_monitor *monitor, const void *addr,
 static void rocr_mm_unsubscribe(struct ofi_mem_monitor *monitor,
 				const void *addr, size_t len,
 				union ofi_mr_hmem_info *hmem_info);
-static bool rocr_mm_valid(struct ofi_mem_monitor *monitor, const void *addr,
-			  size_t len, union ofi_mr_hmem_info *hmem_info);
+static bool rocr_mm_valid(struct ofi_mem_monitor *monitor,
+			   const struct ofi_mr_info *info,
+			   struct ofi_mr_entry *entry));
 
 static struct rocr_mm rocr_mm = {
 	.mm = {
@@ -353,8 +354,9 @@ static int rocr_mm_subscribe(struct ofi_mem_monitor *monitor, const void *addr,
 	return ret;
 }
 
-static bool rocr_mm_valid(struct ofi_mem_monitor *monitor, const void *addr,
-			  size_t len, union ofi_mr_hmem_info *hmem_info)
+static bool rocr_mm_valid(struct ofi_mem_monitor *monitor,
+			  const struct ofi_mr_info *info,
+			  struct ofi_mr_entry *entry)
 {
 	/* no-op */
 	return true;
@@ -383,8 +385,9 @@ static void rocr_mm_unsubscribe(struct ofi_mem_monitor *monitor,
 {
 }
 
-static bool rocr_mm_valid(struct ofi_mem_monitor *monitor, const void *addr,
-			  size_t len, union ofi_mr_hmem_info *hmem_info)
+static bool rocr_mm_valid(struct ofi_mem_monitor *monitor,
+			  const struct ofi_mr_info *info,
+			  struct ofi_mr_entry *entry)
 {
 	return false;
 }

include/ofi_mr.h
@@ -129,6 +129,15 @@ union ofi_mr_hmem_info {
 	uint64_t ze_id;
 };
 
+struct ofi_mr_entry {
+	struct ofi_mr_info		info;
+	struct ofi_rbnode		*node;
+	int				use_cnt;
+	struct dlist_entry		list_entry;
+	union ofi_mr_hmem_info		hmem_info;
+	uint8_t				data[];
+};
+
 enum fi_mm_state {
 	FI_MM_STATE_UNSPEC = 0,
 	FI_MM_STATE_IDLE,
@@ -159,8 +168,8 @@ struct ofi_mem_monitor {
 	 * pages behind a given virtual address have changed), the buffer needs
 	 * to be re-registered.
 	 */
-	bool (*valid)(struct ofi_mem_monitor *notifier, const void *addr,
-		      size_t len, union ofi_mr_hmem_info *hmem_info);
+	bool (*valid)(struct ofi_mem_monitor *notifier,
+		      const struct ofi_mr_info *info, struct ofi_mr_entry *entry);
 };
 
 void ofi_monitor_init(struct ofi_mem_monitor *monitor);
@@ -183,6 +192,14 @@ void ofi_monitor_unsubscribe(struct ofi_mem_monitor *monitor,
 			     const void *addr, size_t len,
 			     union ofi_mr_hmem_info *hmem_info);
 
+int ofi_monitor_start_no_op(struct ofi_mem_monitor *monitor);
+void ofi_monitor_stop_no_op(struct ofi_mem_monitor *monitor);
+int ofi_monitor_subscribe_no_op(struct ofi_mem_monitor *notifier,
+				 const void *addr, size_t len,
+				 union ofi_mr_hmem_info *hmem_info);
+void ofi_monitor_unsubscribe_no_op(struct ofi_mem_monitor *notifier,
+				    const void *addr, size_t len,
+				    union ofi_mr_hmem_info *hmem_info);
 extern struct ofi_mem_monitor *default_monitor;
 extern struct ofi_mem_monitor *default_cuda_monitor;
 extern struct ofi_mem_monitor *default_rocr_monitor;
@@ -210,6 +227,7 @@ struct ofi_memhooks {
 extern struct ofi_mem_monitor *memhooks_monitor;
 
 extern struct ofi_mem_monitor *cuda_monitor;
+extern struct ofi_mem_monitor *cuda_ipc_monitor;
 extern struct ofi_mem_monitor *rocr_monitor;
 extern struct ofi_mem_monitor *ze_monitor;
 extern struct ofi_mem_monitor *import_monitor;
@@ -286,15 +304,6 @@ struct ofi_mr_cache_params {
 };
 
 extern struct ofi_mr_cache_params	cache_params;
-
-struct ofi_mr_entry {
-	struct ofi_mr_info		info;
-	struct ofi_rbnode		*node;
-	int				use_cnt;
-	struct dlist_entry		list_entry;
-	union ofi_mr_hmem_info		hmem_info;
-	uint8_t				data[];
-};
 
 #define OFI_HMEM_MAX 6
 

prov/util/src/cuda_ipc_monitor.c
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2021 Intel Corporation
+ * (C) Copyright (c) 2022 Amazon.com, Inc. or its affiliates.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -32,86 +32,37 @@
 
 #include "ofi_mr.h"
 
-#if HAVE_ZE
+#if HAVE_CUDA
 
 #include "ofi_hmem.h"
 
-static int ze_mm_subscribe(struct ofi_mem_monitor *monitor, const void *addr,
-			   size_t len, union ofi_mr_hmem_info *hmem_info)
-{
-	return ze_hmem_get_id(addr, &hmem_info->ze_id);
-}
-
-static void ze_mm_unsubscribe(struct ofi_mem_monitor *monitor,
-			      const void *addr, size_t len,
-			      union ofi_mr_hmem_info *hmem_info)
-{
-	/* no-op */
-}
-
-static bool ze_mm_valid(struct ofi_mem_monitor *monitor,
+static bool cuda_ipc_monitor_valid(struct ofi_mem_monitor *monitor,
 			const struct ofi_mr_info *info,
 			struct ofi_mr_entry *entry)
 {
-	uint64_t id;
-	int ret;
-
-	ret = ze_hmem_get_id(entry->info.iov.iov_base, &id);
-	if (ret)
-		return false;
-
-
-	return id == entry->hmem_info.ze_id;
-}
-
-static int ze_monitor_start(struct ofi_mem_monitor *monitor)
-{
-	/* no-op */
-	return FI_SUCCESS;
+	return (memcmp((void **)&info->ipc_handle,
+		(void **)&entry->info.ipc_handle,
+		sizeof(cudaIpcMemHandle_t)) == 0);
 }
 
 #else
 
-static int ze_mm_subscribe(struct ofi_mem_monitor *monitor, const void *addr,
-			   size_t len, union ofi_mr_hmem_info *hmem_info)
-{
-	return -FI_ENOSYS;
-}
-
-static void ze_mm_unsubscribe(struct ofi_mem_monitor *monitor,
-			      const void *addr, size_t len,
-			      union ofi_mr_hmem_info *hmem_info)
-{
-}
-
-static bool ze_mm_valid(struct ofi_mem_monitor *monitor,
+static bool cuda_ipc_monitor_valid(struct ofi_mem_monitor *monitor,
 			const struct ofi_mr_info *info,
 			struct ofi_mr_entry *entry)
 {
 	return false;
 }
 
-static int ze_monitor_start(struct ofi_mem_monitor *monitor)
-{
-	return -FI_ENOSYS;
-}
-
-#endif /* HAVE_ZE */
-
-void ze_monitor_stop(struct ofi_mem_monitor *monitor)
-{
-	/* no-op */
-}
-
-static struct ofi_mem_monitor ze_mm = {
-	.iface = FI_HMEM_ZE,
+#endif /* HAVE_CUDA */
+static struct ofi_mem_monitor cuda_ipc_monitor_ = {
 	.init = ofi_monitor_init,
 	.cleanup = ofi_monitor_cleanup,
-	.start = ze_monitor_start,
-	.stop = ze_monitor_stop,
-	.subscribe = ze_mm_subscribe,
-	.unsubscribe = ze_mm_unsubscribe,
-	.valid = ze_mm_valid,
+	.start = ofi_monitor_start_no_op,
+	.stop = ofi_monitor_stop_no_op,
+	.subscribe = ofi_monitor_subscribe_no_op,
+	.unsubscribe = ofi_monitor_unsubscribe_no_op,
+	.valid = cuda_ipc_monitor_valid,
 };
 
-struct ofi_mem_monitor *ze_monitor = &ze_mm;
+struct ofi_mem_monitor *cuda_ipc_monitor = &cuda_ipc_monitor_;

prov/util/src/cuda_mem_monitor.c
@@ -67,8 +67,8 @@ static void cuda_mm_unsubscribe(struct ofi_mem_monitor *monitor,
 }
 
 static bool cuda_mm_valid(struct ofi_mem_monitor *monitor,
-			  const void *addr, size_t len,
-			  union ofi_mr_hmem_info *hmem_info)
+			  const struct ofi_mr_info *info,
+			  struct ofi_mr_entry *entry)
 {
 	uint64_t id;
 	CUresult ret;
@@ -78,20 +78,21 @@ static bool cuda_mm_valid(struct ofi_mem_monitor *monitor,
 	 * buffer ID is associated with this mapping.
 	 */
 	ret = ofi_cuPointerGetAttribute(&id, CU_POINTER_ATTRIBUTE_BUFFER_ID,
-					(CUdeviceptr)addr);
-	if (ret == CUDA_SUCCESS && hmem_info->cuda_id == id) {
+					(CUdeviceptr)entry->info.iov.iov_base);
+	if (ret == CUDA_SUCCESS && entry->hmem_info.cuda_id == id) {
 		FI_DBG(&core_prov, FI_LOG_MR,
 		       "CUDA buffer ID %lu still valid for buffer %p\n",
-		       hmem_info->cuda_id, addr);
+		       entry->hmem_info.cuda_id, entry->info.iov.iov_base);
 		return true;
-	} else if (ret == CUDA_SUCCESS && hmem_info->cuda_id != id) {
+	} else if (ret == CUDA_SUCCESS && entry->hmem_info.cuda_id != id) {
 		FI_DBG(&core_prov, FI_LOG_MR,
 		       "CUDA buffer ID %lu invalid for buffer %p\n",
-		       hmem_info->cuda_id, addr);
+		       entry->hmem_info.cuda_id, entry->info.iov.iov_base);
 	} else {
 		FI_WARN(&core_prov, FI_LOG_MR,
 			"Failed to get CUDA buffer ID for buffer %p len %lu\n"
-			"cuPointerGetAttribute() failed: %s:%s\n", addr, len,
+			"cuPointerGetAttribute() failed: %s:%s\n",
+			entry->info.iov.iov_base, entry->info.iov.iov_len,
 			ofi_cudaGetErrorName(ret), ofi_cudaGetErrorString(ret));
 	}
 
@@ -119,8 +120,8 @@ static void cuda_mm_unsubscribe(struct ofi_mem_monitor *monitor,
 }
 
 static bool cuda_mm_valid(struct ofi_mem_monitor *monitor,
-			  const void *addr, size_t len,
-			  union ofi_mr_hmem_info *hmem_info)
+			  const struct ofi_mr_info *info,
+			  struct ofi_mr_entry *entry)
 {
 	return false;
 }

prov/util/src/util_mem_hooks.c
@@ -578,8 +578,8 @@ static void ofi_memhooks_unsubscribe(struct ofi_mem_monitor *monitor,
 }
 
 static bool ofi_memhooks_valid(struct ofi_mem_monitor *monitor,
-			       const void *addr, size_t len,
-			       union ofi_mr_hmem_info *hmem_info)
+			       const struct ofi_mr_info *info,
+			       struct ofi_mr_entry *entry)
 {
 	/* no-op */
 	return true;

prov/util/src/util_mem_monitor.c
@@ -171,6 +171,7 @@ void ofi_monitors_init(void)
 	uffd_monitor->init(uffd_monitor);
 	memhooks_monitor->init(memhooks_monitor);
 	cuda_monitor->init(cuda_monitor);
+	cuda_ipc_monitor->init(cuda_ipc_monitor);
 	rocr_monitor->init(rocr_monitor);
 	ze_monitor->init(ze_monitor);
 	import_monitor->init(import_monitor);
@@ -460,6 +461,28 @@ void ofi_monitor_unsubscribe(struct ofi_mem_monitor *monitor,
 	monitor->unsubscribe(monitor, addr, len, hmem_info);
 }
 
+int ofi_monitor_start_no_op(struct ofi_mem_monitor *monitor)
+{
+	return FI_SUCCESS;
+}
+
+void ofi_monitor_stop_no_op(struct ofi_mem_monitor *monitor)
+{
+}
+
+int ofi_monitor_subscribe_no_op(struct ofi_mem_monitor *notifier,
+			 const void *addr, size_t len,
+			 union ofi_mr_hmem_info *hmem_info)
+{
+	return FI_SUCCESS;
+}
+
+void ofi_monitor_unsubscribe_no_op(struct ofi_mem_monitor *notifier,
+			    const void *addr, size_t len,
+			    union ofi_mr_hmem_info *hmem_info)
+{
+}
+
 #if HAVE_UFFD_MONITOR
 
 #include <poll.h>
@@ -596,8 +619,9 @@ static void ofi_uffd_unsubscribe(struct ofi_mem_monitor *monitor,
 	}
 }
 
-static bool ofi_uffd_valid(struct ofi_mem_monitor *monitor, const void *addr,
-			   size_t len, union ofi_mr_hmem_info *hmem_info)
+static bool ofi_uffd_valid(struct ofi_mem_monitor *monitor,
+			   const struct ofi_mr_info *info,
+			   struct ofi_mr_entry *entry)
 {
 	/* no-op */
 	return true;
@@ -685,8 +709,8 @@ static void ofi_import_monitor_unsubscribe(struct ofi_mem_monitor *notifier,
 					   const void *addr, size_t len,
 					   union ofi_mr_hmem_info *hmem_info);
 static bool ofi_import_monitor_valid(struct ofi_mem_monitor *notifier,
-				     const void *addr, size_t len,
-				     union ofi_mr_hmem_info *hmem_info);
+				     const struct ofi_mr_info *info,
+				     struct ofi_mr_entry *entry);
 
 struct ofi_import_monitor {
 	struct ofi_mem_monitor monitor;
@@ -748,11 +772,13 @@ static void ofi_import_monitor_unsubscribe(struct ofi_mem_monitor *notifier,
 }
 
 static bool ofi_import_monitor_valid(struct ofi_mem_monitor *notifier,
-				     const void *addr, size_t len,
-				     union ofi_mr_hmem_info *hmem_info)
+				     const struct ofi_mr_info *info,
+				     struct ofi_mr_entry *entry)
 {
 	assert(impmon.impfid);
-	return impmon.impfid->export_ops->valid(impmon.impfid, addr, len);
+	return impmon.impfid->export_ops->valid(impmon.impfid,
+						entry->info.iov.iov_base,
+						entry->info.iov.iov_len);
 }
 
 static void ofi_import_monitor_notify(struct fid_mem_monitor *monitor,

prov/util/src/util_mr_cache.c
@@ -348,10 +348,7 @@ int ofi_mr_cache_search(struct ofi_mr_cache *cache, const struct ofi_mr_info *in
 
 		if (*entry &&
 		    ofi_iov_within(&info->iov, &(*entry)->info.iov) &&
-		    monitor->valid(monitor,
-				   (const void *)(*entry)->info.iov.iov_base,
-				   (*entry)->info.iov.iov_len,
-				   &(*entry)->hmem_info))
+		    monitor->valid(monitor, info, *entry))
 			goto hit;
 
 		/* Purge regions that overlap with new region */

commit b73c266cc520e57ebb15267efd957908a084cee6

util/src: fix syntax error
Fix syntax error introduced in:
32ead6680c8feb6ee933cf28482a6f3b6bc0a6b8 core and util/prov: introduce cuda ipc monitor

Signed-off-by: Amir Shehata <shehataa@ornl.gov>

prov/util/src/rocr_mem_monitor.c
@@ -59,7 +59,7 @@ static void rocr_mm_unsubscribe(struct ofi_mem_monitor *monitor,
 				union ofi_mr_hmem_info *hmem_info);
 static bool rocr_mm_valid(struct ofi_mem_monitor *monitor,
 			   const struct ofi_mr_info *info,
-			   struct ofi_mr_entry *entry));
+			   struct ofi_mr_entry *entry);
 
 static struct rocr_mm rocr_mm = {
 	.mm = {