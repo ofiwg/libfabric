bbd0ccfbd3f48fa93de2e675db1ae53a5e909673
Charles King <charles.king@cornelisnetworks.com>
2022-10-05 12:28:13 -0400

    prov/opx: Fix for hang - unable to match inbound packets with receive context->src_addr (DAOS CART tests)
    When an EP specified a source address (context->src_addr) with a receive operation, OPX placed all inbound
    packets not matching the source address onto the unexpected queue which eventually caused the EP to hang.
    This occurred, because the OPX Persistent Addressing support required specifically for DAOS was not handling
    the context->src_addr matching with inbound packets.
    
    Early in OPX DAOS support development, a temporary patch to work-around the issue was implemented, in order
    to proceed with all Mercury and CART testing.   This patch replaces the work-around patch with a comprehensive
    fix to address the issue.
    
    Signed-off-by: Charles King <charles.king@cornelisnetworks.com>

--- prov/opx/include/rdma/opx/fi_opx_hfi1_packet.h
+++ prov/opx/include/rdma/opx/fi_opx_hfi1_packet.h
@@ -733,8 +733,14 @@ union fi_opx_hfi1_packet_payload {
 
 
 struct fi_opx_hfi1_ue_packet {
-	struct fi_opx_hfi1_ue_packet *	next;
+	struct fi_opx_hfi1_ue_packet *		next;
 	union fi_opx_hfi1_packet_hdr		hdr;
+	struct {
+		uint32_t rank;
+		uint32_t rank_inst;
+		int rank_pid;
+	} daos_info;
+	uint32_t pad[13];
 	union fi_opx_hfi1_packet_payload	payload;
 } __attribute__((__packed__)) __attribute__((aligned(64)));
 
