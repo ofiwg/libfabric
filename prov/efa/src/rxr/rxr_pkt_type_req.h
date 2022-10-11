0ceb2e7a507088b1df0b27ce78c3009dd3b48c5f
Shi Jin <sjina@amazon.com>
2022-10-11 22:18:32 +0000

    prov/efa: Introduce rxr_pkt_hdr_rma_iov_count function
    This function returns the rma_iov_count in the header,
    if it is part of the pkt header. Otherwise, it returns 0.
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- prov/efa/src/rxr/rxr_pkt_type_req.h
+++ prov/efa/src/rxr/rxr_pkt_type_req.h
@@ -56,6 +56,8 @@ size_t rxr_pkt_req_data_size(struct rxr_pkt_entry *pkt_entry);
 
 size_t rxr_pkt_req_header_size(int pkt_type, uint16_t flags, size_t rma_iov_count);
 
+uint32_t rxr_pkt_hdr_rma_iov_count(struct rxr_pkt_entry *pkt_entry);
+
 size_t rxr_pkt_req_max_header_size(int pkt_type);
 
 size_t rxr_pkt_max_header_size(void);
