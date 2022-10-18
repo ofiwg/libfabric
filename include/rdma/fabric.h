2466c0d87262b49de403ede88caf55a84a20ece8
Sean Hefty <sean.hefty@intel.com>
2022-10-17 17:02:21 -0700

    core/peer: Use a single FI_PEER flag
    Rather than defining a peer flag for every object type that might
    be shared, using a common flag.  This reduces the number of flags
    that we have to consume.
    
    We don't yet have a capabililty bit to indicate if a provider
    supports peer objects.  But it's expected that any such bit would
    need to indicates that a full set of peer capabilities are supported
    for the provider to be usable as a peer.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- include/rdma/fabric.h
+++ include/rdma/fabric.h
@@ -167,11 +167,8 @@ typedef struct fid *fid_t;
 
 #define FI_PEER_TRANSFER	(1ULL << 36)
 #define FI_PEER_DOMAIN		(1ULL << 38)
-#define FI_PEER_AV		(1ULL << 39)
-#define FI_PEER_EQ		(1ULL << 40)
 #define FI_AV_USER_ID		(1ULL << 41)
-#define FI_PEER_SRX		(1ULL << 42)
-#define FI_PEER_CQ		(1ULL << 43)
+#define FI_PEER			(1ULL << 43)
 #define FI_XPU_TRIGGER		(1ULL << 44)
 #define FI_HMEM_HOST_ALLOC	(1ULL << 45)
 #define FI_HMEM_DEVICE_ONLY	(1ULL << 46)
