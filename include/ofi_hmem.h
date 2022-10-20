1140f7e6a2042472873d6d7566f60d9e078fd3a8
Jianxin Xiong <jianxin.xiong@intel.com>
2022-10-20 10:58:37 -0700

    [v1.14.x] hmem/ze: Add support for host register/unregister
    The functionality is provided as ZE extensions so the initialization is
    slightly different from the rest.
    
    Also fixed some function names for consistency.
    
    Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>
    (cherry picked from commit 14e59da24fa0aff6060ee8e965d76542ec70ad53)

--- include/ofi_hmem.h
+++ include/ofi_hmem.h
@@ -148,7 +148,7 @@ int cuda_gdrcopy_dev_unregister(uint64_t handle);
 int ze_hmem_copy(uint64_t device, void *dst, const void *src, size_t size);
 int ze_hmem_init(void);
 int ze_hmem_cleanup(void);
-bool ze_is_addr_valid(const void *addr, uint64_t *device, uint64_t *flags);
+bool ze_hmem_is_addr_valid(const void *addr, uint64_t *device, uint64_t *flags);
 int ze_hmem_get_handle(void *dev_buf, void **handle);
 int ze_hmem_open_handle(void **handle, uint64_t device, void **ipc_ptr);
 int ze_hmem_get_shared_handle(int dev_fd, void *dev_buf, int *ze_fd,
@@ -160,6 +160,8 @@ bool ze_hmem_p2p_enabled(void);
 int ze_hmem_get_base_addr(const void *ptr, void **base, size_t *size);
 int ze_hmem_get_id(const void *ptr, uint64_t *id);
 int *ze_hmem_get_dev_fds(int *nfds);
+int ze_hmem_host_register(void *ptr, size_t size);
+int ze_hmem_host_unregister(void *ptr);
 
 static inline int ofi_memcpy(uint64_t device, void *dest, const void *src,
 			     size_t size)
@@ -193,7 +195,7 @@ static inline int ofi_hmem_no_close_handle(void *ipc_ptr)
 	return -FI_ENOSYS;
 }
 
-static inline int ofi_hmem_register_noop(void *ptr, size_t size)
+static inline int ofi_hmem_host_register_noop(void *ptr, size_t size)
 {
 	return FI_SUCCESS;
 }
