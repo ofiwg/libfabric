981116671aa957267a1ffc032079d5856820d4aa
Sean Hefty <sean.hefty@intel.com>
2022-11-30 15:48:44 -0800

    prov/net: Cleanup saved rx entry when disabling ep
    If we have a saved rx entry, remove the ep from the progress'
    saved list.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- prov/net/src/xnet_progress.c
+++ prov/net/src/xnet_progress.c
@@ -63,7 +63,7 @@ static struct ofi_sockapi xnet_sockapi_socket =
 };
 
 
-static void xnet_clear_saved_rx(struct xnet_ep *ep)
+void xnet_clear_saved_rx(struct xnet_ep *ep)
 {
 	assert(xnet_progress_locked(xnet_ep2_progress(ep)));
 	assert(!dlist_empty(&xnet_ep2_progress(ep)->saved_tag_list));
