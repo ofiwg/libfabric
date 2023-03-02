fd704042a67c66a1cd56fe1f04d9d7c550e361b8
Wei Zhang <wzam@amazon.com>
2023-03-02 04:41:37 +0000

    fabtests/pytest/default: enable fi_cq_data -o writedata test
    Add "fi_cq_data -o writedata" to default test list
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- fabtests/pytest/default/test_cq.py
+++ fabtests/pytest/default/test_cq.py
@@ -7,8 +7,9 @@ def test_cq(cmdline_args):
     test.run()
 
 @pytest.mark.functional
+@pytest.mark.parametrize("operation_type", ["senddata", "writedata"])
 @pytest.mark.parametrize("endpoint_type", ["msg", "rdm", "dgram"])
-def test_cq_data(cmdline_args, endpoint_type):
+def test_cq_data(cmdline_args, operation_type, endpoint_type):
     from common import ClientServerTest
-    test = ClientServerTest(cmdline_args, "fi_cq_data -e " + endpoint_type)
+    test = ClientServerTest(cmdline_args, "fi_cq_data -e " + endpoint_type + " -o " + operation_type)
     test.run()
