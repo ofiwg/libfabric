742961c8f2526da387bee66d2cfc51f60d25810a
Wei Zhang <wzam@amazon.com>
2023-03-02 04:41:03 +0000

    fabtests/pytest/efa: enable fi_cq_data test for EFA provider
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- fabtests/pytest/efa/test_from_default.py
+++ fabtests/pytest/efa/test_from_default.py
@@ -1,5 +1,4 @@
 from default.test_cntr import test_cntr
-from default.test_cq import test_cq
 from default.test_dom import test_dom
 from default.test_eq import test_eq
 from default.test_recv_cancel import test_recv_cancel
