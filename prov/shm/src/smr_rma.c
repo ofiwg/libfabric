aea14542daccb7cda60a798acddef9ff28628a3c
Wei Zhang <wzam@amazon.com>
2023-03-17 01:35:29 +0000

    prov/efa: do not write err completion for FI_EAGAIN error
    Prior to this patch, smr_generic_rma will write error completion
    for any error return of smr_fast_rma.
    
    This patch makes smr_generic_rma to return -FI_EAGAIN, if
    smr_fast_rma() return -FI_EAGAIN. This is because -FI_EAGAIN
    error means the operation has not been completed.
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/shm/src/smr_rma.c
+++ prov/shm/src/smr_rma.c
@@ -145,6 +145,9 @@ static ssize_t smr_generic_rma(struct smr_ep *ep, const struct iovec *iov,
 		if (err) {
 			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 				"error doing fast RMA\n");
+			if (err == -FI_EAGAIN)
+				return err;
+
 			ret = smr_write_err_comp(ep->util_ep.rx_cq, NULL,
 						op_flags, 0, err);
 		} else {
