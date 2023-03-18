ofiwg/libfabric

commit b7f25be5c748d9a82caffc75a8acd7c9123a7f32

prov/shm: smr_generic_rma() wwrite error completion with poistive errno
Prior to this patch, smr_generic_rma() call smr_write_error_comp()
with a negative errno, causing the output cq entry to have
a negative errno.

this patch addressed the issue by using a positive errno.

Signed-off-by: Wei Zhang <wzam@amazon.com>

prov/shm/src/smr_rma.c
@@ -149,7 +149,7 @@ static ssize_t smr_generic_rma(struct smr_ep *ep, const struct iovec *iov,
 				return err;
 
 			ret = smr_write_err_comp(ep->util_ep.rx_cq, NULL,
-						op_flags, 0, err);
+						op_flags, 0, -err);
 		} else {
 			ret = smr_complete_tx(ep, context, op, op_flags);
 		}

commit c1a8e3b3e361fd1c550875807f2effc146d7d487

prov/shm: fix potential deadlock in smr_generic_rma()
Prior commit b7f25be5c7  introduced a bug that can
cause the function smr_generic_rma() to return
without release ep->tx_lock.

This patch fixed the issue.

Signed-off-by: Wei Zhang <wzam@amazon.com>

prov/shm/src/smr_rma.c
@@ -145,8 +145,10 @@ static ssize_t smr_generic_rma(struct smr_ep *ep, const struct iovec *iov,
 		if (err) {
 			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 				"error doing fast RMA\n");
-			if (err == -FI_EAGAIN)
-				return err;
+			if (err == -FI_EAGAIN) {
+				ret = -FI_EAGAIN;
+				goto signal;
+			}
 
 			ret = smr_write_err_comp(ep->util_ep.rx_cq, NULL,
 						op_flags, 0, -err);