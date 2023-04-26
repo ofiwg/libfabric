4a7cfe1b6884c012cee81a0e3f6116cbb9b5ff46
Steve Welch <welch@hpe.com>
2023-04-26 17:19:18 -0500

    prov/cxi: Update fork unit test to use cxil_is_copy_on_fork()
    Update expected behavior using the libcxi copy-on-fork()
    kernel support detection.
    
    NETCASSINI-5083
    
    Signed-off-by: Steve Welch <welch@hpe.com>

--- prov/cxi/test/fork.c
+++ prov/cxi/test/fork.c
@@ -39,6 +39,7 @@
 #include <criterion/parameterized.h>
 #include <pthread.h>
 
+#include "libcxi/libcxi.h"
 #include "cxip.h"
 #include "cxip_test_common.h"
 
@@ -51,58 +52,12 @@
 #define RKEY 0x1U
 #define MAP_HUGE_2MB (21 << MAP_HUGE_SHIFT)
 
-struct linux_version {
-	union {
-		struct {
-			unsigned int kernel;
-			unsigned int major;
-			unsigned int minor;
-		};
-		unsigned int ver[3];
-	};
-};
-
 /* Needs to be marked volatile to prevent hangs due to compiler optimization. */
 static volatile bool child_process_block = true;
 
 static void signal_handler(int sig)
 {
 	child_process_block = false;
-}
-
-static int get_kernel_version(struct linux_version *version)
-{
-	FILE *fd;
-	char buf[256] = {};
-	size_t ret;
-	char *p = buf;
-	int i = 0;
-
-	/* uname() appears to not work. Extract information from version file.
-	 */
-	fd = fopen("/proc/version", "r");
-	if (fd == NULL)
-		return -errno;
-
-	ret = fread(buf, 1, 256, fd);
-
-	fclose(fd);
-
-	if (ret) {
-		while (*p) {
-			if (isdigit(*p)) {
-				version->ver[i] = strtol(p, &p, 10);
-				i++;
-			} else {
-				p++;
-			}
-
-			if (i == 3)
-				return 0;
-		}
-	}
-
-	return -EIO;
 }
 
 static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
@@ -116,14 +71,14 @@ static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
 	int status;
 	struct fi_cq_tagged_entry cqe;
 	pid_t pid;
-	struct linux_version ver;
 	int i = 0;
 	int flags = MAP_PRIVATE | MAP_ANONYMOUS;
 	uint64_t rkey;
 
 	if (odp) {
 		ret = setenv("FI_CXI_FORCE_ODP", "1", 1);
-		cr_assert_eq(ret, 0, "Failed to set FI_CXI_FORCE_ODP %d", -errno);
+		cr_assert_eq(ret, 0, "Failed to set FI_CXI_FORCE_ODP %d",
+			     -errno);
 	}
 
 	if (fork_safe) {
@@ -132,14 +87,12 @@ static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
 
 		if (huge_page) {
 			ret = setenv("CXI_FORK_SAFE_HP", "1", 1);
-			cr_assert_eq(ret, 0, "Failed to set CXI_FORK_SAFE %d", -errno);
+			cr_assert_eq(ret, 0, "Failed to set CXI_FORK_SAFE %d",
+				     -errno);
 		}
 	}
 
 	cxit_setup_msg();
-
-	ret = get_kernel_version(&ver);
-	cr_assert_eq(ret, 0, "Failed to get kernel version %d", ret);
 
 	signal(SIGUSR1, signal_handler);
 
@@ -165,14 +118,21 @@ static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
 
 	/* Register the buffer. The behavior of the child buffer depends upon
 	 * the following
-	 * - If CXI_FORK_SAFE is set, madvise(MADV_DONTFORK) will be issued
-	 * against the page. This will cause the child to segfault.
-	 * - If ODP is not used and the kernel < 5.12, the child process will
-	 * get its data, and the parent process will have data corruption.
-	 * - If ODP is not used and the kernel >= 5.12, the child process will
-	 * get its data, and the parent process will not have data corruption.
+	 * - If CXI_FORK_SAFE is set and copy-on-fork kernel support does not
+	 *   exist, madvise(MADV_DONTFORK) will be issued against the page.
+	 *   This will cause the child to segfault.
+	 * - If CXI_FORK_SAFE is set and copy-on-fork kernel support exists,
+	 *   madvise(MADV_DONTFORK) will NOT be issued against the page. The
+	 *   child process will get its data and the parent process will
+	 *   not have data corruption.
+	 * - If ODP is not used and kernel copy-on-fork is not supported, the
+	 *   child process will get its data, and the parent process will have
+	 *   data corruption.
+	 * - If ODP is not used and the kernel supports copy-on-fork, the child
+	 *   process will get its data, and the parent process will not have
+	 *   data corruption.
 	 * - If ODP is used, the child process will get its data, and the parent
-	 * process will not have data corruption.
+	 *   process will not have data corruption.
 	 */
 	ret = fi_mr_reg(cxit_domain, tgt_buf, XFER_SIZE, FI_REMOTE_WRITE, 0,
 			RKEY, 0, &mr, NULL);
@@ -193,8 +153,8 @@ static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
 		while (child_process_block)
 			sched_yield();
 
-		/* If CXI_FORK_SAFE is set (i.e. fork_safe is true), this will
-		 * segfault.
+		/* If CXI_FORK_SAFE is set (i.e. fork_safe is true) and
+		 * kernel copy-on-fork does not exist, this will segfault.
 		 */
 		if (buf[0] == SECRET)
 			_exit(EXIT_SUCCESS);
@@ -203,9 +163,11 @@ static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
 		_exit(EXIT_FAILURE);
 	}
 
-	/* Writing this these buffers will trigger COW.  Unless
-	* madvise(MADV_DONTFORK) was called, parent process will get new page.
-	*/
+	/* Writing these buffers will trigger COW if copy-on-fork
+	 * kernel support does not exist. If that is the case then unless
+	 * madvise(MADV_DONTFORK) was called, parent process will get a new
+	 * page.
+	 */
 	memset(init_buf, INIT_BUF_VALUE, XFER_SIZE);
 	memset(tgt_buf, TGT_BUF_VALUE, XFER_SIZE);
 
@@ -223,7 +185,7 @@ static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
 
 	validate_tx_event(&cqe, FI_RMA | FI_WRITE, NULL);
 
-	if ((ver.kernel >= 5 && ver.major >= 12) || odp || fork_safe) {
+	if (cxil_is_copy_on_fork() || odp || fork_safe) {
 		for (i = 0; i < XFER_SIZE; i++)
 			cr_assert_eq(init_buf[i], tgt_buf[i], "data corruption with fork");
 	} else {
@@ -233,7 +195,7 @@ static void fork_test_runner(bool odp, bool huge_page, bool fork_safe)
 
 	waitpid(pid, &status, 0);
 
-	if (fork_safe) {
+	if (!cxil_is_copy_on_fork() && fork_safe) {
 		cr_assert_eq(WIFSIGNALED(status), true, "Child was not terminated by signal: is_exit=%d exit=%d is_sig=%d sig=%d",
 			     WIFEXITED(status), WEXITSTATUS(status),
 			     WIFSIGNALED(status), WTERMSIG(status));
@@ -273,7 +235,8 @@ Test(fork, page_aliasing_odp_no_fork_safe_system_page_size)
 
 /* No ODP, no fork safe variables, and system page size. Parent process should
  * not have data corruption regardless of kernel version. Child process should
- * segfault since parent called MADV_DONTFORK on virtual address range.
+ * segfault if copy-on-fork kernel support does not exist (The parent would
+ * have called madvise MADV_DONTFORK if that is the case).
  */
 Test(fork, page_aliasing_no_odp_fork_safe_system_page_size)
 {
@@ -298,9 +261,10 @@ Test(fork, page_aliasing_odp_no_fork_safe_huge_page)
 	fork_test_runner(true, true, false);
 }
 
-/* No ODP, no fork safe variables, and 2MiB page size. Parent process should
+/* No ODP, with fork safe variables, and 2MiB page size. Parent process should
  * not have data corruption regardless of kernel version. Child process should
- * segfault since parent called MADV_DONTFORK on virtual address range.
+ * segfault if the kernel does not support copy-on-fork (since the parent
+ * would have called MADV_DONTFORK on virtual address range).
  */
 Test(fork, page_aliasing_no_odp_fork_safe_huge_page)
 {
