a8b018e019be85035a1e168ab7c970eec40d4515
Wei Zhang <wzam@amazon.com>
2023-04-26 16:13:23 +0000

    prov/efa: move and rename efa_rdm_error_write_msg()
    Move the function efa_rdm_error_write_msg() to efa_rdm_util.c
    and rename it efa_rdm_write_error_msg().
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/efa/src/rdm/efa_rdm_util.h
+++ prov/efa/src/rdm/efa_rdm_util.h
@@ -40,4 +40,6 @@ bool efa_rdm_get_use_device_rdma(uint32_t fabric_api_version);
 
 void efa_rdm_get_desc_for_shm(int numdesc, void **efa_desc, void **shm_desc);
 
+int efa_rdm_write_error_msg(struct rxr_ep *ep, fi_addr_t addr, int err, int prov_errno, void **buf, size_t *buflen);
+
 #endif /* _EFA_RDM_UTIL_H */
