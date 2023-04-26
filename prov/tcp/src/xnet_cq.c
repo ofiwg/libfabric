e1491a9691e2af5f008a72335f72dfc1439cbe65
Sean Hefty <sean.hefty@intel.com>
2023-04-26 13:33:43 -0700

    prov/tcp: Handle case where epoll isn't natively supported
    In the tcp provider, if wait support is requested, the code
    calls ofi_dynpoll_get_fd() to retrieve the epoll fd.  That
    fd can be passed into the progress epoll fd.  This works as
    long as epoll support is available on the platform.
    
    However, if epoll isn't available, ofi_dynpoll_get_fd() will
    return an invalid fd.  We need to handle the non-epoll case
    differently, as we can't rely on chaining the file descriptors
    for event reporting.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>

--- prov/tcp/src/xnet_cq.c
+++ prov/tcp/src/xnet_cq.c
@@ -288,7 +288,7 @@ int xnet_cq_open(struct fid_domain *domain, struct fi_cq_attr *attr,
 	if (ret)
 		goto free_cq;
 
-	if (cq->util_cq.wait) {
+	if (cq->util_cq.wait && ofi_have_epoll) {
 		ret = xnet_cq_add_progress(cq, xnet_cq2_progress(cq),
 					   &cq->util_cq.cq_fid);
 		if (ret)
@@ -434,18 +434,10 @@ static int xnet_cntr_wait_try_func(void *arg)
 	return FI_SUCCESS;
 }
 
-static int xnet_cntr_add_progress(struct util_cntr *cntr,
-				  struct xnet_progress *progress,
-				  void *context)
-{
-	return ofi_wait_add_fd(cntr->wait,
-			       ofi_dynpoll_get_fd(&progress->epoll_fd),
-			       POLLIN, xnet_cntr_wait_try_func, NULL, context);
-}
-
 int xnet_cntr_open(struct fid_domain *fid_domain, struct fi_cntr_attr *attr,
 		   struct fid_cntr **cntr_fid, void *context)
 {
+	struct xnet_progress *progress;
 	struct xnet_domain *domain;
 	struct util_cntr *cntr;
 	struct fi_cntr_attr cntr_attr;
@@ -477,12 +469,15 @@ int xnet_cntr_open(struct fid_domain *fid_domain, struct fi_cntr_attr *attr,
 	if (attr->wait_obj == FI_WAIT_NONE) {
 		cntr->cntr_fid.ops = &xnet_cntr_ops;
 	} else {
-		if (attr->wait_obj == FI_WAIT_FD && ofi_have_epoll)
-			ret = xnet_cntr_add_progress(cntr,
-						     xnet_cntr2_progress(cntr),
-						     &cntr->cntr_fid);
-		else
-			ret = xnet_start_progress(xnet_cntr2_progress(cntr));
+		progress = xnet_cntr2_progress(cntr);
+		if (attr->wait_obj == FI_WAIT_FD && ofi_have_epoll) {
+			ret = ofi_wait_add_fd(cntr->wait,
+					ofi_dynpoll_get_fd(&progress->epoll_fd),
+					POLLIN, xnet_cntr_wait_try_func, NULL,
+					&cntr->cntr_fid);
+		} else {
+			ret = xnet_start_progress(progress);
+		}
 		if (ret)
 			goto cleanup;
 	}
