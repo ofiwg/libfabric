3009e2d9f6ea8cd72183bc3093fbfcc654ee8281
Zach Dworkin <zachary.dworkin@intel.com>
2023-04-27 11:06:36 -0700

    contrib/intel/jenkins: Do not record an extra log file in python scripts
    Since we are switching to slurm we can use the slurm output file as our logfile instead of needing to record a new one
    
    Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

--- contrib/intel/jenkins/common.py
+++ contrib/intel/jenkins/common.py
@@ -6,28 +6,12 @@ import os
 def get_node_name(host, interface):
    return '%s-%s' % (host, interface)
 
-def run_command(command, logdir=None, test_type=None, ofi_build_mode=None):
-    stage_name = os.environ['STAGE_NAME']
-    if (test_type and ('tcp-rxm' in stage_name)):
-        filename = f'{logdir}/MPI_tcp-rxm_{test_type}_{ofi_build_mode}'
-    elif (test_type and ('MPI_net' in stage_name)):
-        filename = f'{logdir}/MPI_net_{test_type}_{ofi_build_mode}'
-    elif (test_type and ofi_build_mode):
-        filename = f'{logdir}/{stage_name}_{test_type}_{ofi_build_mode}'
-    else:
-        filename = f'{logdir}/{stage_name}'
-    print("filename: ".format(filename))
-    if (logdir):
-        f = open(filename, 'a')
+def run_command(command):
     print(" ".join(command))
     p = subprocess.Popen(command, stdout=subprocess.PIPE, text=True)
     print(p.returncode)
-    if (logdir):
-        f.write(" ".join(command) + '\n')
     while True:
         out = p.stdout.read(1)
-        if (logdir):
-            f.write(out)
         if (out == '' and p.poll() != None):
             break
         if (out != ''):
@@ -35,12 +19,7 @@ def run_command(command, logdir=None, test_type=None, ofi_build_mode=None):
             sys.stdout.flush()
     if (p.returncode != 0):
         print("exiting with " + str(p.poll()))
-        if (logdir):
-            f.close()
         sys.exit(p.returncode)
-    if (logdir):
-        f.close()
-
 
 Prov = collections.namedtuple('Prov', 'core util')
 prov_list = [
