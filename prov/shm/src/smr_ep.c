5dce8d9bdf2f9c0180d84e9b61b29e4ab2173783
Wenduo Wang <wenduwan@amazon.com>
2023-05-12 05:43:32 +0000

    prov/shm: do not use inline/inject for read op
    The current inline/inject protocols do not apply to read operations.
    The previous change for gdrcopy optimization mistakenly activated this
    path.
    
    Signed-off-by: Wenduo Wang <wenduwan@amazon.com>

--- prov/shm/src/smr_ep.c
+++ prov/shm/src/smr_ep.c
@@ -638,10 +638,6 @@ int smr_select_proto(enum fi_hmem_iface iface, bool use_ipc, bool cma_avail,
                      bool gdrcopy_avail, uint32_t op, uint64_t total_len,
                      uint64_t op_flags)
 {
-	/* TODO: Move gdrcopy check out of non-cuda fast paths */
-	if (gdrcopy_avail && total_len <= smr_env.max_gdrcopy_size)
-		return total_len <= SMR_MSG_DATA_LEN ? smr_src_inline : smr_src_inject;
-
 	if (op == ofi_op_read_req) {
 		if (use_ipc)
 			return smr_src_ipc;
@@ -649,6 +645,10 @@ int smr_select_proto(enum fi_hmem_iface iface, bool use_ipc, bool cma_avail,
 			return smr_src_iov;
 		return smr_src_sar;
 	}
+
+	/* TODO: Move gdrcopy check out of non-cuda fast paths */
+	if (gdrcopy_avail && total_len <= smr_env.max_gdrcopy_size)
+		return total_len <= SMR_MSG_DATA_LEN ? smr_src_inline : smr_src_inject;
 
 	if (op_flags & FI_INJECT) {
 		if (op_flags & FI_DELIVERY_COMPLETE)
