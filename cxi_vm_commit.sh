b8b9e3694e244aeeb62a1b630b264085ac4f82ad
Ian Ziemba <ian.ziemba@hpe.com>
2023-05-16 16:25:28 -0500

    prov/cxi: Fix commit builder
    The code was not properly being reconfigure and compiled for each test.
    In addition, test errors were not being properly reported.
    
    cxi_vm_commit.sh will now configure, run tests, and validate test
    results. If all tests were good, cxi_vm_commit.sh exit status will be 0.
    
    cxi_vm_pr.sh will call cxi_vm_commit.sh for each commit.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>

--- cxi_vm_commit.sh
+++ cxi_vm_commit.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+git log -1
+
+set -e
+
 ./autogen.sh
 PKG_CONFIG_PATH=$(realpath ../libcxi/install/lib/pkgconfig/) ./configure \
 	--prefix=$PWD/install \
@@ -17,9 +21,47 @@ PKG_CONFIG_PATH=$(realpath ../libcxi/install/lib/pkgconfig/) ./configure \
 	--disable-psm3 \
 	--disable-opx \
 	--enable-debug \
-	--disable-memhooks-monitor \
 	--with-default-monitor=uffd \
 	--with-criterion=$(realpath ../Criterion/build/install/)
 
-make -j install
-ssh -tt localhost "cd ~/workspace/workspace/os-networking-team/cassini-vm/libfabric/prov/cxi/test && ./run_tests_vm.sh"
+make clean
+make -j 8 install
+
+test_dir=$(realpath ./prov/cxi/test)
+test_result_file="run_tests_vm_output.txt"
+ssh -tt localhost "cd ${test_dir}; ./run_tests_vm.sh" | tee ${test_result_file}
+
+set +e
+
+# Search ssh output for the following string. This is a test failure
+# which is not reported as a tap test failure.
+test_error_code=1
+test_error=$(grep "cxitest return non-zero exit code. Possible failures in test teardown" ${test_result_file}) || test_error_code=$(($?^1))
+if [ -z "${test_error}" ] && [ "$test_error_code" -eq "0" ]; then
+	echo "Zero 'non-zero exit codes' failures in output"
+else
+	echo $test_error
+	exit 1
+fi
+
+# Grep all tap out files for "not ok" string. This is a test failure.
+test_failures_code=1
+test_failures=$(grep "not ok" ${test_dir}/*.tap) || test_failures_code=$(($?^1))
+if [ -z "${test_failures}" ] && [ "$test_failures_code" -eq "0" ] ; then
+	echo "Zero 'not ok' failures in tap output"
+else
+	echo $test_failures
+	exit 1
+fi
+
+signed_off=$(git log -1 | grep "Signed-off-by: ")
+if [ -z "${signed_off}" ]; then
+	echo "Commit not signed off"
+	exit 1
+else
+	echo "Commit signed-off check passed"
+fi
+
+echo "Tests passed"
+rm ${test_result_file}
+exit 0
