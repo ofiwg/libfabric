471bda13f48151975e3187bc774435d8ab9f18e4
Shi Jin <sjina@amazon.com>
2023-05-22 15:48:23 +0000

    fabtests/efa: Re-enable skip unexpected_msg test on single node
    The bug has been fixed as part of the util srx framework, re-enable this test.
    
    This reverts commit 3e523a355449f8ec96cd302f647ca84772b091ac.
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- fabtests/pytest/efa/test_unexpected_msg.py
+++ fabtests/pytest/efa/test_unexpected_msg.py
@@ -3,7 +3,5 @@ import pytest
 @pytest.mark.functional
 def test_unexpected_msg(cmdline_args):
     from common import ClientServerTest
-    if cmdline_args.server_id == cmdline_args.client_id:
-        pytest.skip("Temporarily skip single node before the unknown peer bug is fixed")
     test = ClientServerTest(cmdline_args, "fi_unexpected_msg -e rdm -I 10")
     test.run()
