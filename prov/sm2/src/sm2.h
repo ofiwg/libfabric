dc32c00357ff731659fe17502ca2c3d9d2ae9620
Luke Robison <lrbison@amazon.com>
2023-06-14 21:42:39 +0000

    prov/sm2: Add debug checks to pop_xfer_entry
    Signed-off-by: Luke Robison <lrbison@amazon.com>

--- prov/sm2/src/sm2.h
+++ prov/sm2/src/sm2.h
@@ -289,6 +289,15 @@ static inline size_t sm2_pop_xfer_entry(struct sm2_ep *ep,
 		return -FI_EAGAIN;
 
 	*xfer_entry = smr_freestack_pop(sm2_freestack(ep->self_region));
+
+	/* ensure we haven't over-exhausted the freestack: */
+	assert(sm2_freestack(self_region)->free >= 0);
+	/* ensure the freestack pointer is within this endpoint's region of
+	 * memory: */
+	assert((char *) *xfer_entry >= (char *) sm2_freestack(self_region));
+	assert((char *) *xfer_entry <
+	       (char *) sm2_mmap_ep_region(map, ep->gid + 1));
+
 	return FI_SUCCESS;
 }
 
