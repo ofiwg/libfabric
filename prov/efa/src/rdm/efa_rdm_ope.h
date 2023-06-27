076634967c550753364fded22c8e32b382306e8b
Wei Zhang <wzam@amazon.com>
2023-06-27 16:46:55 +0000

    prov/efa: add unit tests for efa_rdm_ope_prepare_to_post_send()
    Check the return value, and returned packet entry cnt and
    packet entry data size array is correct
    
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- prov/efa/src/rdm/efa_rdm_ope.h
+++ prov/efa/src/rdm/efa_rdm_ope.h
@@ -324,6 +324,11 @@ int efa_rdm_rxe_post_local_read_or_queue(struct efa_rdm_ope *rxe,
 					  struct efa_rdm_pke *pkt_entry,
 					  char *pkt_data, size_t data_size);
 
+ssize_t efa_rdm_ope_prepare_to_post_send(struct efa_rdm_ope *ope,
+					 int pkt_type,
+					 int *pkt_entry_cnt,
+					 int *pkt_entry_data_size_vec);
+
 ssize_t efa_rdm_ope_post_send(struct efa_rdm_ope *ope, int pkt_type);
 
 ssize_t efa_rdm_ope_post_send_or_queue(struct efa_rdm_ope *ope, int pkt_type);
