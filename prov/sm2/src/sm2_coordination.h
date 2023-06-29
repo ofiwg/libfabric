804ca7b222142f8e49e085d72c230a5c4393d985
Seth Zegelstein <szegel@amazon.com>
2023-06-29 23:03:04 +0000

    prov/sm2: Reduce size SM2_MAX_UNIVERSE_SIZE to 256
    Now that we are pre-allocating the entire Shared Memory Region, we need
    to be more careful with our memory use.  We are lowering
    SM2_MAX_UNIVERSE_SIZE to 256, and if we find that this is problematic,
    we can raise in the future.
    
    Signed-off-by: Seth Zegelstein <szegel@amazon.com>

--- prov/sm2/src/sm2_coordination.h
+++ prov/sm2/src/sm2_coordination.h
@@ -49,7 +49,7 @@
 #include <rdma/providers/fi_prov.h>
 
 #define SM2_XFER_ENTRY_SIZE   4096
-#define SM2_MAX_UNIVERSE_SIZE 2048
+#define SM2_MAX_UNIVERSE_SIZE 256
 /* TODO: Make the number of XFER ENTRY's configurable */
 #define SM2_NUM_XFER_ENTRY_PER_PEER 1024
 
