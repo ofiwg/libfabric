305fb3d0fbbea6b196d84aaad39fe90eb3e6b2a9
Zach Dworkin <zachary.dworkin@intel.com>
2023-07-24 16:27:10 -0700

    contrib/intel/jenkins: Add a logging supported run_command
    Adding run_logging_command()
    This function allows CI processes to log their own output in the event that they aren't using slurm. This is necessary because current support grabs output from the invocation (runtests.py) command rather than the actual run_command. For tests like daos that don't use slurm, some of the output is missing. This command will allow the stdout of the child process to be piped into the log_file rather than the output of the runtests.py command.
    
    Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

--- contrib/intel/jenkins/common.py
+++ contrib/intel/jenkins/common.py
@@ -21,6 +21,27 @@ def run_command(command):
         print("exiting with " + str(p.poll()))
         sys.exit(p.returncode)
 
+def run_logging_command(command, log_file):
+    print("filename: ".format(log_file))
+    f = open(log_file, 'a')
+    print(" ".join(command))
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, text=True)
+    print(p.returncode)
+    f.write(" ".join(command) + '\n')
+    while True:
+        out = p.stdout.read(1)
+        f.write(out)
+        if (out == '' and p.poll() != None):
+            break
+        if (out != ''):
+            sys.stdout.write(out)
+            sys.stdout.flush()
+    if (p.returncode != 0):
+        print("exiting with " + str(p.poll()))
+        f.close()
+        sys.exit(p.returncode)
+    f.close()
+
 Prov = collections.namedtuple('Prov', 'core util')
 prov_list = [
    Prov('psm3', None),
