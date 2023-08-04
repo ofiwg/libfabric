251babd8f4fda8a0fda8ff529c6fa03a7538746d
Shi Jin <sjina@amazon.com>
2023-08-04 23:53:23 +0000

    [v1.18.x] fabtests/efa: extend the test_unexpected_msg
    Cover more message sizes and counts to stress test provider
    with larger number of unexpected messages for different protocols.
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- fabtests/pytest/efa/test_unexpected_msg.py
+++ fabtests/pytest/efa/test_unexpected_msg.py
@@ -1,7 +1,9 @@
 import pytest
 
 @pytest.mark.functional
-def test_unexpected_msg(cmdline_args):
+@pytest.mark.parametrize("msg_size", ["1", "512", "9000", "1048576"]) # cover various switch points of shm/efa protocols
+@pytest.mark.parametrize("msg_count", ["1", "1024", "2048"]) # below and above efa/shm's default rx size
+def test_unexpected_msg(cmdline_args, msg_size, msg_count):
     from common import ClientServerTest
-    test = ClientServerTest(cmdline_args, "fi_unexpected_msg -e rdm -I 10")
+    test = ClientServerTest(cmdline_args, f"fi_unexpected_msg -e rdm -I 10 -S {msg_size} -M {msg_count}")
     test.run()
