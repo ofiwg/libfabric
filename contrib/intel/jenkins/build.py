95e434168e9e67f352a412d049236704ac6eeeb2
Zach Dworkin <zachary.dworkin@intel.com>
2023-08-14 12:50:05 -0700

    contrib/intel/jenkins: Migrate dsa from standalone testing to main cluster
    Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

--- contrib/intel/jenkins/build.py
+++ contrib/intel/jenkins/build.py
@@ -26,8 +26,6 @@ def build_libfabric(libfab_install_path, mode, cluster=None, ucx=None):
         enable_prov_val='dl'
     if (cluster == 'daos'):
         prov_list = common.daos_prov_list
-    elif (cluster == 'dsa'):
-        prov_list = common.dsa_prov_list
     elif (cluster == 'gpu'):
         prov_list = common.gpu_prov_list
     else:
@@ -120,7 +118,7 @@ if __name__ == "__main__":
                         "build mode", choices=['reg', 'dbg', 'dl'])
 
     parser.add_argument('--build_cluster', help="build libfabric on specified cluster", \
-                        choices=['daos', 'dsa', 'gpu'], default='default')
+                        choices=['daos', 'gpu'], default='default')
     parser.add_argument('--release', help="This job is likely testing a "\
                         "release and will be checked into a git tree.",
                         action='store_true')
