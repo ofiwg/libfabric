e6734a6b601d8f1b82488bed336d0f6bcaac9eb6
Zach Dworkin <zachary.dworkin@intel.com>
2023-09-21 10:10:17 -0700

    contrib/intel/jenkins: Disable failing imb tests
    The failing imb tests aren't tested by IMPI so they will
    be disabled to prevent unecessary failures in the CI.
    
    Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

--- contrib/intel/jenkins/tests.py
+++ contrib/intel/jenkins/tests.py
@@ -602,14 +602,18 @@ class IMBtests(Test):
         self.exclude = {
                         'MPI1':[],
                         'P2P':[],
-                        'EXT':[],
+                        'EXT':[
+                                'Accumulate'
+                              ],
                         'IO':[],
                         'NBC':[],
                         'RMA':[
                                   'Accumulate',
                                   'Get_accumulate',
                                   'Fetch_and_op',
-                                  'Compare_and_swap'
+                                  'Compare_and_swap',
+                                  'All_put_all',
+                                  'All_get_all'
                               ],
                         'MT':[]
                        }
