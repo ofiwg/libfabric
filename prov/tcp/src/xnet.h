81d4384e9b6e1ff44e7c11644fffc7f0743efd6f
Chien Tin Tung <chien.tin.tung@intel.com>
2023-10-09 14:05:32 -0700

    prov/tcp: Set FI_MULTI_RECV for last completed RX slice
    When FI_MULTI_RECV is enabled for an RX buffer, the last
    completed slice of the RX buffer should have FI_MULTI_RECV
    set in the corresponding CQE.
    
    Currently TCP provider assumes the last slice of the RX
    buffer will get completed last and sets FI_MULTI_RECV flag
    when the last slice is used.  Completions can get out of
    order with multiple connections and varied payload sizes
    using the same RX buffer.  Last slice will not always
    complete last.
    
    To make sure the last completed RX buffer slice has
    FI_MULTI_RECV set in the CQE, use a reference counter
    to track outstanding slices.  Count is initialized to 1
    when the RX buffer is first posted and incrememted on
    every slice allocation.  Upon completion, count is
    decremented.  When it reaches 0, FI_MULTI_RECV is set
    for that CQE.
    
    v2 - Minimize impact to non-multi-recv SRX code.
    v3 - Fix completion path, set FI_MULTI_RECV if mrecv is
         NULL and recv_entry is XNET_MULTI_RECV.
    v4 - Check ctrl_flag against XNET_MULTI_RECV first as mrecv
         may be garbage.
    
    Signed-off-by: Chien Tin Tung <chien.tin.tung@intel.com>

--- prov/tcp/src/xnet.h
+++ prov/tcp/src/xnet.h
@@ -392,6 +392,10 @@ static inline void xnet_signal_progress(struct xnet_progress *progress)
 #define XNET_CLAIM_RECV		BIT(10)
 #define XNET_MULTI_RECV		FI_MULTI_RECV /* BIT(16) */
 
+struct xnet_mrecv {
+	size_t			ref_cnt;
+};
+
 struct xnet_xfer_entry {
 	struct slist_entry	entry;
 	void			*user_buf;
@@ -402,7 +406,10 @@ struct xnet_xfer_entry {
 	struct util_cntr	*cntr;
 	uint64_t		tag_seq_no;
 	uint64_t		tag;
-	uint64_t		ignore;
+	union {
+		uint64_t		ignore;
+		struct xnet_mrecv	*mrecv;
+	};
 	fi_addr_t		src_addr;
 	uint64_t		cq_flags;
 	uint32_t		ctrl_flags;
