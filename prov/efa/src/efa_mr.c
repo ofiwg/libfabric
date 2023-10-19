9f604d658277c3a4851506327cb7a75572622767
Shi Jin <sjina@amazon.com>
2023-10-19 22:32:15 +0000

    prov/efa: Onboard ofi_hmem_dev_register API
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- prov/efa/src/efa_mr.c
+++ prov/efa/src/efa_mr.c
@@ -251,7 +251,7 @@ static int efa_mr_hmem_setup(struct efa_mr *efa_mr,
 		efa_mr->peer.device.cuda = attr->device.cuda;
 
 		if (cuda_is_gdrcopy_enabled()) {
-			err = cuda_gdrcopy_dev_register(attr->mr_iov->iov_base, attr->mr_iov->iov_len,
+			err = ofi_hmem_dev_register(FI_HMEM_CUDA, attr->mr_iov->iov_base, attr->mr_iov->iov_len,
 							(uint64_t *)&efa_mr->peer.hmem_data);
 			efa_mr->peer.flags |= OFI_HMEM_DATA_GDRCOPY_HANDLE;
 			if (err) {
@@ -458,7 +458,7 @@ static int efa_mr_dereg_impl(struct efa_mr *efa_mr)
 	if (efa_mr->peer.iface == FI_HMEM_CUDA &&
 	    (efa_mr->peer.flags & OFI_HMEM_DATA_GDRCOPY_HANDLE)) {
 		assert(efa_mr->peer.hmem_data);
-		err = cuda_gdrcopy_dev_unregister((uint64_t)efa_mr->peer.hmem_data);
+		err = ofi_hmem_dev_unregister(FI_HMEM_CUDA, (uint64_t)efa_mr->peer.hmem_data);
 		if (err) {
 			EFA_WARN(FI_LOG_MR,
 				"Unable to de-register cuda handle\n");
@@ -851,7 +851,7 @@ static int efa_mr_reg_impl(struct efa_mr *efa_mr, uint64_t flags, const void *at
 			if (efa_mr->peer.iface == FI_HMEM_CUDA &&
 			    (efa_mr->peer.flags & OFI_HMEM_DATA_GDRCOPY_HANDLE)) {
 					assert(efa_mr->peer.hmem_data);
-					cuda_gdrcopy_dev_unregister((uint64_t)efa_mr->peer.hmem_data);
+					ofi_hmem_dev_unregister(FI_HMEM_CUDA, (uint64_t)efa_mr->peer.hmem_data);
 				}
 
 			return -errno;
