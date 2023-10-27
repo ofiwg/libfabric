e88c5eb0d097748f9db8cdbc57476a029f791de0
Sai Sunku <sunkusa@amazon.com>
2023-10-27 23:27:15 +0000

    prov/efa: Sender switch to long CTS protocol if runt read fails with ENOMR
    Runting read protocol could fail with ENOMR if the EFA provider is unable
    to register the buffer with the NIC. In that case, we should fall back
    to long CTS instead
    
    This commit is for the changes when the sender fails to register the
    source buffer. The sender will switch to the long CTS protocol.
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>
    (cherry picked from commit a29640f6883b1319e2193683b85428fea14b3ecc)

--- prov/efa/src/rdm/efa_rdm_ope.c
+++ prov/efa/src/rdm/efa_rdm_ope.c
@@ -1758,8 +1758,13 @@ ssize_t efa_rdm_ope_post_send_fallback(struct efa_rdm_ope *ope,
 					   int pkt_type, ssize_t err)
 {
 	if (err == -FI_ENOMR) {
+		/* Long read and runting read protocols could fail because of a
+		 * lack of memory registrations. In that case, we retry with
+		 * long CTS protocol
+		 */
 		switch (pkt_type) {
 		case EFA_RDM_LONGREAD_MSGRTM_PKT:
+		case EFA_RDM_RUNTREAD_MSGRTM_PKT:
 			EFA_WARN(FI_LOG_EP_CTRL,
 				 "Sender fallback to long CTS untagged "
 				 "protocol because memory registration limit "
@@ -1767,6 +1772,7 @@ ssize_t efa_rdm_ope_post_send_fallback(struct efa_rdm_ope *ope,
 			return efa_rdm_ope_post_send_or_queue(
 				ope, EFA_RDM_LONGCTS_MSGRTM_PKT);
 		case EFA_RDM_LONGREAD_TAGRTM_PKT:
+		case EFA_RDM_RUNTREAD_TAGRTM_PKT:
 			EFA_WARN(FI_LOG_EP_CTRL,
 				 "Sender fallback to long CTS tagged protocol "
 				 "because memory registration limit was "
