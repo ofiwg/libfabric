91d06d1138d09ec23700b9648f87aeb82f116fe4
Sai Sunku <sunkusa@amazon.com>
2023-10-27 23:27:15 +0000

    prov/efa: Sender switch to long CTS protocol if runt read fails with ENOMR
    Runting read protocol could fail with ENOMR if the EFA provider is unable
    to register the buffer with the NIC. In that case, we should fall back
    to long CTS instead
    
    This commit is for the changes when the sender fails to register the
    source buffer. The sender will switch to the long CTS protocol.
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>
    (cherry picked from commit 1f3348844ee45b6fef4022841529f375bba60e42)

--- prov/efa/src/rdm/efa_rdm_pke_cmd.c
+++ prov/efa/src/rdm/efa_rdm_pke_cmd.c
@@ -131,11 +131,17 @@ int efa_rdm_pke_fill_data(struct efa_rdm_pke *pkt_entry,
 		ret = efa_rdm_pke_init_medium_tagrtm(pkt_entry, ope, data_offset, data_size);
 		break;
 	case EFA_RDM_LONGCTS_MSGRTM_PKT:
-		assert(data_offset == 0 && data_size == -1);
+		/* The data_offset will be non-zero when the long CTS RTM packet
+		 * is sent to continue a runting read transfer after the
+		 * receiver has run out of memory registrations */
+		assert((data_offset == 0 || ope->internal_flags & EFA_RDM_OPE_READ_NACK) && data_size == -1);
 		ret = efa_rdm_pke_init_longcts_msgrtm(pkt_entry, ope);
 		break;
 	case EFA_RDM_LONGCTS_TAGRTM_PKT:
-		assert(data_offset == 0 && data_size == -1);
+		/* The data_offset will be non-zero when the long CTS RTM packet
+		 * is sent to continue a runting read transfer after the
+		 * receiver has run out of memory registrations */
+		assert((data_offset == 0 || ope->internal_flags & EFA_RDM_OPE_READ_NACK) && data_size == -1);
 		ret = efa_rdm_pke_init_longcts_tagrtm(pkt_entry, ope);
 		break;
 	case EFA_RDM_LONGREAD_MSGRTM_PKT:
