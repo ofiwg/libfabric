7b5b580390134b90d5d9a8d47b2058d1a76db7b5
Seth Zegelstein <szegel@amazon.com>
2023-11-04 00:46:50 +0000

    prov/efa: Add testing for small messages without inject
    Add tests to fabtests that test small message sizes without fi_inject.
    By default, fabtests injects all host messages when the message size is
    <= the providers inject size.  This change adds testing coverage for
    fi_send/fi_rma without inject.
    
    Signed-off-by: Seth Zegelstein <szegel@amazon.com>

--- fabtests/pytest/efa/conftest.py
+++ fabtests/pytest/efa/conftest.py
@@ -18,3 +18,10 @@ def memory_type(request):
                                         "r:0,1024,1048576"])
 def message_size(request):
     return request.param
+
+
+@pytest.fixture(scope="module", params=["r:0,4,64",
+                                        "r:4048,4,4148",
+                                        "r:8000,4,9000",])
+def inject_message_size(request):
+    return request.param
