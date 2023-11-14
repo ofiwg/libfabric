a2193267ebb9e304d271e15011733cb803327bef
Sai Sunku <sunkusa@amazon.com>
2023-11-14 22:16:21 +0000

    prov/efa: Add pingpong test after exhausting MRs
    Add a pingpong test that exhausts MRs on both client and server
    
    This test first exhausts MRs on the server and runs a pingpong test
    It then exhausts MRs on the client and runs another pingpong test
    
    A pytest hook is used to run this test after all other tests to prevent
    MR exhaustion from affecting other tests
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>
    (cherry picked from commit a715487fb64203d3f4ccec5627ec32f6b22d13b8)

--- fabtests/pytest/efa/conftest.py
+++ fabtests/pytest/efa/conftest.py
@@ -18,3 +18,23 @@ def memory_type(request):
                                         "r:0,1024,1048576"])
 def message_size(request):
     return request.param
+
+
+@pytest.fixture(scope="module", params=["r:0,4,64",
+                                        "r:4048,4,4148",
+                                        "r:8000,4,9000",])
+def inject_message_size(request):
+    return request.param
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_collection_modifyitems(session, config, items):
+    # Called after collection has been performed, may filter or re-order the items in-place
+    # We use this hook to always run the MR exhaustion test at the end
+    mr_exhaustion_tests, other_tests = [], []
+    for item in items:
+        if "mr_exhaustion" in item.name:
+            mr_exhaustion_tests.append(item)
+        else:
+            other_tests.append(item)
+
+    yield other_tests + mr_exhaustion_tests
