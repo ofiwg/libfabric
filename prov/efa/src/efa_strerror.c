8a0046020b263cda16e17f5bbcf93efa7fa7ca5a
Darryl Abbate <drl@amazon.com>
2023-12-21 11:07:45 -0800

    prov/efa: Add `efa_show_help()`
    This reports long-form help messages to the user for some error codes.
    
    Signed-off-by: Darryl Abbate <drl@amazon.com>
    (cherry picked from commit 702ba3dc0bb0dbc642f5c1163f744c2619c7e64e)

--- prov/efa/src/efa_strerror.c
+++ prov/efa/src/efa_strerror.c
@@ -3,6 +3,8 @@
 
 #include "efa_errno.h"
 
+#include <stdio.h>
+
 #define IO_COMP_STATUS_MESSAGES(code, suffix, ...)	[EFA_IO_COMP_STATUS_##suffix] = #__VA_ARGS__,
 #define PROV_ERRNO_MESSAGES(code, suffix, ...)		[FI_EFA_ERR_##suffix - EFA_PROV_ERRNO_START] = #__VA_ARGS__,
 
@@ -44,4 +46,38 @@ const char *efa_strerror(enum efa_errno err)
 	return EFA_ERRNO_IS_PROV(err)
 		? efa_prov_strerror(err)
 		: efa_io_comp_strerror(err);
+}
+
+/**
+ * @brief Report a long-form help message to the user
+ *
+ * Given a non-negative EFA-specific error code, this function prints a
+ * long-form help message. These help messages are provided as a best effort
+ * attempt at debugging known issues, especially for `EFA_IO_COMP_STATUS_*`
+ * errors from RDMA Core.
+ *
+ * @param[in]	err    An EFA-specific error code
+ *
+ * @todo As this function grows, we should consider reading these long-form
+ * messages from disk, instead of baking the strings in.
+ */
+void efa_show_help(enum efa_errno err) {
+	char *help = "";
+	switch (err) {
+	case EFA_IO_COMP_STATUS_REMOTE_ERROR_BAD_DEST_QPN:
+		help = "This error is detected remotely; "
+		"typically encountered when the peer process is no longer present";
+		break;
+	case EFA_IO_COMP_STATUS_LOCAL_ERROR_UNRESP_REMOTE:
+		help = "This error is detected locally; "
+		"typically caused by a peer hardware failure or "
+		"incorrect inbound/outbound rules in the security group - "
+		"EFA requires \"All traffic\" type allowlisting. "
+		"Please also verify the peer application has not "
+		"terminated unexpectedly.";
+		break;
+	default:
+		return;
+	}
+	fprintf(stderr, "%s\n", help);
 }
