146f0a2d9c9fce5b7eeba127bc863f6b61a4c5f8
Sai Sunku <sunkusa@amazon.com>
2023-12-27 22:36:04 +0000

    fabtests/efa: Get device MR limit from ibv_query_device
    It's better to query the device than to hardcode the limit in case the
    limit changes in the future
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>
    (cherry picked from commit f865559e2b0446d439a02561143252d250a68a21)

--- fabtests/prov/efa/src/efa_exhaust_mr_reg_common.h
+++ fabtests/prov/efa/src/efa_exhaust_mr_reg_common.h
@@ -6,11 +6,12 @@
 #ifndef _EFA_EXHAUST_MR_REG_COMMON_H
 #define _EFA_EXHAUST_MR_REG_COMMON_H
 
-/* The EFA NIC currently supports a maximum of 256 * 1024 = 262144 registrations */
-#define EFA_MR_REG_LIMIT 262144
 #define EFA_MR_REG_BUF_SIZE 128
 
-int ft_efa_setup_ibv_pd(struct ibv_pd **pd);
+int ft_efa_open_ibv_device(struct ibv_context **ctx);
+int ft_efa_close_ibv_device(struct ibv_context *ctx);
+int ft_efa_get_max_mr(struct ibv_context *ctx);
+int ft_efa_setup_ibv_pd(struct ibv_context *ctx, struct ibv_pd **pd);
 void ft_efa_destroy_ibv_pd(struct ibv_pd *pd);
 int ft_efa_register_mr_reg(struct ibv_pd *pd, void **buffers, size_t buf_size,
 			   struct ibv_mr **mr_reg_vec, size_t count, size_t *registered);
