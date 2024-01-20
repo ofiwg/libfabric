91e1e2bebca2326d8449a963bbb962932f8cb513
Sai Sunku <sunkusa@amazon.com>
2024-01-20 01:50:27 +0000

    include, man: Introduce FI_OPT_SHARED_MEMORY_PERMITTED
    FI_OPT_SHARED_MEMORY_PERMITTED is a new endpoint option that controls
    the use of shared memory for intra-node communication
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

--- include/rdma/fi_endpoint.h
+++ include/rdma/fi_endpoint.h
@@ -69,6 +69,7 @@ enum {
 	FI_OPT_FI_HMEM_P2P,		/* int */
 	FI_OPT_XPU_TRIGGER,		/* struct fi_trigger_xpu */
 	FI_OPT_CUDA_API_PERMITTED,	/* bool */
+	FI_OPT_SHARED_MEMORY_PERMITTED, /* bool */
 };
 
 /*
