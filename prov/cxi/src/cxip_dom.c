789c8ff9c04a970841ccfbd543a8e1d9fb1df302
Ian Ziemba <ian.ziemba@hpe.com>
2024-01-23 14:47:49 -0600

    prov/cxi: Define domain idc msg
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>
    (cherry picked from commit 5f9f12074a397df494726a4bf172a4e501e762ea)

--- prov/cxi/src/cxip_dom.c
+++ prov/cxi/src/cxip_dom.c
@@ -287,6 +287,42 @@ out_unlock:
 	return ret;
 }
 
+int cxip_domain_emit_idc_msg(struct cxip_domain *dom, uint16_t vni,
+			     enum cxi_traffic_class tc,
+			     const struct c_cstate_cmd *c_state,
+			     const struct c_idc_msg_hdr *msg, const void *buf,
+			     size_t len, uint64_t flags)
+{
+	int ret;
+	struct cxip_domain_cmdq *cmdq;
+
+	ofi_genlock_lock(&dom->cmdq_lock);
+
+	ret = cxip_domain_find_cmdq(dom, vni, tc, &cmdq);
+	if (ret) {
+		CXIP_WARN("Failed to find command queue: %d\n", ret);
+		goto out_unlock;
+	}
+
+	ret = cxip_cmdq_emit_idc_msg(cmdq->cmdq, c_state, msg, buf, len,
+				     flags);
+	if (ret) {
+		CXIP_WARN("Failed to emit idc msg: %d\n", ret);
+		goto out_unlock;
+	}
+
+	cxi_cq_ring(cmdq->cmdq->dev_cmdq);
+
+	ofi_genlock_unlock(&dom->cmdq_lock);
+
+	return FI_SUCCESS;
+
+out_unlock:
+	ofi_genlock_unlock(&dom->cmdq_lock);
+
+	return ret;
+}
+
 /*
  * cxip_domain_req_alloc() - Allocate a domain control buffer ID
  */
