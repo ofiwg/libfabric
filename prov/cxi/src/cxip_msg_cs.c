89e40f6e8818c11ee5bbd1cae7e7b07117b48eb0
Steve Welch <welch@hpe.com>
2024-02-11 09:53:06 -0600

    prov/cxi: Initial client/server receive logic
    Does not implement hybrid MR yet/selective completion yet.
    
    NETCASSINI-5652
    
    Signed-off-by: Steve Welch <welch@hpe.com>
    (cherry picked from commit bfb4bb7becfc19a1c5a03e249c9378ce8d7dfa1c)
    (cherry picked from commit 120da16d562b98ad194a65e5d694baecd469f5cc)

--- prov/cxi/src/cxip_msg_cs.c
+++ prov/cxi/src/cxip_msg_cs.c
@@ -23,6 +23,9 @@
 #define CXIP_DBG(...) _CXIP_DBG(FI_LOG_EP_CTRL, __VA_ARGS__)
 #define CXIP_INFO(...) _CXIP_INFO(FI_LOG_EP_CTRL, __VA_ARGS__)
 
+#define APPEND_LE_FATAL "Recieve LE resources exhuasted. Requires use " \
+	" of FI_PROTO_CXI endpoint protocol\n"
+
 static void cxip_rnr_recv_pte_cb(struct cxip_pte *pte,
 				 const union c_event *event)
 {
@@ -72,6 +75,119 @@ static void cxip_rnr_recv_pte_cb(struct cxip_pte *pte,
 	}
 }
 
+/*
+ * cxip_cs_recv_req() - Submit Receive request to hardware.
+ */
+static ssize_t cxip_cs_recv_req(struct cxip_req *req, bool restart_seq)
+{
+	struct cxip_rxc *rxc = req->recv.rxc;
+	uint32_t le_flags = 0;
+	union cxip_match_bits mb = {};
+	union cxip_match_bits ib = {
+		.cs_cq_data = 1,
+	};
+	int ret;
+	struct cxip_md *recv_md = req->recv.recv_md;
+	uint64_t recv_iova = 0;
+
+	if (req->recv.tagged) {
+		mb.tagged = 1;
+		mb.cs_tag = req->recv.tag;
+		ib.cs_tag = req->recv.ignore;
+	}
+
+	if (req->recv.match_id == CXI_MATCH_ID_ANY)
+		ib.cs_vni = ~0;
+	else
+		mb.cs_vni = req->recv.vni;
+
+	/* Always set manage_local in Receive LEs. This makes Cassini ignore
+	 * initiator remote_offset in all Puts. With this, remote_offset in Put
+	 * events can be used by the initiator for protocol data. The behavior
+	 * of use_once is not impacted by manage_local.
+	 */
+	le_flags |= C_LE_EVENT_LINK_DISABLE | C_LE_EVENT_UNLINK_DISABLE |
+		    C_LE_MANAGE_LOCAL | C_LE_UNRESTRICTED_BODY_RO |
+		    C_LE_UNRESTRICTED_END_RO | C_LE_OP_PUT;
+
+	if (!req->recv.multi_recv)
+		le_flags |= C_LE_USE_ONCE;
+	if (restart_seq)
+		le_flags |= C_LE_RESTART_SEQ;
+
+	if (recv_md)
+		recv_iova = CXI_VA_TO_IOVA(recv_md->md,
+					   (uint64_t)req->recv.recv_buf +
+					   req->recv.start_offset);
+
+	req->recv.hw_offloaded = true;
+
+	/* Issue Append command */
+	ret = cxip_pte_append(rxc->rx_pte, recv_iova,
+			      req->recv.ulen - req->recv.start_offset,
+			      recv_md ? recv_md->md->lac : 0,
+			      C_PTL_LIST_PRIORITY, req->req_id,
+			      mb.raw, ib.raw, req->recv.match_id,
+			      req->recv.multi_recv ?
+			      rxc->min_multi_recv : 0,
+			      le_flags, NULL, rxc->rx_cmdq,
+			      !(req->recv.flags & FI_MORE));
+	if (ret != FI_SUCCESS) {
+		RXC_WARN(rxc, "Failed to write Append command: %d\n", ret);
+		return ret;
+	}
+
+	return FI_SUCCESS;
+}
+
+/*
+ * cxip_cs_recv_cb() - Process user receive buffer events.
+ *
+ * For the CS protocol a receive buffer is described by an LE linked to
+ * the Priority List. Local unexpected message buffering and rendezvous
+ * messaging are not enabled.
+ */
+static int cxip_cs_recv_cb(struct cxip_req *req, const union c_event *event)
+{
+	struct cxip_rxc_cs *rxc = req->recv.rxc_cs;
+
+	switch (event->hdr.event_type) {
+	case C_EVENT_LINK:
+		/* Success events are disabled */
+		assert(cxi_tgt_event_rc(event) != C_RC_OK);
+
+		/* Failure to link a receive buffer is a fatal operation and
+		 * indicates that FI_PROTO_CXI and portals flow-control is
+		 * required.
+		 */
+		RXC_FATAL(rxc, APPEND_LE_FATAL);
+		break;
+
+	case C_EVENT_UNLINK:
+		assert(!event->tgt_long.auto_unlinked);
+
+		/* If request is for FI_MULTI_RECV and success events are being
+		 * taken (completions required) then cxip_recv_req_report()
+		 * will handle making sure the unlink is not reported prior to
+		 * all messages being reported.
+		 */
+		req->recv.unlinked = true;
+		cxip_recv_req_report(req);
+		cxip_recv_req_free(req);
+
+		return FI_SUCCESS;
+
+	case C_EVENT_PUT:
+		cxip_rxc_record_req_stat(&rxc->base, C_PTL_LIST_PRIORITY,
+					 event->tgt_long.rlength, req);
+		return cxip_complete_put(req, event);
+	default:
+		RXC_FATAL(rxc, CXIP_UNEXPECTED_EVENT,
+			  cxi_event_to_str(event),
+			  cxi_rc_to_str(cxi_event_rc(event)));
+	}
+}
+
 static void cxip_rxc_cs_progress(struct cxip_rxc *rxc)
 {
 	cxip_evtq_progress(&rxc->rx_evtq);
@@ -80,13 +196,12 @@ static void cxip_rxc_cs_progress(struct cxip_rxc *rxc)
 static void cxip_rxc_cs_recv_req_tgt_event(struct cxip_req *req,
 					   const union c_event *event)
 {
-	struct cxip_rxc *rxc = req->recv.rxc;
 	union cxip_match_bits mb = {
 		.raw = event->tgt_long.match_bits
 	};
 	uint32_t init = event->tgt_long.initiator.initiator.process;
 
-	assert(rxc->protocol == FI_PROTO_CXI_CS);
+	assert(req->recv.rxc->protocol == FI_PROTO_CXI_CS);
 	assert(event->hdr.event_type == C_EVENT_PUT);
 
 	req->tag = mb.cs_tag;
@@ -149,8 +264,10 @@ static void cxip_rxc_cs_fini_struct(struct cxip_rxc *rxc)
 	/* Placeholder */
 }
 
-static int cxip_rxc_cs_msg_init(struct cxip_rxc *rxc)
+static int cxip_rxc_cs_msg_init(struct cxip_rxc *rxc_base)
 {
+	struct cxip_rxc_cs *rxc = container_of(rxc_base, struct cxip_rxc_cs,
+					       base);
 	struct cxi_pt_alloc_opts pt_opts = {
 		.use_long_event = 1,
 		.is_matching = 1,
@@ -158,26 +275,28 @@ static int cxip_rxc_cs_msg_init(struct cxip_rxc *rxc)
 	};
 	int ret;
 
-	assert(rxc->protocol == FI_PROTO_CXI_CS);
+	assert(rxc->base.protocol == FI_PROTO_CXI_CS);
 
 	/* If applications AVs are symmetric, use logical FI addresses for
 	 * matching. Otherwise, physical addresses will be used.
 	 */
-	if (rxc->ep_obj->av->symmetric) {
+	if (rxc->base.ep_obj->av->symmetric) {
 		CXIP_DBG("Using logical PTE matching\n");
 		pt_opts.use_logical = 1;
 	}
 
-	ret = cxip_pte_alloc(rxc->ep_obj->ptable,
-			     rxc->rx_evtq.eq, rxc->recv_ptl_idx, false,
-			     &pt_opts, cxip_rnr_recv_pte_cb, rxc, &rxc->rx_pte);
+	ret = cxip_pte_alloc(rxc->base.ep_obj->ptable, rxc->base.rx_evtq.eq,
+			     rxc->base.recv_ptl_idx, false, &pt_opts,
+			     cxip_rnr_recv_pte_cb, &rxc->base,
+			     &rxc->base.rx_pte);
 	if (ret != FI_SUCCESS) {
 		CXIP_WARN("Failed to allocate RX PTE: %d\n", ret);
 		return ret;
 	}
 
 	/* Start accepting Puts. */
-	ret = cxip_pte_set_state(rxc->rx_pte, rxc->rx_cmdq, C_PTLTE_ENABLED, 0);
+	ret = cxip_pte_set_state(rxc->base.rx_pte, rxc->base.rx_cmdq,
+				 C_PTLTE_ENABLED, 0);
 	if (ret != FI_SUCCESS) {
 		CXIP_WARN("cxip_pte_set_state returned: %d\n", ret);
 		goto free_pte;
@@ -186,27 +305,26 @@ static int cxip_rxc_cs_msg_init(struct cxip_rxc *rxc)
 	/* Wait for PTE state change */
 	do {
 		sched_yield();
-		cxip_evtq_progress(&rxc->rx_evtq);
-	} while (rxc->rx_pte->state != C_PTLTE_ENABLED);
+		cxip_evtq_progress(&rxc->base.rx_evtq);
+	} while (rxc->base.rx_pte->state != C_PTLTE_ENABLED);
 
 	return FI_SUCCESS;
 
 free_pte:
-	cxip_pte_free(rxc->rx_pte);
+	cxip_pte_free(rxc->base.rx_pte);
 
 	return ret;
 }
 
 static int cxip_rxc_cs_msg_fini(struct cxip_rxc *rxc_base)
 {
-	/* Placeholder */
+	assert(rxc_base->protocol == FI_PROTO_CXI_CS);
+
 	return FI_SUCCESS;
 }
 
 static void cxip_rxc_cs_cleanup(struct cxip_rxc *rxc_base)
 {
-	/* Placeholder */
-
 	/* Cancel Receives */
 	cxip_rxc_recv_req_cleanup(rxc_base);
 }
@@ -221,8 +339,98 @@ cxip_recv_common(struct cxip_rxc *rxc, void *buf, size_t len, void *desc,
 		 void *context, uint64_t flags, bool tagged,
 		 struct cxip_cntr *comp_cntr)
 {
-	/* Placeholder */
-	return -FI_ENOSYS;
+	struct cxip_req *req = NULL;
+	int ret;
+	uint32_t match_id;
+	uint16_t vni;
+
+	assert(rxc->protocol == FI_PROTO_CXI_CS);
+
+#if ENABLE_DEBUG
+	if (len && !buf) {
+		RXC_WARN(rxc, "Length %ld but local buffer NULL\n", len);
+		return -FI_EINVAL;
+	}
+
+	if (rxc->state == RXC_DISABLED)
+		return -FI_EOPBADSTATE;
+
+	if (tagged) {
+		if (tag & ~CXIP_CS_TAG_MASK || ignore & ~CXIP_CS_TAG_MASK) {
+			RXC_WARN(rxc,
+				 "Invalid tag: %#018lx ignore: %#018lx (%#018lx)\n",
+				 tag, ignore, CXIP_CS_TAG_MASK);
+			return -FI_EINVAL;
+		}
+	}
+#endif
+
+	if (!rxc->selective_completion)
+		flags |= FI_COMPLETION;
+
+	ret = cxip_set_recv_match_id(rxc, src_addr, rxc->ep_obj->av_auth_key &&
+				     (flags & FI_AUTH_KEY), &match_id, &vni);
+	if (ret) {
+		RXC_WARN(rxc, "Error setting match_id: %d %s\n",
+			 ret, fi_strerror(-ret));
+		return ret;
+	}
+
+	/* TODO: implement hybrid MD */
+	ofi_genlock_lock(&rxc->ep_obj->lock);
+	ret = cxip_recv_req_alloc(rxc, buf, len, NULL, &req, cxip_cs_recv_cb);
+	if (ret)
+		goto err;
+
+	req->flags = ((tagged ? FI_TAGGED : FI_MSG) | FI_RECV |
+		       (flags & FI_COMPLETION));
+	req->context = (uint64_t)context;
+	req->recv.cntr = comp_cntr ? comp_cntr : rxc->recv_cntr;
+	req->recv.match_id = match_id;
+	req->recv.vni = vni;
+	req->recv.tag = tag;
+	req->recv.ignore = ignore;
+	req->recv.flags = flags;
+	req->recv.tagged = tagged;
+	req->recv.multi_recv = (flags & FI_MULTI_RECV ? true : false);
+
+	if (!(req->recv.flags & (FI_PEEK | FI_CLAIM))) {
+		ret = cxip_cs_recv_req(req, false);
+		if (ret) {
+			RXC_WARN(rxc, "Receive append failed: %d %s\n",
+				 ret, fi_strerror(-ret));
+			goto free_req;
+		}
+		ofi_genlock_unlock(&rxc->ep_obj->lock);
+
+		RXC_DBG(rxc,
+			"req: %p buf: %p len: %lu src_addr: %ld tag(%c):"
+			" 0x%lx ignore: 0x%lx context: %p\n",
+			req, buf, len, src_addr, tagged ? '*' : '-', tag,
+			ignore, context);
+
+		return FI_SUCCESS;
+	}
+
+	/* No buffered unexpected messages, so FI_PEEK always fails */
+	if (req->recv.flags & FI_PEEK) {
+		req->recv.rc = C_RC_NO_MATCH;
+		cxip_recv_req_peek_complete(req, NULL);
+		ofi_genlock_unlock(&rxc->ep_obj->lock);
+
+		return FI_SUCCESS;
+	}
+
+	/* FI_CLAIM specified by itself cannot be valid */
+	RXC_WARN(rxc, "FI_CLAIM not valid\n");
+	ret = -FI_EINVAL;
+
+free_req:
+	cxip_recv_req_free(req);
+err:
+	ofi_genlock_unlock(&rxc->ep_obj->lock);
+
+	return ret;
 }
 
 static void cxip_txc_cs_progress(struct cxip_txc *txc)
