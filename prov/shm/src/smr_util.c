ofiwg/libfabric

commit 7f23127e8993d610a2caa7c92cb859663d1d52fc

prov/shm: Lock double deletion check
This change solves this coverity issue:
Checking if an AV (address vector) map has already deleted a peer, or if the
peer no longer exists in the AV map must be locked. This is because one
thread can check if a peer exists and get a "false positive" while another
thread is actively destroying that peer. This case would result in a double
destruction which we need to protect against.

Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

prov/shm/src/smr_util.c
@@ -572,15 +572,15 @@ void smr_map_del(struct smr_map *map, int64_t id)
 {
 	struct dlist_entry *entry;
 
-	if (id >= SMR_MAX_PEERS || id < 0 || map->peers[id].peer.id < 0)
-		return;
-
 	pthread_mutex_lock(&ep_list_lock);
 	entry = dlist_find_first_match(&ep_name_list, smr_match_name,
 				       smr_no_prefix(map->peers[id].peer.name));
 	pthread_mutex_unlock(&ep_list_lock);
 
 	ofi_spin_lock(&map->lock);
+	if (id >= SMR_MAX_PEERS || id < 0 || map->peers[id].peer.id < 0)
+		goto unlock;
+
 	if (!entry) {
 		if (map->flags & SMR_FLAG_HMEM_ENABLED)
 			(void) ofi_hmem_host_unregister(map->peers[id].region);
@@ -594,6 +594,7 @@ void smr_map_del(struct smr_map *map, int64_t id)
 	map->peers[id].peer.id = -1;
 	map->num_peers--;
 
+unlock:
 	ofi_spin_unlock(&map->lock);
 }
 

commit dbc348c5d95dcf35c29f0a1ae76476c3505d9f24

prov/shm: Fix new coverity issue introduced by 7f23127
No code path should lead to delete an out of bounds
index but we must assert that the id is within the
allowable range before using it to check a peer's name.


Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

prov/shm/src/smr_util.c
@@ -572,13 +572,15 @@ void smr_map_del(struct smr_map *map, int64_t id)
 {
 	struct dlist_entry *entry;
 
+	assert(id >= 0 && id < SMR_MAX_PEERS);
+
 	pthread_mutex_lock(&ep_list_lock);
 	entry = dlist_find_first_match(&ep_name_list, smr_match_name,
 				       smr_no_prefix(map->peers[id].peer.name));
 	pthread_mutex_unlock(&ep_list_lock);
 
 	ofi_spin_lock(&map->lock);
-	if (id >= SMR_MAX_PEERS || id < 0 || map->peers[id].peer.id < 0)
+	if (map->peers[id].peer.id < 0)
 		goto unlock;
 
 	if (!entry) {