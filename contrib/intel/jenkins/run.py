12794237500238fdba40b9010995b557d2ea9962
Zach Dworkin <zachary.dworkin@intel.com>
2024-02-29 09:17:06 -0800

    contrib/intel/jenkins: Call individual executables in SHMEM
    Change how SHMEM tests get called. The new method will be 
    similar to mpichtestsuite because we will keep track of the 
    executables and run the ones inside of the directories.
    
    Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

--- contrib/intel/jenkins/run.py
+++ contrib/intel/jenkins/run.py
@@ -47,13 +47,14 @@ def fabtests(hw, core, hosts, mode, user_env, log_file, util, way):
         print(f"Skipping {core} {runfabtest.testname} as exec condn fails")
     print('-------------------------------------------------------------------')
 
-def shmemtest(hw, core, hosts, mode, user_env, log_file, util):
+def shmemtest(hw, core, hosts, mode, user_env, log_file, util, weekly):
 
     runshmemtest = tests.ShmemTest(jobname=jbname,buildno=bno,
                                    testname="shmem test", hw=hw, core_prov=core,
                                    fabric=fab, hosts=hosts,
                                    ofi_build_mode=mode, user_env=user_env,
-                                   log_file=log_file, util_prov=util)
+                                   log_file=log_file, util_prov=util,
+                                   weekly=weekly)
 
     print('-------------------------------------------------------------------')
     if (runshmemtest.execute_condn):
