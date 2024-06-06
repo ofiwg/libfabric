b23b208163c2b15099513a7d611097fcb350ce31
Jessie Yang <jiaxiyan@amazon.com>
2024-06-06 11:08:40 -0700

    fabtests/pytest: use different message sizes for rma pingpong
    Signed-off-by: Jessie Yang <jiaxiyan@amazon.com>

--- fabtests/pytest/efa/test_rma_pingpong.py
+++ fabtests/pytest/efa/test_rma_pingpong.py
@@ -3,6 +3,13 @@ from common import perf_progress_model_cli
 import pytest
 
 
+@pytest.fixture(params=["r:4048,4,4148",
+                        "r:8000,4,9000",
+                        "r:17000,4,18000"])
+def rma_pingpong_message_size(request):
+    return request.param
+
+
 @pytest.mark.parametrize("operation_type", ["writedata", "write"])
 @pytest.mark.parametrize("iteration_type",
                          [pytest.param("short", marks=pytest.mark.short),
@@ -12,30 +19,24 @@ def test_rma_pingpong(cmdline_args, iteration_type, operation_type, completion_s
         pytest.skip("no hmem memory support for pingpong_rma write test")
     command = "fi_rma_pingpong -e rdm"
     command = command + " -o " + operation_type + " " + perf_progress_model_cli
-    # rma_pingpong test with data verification takes longer to finish
-    timeout = max(540, cmdline_args.timeout)
-    efa_run_client_server_test(cmdline_args, command, iteration_type, completion_semantic, memory_type, "all", timeout=timeout)
+    efa_run_client_server_test(cmdline_args, command, iteration_type, completion_semantic, memory_type, "all")
 
 
 @pytest.mark.functional
 @pytest.mark.parametrize("operation_type", ["writedata", "write"])
-def test_rma_pingpong_range(cmdline_args, operation_type, completion_semantic, message_size, memory_type):
+def test_rma_pingpong_range(cmdline_args, operation_type, completion_semantic, rma_pingpong_message_size, memory_type):
     if memory_type != "host_to_host" and operation_type == "write":
         pytest.skip("no hmem memory support for pingpong_rma write test")
     command = "fi_rma_pingpong -e rdm"
     command = command + " -o " + operation_type
-    # rma_pingpong test with data verification takes longer to finish
-    timeout = max(540, cmdline_args.timeout)
-    efa_run_client_server_test(cmdline_args, command, "short", completion_semantic, memory_type, message_size, timeout=timeout)
+    efa_run_client_server_test(cmdline_args, command, "short", completion_semantic, memory_type, rma_pingpong_message_size)
 
 
 @pytest.mark.functional
 @pytest.mark.parametrize("operation_type", ["writedata", "write"])
-def test_rma_pingpong_range_no_inject(cmdline_args, operation_type, completion_semantic, inject_message_size, memory_type):
+def test_rma_pingpong_range_no_inject(cmdline_args, operation_type, completion_semantic, rma_pingpong_message_size, memory_type):
     if memory_type != "host_to_host" and operation_type == "write":
         pytest.skip("no hmem memory support for pingpong_rma write test")
     command = "fi_rma_pingpong -e rdm -j 0"
     command = command + " -o " + operation_type
-    # rma_pingpong test with data verification takes longer to finish
-    timeout = max(540, cmdline_args.timeout)
-    efa_run_client_server_test(cmdline_args, command, "short", completion_semantic, "host_to_host", inject_message_size, timeout=timeout)
+    efa_run_client_server_test(cmdline_args, command, "short", completion_semantic, "host_to_host", rma_pingpong_message_size)
