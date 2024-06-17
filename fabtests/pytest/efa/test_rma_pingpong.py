65eb07a4d2abeea952e877c4ee22166b414738d8
Jessie Yang <jiaxiyan@amazon.com>
2024-06-17 11:46:18 -0700

    fabtest/pytest: Skip rma_pingpong write tests
    The current logic to check completion for write in rma_pingpong is
    not reliable on platforms without true RDMA write cap.
    
    Signed-off-by: Jessie Yang <jiaxiyan@amazon.com>

--- fabtests/pytest/efa/test_rma_pingpong.py
+++ fabtests/pytest/efa/test_rma_pingpong.py
@@ -10,33 +10,27 @@ def rma_pingpong_message_size(request):
     return request.param
 
 
-@pytest.mark.parametrize("operation_type", ["writedata", "write"])
+@pytest.mark.parametrize("operation_type", ["writedata"])
 @pytest.mark.parametrize("iteration_type",
                          [pytest.param("short", marks=pytest.mark.short),
                           pytest.param("standard", marks=pytest.mark.standard)])
 def test_rma_pingpong(cmdline_args, iteration_type, operation_type, completion_semantic, memory_type):
-    if memory_type != "host_to_host" and operation_type == "write":
-        pytest.skip("no hmem memory support for pingpong_rma write test")
     command = "fi_rma_pingpong -e rdm"
     command = command + " -o " + operation_type + " " + perf_progress_model_cli
     efa_run_client_server_test(cmdline_args, command, iteration_type, completion_semantic, memory_type, "all")
 
 
 @pytest.mark.functional
-@pytest.mark.parametrize("operation_type", ["writedata", "write"])
+@pytest.mark.parametrize("operation_type", ["writedata"])
 def test_rma_pingpong_range(cmdline_args, operation_type, completion_semantic, rma_pingpong_message_size, memory_type):
-    if memory_type != "host_to_host" and operation_type == "write":
-        pytest.skip("no hmem memory support for pingpong_rma write test")
     command = "fi_rma_pingpong -e rdm"
     command = command + " -o " + operation_type
     efa_run_client_server_test(cmdline_args, command, "short", completion_semantic, memory_type, rma_pingpong_message_size)
 
 
 @pytest.mark.functional
-@pytest.mark.parametrize("operation_type", ["writedata", "write"])
+@pytest.mark.parametrize("operation_type", ["writedata"])
 def test_rma_pingpong_range_no_inject(cmdline_args, operation_type, completion_semantic, rma_pingpong_message_size, memory_type):
-    if memory_type != "host_to_host" and operation_type == "write":
-        pytest.skip("no hmem memory support for pingpong_rma write test")
     command = "fi_rma_pingpong -e rdm -j 0"
     command = command + " -o " + operation_type
     efa_run_client_server_test(cmdline_args, command, "short", completion_semantic, "host_to_host", rma_pingpong_message_size)
