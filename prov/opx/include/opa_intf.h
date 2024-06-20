0ab567257c4b39843d03dafaae43a12ba1e06a9d
Charles Shereda <charles.shereda@cornelisnetworks.com>
2024-06-20 09:37:04 -0400

    prov/opx: Fixed copyright and contact info in header files
    Signed-off-by: Charles Shereda <charles.shereda@cornelisnetworks.com>

--- prov/opx/include/opa_intf.h
+++ prov/opx/include/opa_intf.h
@@ -54,6 +54,7 @@
 */
 
 /* Copyright (c) 2003-2014 Intel Corporation. All rights reserved. */
+/* Copyright (C) 2021-2024 by Cornelis Networks.                   */
 
 #ifndef OPA_INTF_H
 #define OPA_INTF_H
