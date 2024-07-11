1c65b15a7d61ecf9c1db8a2bc9df2b959e3f946b
Shi Jin <sjina@amazon.com>
2024-07-11 06:32:22 +0000

    prov/efa: Add error handling for pke allocation
    When pke allocation failed for user rx buffer,
    it should return ENOMEM
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- prov/efa/src/rdm/efa_rdm_ep_utils.c
+++ prov/efa/src/rdm/efa_rdm_ep_utils.c
@@ -214,6 +214,10 @@ int efa_rdm_ep_post_user_recv_buf(struct efa_rdm_ep *ep, struct efa_rdm_ope *rxe
 	assert(rxe->iov_count > 0  && rxe->iov_count <= ep->rx_iov_limit);
 	assert(rxe->iov[0].iov_len >= ep->msg_prefix_size);
 	pkt_entry = efa_rdm_pke_alloc(ep, ep->user_rx_pkt_pool, EFA_RDM_PKE_FROM_USER_RX_POOL);
+	if (OFI_UNLIKELY(!pkt_entry)) {
+		EFA_WARN(FI_LOG_EP_DATA, "Failed to allocate pkt_entry for user rx\n");
+		return -FI_ENOMEM;
+	}
 
 	pkt_entry->ope = rxe;
 	rxe->state = EFA_RDM_RXE_MATCHED;
