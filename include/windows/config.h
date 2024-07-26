c18fdf53b2be366ce44eb8f00f18580450043b21
Jianxin Xiong <jianxin.xiong@intel.com>
2024-07-26 10:38:45 -0700

    configure: Bump the version to 2.0.0a1
    This only updates the version string.
    
    Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>

--- include/windows/config.h
+++ include/windows/config.h
@@ -256,7 +256,7 @@
 #define PACKAGE_TARNAME PACKAGE
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.22.0rc1"
+#define PACKAGE_VERSION "2.0.0a1"
 
 /* Define to the full name and version of this package. */
 #define PACKAGE_STRING PACKAGE_NAME " " PACKAGE_VERSION
