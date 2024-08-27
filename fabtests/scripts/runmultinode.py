ad3a40c1a11a70c2f6d976513f952d1c36d11d6a
Amir Shehata <shehataa@ornl.gov>
2024-08-27 16:17:55 -0400

    fabtests: multinode test updates
    Made a few updates to the multinode test:
    
    1. accept a -x flag to turn off setting the service/node/flags
      - this is needed to work with CXI
    2. accept a -u flag to set a process manager: pmi or pmix
    3. modify the code to get the rank from the appropriate environment
       variable if a process manager is specified.
    4. Add a runmultinode.py script which enables users to run the test
       using a backing process manager. The python script takes a YAML
       configuration file which defines the environment and test. An example
       python configuration file:
    
    multinode:
        environment:
            FI_MR_CACHE_MAX_SIZE: -1
            FI_MR_CACHE_MAX_COUNT: 524288
            FI_SHM_USE_XPMEM: 1
            FI_LOG_LEVEL: info
        bind-to: core
        map-by-count: 1
        map-by: l3cache
        pattern: full_mesh
    
    Script Usage:
    usage: runmultinode.py [-h] [--dry-run] [--ci CI] [-C CAPABILITY]
                           [-i ITERATIONS] [-l {internal,srun,mpirun}]
                           [-p PROVIDER] [-np NUM_PROCS] [-c CONFIG]
                           [-t PROCS_PER_NODE]
    
    libfabric multinode test with slurm
    
    optional arguments:
      -h, --help            show this help message and exit
      --dry-run             Perform a dry run without making any changes.
      --ci CI               Commands to prepend to test call. Only used with the
                            internal launcher option
      -C CAPABILITY, --capability CAPABILITY
                            libfabric capability
      -i ITERATIONS, --iterations ITERATIONS
                            Number of iterations
      -l {internal,srun,mpirun}, --launcher {internal,srun,mpirun}
                            launcher to use for running job. If nothing is
                            specified, test manages processes internally.
                            Available options: internal, srun and mpirun
    
    Required arguments:
      -p PROVIDER, --provider PROVIDER
                            libfabric provider
      -np NUM_PROCS, --num-procs NUM_PROCS
                            Map process by node, l3cache, etc
      -c CONFIG, --config CONFIG
                            Test configuration
    
    Required if using srun:
      -t PROCS_PER_NODE, --procs-per-node PROCS_PER_NODE
                            Number of procs per node
    
    Running the script:
    
    runmultinode.py -p cxi -i 1 --procs-per-node 8 --num-procs 8 -l srun -c mn.yaml
    
    Signed-off-by: Amir Shehata <shehataa@ornl.gov>

---
+++ fabtests/scripts/runmultinode.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+
+import argparse, builtins, os, sys, yaml, socket
+
+def parse_args():
+	parser = argparse.ArgumentParser(description="libfabric multinode test with slurm")
+	parser.add_argument('--dry-run', action='store_true', help='Perform a dry run without making any changes.')
+	parser.add_argument("--ci", type=str, help="Commands to prepend to test call. Only used with the internal launcher option", default="")
+	parser.add_argument("-C", "--capability", type=str, help="libfabric capability", default="msg")
+	parser.add_argument("-i", "--iterations", type=int , help="Number of iterations", default=1)
+	parser.add_argument("-l", "--launcher", type=str, choices=['internal', 'srun', 'mpirun'], help="launcher to use for running job. If nothing is specified, test manages processes internally. Available options: internal, srun and mpirun", default="internal")
+
+	required = parser.add_argument_group("Required arguments")
+	required.add_argument("-p", "--provider", type=str, help="libfabric provider")
+	required.add_argument("-np", "--num-procs", type=int, help="Map process by node, l3cache, etc")
+	required.add_argument("-c", "--config", type=str, help="Test configuration")
+
+	srun_required = parser.add_argument_group("Required if using srun")
+	srun_required.add_argument("-t", "--procs-per-node", type=int,
+							help="Number of procs per node", default=-1)
+
+	args = parser.parse_args()
+	return args, parser
+
+def parse_config(config):
+	with open(config, "r") as f:
+		yconf = yaml.load(f, Loader=yaml.FullLoader)
+	return yconf
+
+def mpi_env(config):
+	env = config['environment']
+	result = []
+	for k in env.keys():
+		result.append(f"-x {k}")
+	return " ".join(result)
+
+def set_env(config):
+	env = config['environment']
+	for k, v in env.items():
+		os.environ[k] = str(v)
+
+def mpi_mca_params(config):
+	try:
+		mca = config['mca']
+		result = []
+		for k, v in env.items():
+			result.append(f"--mca {k} {v}")
+		return " ".join(result)
+	except:
+		return ""
+
+def mpi_bind_to(config):
+	try:
+		return f"--bind-to {config['bind-to']}"
+	except:
+		return "--bind-to core"
+
+def mpi_map_by(config):
+	try:
+		return f"--map-by ppr:{config['map-by-count']}:{config['map-by']}"
+	except:
+		return "--map-by ppr:1:l3"
+
+def execute_cmd(cmd, dry_run):
+	script_dir = os.path.dirname(os.path.abspath(__file__))
+	sys.path.append(script_dir)
+	from command import Command
+	cmd = Command(cmd, fake=dry_run)
+	rc, out = cmd.exec_cmd()
+	return rc, out
+
+def split_on_commas(expr): 
+	l = []
+	c = o = 0 
+	s = expr
+	stop = False
+	while not stop and c != -1 and o != -1:
+		o = s.find('[')
+		b = s.find(']')
+		c = s.find(',')
+		while c > o and c < b:
+			c = s.find(',', c+1)
+		if len(l):
+			l.pop()
+		if c < o or c > b:
+			l += [s[:s.find(',', c)], s[s.find(',', c)+1:]]
+			s = l[-1]
+		else:
+			l += s.split(',')
+			stop = True
+	for i in range(0, len(l)):
+		l[i] = l[i].strip()
+	return l
+
+def expand_host_list_sub(expr):
+	host_list = []
+
+	# first phase split on the commas first
+	open_br = expr.find('[')
+	close_br = expr.find(']', open_br)
+	if open_br == -1 and close_br == -1:
+		return expr.split(',')
+
+	if open_br == -1 or close_br == -1:
+		return []
+
+	rangestr = expr[open_br+1 : close_br]
+
+	node = expr[:open_br]
+
+	ranges = rangestr.split(',')
+
+	for r in ranges:
+		cur = r.split('-')
+		if len(cur) == 2:
+			pre = "{:0%dd}" % len(cur[0])
+			for idx in range(int(cur[0]), int(cur[1])+1):
+				host_list.append(f'{node}{pre.format(idx)}')
+		elif len(cur) == 1:
+			pre = "{:0%dd}" % len(cur[0])
+			host_list.append(f'{node}{pre.format(int(cur[0]))}')
+
+	return host_list
+
+def expand_host_list(expr):
+	l = split_on_commas(expr)
+	host_list = []
+	for e in l:
+		host_list += expand_host_list_sub(e)
+	return host_list
+
+supported_pm = ['pmi', 'pmi2', 'pmix']
+
+def is_srun_pm_supported():
+	rc, out = execute_cmd('srun --mpi=list', False)
+	if rc:
+		return False
+	input_list = out.split('\n')
+	cleaned_list = [entry.strip() for entry in input_list if entry.strip()]
+	for e in supported_pm:
+		if e in cleaned_list[1:]:
+			return True
+	return False
+
+if __name__ == '__main__':
+
+	# list of providers which do not address specification
+	no_addr_prov = ['cxi']
+
+	args, parser = parse_args()
+
+	if not args.config:
+		print("**A configuration file is required")
+		print(parser.format_help())
+		exit(-1)
+
+	mnode = parse_config(args.config)['multinode']
+	set_env(mnode)
+
+	if args.launcher == 'srun':
+		if not is_srun_pm_supported():
+			print(f"**Supported process managers are: {','.join(supported_pm)}")
+			print(parser.format_help())
+			exit(-1)
+
+	# The script assumes it's already running in a SLURM allocation. It can
+	# then srun fi_multinode
+	#
+	if "pattern" not in mnode:
+		print("Test pattern must be defined in the YAML configuration file")
+		exit()
+
+	if args.provider in no_addr_prov:
+		cmd = f"fi_multinode -n {args.num_procs} -s {socket.gethostname()} " \
+			f"-p {args.provider} -C {args.capability} -z {mnode['pattern']} " \
+			f"-I {args.iterations} -u {args.launcher.lower()} -E -T"
+	else:
+		cmd = f"fi_multinode -n {args.num_procs} -s {socket.gethostname()} " \
+			f"-p {args.provider} -C {args.capability} -z '{mnode['pattern']}' " \
+			f"-I {args.iterations} -u {args.launcher.lower()} -T"
+
+	if args.launcher.lower() == 'mpirun':
+		mpi = f"mpirun {mpi_env(mnode)} {mpi_mca_params(mnode)} {mpi_bind_to(mnode)} "\
+			  f"{mpi_map_by(mnode)} -np {args.num_procs} {cmd}"
+	elif args.launcher.lower() == 'srun':
+		if args.procs_per_node == -1 or args.num_procs == -1:
+			print("**Need to specify --procs-per-node and --num-procs")
+			print(parser.format_help())
+			exit()
+		mpi = f"srun --ntasks-per-node {args.procs_per_node} --ntasks {args.num_procs} "\
+			  f"{cmd}"
+	elif args.launcher.lower() == 'internal':
+		if args.procs_per_node == -1:
+			print("**Need to specify --procs-per-node")
+			print(parser.format_help())
+			exit()
+		hl = ",".join(expand_host_list(os.environ['SLURM_NODELIST']))
+		mpi = f"runmultinode.sh -h {hl} -n {args.procs_per_node} -p {args.provider} " \
+			  f"-C {args.capability} -I {args.iterations} -z {mnode['pattern']}"
+		if args.ci:
+			mpi += f" --ci '{args.ci}'"
+	else:
+		print("**Unsupported launcher")
+		print(parser.format_help())
+		exit()
+
+	rc, out = execute_cmd(mpi, args.dry_run)
+
+	print(f"Command completed with {rc}\n{out}")
