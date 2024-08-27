f8c220b3c7d61a75f7f22169d2313de234457829
Jessie Yang <jiaxiyan@amazon.com>
2024-08-27 11:34:23 -0700

    prov/efa: remove unit test for libfabric 1.1 API
    The related code logic has been removed in 613e69e.
    
    Signed-off-by: Jessie Yang <jiaxiyan@amazon.com>

--- prov/efa/test/efa_unit_test_info.c
+++ prov/efa/test/efa_unit_test_info.c
@@ -45,64 +45,6 @@ void test_info_open_ep_with_wrong_info()
 }
 
 /**
- * @brief test that we support older version of libfabric API version 1.1
- */
-void test_info_open_ep_with_api_1_1_info()
-{
-	struct fi_info *hints, *info;
-	struct fid_fabric *fabric = NULL;
-	struct fid_domain *domain = NULL;
-	struct fid_ep *ep = NULL;
-	int err;
-
-	hints = calloc(sizeof(struct fi_info), 1);
-	assert_non_null(hints);
-
-	hints->domain_attr = calloc(sizeof(struct fi_domain_attr), 1);
-	assert_non_null(hints->domain_attr);
-
-	hints->fabric_attr = calloc(sizeof(struct fi_fabric_attr), 1);
-	assert_non_null(hints->fabric_attr);
-
-	hints->ep_attr = calloc(sizeof(struct fi_ep_attr), 1);
-	assert_non_null(hints->ep_attr);
-
-	hints->fabric_attr->prov_name = "efa";
-	hints->ep_attr->type = FI_EP_RDM;
-
-	/* in libfabric API < 1.5, domain_attr->mr_mode is an enum with
-	 * two options: FI_MR_BASIC or FI_MR_SCALABLE, (EFA does not support FI_MR_SCALABLE).
-	 *
-	 * Additional information about memory registration is specified as bits in
-	 * "mode". For example, the requirement of local memory registration
-	 * is specified as FI_LOCAL_MR.
-	 */
-	hints->mode = FI_LOCAL_MR;
-	hints->domain_attr->mr_mode = FI_MR_BASIC;
-
-	err = fi_getinfo(FI_VERSION(1, 1), NULL, NULL, 0ULL, hints, &info);
-	assert_int_equal(err, 0);
-
-	err = fi_fabric(info->fabric_attr, &fabric, NULL);
-	assert_int_equal(err, 0);
-
-	err = fi_domain(fabric, info, &domain, NULL);
-	assert_int_equal(err, 0);
-
-	err = fi_endpoint(domain, info, &ep, NULL);
-	assert_int_equal(err, 0);
-
-	err = fi_close(&ep->fid);
-	assert_int_equal(err, 0);
-
-	err = fi_close(&domain->fid);
-	assert_int_equal(err, 0);
-
-	err = fi_close(&fabric->fid);
-	assert_int_equal(err, 0);
-}
-
-/**
  * @brief Verify info->tx/rx_attr->msg_order is set according to hints.
  *
  */
