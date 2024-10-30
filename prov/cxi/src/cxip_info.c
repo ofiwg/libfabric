15b4487772efc0d674df3c25b457e81e2cbfb26c
Chuck Fossen <charles.fossen@hpe.com>
2024-10-30 14:03:09 -0500

    prov/cxi: Remove disable_dmabuf_cuda and disable_dmabuf_rocr
    Remove unneeded disable_dmabuf_cuda and disable_dmabuf_rocr
    environment variables.
    
    NETCASSINI-6844
    
    Signed-off-by: Chuck Fossen <charles.fossen@hpe.com>

--- prov/cxi/src/cxip_info.c
+++ prov/cxi/src/cxip_info.c
@@ -608,8 +608,6 @@ struct cxip_environment cxip_env = {
 	.force_odp = false,
 	.ats = false,
 	.iotlb = true,
-	.disable_dmabuf_cuda = false,
-	.disable_dmabuf_rocr = false,
 	.ats_mlock_mode = CXIP_ATS_MLOCK_ALL,
 	.fork_safe_requested = false,
 	.rx_match_mode = CXIP_PTLTE_DEFAULT_MODE,
@@ -770,17 +768,6 @@ static void cxip_env_init(void)
 			"Enables the NIC IOTLB (default %d).", cxip_env.iotlb);
 	fi_param_get_bool(&cxip_prov, "iotlb", &cxip_env.iotlb);
 
-	fi_param_define(&cxip_prov, "disable_dmabuf_cuda", FI_PARAM_BOOL,
-			"Disables the DMABUF interface for CUDA (default %d).",
-			cxip_env.disable_dmabuf_cuda);
-	fi_param_get_bool(&cxip_prov, "disable_dmabuf_cuda",
-			  &cxip_env.disable_dmabuf_cuda);
-
-	fi_param_define(&cxip_prov, "disable_dmabuf_rocr", FI_PARAM_BOOL,
-			"Disables the DMABUF interface for ROCR (default %d).",
-			cxip_env.disable_dmabuf_rocr);
-	fi_param_get_bool(&cxip_prov, "disable_dmabuf_rocr",
-			  &cxip_env.disable_dmabuf_rocr);
 
 	/* Disable cuda DMABUF by default - honors the env if already set */
 	ret = setenv("FI_HMEM_CUDA_USE_DMABUF", "0", 0);
