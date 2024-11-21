5b9979130862bf136a5e6cdad83e3f12e0870bbd
Dariusz Sciebura <dariusz.sciebura@gmail.com>
2024-11-21 14:09:10 +0000

    Make util_ns fileds: run and is_initialized atomics
    Signed-off-by: Dariusz Sciebura <dariusz.sciebura@gmail.com>

--- include/ofi_util.h
+++ include/ofi_util.h
@@ -1243,8 +1243,8 @@ struct util_ns {
 	size_t		name_len;
 	size_t		service_len;
 
-	int		run;
-	int		is_initialized;
+	ofi_atomic32_t	run;
+	ofi_atomic32_t	is_initialized;
 	ofi_atomic32_t	ref;
 
 	ofi_ns_service_cmp_func_t	service_cmp;
