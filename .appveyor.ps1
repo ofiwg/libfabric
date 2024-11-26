b28373ad9f92b727a853db303914278037c569fd
Alexia Ingerson <alexia.ingerson@intel.com>
2024-11-26 13:02:38 -0800

    appveyor: update efa windows header download
    Old link was failing. Instead of downloading and extracting
    from zip file, just clone github repo to copy headers
    
    Signed-off-by: Alexia Ingerson <alexia.ingerson@intel.com>

--- .appveyor.ps1
+++ .appveyor.ps1
@@ -15,15 +15,10 @@ Write-Verbose "moving NetworkDirect headers.."
 move NetDirect\include\* include\windows
 Write-Verbose "done"
 
-$efaWinVersion="1.0.0"
-Write-Verbose "downloading efawin version ${efaWinVersion} files.."
-Invoke-WebRequest -Uri "https://github.com/aws/efawin/archive/refs/tags/v${efaWinVersion}.zip" -OutFile "efawin.zip"
-Write-Verbose "done"
-
-Write-Verbose "extracting efawin files.."
-$wd=$PWD.Path; & { Add-Type -A "System.IO.Compression.FileSystem"; [IO.Compression.ZipFile]::ExtractToDirectory("$wd\efawin.zip", "$wd"); }
+Write-Verbose "downloading efawin files.."
+git clone https://github.com/aws/efawin
 Write-Verbose "done"
 
 Write-Verbose "copying efawin files.."
-xcopy /s efawin-$efaWinVersion\interface\* prov\efa\src\windows\
+xcopy /s efawin\interface\* prov\efa\src\windows\
 Write-Verbose "done"
