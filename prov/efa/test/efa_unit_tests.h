b99163755162fb618c3fbe09779e7462b4aad6bf
Shi Jin <sjina@amazon.com>
2024-12-20 00:31:08 +0000

    [v1.22.x] prov/efa: Detect unsolicited write recv support status on both sides
    Currently, when local support unsolicited write recv while the peer
    doesn't support it, the peer will crash because it expects to get
    a valid wr_id for IBV_WC_RECV_RDMA_WITH_IMM op code. This peer crash
    can cause weird error message on sender side's cq when it is still
    sending data to it. When local doesn't support unsolicited write recv
    while the peer support it, local will get cq error for the rdma op as
    "Unexpected status" as well.
    
    This patch makes the initiator of rdma write imm
    detect the unsolicited write recv support status on both sides. If
    there is inconsistency, the initiator will return error with clear
    error messages that instruct the mitigation.
    
    Signed-off-by: Shi Jin <sjina@amazon.com>
    (cherry picked from commit ed5560a27db86a6aa625b7362150fbe3b086c0cd)

--- prov/efa/test/efa_unit_tests.h
+++ prov/efa/test/efa_unit_tests.h
@@ -126,6 +126,8 @@ void test_efa_rdm_ep_zcpy_recv_cancel();
 void test_efa_rdm_ep_post_handshake_error_handling_pke_exhaustion();
 void test_efa_rdm_ep_rx_refill_threshold_smaller_than_rx_size();
 void test_efa_rdm_ep_rx_refill_threshold_larger_than_rx_size();
+void test_efa_rdm_ep_support_unsolicited_write_recv();
+void test_efa_rdm_ep_rma_inconsistent_unsolicited_write_recv();
 void test_dgram_cq_read_empty_cq();
 void test_ibv_cq_ex_read_empty_cq();
 void test_ibv_cq_ex_read_failed_poll();
