23da0fa65f5cc7e4b564a072968b2be993277dfa
Shi Jin <sjina@amazon.com>
2025-02-18 20:04:02 +0000

    fabtests/prov/efa: Add remote exit early test
    Add tests to make sure when sender exits early in the middle of send,
    receiver polls cq and handle errors gracefully.
    Add options for receiver posts receive, sender posts tagged send, and
    sender posts fi_writedata.
    
    Signed-off-by: Jessie Yang <jiaxiyan@amazon.com>

---
+++ fabtests/pytest/efa/test_remote_exit_early.py
@@ -0,0 +1,29 @@
+import pytest
+from common import ClientServerTest
+
+@pytest.fixture(params=[65536, 131072, 1048576])
+def remote_exit_early_message_size(request):
+    # 64K use medium
+    # 128K use long CTS
+    # 1M use runtread or longread
+    return request.param
+
+@pytest.mark.functional
+def test_remote_exit_early_post_send(cmdline_args, remote_exit_early_message_size):
+    test = ClientServerTest(cmdline_args, "fi_efa_rdm_remote_exit_early",
+                            message_size=remote_exit_early_message_size)
+    test.run()
+
+@pytest.mark.functional
+def test_remote_exit_early_post_tagged(cmdline_args, remote_exit_early_message_size):
+    test = ClientServerTest(cmdline_args, "fi_efa_rdm_remote_exit_early -o tagged",
+                            message_size=remote_exit_early_message_size)
+    test.run()
+
+@pytest.mark.functional
+def test_remote_exit_early_post_writedata(cmdline_args, remote_exit_early_message_size):
+    test = ClientServerTest(cmdline_args, "fi_efa_rdm_remote_exit_early -o writedata",
+                            message_size=remote_exit_early_message_size)
+    test.run()
+
+# TODO: add test with --post-rx after fixing the leak in srx->rx_pool
