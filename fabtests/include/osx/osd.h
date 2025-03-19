c686e394907efa34b72d15bf9fb66827d1eb6e7d
Zach Dworkin <zachary.dworkin@intel.com>
2025-03-19 12:50:41 -0700

    fabtests/include/osx: Add pthread_barrier support to osx
    pthread_barrier is not implemented on mac os. To enable it we must write
    our own functions and use those in place of the ones that come from pthread.
    
    Signed-off-by: Zach Dworkin <zachary.dworkin@intel.com>

--- fabtests/include/osx/osd.h
+++ fabtests/include/osx/osd.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 Los Alamos Nat. Security, LLC. All rights reserved.
+ * Copyright (c) Intel Corporation. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -38,6 +39,8 @@
 #include <sys/time.h>
 #include <time.h>
 
+#include <pthread.h>
+
 #if !HAVE_CLOCK_GETTIME
 #define CLOCK_REALTIME 0
 #define CLOCK_REALTIME_COARSE 0
@@ -55,5 +58,51 @@ int clock_gettime(clockid_t clk_id, struct timespec *tp);
 }
 #endif
 #endif // !HAVE_CLOCK_GETTIME
+
+#if !defined(_POSIX_BARRIERS) || (_POSIX_BARRIERS < 0)
+
+typedef int pthread_barrierattr_t;
+
+typedef struct {
+	int count;
+	int called;
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+} pthread_barrier_t;
+
+static inline int pthread_barrier_init(pthread_barrier_t *barrier,
+                        	       const pthread_barrierattr_t *attr,
+                        	       unsigned count)
+{
+	barrier->count = count;
+	barrier->called = 0;
+	pthread_mutex_init(&barrier->mutex, NULL);
+	pthread_cond_init(&barrier->cond, NULL);
+	return 0;
+}
+
+static inline int pthread_barrier_destroy(pthread_barrier_t *barrier)
+{
+	pthread_mutex_destroy(&barrier->mutex);
+	pthread_cond_destroy(&barrier->cond);
+	return 0;
+}
+
+static inline int pthread_barrier_wait(pthread_barrier_t *barrier)
+{
+	pthread_mutex_lock(&barrier->mutex);
+	barrier->called++;
+	if (barrier->called == barrier->count) {
+		barrier->called = 0;
+		pthread_cond_broadcast(&barrier->cond);
+	} else {
+		pthread_cond_wait(&barrier->cond, &barrier->mutex);
+	}
+
+	pthread_mutex_unlock(&barrier->mutex);
+	return 0;
+}
+
+#endif // _POSIX_BARRIERS
 
 #endif // FABTESTS_OSX_OSD_H
