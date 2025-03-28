944189865aa57930a95bb8fb959c076fa3df5cee
Ian Ziemba <ian.ziemba@hpe.com>
2025-03-28 13:45:33 -0500

    prov/cxi: Remove cached mem reg -FI_EAGAIN comment
    This comment is incorrect. For HMEM, if the mapped region is larger than
    the IOV, the IOV information used to perform the memory registration is
    updated. -FI_EAGAIN is not returned.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>

--- prov/cxi/src/cxip_iomm.c
+++ prov/cxi/src/cxip_iomm.c
@@ -114,10 +114,9 @@ static int cxip_do_map(struct ofi_mr_cache *cache, struct ofi_mr_entry *entry)
 	}
 
 	/* If the md len is larger than the iov_len, the VA and len have
-	 * been aligned to a larger page size. Update the cache memory
-	 * region registered by returning -FI_EAGAIN. Note, that GPU memory
-	 * cannot be aligned since the aligned iov_base may fall outside the
-	 * valid device address.
+	 * been aligned to a larger page size. Note, that GPU memory cannot be
+	 * aligned since the aligned iov_base may fall outside the valid device
+	 * address.
 	 */
 	if (entry->info.iface == FI_HMEM_SYSTEM) {
 		entry->info.iov.iov_base = (void *)md->md->va;
