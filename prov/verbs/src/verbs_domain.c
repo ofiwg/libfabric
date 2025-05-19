ofiwg/libfabric

commit d15a7701d032ce857d1c8e8f912618c2bd79bdbd

prov/verbs: Make a copy of the provider info in the fabric object
With the next changes to allow dynamic reloads of the verbs interfaces,
the fabric can no longer hold a reference to vrb_util_prov.info,
otherwise it may cause a use-after-free if the interface is removed
from that list.

Signed-off-by: Sylvain Didelot <sdidelot@ddn.com>

prov/verbs/src/verbs_domain.c
@@ -325,19 +325,21 @@ vrb_domain(struct fid_fabric *fabric, struct fi_info *info,
 	struct vrb_fabric *fab =
 		 container_of(fabric, struct vrb_fabric,
 			      util_fabric.fabric_fid);
-	const struct fi_info *fi = vrb_get_verbs_info(vrb_util_prov.info,
-							 info->domain_attr->name);
+	struct fi_info *fi = vrb_get_verbs_info(vrb_util_prov.info,
+						info->domain_attr->name);
 	if (!fi)
 		return -FI_EINVAL;
 
 	ret = ofi_check_domain_attr(&vrb_prov, fabric->api_version,
 				    fi->domain_attr, info);
 	if (ret)
-		return ret;
+		goto err0;
 
 	_domain = calloc(1, sizeof *_domain);
-	if (!_domain)
-		return -FI_ENOMEM;
+	if (!_domain) {
+		ret = -FI_ENOMEM;
+		goto err0;
+	}
 
 	ret = ofi_domain_init(fabric, info, &_domain->util_domain, context,
 			      OFI_LOCK_MUTEX);
@@ -438,6 +440,8 @@ err2:
 		VRB_WARN(FI_LOG_DOMAIN, "ofi_domain_close fails");
 err1:
 	free(_domain);
+err0:
+	fi_freeinfo(fi);
 	return ret;
 }
 
@@ -481,6 +485,7 @@ static int vrb_fabric_close(fid_t fid)
 	ret = ofi_fabric_close(&fab->util_fabric);
 	if (ret)
 		return ret;
+	fi_freeinfo(fab->info);
 	free(fab);
 
 	return 0;
@@ -525,7 +530,7 @@ int vrb_fabric(struct fi_fabric_attr *attr, struct fid_fabric **fabric,
 		return ret;
 	}
 
-	fab->info = cur;
+	fab->info = fi_dupinfo(cur);
 
 	*fabric = &fab->util_fabric.fabric_fid;
 	(*fabric)->fid.fclass = FI_CLASS_FABRIC;

prov/verbs/src/verbs_eq.c
@@ -44,14 +44,14 @@ struct vrb_sidr_conn_key {
 	bool			recip;
 };
 
-const struct fi_info *
+struct fi_info *
 vrb_get_verbs_info(const struct fi_info *ilist, const char *domain_name)
 {
 	const struct fi_info *fi;
 
 	for (fi = ilist; fi; fi = fi->next) {
 		if (!strcmp(fi->domain_attr->name, domain_name))
-			return fi;
+			return fi_dupinfo(fi);
 	}
 
 	return NULL;

prov/verbs/src/verbs_ofi.h
@@ -270,10 +270,9 @@ struct verbs_dev_info {
 	struct dlist_entry addrs;
 };
 
-
 struct vrb_fabric {
 	struct util_fabric	util_fabric;
-	const struct fi_info	*info;
+	struct fi_info		*info;
 	struct util_ns		name_server;
 };
 
@@ -863,8 +862,8 @@ int vrb_read_params(void);
 int vrb_getinfo(uint32_t version, const char *node, const char *service,
 		   uint64_t flags, const struct fi_info *hints,
 		   struct fi_info **info);
-const struct fi_info *vrb_get_verbs_info(const struct fi_info *ilist,
-					    const char *domain_name);
+struct fi_info *vrb_get_verbs_info(const struct fi_info *ilist,
+				   const char *domain_name);
 int vrb_set_rai(uint32_t addr_format, void *src_addr, size_t src_addrlen,
 		void *dest_addr, size_t dest_addrlen, uint64_t flags,
 		struct rdma_addrinfo *rai);

commit 648624e13806c35b78a8a370ec5904bc95acc982

prov/verbs: Fix memory leak of fi_info structure in vrb_domain()
The recent commit d15a7701d ("prov/verbs: Make a copy of the provider info
in the fabric object) introduced a regression: the pointer returned by
vrb_get_verbs_info() is never freed.

Rather than adding a missing call to fi_freeinfo(), I believe it's
cleaner to move the domain attribute check directly into the function itself,
while holding the lock.

For such a simple check, duplicating the fi_info structure is unnecessary.

Note: Leak found by ASAN.

Signed-off-by: Sylvain Didelot <sdidelot@ddn.com>

prov/verbs/src/verbs_domain.c
@@ -308,21 +308,23 @@ static struct fi_ops_domain vrb_dgram_domain_ops = {
 	.query_collective = fi_no_query_collective,
 };
 
-static struct fi_info *
-vrb_get_verbs_info(const char *domain_name)
+static int
+vrb_check_verbs_info(struct fid_fabric *fabric, struct fi_info *info)
 {
 	const struct fi_info *fi;
+	int ret = -FI_ENODATA;
 
 	ofi_mutex_lock(&vrb_info_mutex);
 	for (fi = vrb_util_prov.info; fi; fi = fi->next) {
-		if (!strcmp(fi->domain_attr->name, domain_name)) {
-			ofi_mutex_unlock(&vrb_info_mutex);
-			return fi_dupinfo(fi);
+		if (!strcmp(fi->domain_attr->name, info->domain_attr->name)) {
+			ret = ofi_check_domain_attr(&vrb_prov, fabric->api_version,
+						    fi->domain_attr, info);
+			break;
 		}
 	}
 	ofi_mutex_unlock(&vrb_info_mutex);
 
-	return NULL;
+	return ret;
 }
 
 static int
@@ -341,20 +343,13 @@ vrb_domain(struct fid_fabric *fabric, struct fi_info *info,
 	struct vrb_fabric *fab =
 		 container_of(fabric, struct vrb_fabric,
 			      util_fabric.fabric_fid);
-	struct fi_info *fi = vrb_get_verbs_info(info->domain_attr->name);
-	if (!fi)
-		return -FI_EINVAL;
-
-	ret = ofi_check_domain_attr(&vrb_prov, fabric->api_version,
-				    fi->domain_attr, info);
+	ret = vrb_check_verbs_info(fabric, info);
 	if (ret)
-		goto err0;
+		return ret;
 
 	_domain = calloc(1, sizeof *_domain);
-	if (!_domain) {
-		ret = -FI_ENOMEM;
-		goto err0;
-	}
+	if (!_domain)
+		return -FI_ENOMEM;
 
 	ret = ofi_domain_init(fabric, info, &_domain->util_domain, context,
 			      OFI_LOCK_MUTEX);
@@ -455,8 +450,6 @@ err2:
 		VRB_WARN(FI_LOG_DOMAIN, "ofi_domain_close fails");
 err1:
 	free(_domain);
-err0:
-	fi_freeinfo(fi);
 	return ret;
 }
 