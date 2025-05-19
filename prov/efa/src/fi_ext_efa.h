1b397d4e86df3083b2207149d8a1ec586e9edf56
Jessie Yang <jiaxiyan@amazon.com>
2025-05-19 15:59:10 -0700

    prov/efa: Extend domain ops to allow querying of QP and CQ attributes
    Following rdma core changes, enable querying of QP work queue and
    completion queue's attributes in domain operations, which can be
    used for accelerator driven datapath.
    
    Signed-off-by: Jessie Yang <jiaxiyan@amazon.com>

--- prov/efa/src/fi_ext_efa.h
+++ prov/efa/src/fi_ext_efa.h
@@ -21,10 +21,28 @@ enum {
     FI_EFA_MR_ATTR_RDMA_RECV_IC_ID = 1 << 2,
 };
 
+struct fi_efa_wq_attr {
+    uint8_t *buffer;
+    uint32_t entry_size;
+    uint32_t num_entries;
+    uint32_t *doorbell;
+    uint32_t max_batch;
+};
+
+struct fi_efa_cq_attr {
+    uint8_t *buffer;
+    uint32_t entry_size;
+    uint32_t num_entries;
+};
+
 struct fi_efa_ops_domain {
 	int (*query_mr)(struct fid_mr *mr, struct fi_efa_mr_attr *mr_attr);
 	int (*query_addr)(struct fid_ep *ep_fid, fi_addr_t addr, uint16_t *ahn,
 			  uint16_t *remote_qpn, uint32_t *remote_qkey);
+	int (*query_qp_wqs)(struct fid_ep *ep_fid,
+			    struct fi_efa_wq_attr *sq_attr,
+			    struct fi_efa_wq_attr *rq_attr);
+	int (*query_cq)(struct fid_cq *cq_fid, struct fi_efa_cq_attr *cq_attr);
 };
 
 #endif /* _FI_EXT_EFA_H_ */
