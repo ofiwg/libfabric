269a0ef372d68a4a209e4dffda651c84f9ff7f60
Sai Sunku <sunkusa@amazon.com>
2025-05-28 17:23:42 +0000

    [v2.1.x] fabtests/efa: Make FI_EFA_IFACE tests check the Libfabric build
    Calling `/opt/amazon/efa/bin/fi_info` checks the fi_info from the EFA
    installer. This fabtest is supposed to check the getinfo path in the
    current Libfabric build. This commit checks the current build by
    printing the output from fi_efa_info_test
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>
    (cherry picked from commit fa156f8b3e528f26c89604b362c4c69e2b5d58c1)

--- fabtests/prov/efa/src/efa_info_test.c
+++ fabtests/prov/efa/src/efa_info_test.c
@@ -35,6 +35,22 @@
 #include <getopt.h>
 #include <shared.h>
 
+static int print_short_info(struct fi_info *info)
+{
+	struct fi_info *cur;
+
+	for (cur = info; cur; cur = cur->next) {
+		printf("provider: %s\n", cur->fabric_attr->prov_name);
+		printf("    fabric: %s\n", cur->fabric_attr->name),
+		printf("    domain: %s\n", cur->domain_attr->name),
+		printf("    version: %d.%d\n", FI_MAJOR(cur->fabric_attr->prov_version),
+			FI_MINOR(cur->fabric_attr->prov_version));
+		printf("    type: %s\n", fi_tostr(&cur->ep_attr->type, FI_TYPE_EP_TYPE));
+		printf("    protocol: %s\n", fi_tostr(&cur->ep_attr->protocol, FI_TYPE_PROTOCOL));
+	}
+	return EXIT_SUCCESS;
+}
+
 int main(int argc, char **argv)
 {
 	int ret = 0, op;
@@ -74,6 +90,8 @@ int main(int argc, char **argv)
 		FT_PRINTERR("ft_getinfo", -ret);
 		goto out;
 	}
+
+	print_short_info(fi);
 
 	info = fi;
 	while (NULL != info) {
