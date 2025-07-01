bfe7b301ee4980b532c214be5d5c632855fa4469
Shi Jin <sjina@amazon.com>
2025-07-01 23:51:37 +0000

    fabtests/efa: Add multi_ep_mt into pytest suites
    Make this test not run by default for now before it
    becomes stable.
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- fabtests/pytest/efa/test_multi_ep.py
+++ fabtests/pytest/efa/test_multi_ep.py
@@ -1,11 +1,11 @@
 import pytest
+from common import ClientServerTest
 
 @pytest.mark.functional
 @pytest.mark.parametrize("shared_cq", [True, False])
 @pytest.mark.parametrize("shared_av", [True, False])
 def test_multi_ep(cmdline_args, shared_cq, shared_av, rma_fabric):
     # This test requests FI_RMA
-    from common import ClientServerTest
     cmd = "fi_multi_ep -e rdm"
     if shared_cq:
         cmd += " -Q"
@@ -14,3 +14,11 @@ def test_multi_ep(cmdline_args, shared_cq, shared_av, rma_fabric):
     test = ClientServerTest(cmdline_args, cmd, message_size=256, fabric=rma_fabric)
     test.run()
 
+
+# TODO: expand to different meory types
+@pytest.mark.unstable
+@pytest.mark.parametrize("msg_size", [16, 8192, 16384, 1048576])
+def test_multi_ep_mt(cmdline_args, msg_size):
+    cmd = f"fi_efa_multi_ep_mt -e rdm -c 20 -I 100"
+    test = ClientServerTest(cmdline_args, cmd, message_size=msg_size, fabric="efa", timeout=30, additional_env="FI_EFA_ENABLE_SHM_TRANSFER=0")
+    test.run()
