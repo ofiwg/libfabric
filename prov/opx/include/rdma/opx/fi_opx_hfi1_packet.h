c4152caa0505908437aaf7475dc8a184e691d001
Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>
2025-07-08 16:26:42 -0400

    prov/opx: Fix FI_REMOTE_CQ_DATA write
    This fix resolves an issue in OPX with write calls when FI_REMOTE_CQ_DATA is set.
    
    Signed-off-by: Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>
    (cherry picked from commit 519e5494f3fc044d0c0130dfbb427d9399b33ca6)

--- prov/opx/include/rdma/opx/fi_opx_hfi1_packet.h
+++ prov/opx/include/rdma/opx/fi_opx_hfi1_packet.h
@@ -983,7 +983,14 @@ union opx_hfi1_packet_hdr {
 
 	/*    OPX RMA RTS HEADER                   */
 	struct {
-		uint64_t reserved[5]; /* QW[0-4] */
+		uint64_t reserved[2]; /* QW[0-1] */
+
+		/* QW[2] BTH (unused)*/
+		uint16_t reserved_1[3];
+		uint16_t origin_rx;
+
+		/* QW[3-4] BTH/KDETH */
+		uint64_t reserved_3[2];
 
 		/* QW[5] SW */
 		uint8_t	 opcode;
