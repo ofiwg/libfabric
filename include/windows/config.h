20a6b965d806b402a476a3770bb0a8480a261360
Jessie Yang <jiaxiyan@amazon.com>
2025-08-12 16:11:52 -0700

    [v2.2.x]prov/efa: Support blocking cq read in efa-direct
    Implement fi_cq_sread and fi_cq_signal to support blocking completion
    queue operations with timeout and signaling capabilities.
    Support FI_WAIT_UNSPEC, FI_CQ_COND_NONE and FI_CQ_COND_THRESHOLD.
    Check for ibv_create_comp_channel and ibv_get_cq_event symbols to
    make it compile on windows.
    
    Signed-off-by: Jessie Yang <jiaxiyan@amazon.com>
    (cherry picked from commit a53cd09285a10ebb8ac0438027c4350d3b3ef501)

--- include/windows/config.h
+++ include/windows/config.h
@@ -43,6 +43,9 @@
 /* Indicates if ibv_reg_dmabuf_mr verbs is available */
 #define HAVE_EFA_DMABUF_MR 0
 
+/* Indicates if ibv functions for cq notification are available */
+#define HAVE_EFA_CQ_NOTIFICATION 0
+
 /* Define to 1 if host_clock_get_service is available. */
 /* #undef HAVE_HOST_GET_CLOCK_SERVICE */
 
