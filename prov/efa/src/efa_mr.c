a6586529b8a723068517851729d4b0f57a6ba177
Darryl Abbate <drl@amazon.com>
2025-09-02 14:41:53 -0700

    [v2.4.x] prov/efa: Flatten `efa_mr_peer.device`
    This was ostensibly designed to match fi_mr_attr.device, but should
    instead be treated more like a subset of ofi_mr_info, where device is
    simply a uint64_t. Aliasing fi_mr_attr.device is already silly since
    fi_mr_attr.iface should tell you the device type.
    
    Signed-off-by: Darryl Abbate <drl@amazon.com>
    (cherry picked from commit 8cae0a4060284dc98ea9d3964879dfd0ff7bd0ca)

--- prov/efa/src/efa_mr.c
+++ prov/efa/src/efa_mr.c
@@ -213,13 +213,12 @@ static int efa_mr_hmem_setup(struct efa_mr *efa_mr,
 		efa_mr->peer.iface = FI_HMEM_SYSTEM;
 	}
 
-	/* efa_mr->peer.device is an union. Setting reserved to 0 cleared everything in it (cuda, neuron, synapseai etc) */
-	efa_mr->peer.device.reserved = 0;
+	efa_mr->peer.device = 0;
 	efa_mr->peer.flags &= ~OFI_HMEM_DATA_DEV_REG_HANDLE;
 	efa_mr->peer.hmem_data = NULL;
 	if (efa_mr->peer.iface == FI_HMEM_CUDA) {
 		efa_mr->needs_sync = true;
-		efa_mr->peer.device.cuda = attr->device.cuda;
+		efa_mr->peer.device = attr->device.cuda;
 
 		/* Only attempt GDRCopy registrations for efa rdm path */
 		if (efa_mr->domain->info_type == EFA_INFO_RDM && !(flags & FI_MR_DMABUF) && cuda_is_gdrcopy_enabled()) {
@@ -237,9 +236,9 @@ static int efa_mr_hmem_setup(struct efa_mr *efa_mr,
 			}
 		}
 	} else if (attr->iface == FI_HMEM_NEURON) {
-		efa_mr->peer.device.neuron = attr->device.neuron;
+		efa_mr->peer.device = attr->device.neuron;
 	} else if (attr->iface == FI_HMEM_SYNAPSEAI) {
-		efa_mr->peer.device.synapseai = attr->device.synapseai;
+		efa_mr->peer.device = attr->device.synapseai;
 	}
 
 	return FI_SUCCESS;
