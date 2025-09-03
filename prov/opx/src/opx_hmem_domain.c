de72ec59f130066dafffd0ee955384c71395036b
Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>
2025-09-03 13:23:50 -0400

    [v2.3.x] prov/opx: Only open IPC cache if HMEM initialized and IPC enabled
    An OPX build with ROCR resulted in a segmentation fault for a CPU only memory workload because of an error opening the IPC cache when ROCR IPC is enabled, but ROCR HMEM was not initialized. The log shows:
    	opx:domain:opx_hmem_open_domain():191<warn> Error opening IPC Cache ret=-38 (Unknown error -38).
    
    The OPX open domain path has been updated to only open the IPC cache if HMEM is initialized and IPC is enabled.
    
    Signed-off-by: Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>
    (cherry picked from commit 95a9e8da02cabbb96820fa90f7c78247ed4135cf)

--- prov/opx/src/opx_hmem_domain.c
+++ prov/opx/src/opx_hmem_domain.c
@@ -192,11 +192,15 @@ int opx_hmem_open_domain(struct opx_hmem_fabric *hmem_fabric, struct fi_info *in
 		return ret;
 	}
 
-	ret = ofi_ipc_cache_open(&new_hmem_domain->ipc_cache, &new_hmem_domain->util_domain);
-	if (ret) {
-		opx_hmem_close_domain(new_hmem_domain, 0);
-		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "Error opening IPC Cache ret=%d (%s)\n", ret, strerror(ret));
-		return ret;
+	if ((hmem_ops[FI_HMEM_CUDA].initialized && ofi_hmem_is_ipc_enabled(FI_HMEM_CUDA)) ||
+	    (hmem_ops[FI_HMEM_ROCR].initialized && ofi_hmem_is_ipc_enabled(FI_HMEM_ROCR))) {
+		ret = ofi_ipc_cache_open(&new_hmem_domain->ipc_cache, &new_hmem_domain->util_domain);
+		if (ret) {
+			opx_hmem_close_domain(new_hmem_domain, 0);
+			FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "Error opening IPC Cache ret=%d (%s)\n", ret,
+				strerror(ret));
+			return ret;
+		}
 	}
 
 	ret = ofi_bufpool_create(&new_hmem_domain->hmem_stream.event_pool, sizeof(union opx_hmem_event), 0, UINT_MAX,
