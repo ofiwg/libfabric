5c21e2bd5727609d752b799fd6d40f32efec2130
Ben Lynam <ben.lynam@cornelisnetworks.com>
2025-10-07 14:50:08 -0500

    prov/opx: Register MRs with HFI service
    Signed-off-by: Ben Lynam <Ben.Lynam@cornelisnetworks.com>

--- prov/opx/include/rdma/opx/fi_opx_domain.h
+++ prov/opx/include/rdma/opx/fi_opx_domain.h
@@ -110,7 +110,20 @@ struct fi_opx_domain {
 
 #ifdef HFISVC
 	struct {
-		hfisvc_client_t	    handle;
+		hfisvc_client_t handle;
+
+		/**
+		 * @brief Command queue used by the domain for issuing commands to the
+		 * hfisvc where we are opening/closing hfisvc memory regions.
+		 */
+		hfisvc_client_command_queue_t mr_command_queue;
+
+		/**
+		 * @brief Completion queue used by the domain for handling completions from the
+		 * hfisvc where we are opening/closing hfisvc memory regions.
+		 */
+		hfisvc_client_completion_queue_t mr_completion_queue;
+
 		opx_hfisvc_keyset_t access_key_set;
 		int64_t		    ref_cnt;
 		hfisvc_client_key_t client_key;
@@ -144,21 +157,53 @@ struct fi_opx_av {
 	uint32_t	   table_count; /* table, not av, count */
 };
 
-struct fi_opx_mr {
-	struct fid_mr	      mr_fid;
-	struct fi_opx_domain *domain;
-	struct fi_mr_attr     attr;
-	struct iovec	      iov;
-	uint64_t	      flags;
-	uint64_t	      cntr_bflags;
-	struct fi_opx_cntr   *cntr;
-	struct fi_opx_ep     *ep;
-	uint64_t	      hmem_dev_reg_handle;
-	uint8_t		      hmem_unified;
-	uint8_t		      unused[7];
-	UT_hash_handle	      hh;
-	void		     *base_addr;
+enum opx_mr_hfisvc_state {
+	OPX_MR_HFISVC_NOT_REGISTERED = 0,
+	OPX_MR_HFISVC_PENDING_OPEN,
+	OPX_MR_HFISVC_OPENED,
+	OPX_MR_HFISVC_PENDING_CLOSE,
+	OPX_MR_HFISVC_CLOSED,
 };
+
+struct fi_opx_mr {
+	/* == CACHE LINE 0-2 == */
+	struct fid_mr	      mr_fid; // 40 bytes
+	struct fi_mr_attr     attr;   // 112 bytes
+	struct fi_opx_domain *domain;
+	struct fi_opx_ep     *ep;
+	void		     *base_addr;
+	struct iovec	      iov;
+
+	/* == CACHE LINE 3 == */
+	uint64_t	    flags;
+	uint64_t	    cntr_bflags;
+	struct fi_opx_cntr *cntr;
+	struct {
+		union {
+			uint32_t reserved;
+#ifdef HFISVC
+			hfisvc_client_mr_t mr_handle;
+#endif
+		};
+		enum opx_mr_hfisvc_state state;
+		uint32_t		 access_key;
+		uint32_t		 padding;
+	} hfisvc;
+	uint64_t hmem_dev_reg_handle;
+	uint8_t	 hmem_unified;
+	uint8_t	 unused[7];
+	uint64_t unused_cacheline3_qw;
+
+	/* == CACHE LINE 4 == */
+	UT_hash_handle hh; // 56 bytes
+	uint64_t       unused_cacheline4_qw;
+} __attribute__((__aligned__(FI_OPX_CACHE_LINE_SIZE))) __attribute__((__packed__));
+OPX_COMPILE_TIME_ASSERT(sizeof(struct fi_opx_mr) == (FI_OPX_CACHE_LINE_SIZE * 5),
+			"Size of fi_opx_mr should be 5 cachelines!");
+OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_mr, flags) == (FI_OPX_CACHE_LINE_SIZE * 3),
+			"Offset of fi_opx_mr->flags should start at cacheline 3!");
+OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_mr, hh) == (FI_OPX_CACHE_LINE_SIZE * 4),
+			"Offset of fi_opx_mr->hh should start at cacheline 4!");
 
 static inline uint32_t fi_opx_domain_get_tx_max(struct fid_domain *domain)
 {
@@ -171,6 +216,42 @@ static inline uint32_t fi_opx_domain_get_rx_max(struct fid_domain *domain)
 }
 
 #ifdef HFISVC
+
+__OPX_FORCE_INLINE__
+void opx_domain_hfisvc_poll(struct fi_opx_domain *opx_domain)
+{
+	struct hfisvc_client_cq_entry hfisvc_out[64];
+	size_t n = hfisvc_client_cq_read(opx_domain->hfisvc.mr_completion_queue, 0ul /* flags */, hfisvc_out, 64);
+	while (n > 0) {
+		for (size_t i = 0; i < n; ++i) {
+			if (hfisvc_out[i].status != HFISVC_CLIENT_CQ_ENTRY_STATUS_SUCCESS) {
+				// TODO: FI_WARN, post some kind of error to the error queue
+				fprintf(stderr, "Completion error: status was %d\n", hfisvc_out[i].status);
+				abort();
+			}
+			assert(hfisvc_out[i].status == HFISVC_CLIENT_CQ_ENTRY_STATUS_SUCCESS);
+			assert(hfisvc_out[i].type == HFISVC_CLIENT_CQ_ENTRY_TYPE_MR);
+
+			struct fi_opx_mr  *opx_mr    = (struct fi_opx_mr *) hfisvc_out[i].app_context;
+			hfisvc_client_mr_t mr_handle = hfisvc_out[i].type_mr.mr;
+
+			if (opx_mr->hfisvc.state == OPX_MR_HFISVC_PENDING_OPEN) {
+				opx_mr->hfisvc.mr_handle = mr_handle;
+				opx_mr->hfisvc.state	 = OPX_MR_HFISVC_OPENED;
+			} else if (opx_mr->hfisvc.state == OPX_MR_HFISVC_PENDING_CLOSE) {
+				assert(opx_mr->hfisvc.mr_handle == mr_handle);
+				opx_mr->hfisvc.state = OPX_MR_HFISVC_CLOSED;
+			} else {
+				// TODO: FI_WARN, post some kind of error to the error queue
+				fprintf(stderr, "(%d) %s:%s():%d Got unexpected completion for opx_mr=%p state=%d\n",
+					getpid(), __FILE__, __func__, __LINE__, opx_mr, opx_mr->hfisvc.state);
+				abort();
+			}
+		}
+		n = hfisvc_client_cq_read(opx_domain->hfisvc.mr_completion_queue, 0ul /* flags */, hfisvc_out, 64);
+	}
+}
+
 int opx_domain_hfisvc_init(struct fi_opx_domain *domain, const enum hfisvc_client_connect_type type, const int fd);
 #endif
 
