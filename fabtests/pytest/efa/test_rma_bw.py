cf08fd7a7eb62a8e4e4304a65c595d8567a4e561
Shi Jin <sjina@amazon.com>
2025-10-16 20:47:29 +0000

    [v2.3.x] fabtests/pytest/efa: Run FI_MORE test with more iterations
    short iteration types only run 5 iterations, which doesn't have enough
    coverage for the FI_MORE use cases which should be able to work for wqe
    cnt for what ever larger number
    
    Signed-off-by: Shi Jin <sjina@amazon.com>
    (cherry picked from commit 5b48de24af4a65fc31c1c5c387ad92b6a9f5afb8)

--- fabtests/pytest/efa/test_rma_bw.py
+++ fabtests/pytest/efa/test_rma_bw.py
@@ -71,12 +71,16 @@ def test_rma_bw_1G(cmdline_args, operation_type, rma_bw_completion_semantic):
 
 @pytest.mark.functional
 @pytest.mark.parametrize("operation_type", ["writedata", "write"])
-def test_rma_bw_use_fi_more(cmdline_args, operation_type, rma_bw_completion_semantic, inject_message_size, direct_rma_size, rma_fabric):
+@pytest.mark.parametrize("iteration_type",
+                         ["5", # smaller than max batch wqe cnt (16)
+                          "48", # larger than max batch wqe cnt
+                          "128"]) # larger than window size (64)
+def test_rma_bw_use_fi_more(cmdline_args, operation_type, iteration_type, rma_bw_completion_semantic, inject_message_size, direct_rma_size, rma_fabric):
     command = "fi_rma_bw -e rdm -j 0 --use-fi-more"
     command = command + " -o " + operation_type
     # rma_bw test with data verification takes longer to finish
     timeout = max(540, cmdline_args.timeout)
-    efa_run_client_server_test(cmdline_args, command, "short", rma_bw_completion_semantic,
+    efa_run_client_server_test(cmdline_args, command, iteration_type, rma_bw_completion_semantic,
                                "host_to_host", direct_rma_size if rma_fabric == "efa-direct" else inject_message_size,
                                timeout=timeout, fabric=rma_fabric)
 
