ofiwg/libfabric

commit 10ca04b191cb05467b3f5f562ee52565d03c9735

prov/efa: Optimize the WQE post in data path direct
Replace WR session-based API with consolidated post_send/read/write
functions that construct WQE locally as stack variables and use
word-by-word assignment instead of mmio_write64.

Also implement such consolidated post interface for rdma-core path
and use it uniformly among efa-direct and efa protocol.

The performance benefits:

1. replace the mmio_write64 into pure register movs in the WQE write
2. eliminating most branchings on various RDMA-core WR API
3. removes the is_wr_started checking on the data path direct path

Signed-off-by: Shi Jin <sjina@amazon.com>

prov/efa/src/rdm/efa_rdm_pke.c
@@ -402,7 +402,6 @@ void efa_rdm_pke_append(struct efa_rdm_pke *dst,
 ssize_t efa_rdm_pke_sendv(struct efa_rdm_pke **pkt_entry_vec,
 			  int pkt_entry_cnt, uint64_t flags)
 {
-	struct efa_qp *qp;
 	struct efa_conn *conn;
 	struct efa_rdm_ep *ep;
 	struct efa_rdm_pke *pkt_entry;
@@ -410,6 +409,10 @@ ssize_t efa_rdm_pke_sendv(struct efa_rdm_pke **pkt_entry_vec,
 	struct ibv_sge sg_list[2];  /* efa device support up to 2 iov */
 	struct ibv_data_buf inline_data_list[2];
 	int ret = 0, pkt_idx, iov_cnt;
+	bool use_inline;
+	uint64_t flags_in_loop;
+	uint16_t cq_data = 0;
+	uint32_t qpn, qkey;
 
 	assert(pkt_entry_cnt);
 	ep = pkt_entry_vec[0]->ep;
@@ -424,26 +427,14 @@ ssize_t efa_rdm_pke_sendv(struct efa_rdm_pke **pkt_entry_vec,
 	conn = pkt_entry_vec[0]->peer->conn;
 	assert(conn && conn->ep_addr);
 
-	qp = ep->base_ep.qp;
-	if (!ep->base_ep.is_wr_started) {
-		efa_qp_wr_start(qp);
-		ep->base_ep.is_wr_started = true;
-	}
 	for (pkt_idx = 0; pkt_idx < pkt_entry_cnt; ++pkt_idx) {
 		pkt_entry = pkt_entry_vec[pkt_idx];
 		assert(pkt_entry->peer == peer);
 
-		qp->ibv_qp_ex->wr_id = (uintptr_t)pkt_entry;
-		if ((pkt_entry->ope->fi_flags & FI_REMOTE_CQ_DATA) &&
-		    (pkt_entry->flags & EFA_RDM_PKE_SEND_TO_USER_RECV_QP)) {
-			/* Currently this is only expected for eager pkts */
-			assert(pkt_entry_cnt == 1);
-			efa_qp_wr_send_imm(qp, pkt_entry->ope->cq_entry.data);
-		} else {
-			efa_qp_wr_send(qp);
-		}
-		if (pkt_entry->pkt_size <= efa_rdm_ep_domain(ep)->device->efa_attr.inline_buf_size &&
-	            !efa_mr_is_hmem((struct efa_mr *)pkt_entry->payload_mr)) {
+		use_inline = (pkt_entry->pkt_size <= efa_rdm_ep_domain(ep)->device->efa_attr.inline_buf_size &&
+	            !efa_mr_is_hmem((struct efa_mr *)pkt_entry->payload_mr));
+
+		if (use_inline) {
 			iov_cnt = 1;
 			inline_data_list[0].addr = pkt_entry->wiredata;
 			inline_data_list[0].length = pkt_entry->pkt_size - pkt_entry->payload_size;
@@ -452,8 +443,6 @@ ssize_t efa_rdm_pke_sendv(struct efa_rdm_pke **pkt_entry_vec,
 				inline_data_list[1].addr = pkt_entry->payload;
 				inline_data_list[1].length = pkt_entry->payload_size;
 			}
-
-			efa_qp_wr_set_inline_data_list(qp, iov_cnt, inline_data_list);
 		} else {
 			iov_cnt = 1;
 			sg_list[0].addr = (uintptr_t)pkt_entry->wiredata;
@@ -465,18 +454,36 @@ ssize_t efa_rdm_pke_sendv(struct efa_rdm_pke **pkt_entry_vec,
 				sg_list[1].length = pkt_entry->payload_size;
 				sg_list[1].lkey = ((struct efa_mr *)pkt_entry->payload_mr)->ibv_mr->lkey;
 			}
-
-			efa_qp_wr_set_sge_list(qp, iov_cnt, sg_list);
 		}
 
+		flags_in_loop = flags;
 		if (pkt_entry->flags & EFA_RDM_PKE_SEND_TO_USER_RECV_QP) {
+			/* Currently this is only expected for eager pkts */
+			assert(pkt_entry_cnt == 1);
 			assert(peer->extra_info[0] & EFA_RDM_EXTRA_FEATURE_REQUEST_USER_RECV_QP);
-			efa_qp_wr_set_ud_addr(qp, conn->ah,
-				   peer->user_recv_qp.qpn, peer->user_recv_qp.qkey);
+			if (pkt_entry->flags & EFA_RDM_PKE_SEND_TO_USER_RECV_QP) {
+				flags_in_loop |= FI_REMOTE_CQ_DATA;
+				cq_data = pkt_entry->ope->cq_entry.data;
+			}
+			qpn = peer->user_recv_qp.qpn;
+			qkey = peer->user_recv_qp.qkey;
 		} else {
-			efa_qp_wr_set_ud_addr(qp, conn->ah,
-				   conn->ep_addr->qpn, conn->ep_addr->qkey);
+			qpn = conn->ep_addr->qpn;
+			qkey = conn->ep_addr->qkey;
 		}
+
+		/* This will make efa_qp_post_send not ring the doorbell until the last itertion of the loop */
+		if (pkt_idx != pkt_entry_cnt - 1)
+			flags_in_loop |= FI_MORE;
+
+		ret = efa_qp_post_send(ep->base_ep.qp, sg_list, inline_data_list, iov_cnt, use_inline,
+				       (uintptr_t)pkt_entry,
+				       cq_data,
+				       flags_in_loop, conn->ah,
+				       qpn, qkey);
+
+		if (OFI_UNLIKELY(ret))
+			break;
 
 #if ENABLE_DEBUG
 		dlist_insert_tail(&pkt_entry->dbg_entry, &ep->tx_pkt_list);
@@ -488,11 +495,6 @@ ssize_t efa_rdm_pke_sendv(struct efa_rdm_pke **pkt_entry_vec,
 #if HAVE_LTTNG
 		efa_rdm_tracepoint_wr_id_post_send((void *)pkt_entry);
 #endif
-	}
-
-	if (!(flags & FI_MORE)) {
-		ret = efa_qp_wr_complete(qp);
-		ep->base_ep.is_wr_started = false;
 	}
 
 	if (OFI_UNLIKELY(ret)) {
@@ -528,33 +530,34 @@ int efa_rdm_pke_read(struct efa_rdm_pke *pkt_entry,
 	struct ibv_sge sge;
 	struct efa_rdm_ope *txe;
 	int err = 0;
+	struct efa_ah *ah;
+	uint32_t qpn, qkey;
 
 	ep = pkt_entry->ep;
 	assert(ep);
+	qp = ep->base_ep.qp;
 	txe = pkt_entry->ope;
 
-	if (txe->peer == NULL)
+	if (txe->peer == NULL) {
 		pkt_entry->flags |= EFA_RDM_PKE_LOCAL_READ;
-
-	qp = ep->base_ep.qp;
-	efa_qp_wr_start(qp);
-	qp->ibv_qp_ex->wr_id = (uintptr_t)pkt_entry;
-	efa_qp_wr_rdma_read(qp, remote_key, remote_buf);
+		ah = ep->base_ep.self_ah;
+		qpn = qp->qp_num;
+		qkey = qp->qkey;
+	} else {
+		conn = pkt_entry->peer->conn;
+		assert(conn && conn->ep_addr);
+		ah = conn->ah;
+		qpn = conn->ep_addr->qpn;
+		qkey = conn->ep_addr->qkey;
+	}
 
 	sge.addr = (uint64_t)local_buf;
 	sge.length = len;
 	sge.lkey = ((struct efa_mr *)desc)->ibv_mr->lkey;
 
-	efa_qp_wr_set_sge_list(qp, 1, &sge);
-	if (txe->peer == NULL) {
-		efa_qp_wr_set_ud_addr(qp, ep->base_ep.self_ah,
-				   qp->qp_num, qp->qkey);
-	} else {
-		conn = pkt_entry->peer->conn;
-		assert(conn && conn->ep_addr);
-		efa_qp_wr_set_ud_addr(qp, conn->ah,
-				   conn->ep_addr->qpn, conn->ep_addr->qkey);
-	}
+	err = efa_qp_post_read(qp, &sge, 1, remote_key, remote_buf,
+			       (uintptr_t)pkt_entry, 0,
+			       ah, qpn, qkey);
 
 #if ENABLE_DEBUG
 	dlist_insert_tail(&pkt_entry->dbg_entry, &ep->tx_pkt_list);
@@ -570,8 +573,6 @@ int efa_rdm_pke_read(struct efa_rdm_pke *pkt_entry,
 #if HAVE_LTTNG
 	efa_rdm_tracepoint_wr_id_post_read((void *)pkt_entry);
 #endif
-
-	err = efa_qp_wr_complete(qp);
 
 	if (OFI_UNLIKELY(err))
 		return (err == ENOMEM) ? -FI_EAGAIN : -err;
@@ -606,9 +607,13 @@ int efa_rdm_pke_write(struct efa_rdm_pke *pkt_entry)
 	uint64_t remote_buf;
 	size_t remote_key;
 	int err = 0;
+	struct efa_ah *ah;
+	uint32_t qpn, qkey;
+	uint16_t cq_data = 0;
 
 	ep = pkt_entry->ep;
 	assert(ep);
+	qp = ep->base_ep.qp;
 	txe = pkt_entry->ope;
 
 	rma_context_pkt = (struct efa_rdm_rma_context_pkt *)pkt_entry->wiredata;
@@ -621,43 +626,35 @@ int efa_rdm_pke_write(struct efa_rdm_pke *pkt_entry)
 	assert(((struct efa_mr *)desc)->ibv_mr);
 
 	self_comm = (txe->peer == NULL);
-	if (self_comm)
+	if (self_comm) {
 		pkt_entry->flags |= EFA_RDM_PKE_LOCAL_WRITE;
-
-	qp = ep->base_ep.qp;
-	if (!ep->base_ep.is_wr_started) {
-		efa_qp_wr_start(qp);
-		ep->base_ep.is_wr_started = true;
+		ah = ep->base_ep.self_ah;
+		qpn = qp->qp_num;
+		qkey = qp->qkey;
+	} else {
+		conn = pkt_entry->peer->conn;
+		assert(conn && conn->ep_addr);
+		ah = conn->ah;
+		qpn = conn->ep_addr->qpn;
+		qkey = conn->ep_addr->qkey;
 	}
-	qp->ibv_qp_ex->wr_id = (uintptr_t)pkt_entry;
 
 	if (txe->fi_flags & FI_REMOTE_CQ_DATA) {
 		/* assert that we are sending the entire buffer as a
 			   single IOV when immediate data is also included. */
 		assert(len == txe->bytes_write_total_len);
-		efa_qp_wr_rdma_write_imm(qp, remote_key, remote_buf,
-				      txe->cq_entry.data);
-	} else {
-		efa_qp_wr_rdma_write(qp, remote_key, remote_buf);
+		cq_data = txe->cq_entry.data;
 	}
 
 	sge.addr = (uint64_t)local_buf;
 	sge.length = len;
 	sge.lkey = ((struct efa_mr *)desc)->ibv_mr->lkey;
 
-	/* As an optimization, we should consider implementing multiple-
-		   iov writes using an IBV wr with multiple sge entries.
-		   For now, each WR contains only one sge. */
-	efa_qp_wr_set_sge_list(qp, 1, &sge);
-	if (self_comm) {
-		efa_qp_wr_set_ud_addr(qp, ep->base_ep.self_ah,
-				   qp->qp_num, qp->qkey);
-	} else {
-		conn = pkt_entry->peer->conn;
-		assert(conn && conn->ep_addr);
-		efa_qp_wr_set_ud_addr(qp, conn->ah,
-				   conn->ep_addr->qpn, conn->ep_addr->qkey);
-	}
+	err = efa_qp_post_write(qp, &sge, 1, remote_key, remote_buf,
+				(uintptr_t)pkt_entry,
+				cq_data,
+				txe->fi_flags,
+				ah, qpn, qkey);
 
 #if ENABLE_DEBUG
 	dlist_insert_tail(&pkt_entry->dbg_entry, &ep->tx_pkt_list);
@@ -665,11 +662,6 @@ int efa_rdm_pke_write(struct efa_rdm_pke *pkt_entry)
 #if HAVE_LTTNG
 	efa_rdm_tracepoint_wr_id_post_write((void *)pkt_entry);
 #endif
-
-	if (!(txe->fi_flags & FI_MORE)) {
-		err = efa_qp_wr_complete(qp);
-		ep->base_ep.is_wr_started = false;
-	}
 
 	if (OFI_UNLIKELY(err))
 		return (err == ENOMEM) ? -FI_EAGAIN : -err;

prov/efa/Makefile.include
@@ -189,16 +189,9 @@ prov_efa_test_efa_unit_test_LDFLAGS = $(cmocka_rpath) $(efa_LDFLAGS) $(cmocka_LD
 					-Wl,--wrap=efa_rdm_ope_post_send \
 					-Wl,--wrap=efa_device_support_unsolicited_write_recv \
 					-Wl,--wrap=efa_qp_post_recv \
-					-Wl,--wrap=efa_qp_wr_complete \
-					-Wl,--wrap=efa_qp_wr_send \
-					-Wl,--wrap=efa_qp_wr_start \
-					-Wl,--wrap=efa_qp_wr_rdma_read \
-					-Wl,--wrap=efa_qp_wr_rdma_write \
-					-Wl,--wrap=efa_qp_wr_rdma_write_imm \
-					-Wl,--wrap=efa_qp_wr_send_imm \
-					-Wl,--wrap=efa_qp_wr_set_inline_data_list \
-					-Wl,--wrap=efa_qp_wr_set_sge_list \
-					-Wl,--wrap=efa_qp_wr_set_ud_addr \
+					-Wl,--wrap=efa_qp_post_send \
+					-Wl,--wrap=efa_qp_post_read \
+					-Wl,--wrap=efa_qp_post_write \
 					-Wl,--wrap=efa_ibv_cq_start_poll \
 					-Wl,--wrap=efa_ibv_cq_next_poll \
 					-Wl,--wrap=efa_ibv_cq_end_poll \

prov/efa/src/efa_data_path_direct_entry.h
@@ -120,267 +120,9 @@ ring_db:
 	return err;
 }
 
-/**
- * @brief Complete a work request session
- *
- * Finalizes the current work request session by posting any pending
- * work queue entries to hardware and handling error conditions.
- * This function mirrors efa_send_wr_complete functionality.
- *
- * @param qp Pointer to the EFA queue pair
- * @return 0 on success, error code if session had errors
- */
-static inline int efa_data_path_direct_wr_complete(struct efa_qp *qp)
-{
-	/* See: efa_send_wr_complete. */
 
-	struct efa_data_path_direct_sq *sq = &qp->data_path_direct_qp.sq;
 
-	if (OFI_UNLIKELY(qp->data_path_direct_qp.wr_session_err)) {
-		sq->wq.wqe_posted -= sq->num_wqe_pending;
-		sq->wq.pc -= sq->num_wqe_pending;
-		goto out;
-	}
-	/* it should not be possible to get here with sq->num_wqe_pending==0 */
-	assert(sq->num_wqe_pending);
 
-	efa_data_path_direct_send_wr_post_working(qp, sq, true);
-
-out:
-	return qp->data_path_direct_qp.wr_session_err;
-}
-
-/**
- * @brief Prepare an RDMA read work request
- *
- * Sets up a work queue entry for an RDMA read operation, configuring
- * the remote memory key and address for the read operation.
- *
- * @param efaqp Pointer to the EFA queue pair
- * @param rkey Remote memory key for the target buffer
- * @param remote_addr Remote memory address to read from
- */
-static inline void efa_data_path_direct_wr_rdma_read(struct efa_qp *efaqp,
-						     uint32_t rkey,
-						     uint64_t remote_addr)
-{
-	struct efa_io_tx_wqe *tx_wqe;
-
-	tx_wqe = efa_data_path_direct_send_wr_common(efaqp, EFA_IO_RDMA_READ);
-	if (OFI_UNLIKELY(!tx_wqe))
-		return;
-
-	efa_send_wr_set_rdma_addr(tx_wqe, rkey, remote_addr);
-}
-
-/**
- * @brief Prepare an RDMA write work request
- *
- * Sets up a work queue entry for an RDMA write operation, configuring
- * the remote memory key and address for the write operation.
- *
- * @param efaqp Pointer to the EFA queue pair
- * @param rkey Remote memory key for the target buffer
- * @param remote_addr Remote memory address to write to
- */
-static inline void efa_data_path_direct_wr_rdma_write(struct efa_qp *efaqp,
-						      uint32_t rkey,
-						      uint64_t remote_addr)
-{
-	struct efa_io_tx_wqe *tx_wqe;
-
-	tx_wqe = efa_data_path_direct_send_wr_common(efaqp, EFA_IO_RDMA_WRITE);
-	if (OFI_UNLIKELY(!tx_wqe))
-		return;
-
-	efa_send_wr_set_rdma_addr(tx_wqe, rkey, remote_addr);
-}
-
-/**
- * @brief Prepare an RDMA write with immediate data work request
- *
- * Sets up a work queue entry for an RDMA write operation with immediate
- * data, configuring the remote memory key, address, and immediate data.
- *
- * @param efaqp Pointer to the EFA queue pair
- * @param rkey Remote memory key for the target buffer
- * @param remote_addr Remote memory address to write to
- * @param imm_data Immediate data to include with the write
- */
-static inline void efa_data_path_direct_wr_rdma_write_imm(struct efa_qp *efaqp,
-							  uint32_t rkey,
-							  uint64_t remote_addr,
-							  __be32 imm_data)
-{
-	struct efa_io_tx_wqe *tx_wqe;
-
-	tx_wqe = efa_data_path_direct_send_wr_common(efaqp, EFA_IO_RDMA_WRITE);
-	if (OFI_UNLIKELY(!tx_wqe))
-		return;
-
-	efa_send_wr_set_rdma_addr(tx_wqe, rkey, remote_addr);
-	efa_send_wr_set_imm_data(tx_wqe, imm_data);
-}
-
-/**
- * @brief Prepare a send work request
- *
- * Sets up a work queue entry for a basic send operation.
- *
- * @param efaqp Pointer to the EFA queue pair
- */
-static inline void efa_data_path_direct_wr_send(struct efa_qp *efaqp)
-{
-	efa_data_path_direct_send_wr_common(efaqp, EFA_IO_SEND);
-}
-
-/**
- * @brief Prepare a send with immediate data work request
- *
- * Sets up a work queue entry for a send operation with immediate data.
- *
- * @param efaqp Pointer to the EFA queue pair
- * @param imm_data Immediate data to include with the send
- */
-static inline void efa_data_path_direct_wr_send_imm(struct efa_qp *efaqp, __be32 imm_data)
-{
-	struct efa_io_tx_wqe *tx_wqe;
-
-	tx_wqe = efa_data_path_direct_send_wr_common(efaqp, EFA_IO_SEND);
-	if (OFI_UNLIKELY(!tx_wqe))
-		return;
-
-	efa_send_wr_set_imm_data(tx_wqe, imm_data);
-}
-
-/**
- * @brief Set inline data for the current work request
- *
- * Copies data from the provided buffer list directly into the work queue
- * entry for inline transmission. This avoids the need for separate DMA
- * operations for small data transfers.
- *
- * @param efa_qp Pointer to the EFA queue pair
- * @param num_buf Number of buffers in the buffer list
- * @param buf_list Array of data buffers to copy inline
- */
-static inline void
-efa_data_path_direct_wr_set_inline_data_list(struct efa_qp *efa_qp,
-					     size_t num_buf,
-					     const struct ibv_data_buf *buf_list)
-{
-	struct efa_data_path_direct_qp *qp = &efa_qp->data_path_direct_qp;
-	struct efa_io_tx_wqe *tx_wqe = &qp->sq.curr_tx_wqe;
-	uint32_t total_length = 0;
-	uint32_t length;
-	size_t i;
-
-	if (OFI_UNLIKELY(qp->wr_session_err))
-		return;
-
-	for (i = 0; i < num_buf; i++) {
-		length = buf_list[i].length;
-
-		memcpy(tx_wqe->data.inline_data + total_length,
-		       buf_list[i].addr, length);
-		total_length += length;
-	}
-
-	EFA_SET(&tx_wqe->meta.ctrl1, EFA_IO_TX_META_DESC_INLINE_MSG, 1);
-	tx_wqe->meta.length = total_length;
-}
-
-/**
- * @brief Set scatter-gather list for the current work request
- *
- * Configures the scatter-gather elements for the current work request,
- * handling different operation types (SEND, RDMA_READ, RDMA_WRITE) with
- * appropriate buffer descriptor setup.
- *
- * @param efa_qp Pointer to the EFA queue pair
- * @param num_sge Number of scatter-gather elements
- * @param sg_list Array of scatter-gather elements
- */
-static inline void efa_data_path_direct_wr_set_sge_list(struct efa_qp *efa_qp,
-							size_t num_sge,
-							const struct ibv_sge *sg_list)
-{
-	struct efa_data_path_direct_qp *qp = &efa_qp->data_path_direct_qp;
-	struct efa_io_rdma_req *rdma_req;
-	struct efa_io_tx_wqe *tx_wqe;
-	struct efa_data_path_direct_sq *sq = &qp->sq;
-	uint8_t op_type;
-
-	if (OFI_UNLIKELY(qp->wr_session_err))
-		return;
-
-	tx_wqe = &sq->curr_tx_wqe;
-	op_type = EFA_GET(&tx_wqe->meta.ctrl1, EFA_IO_TX_META_DESC_OP_TYPE);
-	switch (op_type) {
-	case EFA_IO_SEND:
-		efa_post_send_sgl(tx_wqe->data.sgl, sg_list, num_sge);
-		break;
-	case EFA_IO_RDMA_READ:
-	case EFA_IO_RDMA_WRITE:
-		if (OFI_UNLIKELY(num_sge > EFA_IO_TX_DESC_NUM_RDMA_BUFS)) {
-			EFA_WARN(FI_LOG_EP_DATA, "EFA device doesn't support > %d iov for rdma operations\n", EFA_IO_TX_DESC_NUM_RDMA_BUFS);
-			qp->wr_session_err = EINVAL;
-			return;
-		}
-		rdma_req = &tx_wqe->data.rdma_req;
-		rdma_req->remote_mem.length =
-			efa_sge_total_bytes(sg_list, num_sge);
-		efa_post_send_sgl(rdma_req->local_mem, sg_list, num_sge);
-		break;
-	default:
-		return;
-	}
-
-	tx_wqe->meta.length = num_sge;
-}
-
-/**
- * @brief Set unreliable datagram addressing information
- *
- * Configures the addressing information for unreliable datagram operations,
- * including the address handle, remote queue pair number, and queue key.
- *
- * @param efaqp Pointer to the EFA queue pair
- * @param ah Pointer to the address handle
- * @param remote_qpn Remote queue pair number
- * @param remote_qkey Remote queue key
- */
-static inline void efa_data_path_direct_wr_set_ud_addr(struct efa_qp *efaqp,
-						       struct efa_ah *ah,
-						       uint32_t remote_qpn,
-						       uint32_t remote_qkey)
-{
-	struct efa_io_tx_wqe *tx_wqe;
-
-	if (OFI_UNLIKELY(efaqp->data_path_direct_qp.wr_session_err))
-		return;
-
-	tx_wqe = &efaqp->data_path_direct_qp.sq.curr_tx_wqe;
-
-	tx_wqe->meta.dest_qp_num = remote_qpn;
-	tx_wqe->meta.ah = ah->ahn;
-	tx_wqe->meta.qkey = remote_qkey;
-}
-
-/**
- * @brief Start a new work request session
- *
- * Initializes a new work request session by clearing error state and
- * starting write-combining memory operations for optimal performance.
- *
- * @param qp Pointer to the EFA queue pair
- */
-static inline void efa_data_path_direct_wr_start(struct efa_qp *qp)
-{
-	qp->data_path_direct_qp.wr_session_err = 0;
-	qp->data_path_direct_qp.sq.num_wqe_pending = 0;
-	mmio_wc_start();
-}
 
 /**
  * @brief Start polling for completion queue entries
@@ -613,6 +355,251 @@ static inline int efa_data_path_direct_req_notify_cq(struct efa_ibv_cq *ibv_cq,
 	if (ibv_cq->data_path_direct.db)
 		efa_update_cq_doorbell(&ibv_cq->data_path_direct, true);
 #endif
+
+	return 0;
+}
+
+/**
+ * @brief Consolidated send operation - builds WQE as stack variable and posts directly
+ * @param qp EFA queue pair
+ * @param sge_list Pre-prepared SGE list (used when use_inline=false)
+ * @param inline_data_list Pre-prepared inline data list (used when use_inline=true)
+ * @param iov_count Number of SGE entries or inline data buffers
+ * @param use_inline True to use inline data, false to use SGE list
+ * @param wr_id Work request ID (pre-prepared by caller)
+ * @param data Immediate data (used when FI_REMOTE_CQ_DATA flag is set)
+ * @param flags Operation flags
+ * @param ah Address handle
+ * @param qpn Remote queue pair number
+ * @param qkey Remote queue key
+ */
+static inline int efa_data_path_direct_post_send(
+		struct efa_qp *qp,
+		const struct ibv_sge *sge_list,
+		const struct ibv_data_buf *inline_data_list,
+		size_t iov_count,
+		bool use_inline,
+		uintptr_t wr_id,
+		uint64_t data,
+		uint64_t flags,
+		struct efa_ah *ah,
+		uint32_t qpn,
+		uint32_t qkey)
+{
+	struct efa_data_path_direct_sq *sq = &qp->data_path_direct_qp.sq;
+	struct efa_io_tx_wqe local_wqe = {0}; /* Stack variable - can be in registers */
+	struct efa_io_tx_meta_desc *meta_desc = &local_wqe.meta;
+	int err = 0;
+
+	/* Validate queue space */
+	err = efa_post_send_validate(qp);
+	if (OFI_UNLIKELY(err)) {
+		/* ring db for earlier wqes if there is any */
+		if (sq->num_wqe_pending)
+			efa_data_path_direct_send_wr_ring_db(sq);
+		return err;
+	}
+
+	/* when reaching the sq max_batch, ring the db */
+	if (sq->num_wqe_pending == sq->wq.max_batch)
+		efa_data_path_direct_send_wr_ring_db(sq);
+
+	/* Set work request ID */
+	qp->ibv_qp_ex->wr_id = wr_id;
+
+	/* Build metadata in local stack variable */
+	efa_data_path_direct_set_ud_addr(meta_desc, ah, qpn, qkey);
+	meta_desc->req_id = efa_wq_get_next_wrid_idx(&sq->wq, qp->ibv_qp_ex->wr_id);
+
+	/* Set common control flags */
+	efa_set_common_ctrl_flags(meta_desc, sq, EFA_IO_SEND);
+	if (flags & FI_REMOTE_CQ_DATA) {
+		efa_send_wr_set_imm_data(meta_desc, data);
+	}
+
+	/* Handle inline data or SGE list */
+	if (use_inline) {
+		/* Inline data path - caller has prepared inline_data_list */
+		efa_data_path_direct_set_inline_data(&local_wqe, iov_count, inline_data_list);
+	} else {
+		/* SGE list path - caller has prepared sge_list */
+		efa_data_path_direct_set_sgl(local_wqe.data.sgl, meta_desc, sge_list, iov_count);
+	}
+
+	efa_data_path_direct_send_wr_post(qp, sq, &local_wqe);
+
+	/* Update queue state */
+	efa_sq_advance_post_idx(sq);
+	sq->num_wqe_pending++;
+
+	if (!(flags & FI_MORE))
+		efa_data_path_direct_send_wr_ring_db(sq);
+
+	return 0;
+}
+
+/**
+ * @brief Consolidated RDMA read operation - builds WQE as stack variable and posts directly
+ * @param qp EFA queue pair
+ * @param sge_list Pre-prepared SGE list for local buffers
+ * @param sge_count Number of SGE entries
+ * @param remote_key Remote memory key
+ * @param remote_addr Remote memory address
+ * @param wr_id Work request ID (pre-prepared by caller)
+ * @param flags Operation flags
+ * @param ah Address handle
+ * @param qpn Remote queue pair number
+ * @param qkey Remote queue key
+ */
+static inline int efa_data_path_direct_post_read(
+		struct efa_qp *qp,
+		const struct ibv_sge *sge_list,
+		size_t sge_count,
+		uint32_t remote_key,
+		uint64_t remote_addr,
+		uintptr_t wr_id,
+		uint64_t flags,
+		struct efa_ah *ah,
+		uint32_t qpn,
+		uint32_t qkey)
+{
+	struct efa_data_path_direct_sq *sq = &qp->data_path_direct_qp.sq;
+	struct efa_io_tx_wqe local_wqe = {0}; /* Stack variable - can be in registers */
+	struct efa_io_tx_meta_desc *meta_desc = &local_wqe.meta;
+	struct efa_io_remote_mem_addr *remote_mem = &local_wqe.data.rdma_req.remote_mem;
+	int err;
+
+	/* Validate queue space */
+	err = efa_post_send_validate(qp);
+	if (OFI_UNLIKELY(err)) {
+		/* ring db for earlier wqes if there is any */
+		if (sq->num_wqe_pending)
+			efa_data_path_direct_send_wr_ring_db(sq);
+		return err;
+	}
+
+	/* Validate SGE count for RDMA operations */
+	if (OFI_UNLIKELY(sge_count > EFA_IO_TX_DESC_NUM_RDMA_BUFS)) {
+		EFA_WARN(FI_LOG_EP_DATA, "EFA device doesn't support > %d iov for rdma operations\n", EFA_IO_TX_DESC_NUM_RDMA_BUFS);
+		return EINVAL;
+	}
+
+	/* when reaching the sq max_batch, ring the db */
+	if (sq->num_wqe_pending == sq->wq.max_batch)
+		efa_data_path_direct_send_wr_ring_db(sq);
+
+	/* Set work request ID */
+	qp->ibv_qp_ex->wr_id = wr_id;
+
+	/* Build metadata in local stack variable */
+	efa_data_path_direct_set_ud_addr(meta_desc, ah, qpn, qkey);
+	meta_desc->req_id = efa_wq_get_next_wrid_idx(&sq->wq, qp->ibv_qp_ex->wr_id);
+
+	/* Set common control flags for RDMA READ */
+	efa_set_common_ctrl_flags(meta_desc, sq, EFA_IO_RDMA_READ);
+
+	/* Set remote memory information */
+	efa_send_wr_set_rdma_addr(remote_mem, remote_key, remote_addr);
+	remote_mem->length = efa_sge_total_bytes(sge_list, sge_count);
+
+	/* Set local SGE list - caller has prepared sge_list */
+	efa_data_path_direct_set_sgl(local_wqe.data.rdma_req.local_mem, meta_desc, sge_list, sge_count);
+
+	efa_data_path_direct_send_wr_post(qp, sq, &local_wqe);
+
+	/* Update queue state */
+	efa_sq_advance_post_idx(sq);
+	sq->num_wqe_pending++;
+
+	/* Ring doorbell if required */
+	if (!(flags & FI_MORE))
+		efa_data_path_direct_send_wr_ring_db(sq);
+
+	return 0;
+}
+
+/**
+ * @brief Consolidated RDMA write operation - builds WQE as stack variable and posts directly
+ * @param qp EFA queue pair
+ * @param sge_list Pre-prepared SGE list for local buffers
+ * @param sge_count Number of SGE entries
+ * @param remote_key Remote memory key
+ * @param remote_addr Remote memory address
+ * @param wr_id Work request ID (pre-prepared by caller)
+ * @param data Immediate data (used when FI_REMOTE_CQ_DATA flag is set)
+ * @param flags Operation flags
+ * @param ah Address handle
+ * @param qpn Remote queue pair number
+ * @param qkey Remote queue key
+ */
+static inline int
+efa_data_path_direct_post_write(
+		struct efa_qp *qp,
+		const struct ibv_sge *sge_list,
+		size_t sge_count,
+		uint32_t remote_key,
+		uint64_t remote_addr,
+		uintptr_t wr_id,
+		uint64_t data,
+		uint64_t flags,
+		struct efa_ah *ah,
+		uint32_t qpn,
+		uint32_t qkey)
+{
+	struct efa_data_path_direct_sq *sq = &qp->data_path_direct_qp.sq;
+	struct efa_io_tx_wqe local_wqe = {0}; /* Stack variable - can be in registers */
+	struct efa_io_tx_meta_desc *meta_desc = &local_wqe.meta;
+	struct efa_io_remote_mem_addr *remote_mem = &local_wqe.data.rdma_req.remote_mem;
+	int err;
+
+	/* Validate queue space */
+	err = efa_post_send_validate(qp);
+	if (OFI_UNLIKELY(err)) {
+		/* ring db for earlier wqes if there is any */
+		if (sq->num_wqe_pending)
+			efa_data_path_direct_send_wr_ring_db(sq);
+		return err;
+	}
+
+	/* Validate SGE count for RDMA operations */
+	if (OFI_UNLIKELY(sge_count > EFA_IO_TX_DESC_NUM_RDMA_BUFS)) {
+		EFA_WARN(FI_LOG_EP_DATA, "EFA device doesn't support > %d iov for rdma operations\n", EFA_IO_TX_DESC_NUM_RDMA_BUFS);
+		return EINVAL;
+	}
+
+	/* when reaching the sq max_batch, ring the db */
+	if (sq->num_wqe_pending == sq->wq.max_batch)
+		efa_data_path_direct_send_wr_ring_db(sq);
+
+	/* Set work request ID */
+	qp->ibv_qp_ex->wr_id = wr_id;
+
+	/* Build metadata in local stack variable */
+	efa_data_path_direct_set_ud_addr(meta_desc, ah, qpn, qkey);
+	meta_desc->req_id = efa_wq_get_next_wrid_idx(&sq->wq, qp->ibv_qp_ex->wr_id);
+
+	/* Set common control flags for RDMA WRITE */
+	efa_set_common_ctrl_flags(meta_desc, sq, EFA_IO_RDMA_WRITE);
+	if (flags & FI_REMOTE_CQ_DATA) {
+		efa_send_wr_set_imm_data(meta_desc, data);
+	}
+
+	/* Set remote memory information */
+	efa_send_wr_set_rdma_addr(remote_mem, remote_key, remote_addr);
+	remote_mem->length = efa_sge_total_bytes(sge_list, sge_count);
+
+	/* Set local SGE list - caller has prepared sge_list */
+	efa_data_path_direct_set_sgl(local_wqe.data.rdma_req.local_mem, meta_desc, sge_list, sge_count);
+
+	efa_data_path_direct_send_wr_post(qp, sq, &local_wqe);
+
+	/* Update queue state */
+	efa_sq_advance_post_idx(sq);
+	sq->num_wqe_pending++;
+
+	/* Ring doorbell if required */
+	if (!(flags & FI_MORE))
+		efa_data_path_direct_send_wr_ring_db(sq);
 
 	return 0;
 }

commit 4c5d84f016014b493abf2bef54eca9a24d052cc6

prov/efa: Fix cq data size in efa-rdm pkt post
commit 10ca04b1 introduced a bug that introduces an
16 bit cq data intermediate variable when passing to
device. uint16_t is too small and
will silently corrupt when application is using more
than 16 bits while efa provider supports 32 bits.

This patch fixes it.

Signed-off-by: Shi Jin <sjina@amazon.com>

prov/efa/src/rdm/efa_rdm_pke.c
@@ -423,7 +423,7 @@ ssize_t efa_rdm_pke_sendv(struct efa_rdm_pke **pkt_entry_vec,
 	int ret = 0, pkt_idx, iov_cnt;
 	bool use_inline;
 	uint64_t flags_in_loop;
-	uint16_t cq_data = 0;
+	uint64_t cq_data = 0;
 	uint32_t qpn, qkey;
 	uint64_t wr_id;
 
@@ -633,7 +633,7 @@ int efa_rdm_pke_write(struct efa_rdm_pke *pkt_entry)
 	int err = 0;
 	struct efa_ah *ah;
 	uint32_t qpn, qkey;
-	uint16_t cq_data = 0;
+	uint64_t cq_data = 0;
 	uint64_t wr_id;
 
 	ep = pkt_entry->ep;