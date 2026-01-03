04818817af055555c4a086fa31a6edaf6a3ccd93
Shi Jin <sjina@amazon.com>
2026-01-03 01:50:22 +0000

    fabtests/efa/pytest: Add receiver ep cycle test
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- fabtests/pytest/efa/test_multi_ep_stress.py
+++ fabtests/pytest/efa/test_multi_ep_stress.py
@@ -56,3 +56,17 @@ def test_multi_ep_stress_transient_client(cmdline_args):
     cmd = f"fi_efa_multi_ep_stress --sender-addr {cmdline_args.client_id} --sender-ep-cycles 5"
     test = ClientServerTest(cmdline_args, cmd, message_size=1024, fabric="efa", additional_env="FI_EFA_ENABLE_SHM_TRANSFER=0")
     test.run()
+
+@pytest.mark.unstable
+@pytest.mark.parametrize("configuration", [{"sender_workers": 2, "receiver_workers": 4, "sender_ep_cycles": 10, "receiver_ep_cycles": 20},
+                                           {"sender_workers": 4, "receiver_workers": 2, "sender_ep_cycles": 20, "receiver_ep_cycles": 10},
+                                           {"sender_workers": 2, "receiver_workers": 4, "sender_ep_cycles": 20, "receiver_ep_cycles": 10}
+                                            ])
+def test_multi_ep_stress_multi_and_transient_sender_receiver(cmdline_args, configuration):
+    sender_workers = configuration["sender_workers"]
+    receiver_workers = configuration["receiver_workers"]
+    sender_ep_cycles = configuration["sender_ep_cycles"]
+    receiver_ep_cycles = configuration["receiver_ep_cycles"]
+    cmd = f"fi_efa_multi_ep_stress --sender-addr {cmdline_args.client_id} --sender-workers {sender_workers} --receiver-workers {receiver_workers} --sender-ep-cycles {sender_ep_cycles} --receiver-ep-cycles {receiver_ep_cycles}"
+    test = ClientServerTest(cmdline_args, cmd, message_size=1024, fabric="efa", additional_env="FI_EFA_ENABLE_SHM_TRANSFER=0")
+    test.run()
