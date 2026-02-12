8c8a85fc006cd397300721e5f1e74ad53231166b
Seth Zegelstein <szegel@amazon.com>
2026-02-12 10:41:12 -0700

    prov/efa: Support efa-direct 0-byte MSG send operations
    Enable 0-byte message send operations by detecting len == 0 in
    efa_post_send and using the pre-registered bounce buffer as inline
    data. This centralizes the 0-byte handling logic in efa_post_send,
    allowing efa_ep_msg_inject to remain simple and consistent.
    
    The implementation sets up the bounce buffer as inline data with
    iov_count = 1 and use_inline = true, then continues with the normal
    send path.
    
    Signed-off-by: Seth Zegelstein <szegel@amazon.com>

--- prov/efa/src/efa_msg.c
+++ prov/efa/src/efa_msg.c
@@ -191,21 +191,24 @@ static ssize_t efa_ep_recvv(struct fid_ep *ep_fid, const struct iovec *iov, void
 
 static inline ssize_t efa_post_send(struct efa_base_ep *base_ep, const struct fi_msg *msg, uint64_t flags)
 {
+	struct efa_domain *domain = base_ep->domain;
 	struct efa_qp *qp = base_ep->qp;
 	struct efa_conn *conn;
 	struct ibv_sge sg_list[2];  /* efa device support up to 2 iov */
 	struct ibv_data_buf inline_data_list[2];
 	size_t len, i;
+	size_t iov_count = msg->iov_count;
 	bool use_inline;
 	int ret = 0;
 	uintptr_t wr_id;
 
 	efa_tracepoint(send_begin_msg_context, (size_t) msg->context, (size_t) msg->addr);
 
+	len = ofi_total_iov_len(msg->msg_iov, msg->iov_count);
+
 	EFA_DBG(FI_LOG_EP_DATA,
 		"total len: %zu, addr: %lu, context: %lx, flags: %lx\n",
-		ofi_total_iov_len(msg->msg_iov, msg->iov_count),
-		msg->addr, (size_t) msg->context, flags);
+		len, msg->addr, (size_t) msg->context, flags);
 
 	dump_msg(msg, "send");
 
@@ -213,8 +216,6 @@ static inline ssize_t efa_post_send(struct efa_base_ep *base_ep, const struct fi
 	assert(conn && conn->ep_addr);
 
 	assert(msg->iov_count <= base_ep->info->tx_attr->iov_limit);
-
-	len = ofi_total_iov_len(msg->msg_iov, msg->iov_count);
 
 	if (qp->ibv_qp->qp_type == IBV_QPT_UD) {
 		assert(msg->msg_iov[0].iov_len >= base_ep->info->ep_attr->msg_prefix_size);
@@ -228,6 +229,15 @@ static inline ssize_t efa_post_send(struct efa_base_ep *base_ep, const struct fi
 	/* Prepare work request ID */
 	wr_id = (uintptr_t) efa_fill_context(
 		msg->context, msg->addr, flags, FI_SEND | FI_MSG);
+
+	/* Handle 0-byte send with bounce buffer */
+	if (len == 0) {
+		inline_data_list[0].addr = domain->zero_byte_bounce_buf;
+		inline_data_list[0].length = 0;
+		iov_count = 1;
+		use_inline = true;
+		goto post;
+	}
 
 	/* Determine if we should use inline data */
 	use_inline = (len <= base_ep->domain->device->efa_attr.inline_buf_size &&
@@ -268,8 +278,9 @@ static inline ssize_t efa_post_send(struct efa_base_ep *base_ep, const struct fi
 		}
 	}
 
+post:
 	/* Use consolidated send function */
-	ret = efa_qp_post_send(qp, sg_list, inline_data_list, msg->iov_count,
+	ret = efa_qp_post_send(qp, sg_list, inline_data_list, iov_count,
 			       use_inline, wr_id, msg->data, flags,
 			       conn->ah, conn->ep_addr->qpn, conn->ep_addr->qkey);
 	if (OFI_UNLIKELY(ret))
