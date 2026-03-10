03c53d1724d6f1fe9b8efe6f8c33c10df4a6aa42
Sai Sunku <sunkusa@amazon.com>
2026-03-10 20:46:22 +0000

    prov/efa: Introduce the efa_rdm_proto protocol interface
    Add efa_rdm_proto.h defining the efa_rdm_proto struct which serves as
    the interface for all EFA RDM protocols. The interface includes:
    
    - can_use_protocol_for_send: determines if this protocol can handle a
      given send operation based on message size, peer capabilities, etc.
    - construct_tx_pkes: builds the TX packet entries for the operation
    - handle_tx_pkes_posted: post-send hook for protocol-specific
      bookkeeping
    - Packet type fields for msg/tagged/DC variants
    
    Add efa_rdm_proto.c with the protocol registry array and the
    efa_rdm_proto_send_pkes_posted_no_op default implementation.
    
    The RX path will be introduced in later commits.
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

---
+++ prov/efa/src/rdm/efa_rdm_proto.c
@@ -0,0 +1,13 @@
+/* Copyright Amazon.com, Inc. or its affiliates. All rights reserved. */
+/* SPDX-License-Identifier: BSD-2-Clause OR GPL-2.0-only */
+
+#include "efa_rdm_proto.h"
+#include "efa.h"
+
+void efa_rdm_proto_select_send_protocol(struct efa_rdm_ep *ep,
+					struct efa_rdm_peer *peer,
+					const struct fi_msg *msg, uint32_t op,
+					uint64_t flags, struct efa_rdm_ope *txe,
+					struct efa_rdm_proto **proto)
+{
+}
