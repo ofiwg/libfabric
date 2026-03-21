322f2a657eb0003529daee825a5b645a8cf91064
Sai Sunku <sunkusa@amazon.com>
2026-03-21 02:12:16 +0000

    prov/efa: Add long CTS protocol to the new interface
    Add efa_rdm_proto_longcts.c and efa_rdm_proto_longcts.h implementing
    the long CTS (Credit-based Transfer with Send) protocol. This protocol
    handles messages larger than the medium threshold when RDMA read is not
    available or the sender's memory is not registered.
    
    The sender transmits the first data chunk along with the RTM header,
    then waits for a CTS (Clear To Send) packet from the receiver before
    sending the remaining data in CTSDATA packets. The send completion
    callback tracks bytes_acked and only reports completion when all data
    has been acknowledged. For DC long CTS, the TXE is held until both all
    send completions and the receipt packet have been received.
    
    The old long CTS RTM init functions are removed from efa_rdm_pke_rtm.c.
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

--- prov/efa/src/rdm/efa_rdm_proto.c
+++ prov/efa/src/rdm/efa_rdm_proto.c
@@ -5,6 +5,7 @@
 #include "efa_rdm_proto.h"
 #include "efa_rdm_proto_eager.h"
 #include "efa_rdm_proto_medium.h"
+#include "efa_rdm_proto_longcts.h"
 
 #define EFA_RDM_MAX_PROTO 8
 
@@ -15,6 +16,8 @@
 struct efa_rdm_proto *efa_rdm_protocols [EFA_RDM_MAX_PROTO] = {
 	&efa_rdm_proto_eager,
 	&efa_rdm_proto_medium,
+	/* Long CTS should be the last protocol because it can always be used */
+	&efa_rdm_proto_longcts,
 	NULL,	/* Sentinel used to stop iteration */
 };
 
@@ -105,6 +108,7 @@ int efa_rdm_proto_select_send_protocol(struct efa_rdm_ep *ep,
 		if (selected_proto->can_use_protocol_for_send(txe, req_pkt_type,
 							      header_flags, iface)) {
 			*proto = selected_proto;
+			EFA_INFO(FI_LOG_EP_DATA, "Selected protocol: %s\n", selected_proto->name);
 			return FI_SUCCESS;
 		}
 	}
