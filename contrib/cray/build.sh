7c5bf75f877c1fd00cd27a64f935a57e6d86516a
Terrence Mitchem <terrence.mitchem@hpe.com>
2026-04-29 22:22:23 -0500

    contrib/cray: Update the HPE SCB build files
    - Add missing PRODUCT value to Jenkins entrypoints
    - Respect previous values for CE_CONFIG_BRANCH for all build types
    
    Signed-off-by: Terrence Mitchem <terrence.mitchem@hpe.com>

--- contrib/cray/build.sh
+++ contrib/cray/build.sh
@@ -9,9 +9,9 @@ CE_BUILD_SCRIPT_REPO=hpc-shs-ce-devops
 
 # Use appropriate ce-devops branch
 if [[ -n "${BRANCH_NAME:-}" && "$BRANCH_NAME" =~ "release/shs-" ]]; then
-    CE_CONFIG_BRANCH=$BRANCH_NAME
+    CE_CONFIG_BRANCH=${CE_CONFIG_BRANCH:-$BRANCH_NAME}
 elif [[ -n "${CHANGE_TARGET:-}" && "$CHANGE_TARGET" =~ "release/shs-" ]]; then
-    CE_CONFIG_BRANCH=$CHANGE_TARGET
+    CE_CONFIG_BRANCH=${CE_CONFIG_BRANCH:-$CHANGE_TARGET}
 else
     CE_CONFIG_BRANCH=${CE_CONFIG_BRANCH:-main}
 fi
