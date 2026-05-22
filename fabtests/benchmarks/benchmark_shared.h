23a45c367e7f94da097c7419e3fb9ec4884d916c
Sai Sunku <sunkusa@amazon.com>
2026-05-22 06:56:31 +0000

    fabtests: rename pingpong_rma to pingpong_rma_write
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

--- fabtests/benchmarks/benchmark_shared.h
+++ fabtests/benchmarks/benchmark_shared.h
@@ -48,7 +48,7 @@ void ft_benchmark_usage(void);
 int pingpong(void);
 int run_pingpong(void);
 int bandwidth(void);
-int pingpong_rma(enum ft_rma_opcodes rma_op, struct fi_rma_iov *remote);
+int pingpong_rma_write(enum ft_rma_opcodes rma_op, struct fi_rma_iov *remote);
 int bandwidth_rma(enum ft_rma_opcodes op, struct fi_rma_iov *remote);
 int rma_tx_completion(enum ft_rma_opcodes rma_op, struct fi_rma_iov *remote);
 
