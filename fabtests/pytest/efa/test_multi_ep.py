f0eead100d11601500e0ee624df0b537a35033f1
Alexey Novikov <nalexey@amazon.com>
2026-06-05 15:19:02 -0700

    fabtests/efa: Delete multi_ep_mt test
    The test is replaced by multi_ep_stress.
    
    Signed-off-by: Alexey Novikov <nalexey@amazon.com>

--- fabtests/pytest/efa/test_multi_ep.py
+++ fabtests/pytest/efa/test_multi_ep.py
@@ -15,16 +15,3 @@ def test_multi_ep(cmdline_args, shared_cq, shared_av, rma_fabric):
     test = ClientServerTest(cmdline_args, cmd, message_size=256, fabric=rma_fabric)
     test.run()
 
-
-# TODO: expand to different meory types
-@pytest.mark.unstable
-@pytest.mark.parametrize("msg_size", [16, 8192, 16384, 1048576])
-@pytest.mark.parametrize("shared_av", [True, False])
-def test_multi_ep_mt(cmdline_args, msg_size, shared_av):
-    cmd = f"fi_efa_multi_ep_mt -e rdm -c 20 -I 100"
-
-    if shared_av:
-        cmd += " -A"
-
-    test = ClientServerTest(cmdline_args, cmd, message_size=msg_size, fabric="efa", timeout=30, additional_env="FI_EFA_ENABLE_SHM_TRANSFER=0")
-    test.run()
