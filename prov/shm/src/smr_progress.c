ofiwg/libfabric

commit b34ed0c1dea4fdcb71ea2adf6da1ff2605edba3e

prov/shm: refactor and simplify protocol code
When the shm provider was first released, it only had
three protocol (inline, inject, iov), making the protocol
selection more or less straightforward. However, as the
provider has developed, three new protocols have been added
(mmap, sar, ipc). The addition of these checks and protocols
has added a lot of complexity to the code, resulting in
very convoluted and hard to follow code with many duplicate
checks.

This patch simplifies all of the protocol code by
isolating each protocol into separate functions, accessible
through an array of function pointers by protocol enum.

These functions are used by the msg, tagged, and rma calls
which all take similar code paths. The atomic calls are
separated since they follow a slightly different pattern
because of the extra atomic/datatype fields.

This patch also makes the following two indirectly related
changes:
- smr_complete_tx now takes in 64bit FI flags instead
of the 16bit SMR flag, saving instructions to convert
from SMR to FI for the completion flags. Add 64bit op
flags to tx_entry to save them for response entry
completions
- Remove redundant atomic fetch/compare inline path and
force all compare and fetch atomics through the inject.
The old code was not using this path anyway because
SMR_RMA_REQ was always set for fetch and compares. Removing
this path simplifies atomic protocol selection as well
- Fix incorrect atomic inject length assert from count to len

Signed-off-by: aingerson <alexia.ingerson@intel.com>

prov/shm/src/smr_progress.c
@@ -213,7 +213,7 @@ static void smr_progress_resp(struct smr_ep *ep)
 			break;
 
 		ret = smr_complete_tx(ep, pending->context,
-				  pending->cmd.msg.hdr.op, pending->cmd.msg.hdr.op_flags,
+				  pending->cmd.msg.hdr.op, pending->op_flags,
 				  -(resp->status));
 		if (ret) {
 			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
@@ -577,21 +577,12 @@ static int smr_progress_inline_atomic(struct smr_cmd *cmd, struct fi_ioc *ioc,
 			       size_t ioc_count, size_t *len)
 {
 	int i;
-	uint8_t *src, *comp;
+	uint8_t *src = cmd->msg.data.msg;
 
-	switch (cmd->msg.hdr.op) {
-	case ofi_op_atomic_compare:
-		src = cmd->msg.data.buf;
-		comp = cmd->msg.data.comp;
-		break;
-	default:
-		src = cmd->msg.data.msg;
-		comp = NULL;
-		break;
-	}
+	assert(cmd->msg.hdr.op == ofi_op_atomic);
 
 	for (i = *len = 0; i < ioc_count && *len < cmd->msg.hdr.size; i++) {
-		smr_do_atomic(&src[*len], ioc[i].addr, comp ? &comp[*len] : NULL,
+		smr_do_atomic(&src[*len], ioc[i].addr, NULL,
 			      cmd->msg.hdr.datatype, cmd->msg.hdr.atomic_op,
 			      ioc[i].count, cmd->msg.hdr.op_flags);
 		*len += ioc[i].count * ofi_datatype_size(cmd->msg.hdr.datatype);

include/ofi_shm.h
@@ -78,6 +78,7 @@ enum {
 	smr_src_mmap,	/* mmap-based fallback protocol */
 	smr_src_sar,	/* segmentation fallback protocol */
 	smr_src_ipc,	/* device IPC handle protocol */
+	smr_src_max,
 };
 
 //reserves 0-255 for defined ops and room for new ops
@@ -126,7 +127,6 @@ struct smr_msg_hdr {
 } __attribute__ ((aligned(16)));
 
 #define SMR_MSG_DATA_LEN	(SMR_CMD_SIZE - sizeof(struct smr_msg_hdr))
-#define SMR_COMP_DATA_LEN	(SMR_MSG_DATA_LEN / 2)
 
 #define IPC_HANDLE_SIZE		64
 struct smr_ipc_info {
@@ -147,10 +147,6 @@ union smr_cmd_data {
 		size_t		iov_count;
 		struct iovec	iov[(SMR_MSG_DATA_LEN - sizeof(size_t)) /
 				    sizeof(struct iovec)];
-	};
-	struct {
-		uint8_t		buf[SMR_COMP_DATA_LEN];
-		uint8_t		comp[SMR_COMP_DATA_LEN];
 	};
 	struct {
 		uint64_t	sar;

prov/shm/src/smr.h
@@ -125,6 +125,7 @@ struct smr_tx_entry {
 	void		*context;
 	struct iovec	iov[SMR_IOV_LIMIT];
 	uint32_t	iov_count;
+	uint64_t	op_flags;
 	size_t		bytes_done;
 	int		next;
 	void		*map_ptr;
@@ -311,32 +312,9 @@ int64_t smr_verify_peer(struct smr_ep *ep, fi_addr_t fi_addr);
 void smr_format_pend_resp(struct smr_tx_entry *pend, struct smr_cmd *cmd,
 			  void *context, enum fi_hmem_iface iface, uint64_t device,
 			  const struct iovec *iov, uint32_t iov_count,
-			  int64_t id, struct smr_resp *resp);
+			  uint64_t op_flags, int64_t id, struct smr_resp *resp);
 void smr_generic_format(struct smr_cmd *cmd, int64_t peer_id, uint32_t op,
 			uint64_t tag, uint64_t data, uint64_t op_flags);
-void smr_format_inline(struct smr_cmd *cmd, enum fi_hmem_iface iface,
-		       uint64_t device, const struct iovec *iov, size_t count);
-void smr_format_inject(struct smr_cmd *cmd, enum fi_hmem_iface iface, uint64_t device,
-		       const struct iovec *iov, size_t count,
-		       struct smr_region *smr, struct smr_inject_buf *tx_buf);
-void smr_format_iov(struct smr_cmd *cmd, const struct iovec *iov, size_t count,
-		    size_t total_len, struct smr_region *smr,
-		    struct smr_resp *resp);
-int smr_format_ze_ipc(struct smr_ep *ep, int64_t id, struct smr_cmd *cmd,
-		      const struct iovec *iov, uint64_t device,
-		      size_t total_len, struct smr_region *smr,
-		      struct smr_resp *resp, struct smr_tx_entry *pend);
-int smr_format_ipc(struct smr_cmd *cmd, void *ptr,
-		   size_t len, struct smr_region *smr,
-		   struct smr_resp *resp, enum fi_hmem_iface iface);
-int smr_format_mmap(struct smr_ep *ep, struct smr_cmd *cmd,
-		    const struct iovec *iov, size_t count, size_t total_len,
-		    struct smr_tx_entry *pend, struct smr_resp *resp);
-int smr_format_sar(struct smr_cmd *cmd, enum fi_hmem_iface iface, uint64_t deivce,
-		   const struct iovec *iov, size_t count,
-		   size_t total_len, struct smr_region *smr,
-		   struct smr_region *peer_smr, int64_t id,
-		   struct smr_tx_entry *pending, struct smr_resp *resp);
 size_t smr_copy_to_sar(struct smr_sar_msg *sar_msg, struct smr_resp *resp,
 		       struct smr_cmd *cmd, enum fi_hmem_iface,
 		       uint64_t device, const struct iovec *iov, size_t count,
@@ -345,9 +323,17 @@ size_t smr_copy_from_sar(struct smr_sar_msg *sar_msg, struct smr_resp *resp,
 			 struct smr_cmd *cmd, enum fi_hmem_iface iface,
 			 uint64_t device, const struct iovec *iov, size_t count,
 			 size_t *bytes_done, int *next);
+int smr_select_proto(bool use_ipc, bool cma_avail, enum fi_hmem_iface iface,
+		     uint32_t op, uint64_t total_len, uint64_t op_flags);
+typedef ssize_t (*smr_proto_func)(struct smr_ep *ep, struct smr_region *peer_smr,
+		int64_t id, int64_t peer_id, uint32_t op, uint64_t tag,
+		uint64_t data, uint64_t op_flags, enum fi_hmem_iface iface,
+		uint64_t device, const struct iovec *iov, size_t iov_count,
+		size_t total_len, void *context);
+extern smr_proto_func smr_proto_ops[smr_src_max];
 
 int smr_complete_tx(struct smr_ep *ep, void *context, uint32_t op,
-		uint16_t flags, uint64_t err);
+		uint64_t flags, uint64_t err);
 int smr_tx_comp(struct smr_ep *ep, void *context, uint32_t op,
 		uint16_t flags, uint64_t err);
 int smr_tx_comp_signal(struct smr_ep *ep, void *context, uint32_t op,

prov/shm/src/smr_atomic.c
@@ -55,35 +55,31 @@ static void smr_generic_atomic_format(struct smr_cmd *cmd, uint8_t datatype,
 
 static void smr_format_inline_atomic(struct smr_cmd *cmd,
 				     enum fi_hmem_iface iface, uint64_t device,
-				     const struct iovec *iov, size_t count,
-				     const struct iovec *compv,
-				     size_t comp_count)
+				     const struct iovec *iov, size_t count)
 {
-	size_t comp_size;
-
 	cmd->msg.hdr.op_src = smr_src_inline;
 
-	switch (cmd->msg.hdr.op) {
-	case ofi_op_atomic:
-	case ofi_op_atomic_fetch:
-		cmd->msg.hdr.size = ofi_copy_from_hmem_iov(cmd->msg.data.msg,
-						SMR_MSG_DATA_LEN, iface, device,
-						iov, count, 0);
-		break;
-	case ofi_op_atomic_compare:
-		cmd->msg.hdr.size = ofi_copy_from_hmem_iov(cmd->msg.data.buf,
-						SMR_MSG_DATA_LEN, iface, device,
-						iov, count, 0);
-		comp_size = ofi_copy_from_hmem_iov(cmd->msg.data.comp,
-						SMR_MSG_DATA_LEN, iface, device,
-						compv, comp_count, 0);
-		if (comp_size != cmd->msg.hdr.size)
-			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
-				"atomic and compare buffer size mismatch\n");
-		break;
-	default:
-		break;
-	}
+	cmd->msg.hdr.size = ofi_copy_from_hmem_iov(cmd->msg.data.msg,
+					SMR_MSG_DATA_LEN, iface, device,
+					iov, count, 0);
+}
+
+static void smr_do_atomic_inline(struct smr_ep *ep, struct smr_region *peer_smr,
+			int64_t id, int64_t peer_id, uint32_t op,
+			uint64_t op_flags, enum fi_hmem_iface iface,
+			uint64_t device, uint8_t datatype, uint8_t atomic_op,
+			const struct iovec *iov, size_t iov_count,
+			size_t total_len)
+{
+	struct smr_cmd *cmd;
+
+	cmd = ofi_cirque_next(smr_cmd_queue(peer_smr));
+	smr_generic_format(cmd, peer_id, op, 0, 0, op_flags);
+	smr_generic_atomic_format(cmd, datatype, atomic_op);
+	smr_format_inline_atomic(cmd, iface, device, iov, iov_count);
+
+	ofi_cirque_commit(smr_cmd_queue(peer_smr));
+	peer_smr->cmd_cnt--;
 }
 
 static void smr_format_inject_atomic(struct smr_cmd *cmd,
@@ -123,6 +119,59 @@ static void smr_format_inject_atomic(struct smr_cmd *cmd,
 	}
 }
 
+static ssize_t smr_do_atomic_inject(struct smr_ep *ep, struct smr_region *peer_smr,
+			int64_t id, int64_t peer_id, uint32_t op,
+			uint64_t op_flags, enum fi_hmem_iface iface,
+			uint64_t device, uint8_t datatype, uint8_t atomic_op,
+			const struct iovec *iov, size_t iov_count,
+			const struct iovec *resultv, size_t result_count,
+			const struct iovec *compv, size_t comp_count,
+			size_t total_len, void *context, uint16_t smr_flags)
+{
+	struct smr_cmd *cmd;
+	struct smr_inject_buf *tx_buf;
+	struct smr_tx_entry *pend;
+	struct smr_resp *resp;
+
+	cmd = ofi_cirque_next(smr_cmd_queue(peer_smr));
+	tx_buf = smr_freestack_pop(smr_inject_pool(peer_smr));
+
+	smr_generic_format(cmd, peer_id, op, 0, 0, op_flags);
+	smr_generic_atomic_format(cmd, datatype, atomic_op);
+	smr_format_inject_atomic(cmd, iface, device, iov, iov_count, 
+				 resultv, result_count, compv, comp_count,
+				 peer_smr, tx_buf);
+
+	if (smr_flags & SMR_RMA_REQ || op_flags & FI_DELIVERY_COMPLETE) {
+		if (ofi_cirque_isfull(smr_resp_queue(ep->region))) {
+			smr_freestack_push(smr_inject_pool(peer_smr), tx_buf);
+			return -FI_EAGAIN;
+		}
+		resp = ofi_cirque_next(smr_resp_queue(ep->region));
+		pend = ofi_freestack_pop(ep->pend_fs);
+		smr_format_pend_resp(pend, cmd, context, iface, device, resultv,
+				     result_count, op_flags, id, resp);
+		cmd->msg.hdr.data = smr_get_offset(ep->region, resp);
+		ofi_cirque_commit(smr_resp_queue(ep->region));
+	}
+
+	cmd->msg.hdr.op_flags |= smr_flags;
+	ofi_cirque_commit(smr_cmd_queue(peer_smr));
+	peer_smr->cmd_cnt--;
+
+	return FI_SUCCESS;
+}
+
+static int smr_select_atomic_proto(uint32_t op, uint64_t total_len,
+				   uint64_t op_flags)
+{
+	if (op == ofi_op_atomic_compare || op == ofi_op_atomic_fetch ||
+	    op_flags & FI_DELIVERY_COMPLETE || total_len > SMR_MSG_DATA_LEN)
+		return smr_src_inject;
+
+	return smr_src_inline;
+}
+
 static ssize_t smr_generic_atomic(struct smr_ep *ep,
 			const struct fi_ioc *ioc, void **desc, size_t count,
 			const struct fi_ioc *compare_ioc, void **compare_desc,
@@ -133,19 +182,16 @@ static ssize_t smr_generic_atomic(struct smr_ep *ep,
 			enum fi_op atomic_op, void *context, uint32_t op,
 			uint64_t op_flags)
 {
-	struct smr_region *peer_smr;
-	struct smr_inject_buf *tx_buf;
-	struct smr_tx_entry *pend;
-	struct smr_resp *resp = NULL;
 	struct smr_cmd *cmd;
+	struct smr_region *peer_smr;
 	struct iovec iov[SMR_IOV_LIMIT];
 	struct iovec compare_iov[SMR_IOV_LIMIT];
 	struct iovec result_iov[SMR_IOV_LIMIT];
 	enum fi_hmem_iface iface;
 	uint64_t device;
+	uint16_t smr_flags = 0;
 	int64_t id, peer_id;
-	int err = 0;
-	uint16_t flags = 0;
+	int err = 0, proto;
 	ssize_t ret = 0;
 	size_t total_len;
 
@@ -173,7 +219,6 @@ static ssize_t smr_generic_atomic(struct smr_ep *ep,
 		goto unlock_cq;
 	}
 
-	cmd = ofi_cirque_next(smr_cmd_queue(peer_smr));
 	total_len = ofi_datatype_size(datatype) * ofi_total_ioc_cnt(ioc, count);
 
 	switch (op) {
@@ -187,7 +232,7 @@ static ssize_t smr_generic_atomic(struct smr_ep *ep,
 		assert(result_ioc);
 		ofi_ioc_to_iov(result_ioc, result_iov, result_count,
 			       ofi_datatype_size(datatype));
-		flags |= SMR_RMA_REQ;
+		smr_flags = SMR_RMA_REQ;
 		/* fall through */
 	case ofi_op_atomic:
 		if (atomic_op != FI_ATOMIC_READ) {
@@ -203,45 +248,24 @@ static ssize_t smr_generic_atomic(struct smr_ep *ep,
 
 	iface = smr_get_mr_hmem_iface(ep->util_ep.domain, desc, &device);
 
-	smr_generic_format(cmd, peer_id, op, 0, 0, op_flags);
-	smr_generic_atomic_format(cmd, datatype, atomic_op);
+	proto = smr_select_atomic_proto(op, total_len, op_flags);
 
-	if (total_len <= SMR_MSG_DATA_LEN && !(flags & SMR_RMA_REQ) &&
-	    !(op_flags & FI_DELIVERY_COMPLETE)) {
-		smr_format_inline_atomic(cmd, iface, device, iov, count, compare_iov,
-					 compare_count);
-	} else if (total_len <= SMR_INJECT_SIZE) {
-		tx_buf = smr_freestack_pop(smr_inject_pool(peer_smr));
-		smr_format_inject_atomic(cmd, iface, device, iov, count, result_iov,
-					 result_count, compare_iov, compare_count,
-					 peer_smr, tx_buf);
-		if (flags & SMR_RMA_REQ || op_flags & FI_DELIVERY_COMPLETE) {
-			if (ofi_cirque_isfull(smr_resp_queue(ep->region))) {
-				smr_freestack_push(smr_inject_pool(peer_smr), tx_buf);
-				ret = -FI_EAGAIN;
-				goto unlock_cq;
-			}
-			resp = ofi_cirque_next(smr_resp_queue(ep->region));
-			pend = ofi_freestack_pop(ep->pend_fs);
-			smr_format_pend_resp(pend, cmd, context, iface, device, result_iov,
-					     result_count, id, resp);
-			cmd->msg.hdr.data = smr_get_offset(ep->region, resp);
-			ofi_cirque_commit(smr_resp_queue(ep->region));
-		}
+	if (proto == smr_src_inline) {
+		smr_do_atomic_inline(ep, peer_smr, id, peer_id, ofi_op_atomic,
+			 	     op_flags, iface, device, datatype, atomic_op,
+				     iov, count, total_len);
 	} else {
-		FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
-			"message too large\n");
-		ret = -FI_EINVAL;
-		goto unlock_cq;
+		ret = smr_do_atomic_inject(ep, peer_smr, id, peer_id, op,
+				op_flags, iface, device, datatype, atomic_op,
+				iov, count, result_iov, result_count,
+				compare_iov, compare_count, total_len, context,
+				smr_flags);
+		if (ret)
+			goto unlock_cq;
 	}
-	cmd->msg.hdr.op_flags |= flags;
 
-	ofi_cirque_commit(smr_cmd_queue(peer_smr));
-	peer_smr->cmd_cnt--;
-
-	if (!resp) {
-		ret = smr_complete_tx(ep, context, op, cmd->msg.hdr.op_flags,
-				      err);
+	if (!(smr_flags & SMR_RMA_REQ) && !(op_flags & FI_DELIVERY_COMPLETE)) {
+		ret = smr_complete_tx(ep, context, op, op_flags, err);
 		if (ret) {
 			FI_WARN(&smr_prov, FI_LOG_EP_CTRL,
 				"unable to process tx completion\n");
@@ -320,17 +344,14 @@ static ssize_t smr_atomic_inject(struct fid_ep *ep_fid, const void *buf,
 			size_t count, fi_addr_t dest_addr, uint64_t addr,
 			uint64_t key, enum fi_datatype datatype, enum fi_op op)
 {
+	struct smr_cmd *cmd;
 	struct smr_ep *ep;
 	struct smr_region *peer_smr;
-	struct smr_inject_buf *tx_buf;
-	struct smr_cmd *cmd;
 	struct iovec iov;
 	struct fi_rma_ioc rma_ioc;
 	int64_t id, peer_id;
 	ssize_t ret = 0;
 	size_t total_len;
-
-	assert(count <= SMR_INJECT_SIZE);
 
 	ep = container_of(ep_fid, struct smr_ep, util_ep.ep_fid.fid);
 
@@ -349,6 +370,7 @@ static ssize_t smr_atomic_inject(struct fid_ep *ep_fid, const void *buf,
 
 	cmd = ofi_cirque_next(smr_cmd_queue(peer_smr));
 	total_len = count * ofi_datatype_size(datatype);
+	assert(total_len <= SMR_INJECT_SIZE);
 
 	iov.iov_base = (void *) buf;
 	iov.iov_len = total_len;
@@ -357,19 +379,19 @@ static ssize_t smr_atomic_inject(struct fid_ep *ep_fid, const void *buf,
 	rma_ioc.count = count;
 	rma_ioc.key = key;
 
-	smr_generic_format(cmd, peer_id, ofi_op_atomic, 0, 0, 0);
-	smr_generic_atomic_format(cmd, datatype, op);
-
 	if (total_len <= SMR_MSG_DATA_LEN) {
-		smr_format_inline_atomic(cmd, FI_HMEM_SYSTEM, 0, &iov, 1, NULL, 0);
+		smr_do_atomic_inline(ep, peer_smr, id, peer_id, ofi_op_atomic,
+			 	     0, FI_HMEM_SYSTEM, 0, datatype, op,
+				     &iov, 1, total_len);
 	} else if (total_len <= SMR_INJECT_SIZE) {
-		tx_buf = smr_freestack_pop(smr_inject_pool(peer_smr));
-		smr_format_inject_atomic(cmd, FI_HMEM_SYSTEM, 0, &iov, 1, NULL,
-					 0, NULL, 0, peer_smr, tx_buf);
+		ret = smr_do_atomic_inject(ep, peer_smr, id, peer_id,
+				ofi_op_atomic, 0, FI_HMEM_SYSTEM, 0, datatype,
+				op, &iov, 1, NULL, 0, NULL, 0, total_len,
+				NULL, 0);
+		if (ret)
+			goto unlock_region;
 	}
 
-	ofi_cirque_commit(smr_cmd_queue(peer_smr));
-	peer_smr->cmd_cnt--;
 	cmd = ofi_cirque_next(smr_cmd_queue(peer_smr));
 	smr_format_rma_ioc(cmd, &rma_ioc, 1);
 	ofi_cirque_commit(smr_cmd_queue(peer_smr));

prov/shm/src/smr_comp.c
@@ -38,11 +38,11 @@
 #include "smr.h"
 
 int smr_complete_tx(struct smr_ep *ep, void *context, uint32_t op,
-		    uint16_t flags, uint64_t err)
+		    uint64_t flags, uint64_t err)
 {
 	ofi_ep_tx_cntr_inc_func(&ep->util_ep, op);
 
-	if (!err && !(flags & SMR_TX_COMPLETION))
+	if (!err && !(flags & FI_COMPLETION))
 		return 0;
 
 	return ep->tx_comp(ep, context, op, flags, err);

commit 7b601ce9e70b67f1b7a05cdb9d85d72084f28b3b

prov/shm: fix double cmd-return for async IPC protocol
When the async IPC copy path is used, smr_ipc_async_copy sets
cmd->hdr.rx_ctx and defers cmd return to smr_progress_async_ipc after
DMA completes. However, both smr_start_common (messaging path) and
smr_progress_cmd_rma (RMA path) unconditionally returned the cmd
immediately when SMR_RETURN_CMD was set, causing a double-return that
corrupts the sender's command slot pool and deadlocks under high message
rates (observed as a hang in fi_rma_bw with CUDA buffers). Fix by
checking for async IPC in progress (proto == smr_proto_ipc && rx_ctx !=
0) in both paths and skipping the immediate return, matching the
existing DSA SAR handling pattern.

Fixes: b34ed0c1d ("prov/shm: refactor and simplify protocol code")
Signed-off-by: Shi Jin <sjina@amazon.com>

prov/shm/src/smr_progress.c
@@ -750,6 +750,8 @@ static int smr_start_common(struct smr_ep *ep, struct smr_cmd *cmd,
 		pend = (struct smr_pend_entry *) cmd->hdr.rx_ctx;
 		if (pend->sar_copy_fn == &smr_dsa_copy_sar)
 			return_cmd = false;
+	} else if (cmd->hdr.proto == smr_proto_ipc && cmd->hdr.rx_ctx) {
+		return_cmd = false;
 	}
 
 	if (return_cmd)
@@ -1229,6 +1231,8 @@ static int smr_progress_cmd_rma(struct smr_ep *ep, struct smr_cmd *cmd)
 			pend = (struct smr_pend_entry *) cmd->hdr.rx_ctx;
 			if (pend->sar_copy_fn == &smr_dsa_copy_sar)
 				return_cmd = false;
+		} else if (cmd->hdr.proto == smr_proto_ipc) {
+			return_cmd = false;
 		}
 		goto out;
 	}