08b63c76068e3af668fec2e91e93f42768767fee
Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>
2026-07-16 22:55:13 -0400

    prov/opx: Use CUDA stream and event types directly
    Remove the OPX HMEM stream/event unions now that they are only used by
    the CUDA path. Replace the generic opx_hmem_* wrappers with CUDA-specific
    helpers that use CUstream and CUevent directly, and store the CUDA stream
    directly in the OPX HMEM domain.
    
    Signed-off-by: Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>

--- prov/opx/src/opx_hmem_domain.c
+++ prov/opx/src/opx_hmem_domain.c
@@ -151,12 +151,9 @@ int opx_hmem_close_domain(struct opx_hmem_domain *hmem_domain, int locked)
 		hmem_domain->ipc_cache = NULL;
 	}
 
-	opx_hmem_stream_destroy(hmem_domain->hmem_stream.type, hmem_domain->hmem_stream.stream);
-
-	if (hmem_domain->hmem_stream.event_pool) {
-		ofi_bufpool_destroy(hmem_domain->hmem_stream.event_pool);
-		hmem_domain->hmem_stream.event_pool = NULL;
-	}
+#if HAVE_CUDA
+	opx_hmem_cuda_stream_destroy(hmem_domain->cuda.stream);
+#endif
 
 	ofi_domain_close(&hmem_domain->util_domain);
 	free(hmem_domain);
@@ -204,15 +201,6 @@ int opx_hmem_open_domain(struct opx_hmem_fabric *hmem_fabric, struct fi_info *in
 				strerror(ret));
 			return ret;
 		}
-	}
-
-	ret = ofi_bufpool_create(&new_hmem_domain->hmem_stream.event_pool, sizeof(union opx_hmem_event), 0, UINT_MAX,
-				 16, OFI_BUFPOOL_NO_ZERO);
-	if (ret) {
-		opx_hmem_close_domain(new_hmem_domain, 0);
-		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "Error creating HMEM event pool ret=%d (%s)\n", ret,
-			strerror(ret));
-		return ret;
 	}
 
 	new_hmem_domain->util_domain.domain_fid.fid.fclass  = FI_CLASS_DOMAIN;
