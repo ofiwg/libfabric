f42b83dd5e909d539ed2de9fcaab96d2f957999e
Brian Zhao <brianzzj@amazon.com>
2026-07-29 12:14:38 -0700

    prov/efa: fix deadlock in efa_ah_alloc nomem release
    Previously, if the efa_ah malloc fails, the util_domain lock
    isn't released. This will lead to a potential deadlock.
    This commit fixes it and adds a unit test to assert on this.
    A mechanism to inject malloc failure in the gtest suite is introduced.
    
    Signed-off-by: Brian Zhao <brianzzj@amazon.com>

---
+++ prov/efa/test/gtest/efa_gtest_ah_utils.c
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: BSD-2-Clause OR GPL-2.0-only */
+/* SPDX-FileCopyrightText: Copyright Amazon.com, Inc. or its affiliates. All rights reserved. */
+
+#include "efa.h"
+#include "efa_ah.h"
+#include "efa_base_ep.h"
+#include "efa_gtest_ah_utils.h"
+#include "efa_gtest_common_helpers.h"
+
+struct efa_ah *efa_test_ah_alloc_fabricated_gid(struct fid_ep *ep)
+{
+	struct efa_base_ep *base_ep =
+		container_of(ep, struct efa_base_ep, util_ep.ep_fid);
+	struct efa_ep_addr raw_addr;
+
+	efa_test_fabricate_addr(ep, &raw_addr);
+	return efa_ah_alloc(base_ep->domain, raw_addr.raw, false);
+}
