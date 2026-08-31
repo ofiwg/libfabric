a64e2dbacf136be4caef7adb378dc5f1b7efd69d
Sai Sunku <sunkusa@amazon.com>
2026-08-31 21:40:32 +0000

    prov/efa: Add name field to the protocol interface
    Add a human readable name to struct efa_rdm_proto.
    
    Also add a log entry at debug level which logs the protocol that was
    selected.
    
    No functional change.
    
    Signed-off-by: Sai Sunku <sunkusa@amazon.com>

--- prov/efa/src/rdm/efa_rdm_proto_zero_copy.c
+++ prov/efa/src/rdm/efa_rdm_proto_zero_copy.c
@@ -16,6 +16,7 @@
  */
 
 struct efa_rdm_proto efa_rdm_proto_zero_copy = {
+	.name = "zero_copy",
 	.construct_tx_pkes = &efa_rdm_proto_zero_copy_construct_tx_pkes,
 	.handle_tx_pkes_posted = &efa_rdm_proto_handle_tx_pkes_posted_no_op,
 };
