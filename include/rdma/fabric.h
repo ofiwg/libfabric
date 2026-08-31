75e20f4a6b1b0ab808bca2121953fabe0a38a41a
Jianxin Xiong <jianxin.xiong@intel.com>
2026-08-30 18:15:54 -0700

    v2.7.0rc1
    Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>

--- include/rdma/fabric.h
+++ include/rdma/fabric.h
@@ -73,7 +73,7 @@ extern "C" {
 #endif
 
 #define FI_MAJOR_VERSION 2
-#define FI_MINOR_VERSION 6
+#define FI_MINOR_VERSION 7
 #define FI_REVISION_VERSION 0
 
 /* Removing these breaks the build for some apps.
